8fd0fa9535f43e14183d5908adca0ed1134d075c
Emmie Nilsson <emmie.nilsson@gmail.com>
2020-02-11 17:36:00 +0100

    Added POST route for liking a thought. Included error message.

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ import cors from 'cors'
 import mongoose from 'mongoose'
 
 const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/happyThoughts"
-mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false })
 mongoose.Promise = Promise
 
 const Thought = mongoose.model('Thought', {
@@ -43,7 +43,18 @@ app.post('/', async (req, res) => {
     await newThought.save()
     res.status(201).json(newThought)
   } catch (err) {
-    res.status(400).json({ message: "Could not save thought", error: err.errors })
+    res.status(400).json({ message: 'Could not save thought', error: err.errors })
+  }
+})
+
+app.post('/:thoughtId/like', async (req, res) => {
+  const thoughtId = req.params.thoughtId
+
+  try {
+    const likedThought = await Thought.findOneAndUpdate({ '_id': thoughtId }, { $inc: { 'hearts': 1 } }, { new: true })
+    res.status(201).json(likedThought)
+  } catch (err) {
+    res.status(400).json({ message: "Could not like thought" })
   }
 })
 
