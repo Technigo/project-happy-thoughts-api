2083368747108ebbe0a037e885975ed496f790b6
Theresa Ulwahn <theresaulwahn@gmail.com>
2020-02-11 16:35:41 +0100

    started up and get the postman and database on Compass to work. First endpoint works with post and get. porblems with adding a heart as a lik to count

--- server.js
+++ server.js
@@ -7,6 +7,23 @@ const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/happyThoughts"
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
 mongoose.Promise = Promise
 
+const Thought = mongoose.model('Thought', {
+  text: {
+    type: String,
+    required: true,
+    minlength: 5,
+    maxlength: 140
+  },
+  complete: {
+    type: Boolean,
+    default: false
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
+})
+
 // Defines the port the app will run on. Defaults to 8080, but can be 
 // overridden when starting the server. For example:
 //
@@ -21,6 +38,40 @@ app.use(bodyParser.json())
 // Start defining your routes here
 app.get('/', (req, res) => {
   res.send('Hello world')
+})
+
+app.get('/thoughts', async (req, res) => {
+  const thoughts = await Thought.find().sort({createdAt: 'desc'}).limit(140).exec()
+  res.json(thoughts)
+})
+
+app.post('/thoughts', async (req, res) => {
+  //Retrieve the information sent by the client to our API endpoint
+  const {text, complete} = req.body
+
+  // Use our mongoose model to create the database entry
+  const thought = new Thought({text, complete})
+
+  try {
+    // Success
+    const savedThought = await thought.save()
+    res.status(201).json(savedThought)
+  }catch (err) {
+    res.status(400).json({message: 'Could not save message to the Database', error: err.errors}) 
+  }
+})
+
+app.post('/:thoughtId/like', async (req, res) => {
+  //Update the heart Number
+  // const heart = req.params.id
+  // const like = heart.find(createdAt).count(heart + 1)
+  const like = await like.find(req.params.id).count({ type: Number }, function (err, count) {
+    if (err)
+      console.log('hearts', count);
+  });
+  like.save()
+  res.json(like)
+  //add a error message if id is not found.
 })
 
 // Start the server
