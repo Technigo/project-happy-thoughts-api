e41c1f0b3a931b5fa12bce4a87db1ae65d037281
codebyfredrik <50440682+codebyfredrik@users.noreply.github.com>
2020-02-11 16:12:35 +0100

    Implementing route handlar to get all thoughts

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,20 @@
 import express from 'express';
+import Thought from '../models/thought';
 
 const router = express.Router();
 
-router.get('/', (req, res) => {});
+router.get('/', async (req, res) => {
+  const thoughts = await Thought.find();
+
+  if (thoughts.length > 0) {
+    res.json(thoughts);
+  } else {
+    res.status(404).json({
+      statusCode: 404,
+      error: `No happy thoughts found.`
+    });
+  }
+});
 
 router.post('/', (req, res) => {});
 
