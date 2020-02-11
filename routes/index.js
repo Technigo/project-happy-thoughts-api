33182b1386fa575cd5775e343254d3b26736a166
codebyfredrik <50440682+codebyfredrik@users.noreply.github.com>
2020-02-11 14:54:35 +0100

    Adding routes

---
+++ routes/index.js
@@ -0,0 +1,9 @@
+import express from 'express';
+
+const router = express.Router();
+
+router.get('/', (req, res) => {});
+
+router.post('/', (req, res) => {});
+
+router.post('/:thoughId/like', (req, res) => {});
