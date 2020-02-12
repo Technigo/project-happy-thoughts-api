b88f47fd852e558a02e3f8d3cde1ab61e12250cd
nyblad <nyblad@hotmail.com>
2020-02-12 08:45:29 +0100

    Updated routes for get and post

--- server.js
+++ server.js
@@ -7,10 +7,14 @@ const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/happyThoughts"
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
 mongoose.Promise = Promise
 
-// Defines the port the app will run on. Defaults to 8080, but can be 
-// overridden when starting the server. For example:
-//
-//   PORT=9000 npm start
+// MODEL FOR THOUGHT
+const Thought = mongoose.model('Thought', {
+  message: { type: String, requierd: true, minlength: 5, maxlength: 140 },
+  hearts: { type: Number, default: 0 },
+  createdAt: { type: Date, default: Date.now }
+})
+
+// PORT & APP SETUP
 const port = process.env.PORT || 8080
 const app = express()
 
@@ -18,9 +22,39 @@ const app = express()
 app.use(cors())
 app.use(bodyParser.json())
 
-// Start defining your routes here
-app.get('/', (req, res) => {
-  res.send('Hello world')
+
+// THOUGHTS
+app.get('/', async (req, res) => {
+  // Returning 20 thoughts order by last created first
+  const thoughts = await Thought.find().sort({ createdAt: 'desc' }).limit(20).exec()
+  res.json(thoughts)
+})
+
+app.post('/', (req, res) => {
+  const { message } = req.body
+  const thought = new Thought({ message })
+  try {
+    //Sucess
+    const savedThought = await thought.save()
+    res.status(201).json(savedThought)
+  } catch (err) {
+    // Failed
+    res.status(400).json({ message: 'Could not send thought', error: err.errors })
+  }
+})
+
+app.post('/:thoughtId/like', (req, res) => {
+  const { thoughtId } = req.params
+  const thought = await Thought.findById(thoughtId)
+
+  if (thought) {
+    thought.heart += 1
+    thought.save()
+    res.json(thought)
+  } else {
+    res.status(404).json({ message: 'Could not find thought', error: err.errors })
+
+  }
 })
 
 // Start the server
