e4dbd469edc8ce22e9ec4a96facb8bd759532e2d
horsefacegirl <51134090+horsefacegirl@users.noreply.github.com>
2020-02-12 15:11:29 +0100

    Add like-endpoint
    -Adding hearts to every liked post
    -Error handling if not found

--- server.js
+++ server.js
@@ -57,6 +57,19 @@ app.post("/thoughts", async (req, res) => {
   }
 });
 
+app.post("/thoughts/:thoughtId/like", async (req, res) => {
+  const { thoughtId } = req.params;
+  const thoughtLiked = await Thought.findById(thoughtId);
+
+  if (thoughtLiked) {
+    thoughtLiked.heart += 1;
+    thoughtLiked.save();
+    res.json(thoughtLiked);
+  } else {
+    res.status(404).json({ message: "Could not find happy thought", error: err.errors });
+  }
+});
+
 // Start the server
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
