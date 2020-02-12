770513223b601de30ec69c84f11bd38c836bef6e
nasimmhn <nasim.mahzoun@gmail.com>
2020-02-12 13:08:20 +0100

    init commit- set up the get api

--- server.js
+++ server.js
@@ -7,9 +7,26 @@ const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/happyThoughts"
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
 mongoose.Promise = Promise
 
-// Defines the port the app will run on. Defaults to 8080, but can be 
-// overridden when starting the server. For example:
-//
+
+const Thought = mongoose.model('Thought', {
+  message: {
+    type: String,
+    required: true,
+    minlength: 5,
+    maxlength: 140
+  },
+  heart: {
+    type: Number,
+    default: 0
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+
+  }
+})
+
+
 //   PORT=9000 npm start
 const port = process.env.PORT || 8080
 const app = express()
@@ -18,9 +35,43 @@ const app = express()
 app.use(cors())
 app.use(bodyParser.json())
 
+if (process.env.RESET_DB) {
+  const seedDatabase = async () => {
+    await Thought.deleteMany({})
+
+    data.forEach((thought) => {
+      new Thought(thought).save()
+    })
+  }
+
+  seedDatabase()
+}
+
 // Start defining your routes here
 app.get('/', (req, res) => {
   res.send('Hello world')
+})
+
+app.get('/thoughts', async (req, res) => {
+  const thought = await Thought.find().sort({ createdAt: 'desc', heart: +1 }).limit(20).exec()
+  res.json(thought)
+})
+
+app.post('/thoughts', async (req, res) => {
+  //Retrieve the information sent by the client to our API endpoint
+  const { message } = req.body
+  //use our mongoose model to create the database entry
+  const thought = new Thought({ message })
+
+  try {
+    //Success
+    const savedThought = await thought.save()
+    res.status(201).json(savedThought)
+  } catch (err) {
+    res.status(400).json({ message: 'Could not save thought to the Database', error: err.errors })
+
+  }
+
 })
 
 // Start the server
