72ef38b581ce08c64f196c39e8ac9b75af98232e
Ivett Backlund <ivett@Ivetts-MacBook-Air-13.local>
2020-02-13 18:01:40 +0100

    changes in my routes

--- server.js
+++ server.js
@@ -25,7 +25,10 @@ const Thought = mongoose.model('Thought', {
   hearts: {
     type: Number
   },
-  createdAt: {}
+  createdAt: {},
+  posted:{
+    type
+  }
 
 })
 
@@ -49,7 +52,31 @@ app.use(bodyParser.json())
 app.get('/', (req, res) => {
   res.send('Hello world')
 })
+// How to make a route where you post your new messages? 
 
+
+// When you want to update a message: 
+// 1) Get the message that you want to update (by id)
+// 2) Change the part of the message that you want to update
+
+
+// app.get('/thoughts/posted')
+app.get('/messages', (req, res) =>{
+  const queryString = req.query.q
+  const queryRegex = new RegExp (queryString, "i")
+  Thought.find({'message':queryRegex, 'createdAt': })
+  .sort({'num_pages': -1})
+  .limit(20)
+  .then((results)=> {
+    // succesfull
+    req.send(results)
+  }).catch((err)=>{
+    //error/ failure
+    console.log('Error'+ err)
+    res.json(null)
+  })
+  
+})
 // Start the server
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`)
