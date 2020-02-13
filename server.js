38da3fb20f79257920c7bf4da00885e20ecc14c7
Jenfi <jennifer_fick@hotmail.com>
2020-02-13 17:00:28 +0100

    created model

--- server.js
+++ server.js
@@ -10,15 +10,21 @@ mongoose.Promise = Promise
 
 const Thought = mongoose.model('Thought', {
   message: {
-    type: String
+    type: String,
+    required: true,
+    minlength: 5,
+    maxlength: 140
   },
   heart: {
     type: Number,
-    // default: 0 - Testa först utan för att se vad som händer
+    // default: 0, - Testa först utan för att se vad som händer! Men det ska vara 0
+    // Should not be assignable when creating a new thought. For example, if I send a POST request to / to create a new thought with this JSON body; { "message": "Hello", "hearts": 9000 }, then the hearts property should be ignored, and the object we store in mongo should have 0 hearts.
   },
   createdAt: {
     type: Date,
-    default: Date.now
+    // default: () => Date.now()
+    default: () => new Date
+    // Should not be assignable when creating a new thought.
   }
 })
 // Defines the port the app will run on. Defaults to 8080, but can be 
@@ -35,6 +41,7 @@ app.use(bodyParser.json())
 // Start defining your routes here
 app.get('/', (req, res) => {
   res.send('Hello world')
+  const thoughts = req.p
 })
 
 // Start the server
