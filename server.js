fd9b23e8f724170cc3e43395274e04288eb0d7e7
Angelica Brodin <brodin.angelica@gmail.com>
2020-02-13 15:01:57 +0100

    set up the mongoose modul and the get and post requests

--- server.js
+++ server.js
@@ -7,6 +7,27 @@ const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/happyThoughts"
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
 mongoose.Promise = Promise
 
+const Thought = mongoose.model('Thought', {
+  message: {
+    type: String,
+    required: true,
+    minlength: 5,
+    maxlength: 140
+  },
+  heart: {
+    type: Number,
+    default: 0,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
+  // thoughtId: {
+  //   type: String,
+  //   unique: true
+  // }
+})
+
 // Defines the port the app will run on. Defaults to 8080, but can be 
 // overridden when starting the server. For example:
 //
@@ -17,6 +38,33 @@ const app = express()
 // Add middlewares to enable cors and json body parsing
 app.use(cors())
 app.use(bodyParser.json())
+
+
+app.get('/thoughts', async (req, res) => {
+  const thoughts = await Thought.find().sort({ createdAt: 'desc' }).limit(20).exec()
+  res.json(thoughts)
+})
+
+app.post('/thoughts', async (req, res) => {
+  // Retrieve the information sent by the client to our API endpoint
+  const { message } = req.body
+
+  // Use our mongoose model to create the database entry
+  const thought = new Thought({ message })
+
+  try {
+    const savedThought = await thought.save()
+    res.status(201).json(savedThought)
+  } catch (err) {
+    res.status(400).json({ text: 'Could not send Happy Thought, please try again', error: err.errors })
+  }
+})
+
+app.post('/thoughts/:thoughtId/like', async (req, res) => {
+  const { thoughtId } = req.params
+  await Thought.updateOne({ 'thoughtId': thoughtId }, { '$inc': { 'heart': 1 } })
+  res.status(201).json()
+})
 
 // Start defining your routes here
 app.get('/', (req, res) => {
