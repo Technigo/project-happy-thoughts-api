4456e8dad1282f65308c7e3c75ef66963dfaf782
AnnaTerelius <tereliusanna@hotmail.com>
2020-02-13 08:50:46 +0100

    initial commit

--- server.js
+++ server.js
@@ -7,6 +7,23 @@ const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/happyThoughts"
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
 mongoose.Promise = Promise
 
+const Thought = mongoose.model('Thought', {
+  message: {
+    type: String,
+    required: true,
+    minlength: 5,
+    maxlength: 140
+  },
+  heart: {
+    type: Number,
+    default: 0
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  },
+})
+
 // Defines the port the app will run on. Defaults to 8080, but can be 
 // overridden when starting the server. For example:
 //
@@ -19,8 +36,43 @@ app.use(cors())
 app.use(bodyParser.json())
 
 // Start defining your routes here
-app.get('/', (req, res) => {
-  res.send('Hello world')
+//app.get('/', (req, res) => {
+  //res.send('Hello world API')
+//})
+
+app.get('/', async (req, res) => {
+  const thoughts = await Thought.find().sort({createdAt: 'desc'}).limit(20).exec();
+  res.json(thoughts);
+})
+
+app.post('/', async (req, res) => {
+  //get the info send by user to our API endpoint
+  const {message, heart} = req.body;
+
+  //use our mongoose model to create database entry
+  const thought = new Thought({message, heart});
+
+  try{
+    const savedThought = await thought.save();
+    res.status(201).json(savedThought);
+  }catch(err){
+    console.log(err)
+    res.status(400).json({message: 'could not save thought to Database', error:err.errors});
+  }
+})
+
+app.post('/:thoughtId/like', async (req, res) => {
+  const {thoughtId} = req.params;
+  console.log(`POST /${thoughtId}/like`);
+
+  try{
+    await Thought.updateOne({_id : thoughtId}, {'$inc': {'heart' :1}})
+    res.status(201).json();
+   
+  }catch(err){
+    console.log(err)
+    res.status(400).json({message: 'could not save like to Database due to thought not found', error:err.errors});
+  }
 })
 
 // Start the server
