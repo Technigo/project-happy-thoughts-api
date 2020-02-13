e42ddf4404f82952d4b4b6973b156aea6a647320
Theresa Ulwahn <theresaulwahn@gmail.com>
2020-02-13 15:04:58 +0100

    changed my endpoints to '/' instead of having thoughts

--- server.js
+++ server.js
@@ -36,16 +36,16 @@ app.use(cors())
 app.use(bodyParser.json())
 
 // Start defining your routes here
-app.get('/', (req, res) => {
-  res.send('Hello world')
-})
+// app.get('/', (req, res) => {
+//   res.send('Hello world')
+// })
 
-app.get('/thoughts', async (req, res) => {
+app.get('/', async (req, res) => {
   const thoughts = await Thought.find().sort({createdAt: 'desc'}).limit(140).exec()
   res.json(thoughts)
 })
 
-app.post('/thoughts', async (req, res) => {
+app.post('/', async (req, res) => {
   //Retrieve the information sent by the client to our API endpoint
   const {text, like} = req.body.message
 
