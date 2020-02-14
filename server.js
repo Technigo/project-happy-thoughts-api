d76bd2e344562ba261a0cd740e53e4c56bc96c6c
anna-ip <anna.almestal@gmail.com>
2020-02-14 14:10:29 +0100

    changed the routes name

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ import mongoose from 'mongoose'
 const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/happyThoughts"
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
 mongoose.Promise = Promise
+mongoose.set('useFindAndModify', false);
 
 const Thought = mongoose.model('Thought', {
   message: {
@@ -15,15 +16,30 @@ const Thought = mongoose.model('Thought', {
     maxlength: 140
   },
   heart: {
-    Type: Number,
+    type: Number,
     default: 0,
   },
   createdAt: {
     type: Date,
     default: Date.now
   }
-
 })
+
+
+// if (process.env.RESET_DATABASE) {
+//   console.log('Resetting the database')
+
+//   const seedDatabase = async () => {
+//     //   await Thought.deleteMany({})
+
+
+//     //   Thought.forEach((data) => {
+//     //     new Tought(data).save()
+
+//     //   })
+//   }
+//   seedDatabase()
+// }
 
 // Defines the port the app will run on. Defaults to 8080, but can be 
 // overridden when starting the server. For example:
@@ -37,16 +53,16 @@ app.use(cors())
 app.use(bodyParser.json())
 
 // Start defining your routes here
-app.get('/', (req, res) => {
-  res.send('Hello world')
-})
+// app.get('/', (req, res) => {
+//   res.send('Hello world')
+// })
 
-app.get('/thoughts', async (req, res) => {
+app.get('/', async (req, res) => {
   const thought = await Thought.find().sort({ createdAt: 'desc' }).limit(20).exec()
   res.json(thought)
 })
 
-app.post('/thoughts', async (req, res) => {
+app.post('/', async (req, res) => {
   const { message } = req.body
   const thought = new Thought({ message })
 
@@ -55,6 +71,21 @@ app.post('/thoughts', async (req, res) => {
     res.status(201).json(savedThought)
   } catch (err) {
     res.status(400).json({ message: 'Could not save to the Database', error: err.errors })
+  }
+})
+
+app.post('/:id/like', async (req, res) => {
+  // this endpoint should update the heart Number
+  try {
+    const like = await Thought.findOneAndUpdate(
+      { "_id": req.params.id }, //filter
+      { $inc: { "heart": 1 } },//update
+      { new: true } //updates the number of hearts in POST
+    )
+    res.status(201).json(like)
+
+  } catch (err) {
+    res.status(400).json({ message: 'Could not save your like', error: err.errors })
   }
 })
 
