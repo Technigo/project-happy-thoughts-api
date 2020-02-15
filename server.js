bc9e70fa3a3f7adf46497c4d6e8ba5ceec84cf52
Linda <isell.linda@gmail.com>
2020-02-15 15:01:17 +0100

    Comment out seedDataBase code

--- server.js
+++ server.js
@@ -28,21 +28,24 @@ const Thought = mongoose.model('Thought', {
     required: false,
     default: "anonymous",
     maxlength: 50
+  },
+  tag: {
+    type: String,
   }
 })
 
-//SEEDING FOR ADDING NEW DATA
-if (process.env.RESET_DB) {
-  console.log('Resetting database')
-  const seedDatabase = async () => {
-    await Thought.deleteMany({})
+//SEEDING FOR ADDING NEW DATA. When do I need to use/do this??
+// if (process.env.RESET_DB) {
+//   console.log('Resetting database')
+//   const seedDatabase = async () => {
+//     await Thought.deleteMany({})
 
-    thought.forEach(() => {
-      new Thought().save()
-    })
-  }
-  seedDatabase()
-}
+//     thought.forEach(() => {
+//       new Thought().save()
+//     })
+//   }
+//   seedDatabase()
+// }
 
 // Defines the port the app will run on. Defaults to 8080, but can be 
 // overridden when starting the server. For example:
@@ -62,19 +65,52 @@ app.use((req, res, next) => {
   }
 })
 
+app.get('/', async (req, res) => {
+  const { sort } = req.query
+  const sortData = (sort) => {
+    if (sort === 'dates') {
+      return { createdAt: 'asc' }
+    } else if (sort === 'hearts') {
+      return { hearts: 'desc' }
+    } else {
+      return { createdAt: 'desc' }
+    }
+  }
+  let thoughts = await Thought.find().sort(sortData(sort)).limit(20).exec()
+  res.json(thoughts)
+})
+
 
 //GET A LIST OF THE THOUGHTS IN THE DATABASE. desc = descending order
 app.get('/', async (req, res) => {
   const thoughts = await Thought.find().sort({ createdAt: 'desc' }).limit(20).exec()
+  const { tag } = req.query
+  if (tag) {
+    const filteredThoughts = await Thought.find({ tag: tag })
+    res.json(filteredThoughts)
+  }
   res.json(thoughts)
+
+  // const { sort } = req.query
+  // const sortData = (sort) => {
+  //   if (sort === 'dates') {
+  //     return { createdAt: 'asc' }
+  //   } else if (sort === 'hearts') {
+  //     return { hearts: 'desc' }
+  //   } else {
+  //     return { createdAt: 'desc' }
+  //   }
+  // }
+  // let thoughts = await Thought.find().sort(sortData(sort)).limit(20).exec()
+  // res.json(thoughts)
 })
 
 //POST/SEND INFORMATION IN A REQUEST
 app.post('/', async (req, res) => {
   //Retrieve the information sent by the client to our API endpoint
-  const { message, name } = req.body
+  const { message, name, tag } = req.body
   //use our mongoose model to create the database entry
-  const thought = new Thought({ message, name })
+  const thought = new Thought({ message, name, tag })
   try {
     //Success
     const savedThought = await thought.save()
@@ -86,21 +122,38 @@ app.post('/', async (req, res) => {
   }
 })
 
-app.post("/:thoughtID/like", async (req, res) => {
-  //Find specific thought
-  const thoughtID = req.params.thoughtID
-  const thought = await Thought.findById(thoughtID)
+// app.post("/:thoughtID/like", async (req, res) => {
+//   //Find specific thought
+//   const thoughtID = req.params.thoughtID
+//   const thought = await Thought.findById(thoughtID)
 
-  if (thought) {
-    //Success case
-    thought.hearts += 1
-    thought.save()
-    res.status(201).json(thought)
-  }
-  else {
-    res.status(404).json({ message: `No thought with id: ${thoughtID} `, error: err.errors })
+//   if (thought) {
+//     //Success case
+//     thought.hearts += 1
+//     thought.save()
+//     res.status(201).json(thought)
+//   }
+//   else {
+//     res.status(404).json({ message: `No thought with id: ${thoughtID} `, error: err.errors })
+//   }
+// })
+
+//Jennies way of finding thougtId for likes
+app.post("/:id/like", async (req, res) => {
+  try {
+    const thought = await Thought.findOneAndUpdate(
+      { _id: req.params.id },
+      { $inc: { hearts: 1 } },
+      { new: true }
+    )
+    res.json(thought)
+  } catch (err) {
+    res
+      .status(400)
+      .json({ message: "could not update heart", errors: err.errors })
   }
 })
+
 
 // Start the server
 app.listen(port, () => {
