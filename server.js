4209f7f6461bac095fb1b9d51726f73b2e45031f
Jenny Jacobsson <enni@oans.net>
2020-02-17 17:01:17 +0100

    added name property

--- server.js
+++ server.js
@@ -8,6 +8,13 @@ mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
 mongoose.Promise = Promise
 
 const Thought = mongoose.model('Thought', {
+
+  name: {
+    type: String,
+    maxlength: 50,
+    default: 'Anonymous'
+  },
+
   message: {
     type: String,
     required: true,
@@ -23,7 +30,6 @@ const Thought = mongoose.model('Thought', {
   createdAt: {
     type: Date,
     default: Date.now
-
   },
 })
 
@@ -41,7 +47,11 @@ app.use(bodyParser.json())
 
 app.get('/', async (req, res) => {
   const thoughts = await Thought.find().sort({ createdAt: 'desc' }).limit(20).exec()
-  res.json(thoughts)
+  if (thoughts.length > 0) {
+    res.json(thoughts)
+  } else {
+    res.status(400).json({ message: "Fill up this empty space with your happiness" })
+  }
 })
 
 app.post('/:thoughtId/like', async (req, res) => {
@@ -50,13 +60,14 @@ app.post('/:thoughtId/like', async (req, res) => {
     await Thought.updateOne({ '_id': thoughtId }, { '$inc': { 'hearts': 1 } })
     res.status(201).json({ message: `Thought Id nr: ${thoughtId} got one heart update` })
   } catch (err) {
-    res.status(400).json({ message: 'Could not find thought', errors: err.error })
+    res.status(400).json({ message: 'Could not update heart', errors: err.error })
   }
 })
 
 app.post('/', async (req, res) => {
   try {
-    const thought = await new Thought({ message: req.body.message }).save()
+    const { message, name } = req.body
+    const thought = await new Thought({ message, name }).save()
     res.status(201).json(thought)
   } catch (err) {
     res.status(400).json({ message: 'Could not save thought', errors: err.error })
