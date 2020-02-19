484b579ce2c11da86441dc94cdc02fa51844ceb0
HannaCharlotteHolm <50885736+HannaCharlotteHolm@users.noreply.github.com>
2020-02-19 17:59:18 +0100

    Done

--- server.js
+++ server.js
@@ -2,10 +2,30 @@ import express from 'express'
 import bodyParser from 'body-parser'
 import cors from 'cors'
 import mongoose from 'mongoose'
+import dotenv from 'dotenv'
+
+dotenv.config()
 
 const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/happyThoughts"
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
 mongoose.Promise = Promise
+
+const Thought = mongoose.model('Thought', {
+  message: {
+    type: String,
+    required: true,
+    minlenght: 5,
+    maxlenght: 140
+  },
+  hearts: {
+    type: Number,
+    default: 0
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
+})
 
 // Defines the port the app will run on. Defaults to 8080, but can be 
 // overridden when starting the server. For example:
@@ -19,8 +39,35 @@ app.use(cors())
 app.use(bodyParser.json())
 
 // Start defining your routes here
-app.get('/', (req, res) => {
-  res.send('Hello world')
+app.get('/', async (req, res) => {
+  const thoughts = await Thought.find().sort({ createdAt: 'desc' }).limit(20).exec()
+  res.json(thoughts)
+})
+
+app.post('/', async (req, res) => {
+  const { message } = req.body
+
+  const thought = new Thought({ message })
+
+  try {
+    const savedThought = await thought.save()
+    res.status(201).json(savedThought)
+  } catch (err) {
+    res.status(400).json({ message: 'Could not save message', error: err.errors })
+  }
+
+})
+
+app.post('/:id/like', async (req, res) => {
+  const thought = await Thought.findById(req.params.id)
+
+  if (thought) {
+    thought.hearts += 1
+    const savedThought = await thought.save()
+    res.status(201).json(savedThought)
+  } else {
+    res.status(400).json({ message: 'This thought do not exist', error: err.errors })
+  }
 })
 
 // Start the server
