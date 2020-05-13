186fbc8b1f92f561f7806dbadbbd738c4edaa300
Christina Persson <christina@MacBook-Pro.local>
2020-05-13 20:34:47 +0200

    Changed order thougts are displayed

--- server.js
+++ server.js
@@ -42,7 +42,7 @@ app.get("/", (req, res) => {
 
 app.get("/thoughts", async (req, res) => {
   const thoughts = await Thought.find()
-    .sort({ createdAt: "desc" })
+    .sort({ createdAt: -1 })
     .limit(20)
     .exec();
   res.json(thoughts);
