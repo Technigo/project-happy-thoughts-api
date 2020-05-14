d5c162bc8f9354be1a86e63b5e8905a080806c1b
Christina Persson <christina@MacBook-Pro.local>
2020-05-14 15:49:26 +0200

    Changed thoughts so i also save amount of pages avilable

--- server.js
+++ server.js
@@ -55,7 +55,7 @@ app.get("/thoughts", async (req, res) => {
     .limit(perPage)
     .skip(skip)
     .exec();
-  res.json([{ pages: pages }, thoughts]);
+  res.json([pages, thoughts]);
 });
 
 app.post("/thoughts", async (req, res) => {
