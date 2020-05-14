b1e230d42b510bf0a2ea00a9ca1d5fc7576c72fd
annesophiegendron <annesophie.gendron@gmail.com>
2020-05-14 23:19:20 +0200

    mongoose model created

--- server.js
+++ server.js
@@ -7,20 +7,70 @@ const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/happyThoughts"
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
 mongoose.Promise = Promise
 
-// Defines the port the app will run on. Defaults to 8080, but can be 
-// overridden when starting the server. For example:
-//
+const Thought = mongoose.model('Thought', {
+  message: {
+    type: String,
+    required: true,
+    minlength: 5,
+    maxlength: 140
+  },
+  hearts: {
+    type: Number,
+    default: 0
+  },
+  createdAt: {
+    type: Date,
+    default: () => Date.now()
+  },
+  name: {
+    type: String,
+    required: false,
+    default: "anonymous",
+    maxlength: 50
+  }
+})
+
 //   PORT=9000 npm start
-const port = process.env.PORT || 8080
+const port = process.env.PORT || 8000
 const app = express()
 
-// Add middlewares to enable cors and json body parsing
+// MIDDLEWARES to enable cors and json body parsing
 app.use(cors())
 app.use(bodyParser.json())
 
-// Start defining your routes here
-app.get('/', (req, res) => {
-  res.send('Hello world')
+// if database not connected
+// app.use((req, res, next) => {
+//   if (mongoose.connection.readyState === 1) {
+//     next()
+//   } else {
+//     res.status(503).json({ error: 'service unavailable' })
+//   }
+// })
+
+// ROUTES
+app.get('/', async (req, res) => {
+  res.send('Hello Anne-Sophie')
+})
+
+app.get('/thoughts', async (req, res) => {
+  const thoughts = await Thought.find().sort({ createdAt: 'desc' }).limit(20).exec()
+  res.json(thoughts)
+})
+
+app.post('/thoughts', async (req, res) => {
+  // Retrieve the information sent by the client to our API endpoint
+  const { message, heart } = req.body
+
+  // Use our mongoose model to create the database entry
+  const thought = new Thought({ message, heart })
+
+  try {
+    //successful
+    const savedThought = await thought.save()
+    res.status(201).json(savedThought)
+  } catch (err) {
+    res.status(400).json({ message: "Could not save thought to the database", error: err.errors })
+  }
 })
 
 // Start the server
