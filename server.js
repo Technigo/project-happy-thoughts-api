154f9bd9f17cdfeb0e51eb568253839a4b093faa
William Jensen <william.riis93@gmail.com>
2020-05-14 15:29:03 +0200

    finished basic requirements

--- server.js
+++ server.js
@@ -14,13 +14,76 @@ mongoose.Promise = Promise
 const port = process.env.PORT || 8080
 const app = express()
 
+
+
 // Add middlewares to enable cors and json body parsing
 app.use(cors())
 app.use(bodyParser.json())
 
+const Thought = mongoose.model('Thought', {
+  message: {
+    type: String,
+    required: true,
+    minlength: 5,
+    maxlength: 140
+  },
+  hearts: {
+    type: Number,
+    default: 0
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now()
+  },
+  postedBy: {
+    type: String,
+    default: 'Anonymous',
+    maxlength: 20
+  }
+
+})
+
 // Start defining your routes here
-app.get('/', (req, res) => {
-  res.send('Hello world')
+
+
+app.get('/', async (req, res) => {
+  const PAGE_SIZE = 20;
+  const page = req.query.page || 1
+  const thoughts = await Thought.find().sort({ createdAt: -1 }).limit(PAGE_SIZE)
+  res.json(thoughts)
+})
+
+app.post('/', async (req, res) => {
+  const PAGE_SIZE = 20;
+  const page = req.query.page || 1
+
+  try {
+    await new Thought({
+      message: req.body.message,
+      postedBy: req.body.user || 'Anonymous'
+    }).save()
+    const thoughts = await Thought.find().sort({ createdAt: -1 }).limit(PAGE_SIZE)
+    res.status(200).json(thoughts)
+  } catch (err) {
+    res.status(400).json({
+      message: 'Could not save thought', error: err.message
+    })
+  }
+})
+
+app.post('/:thoughtId/like', async (req, res) => {
+  try {
+    const foundThought = await Thought.findOne({ _id: req.params.thoughtId })
+    const oldHeartCount = foundThought.hearts
+    const likedThought = await Thought.findOneAndUpdate({ _id: req.params.thoughtId }, { hearts: oldHeartCount + 1 }, { new: true })
+    res.json(likedThought)
+
+  } catch (err) {
+    res.status(400).json({ error: 'Thought not found.' })
+  }
+
+
+
 })
 
 // Start the server
