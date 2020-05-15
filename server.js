53d2dd75e2827531f32766c617a64beca378a65f
bealun <53275597+bealun@users.noreply.github.com>
2020-05-15 15:34:32 +0200

    get and post works

--- server.js
+++ server.js
@@ -7,6 +7,24 @@ const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/happyThoughts"
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
 mongoose.Promise = Promise
 
+// Create Thought with type and requirements
+const Thought = mongoose.model('Thought', {
+  message: {
+    type: String,
+    required: true,
+    minlength: 5,
+    maxlength: 140,
+  },
+  hearts: {
+    type: Number,
+    default: 0,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  }
+})
+
 // Defines the port the app will run on. Defaults to 8080, but can be 
 // overridden when starting the server. For example:
 //
@@ -19,8 +37,29 @@ app.use(cors())
 app.use(bodyParser.json())
 
 // Start defining your routes here
-app.get('/', (req, res) => {
-  res.send('Hello world')
+// app.get('/', (req, res) => {
+//   res.send('Happy Thoughts Project 💌')
+// })
+
+app.get('/', async (req, res) => {
+  const thoughts = await Thought.find().sort({ createdAt: 'desc' }).limit(20).exec()
+
+  if (thoughts) {
+    res.status(200).json(thoughts)
+  } else {
+    res.status(400).json({ message: 'Could not post your thought', error: err.errors })
+  }
+})
+
+app.post('/', async (req, res) => {
+  const { message } = req.body
+
+  try {
+    const thought = await new Thought({ message }).save()
+    res.status(201).json(thought)
+  } catch (err) {
+    res.status(400).json({ message: 'Could not save thought', error: err.errors })
+  }
 })
 
 // Start the server
