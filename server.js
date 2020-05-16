ee7a1d77a0ff068a601dbd1acf4c25d4706bf760
elsareutersward <elsa.reutersward@gmail.com>
2020-05-16 14:43:47 +0200

    Addition of get and post as codealong

--- server.js
+++ server.js
@@ -1,29 +1,54 @@
-import express from 'express'
-import bodyParser from 'body-parser'
-import cors from 'cors'
-import mongoose from 'mongoose'
+import express from 'express';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+import mongoose from 'mongoose';
 
 const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/happyThoughts"
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
 mongoose.Promise = Promise
 
-// Defines the port the app will run on. Defaults to 8080, but can be 
-// overridden when starting the server. For example:
-//
-//   PORT=9000 npm start
+const Thought = mongoose.model('Thought', {
+  message: {
+    type: String,
+    required: true, 
+    minlength: 5,
+    maxlength: 140
+  },
+  liked: {
+    type: Boolean,
+    default: false
+  },
+  createdAt: { 
+    type: Date, 
+    default: Date.now
+  }
+});
+
 const port = process.env.PORT || 8080
-const app = express()
+const app = express();
 
-// Add middlewares to enable cors and json body parsing
-app.use(cors())
-app.use(bodyParser.json())
+app.use(cors());
+app.use(bodyParser.json());
 
-// Start defining your routes here
-app.get('/', (req, res) => {
-  res.send('Hello world')
+app.get('/', async (req, res) => {
+  const thoughts = await Thought.find().sort({createdAt: 'desc'}).limit(20).exec();
+  res.json(thoughts);
+});
+
+app.post('/', async (req, res) => {
+  try {
+    const { message } = req.body
+    const thought = await new Thought({ message }).save();
+    res.status(201).json(thought);
+  } catch (err) {
+    res.status(400).json({error: 'Could not save thought to the Database', errors:err.errors});
+  }
+});
+
+app.post('/:thoughtID/like', async (req, res) => {
+
 })
 
-// Start the server
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`)
-})
+});
