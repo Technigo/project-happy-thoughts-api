e32b0f5af4f143044aa5f78deed07e940aad5511
Lisas Sturegard <lisasturegard@hotmail.com>
2020-05-16 14:17:42 +0200

    Setup of project, created Thoughgt module and basic get- and post for thoughts

---
+++ models/thought.js
@@ -0,0 +1,9 @@
+import mongoose from 'mongoose'
+
+const Thought = mongoose.model('Thought', {
+  message: { type: String, requiered: true, minlength: 5, maxlength: 140 },
+  hearts: { type: Number, default: 0 },
+  createdAt: { type: Date, default: Date.now }
+})
+
+export default Thought
--- server.js
+++ server.js
@@ -2,28 +2,40 @@ import express from 'express'
 import bodyParser from 'body-parser'
 import cors from 'cors'
 import mongoose from 'mongoose'
+import Thought from './models/thought'
 
 const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/happyThoughts"
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
 mongoose.Promise = Promise
 
-// Defines the port the app will run on. Defaults to 8080, but can be 
-// overridden when starting the server. For example:
-//
-//   PORT=9000 npm start
 const port = process.env.PORT || 8080
 const app = express()
 
-// Add middlewares to enable cors and json body parsing
 app.use(cors())
 app.use(bodyParser.json())
 
-// Start defining your routes here
+
 app.get('/', (req, res) => {
-  res.send('Hello world')
+  res.send('Happy Thoughts database')
 })
 
-// Start the server
+app.get('/thoughts', async (req, res) => {
+  const thoughts = await Thought.find().sort({createdAt: -1}).limit(20).exec()
+  res.json(thoughts)
+})
+
+app.post('/thoughts', async (req, res) => {
+  const {message, hearts} = req.body
+  const thought = Thought({message, hearts})
+
+  try {
+    const savedThought = await thought.save()
+    res.status(201).json(savedThought)
+  } catch (err) {
+    res.status(400).json({message: 'Could not save thought to the database', error:err.errors})
+  }
+})
+
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`)
 })
