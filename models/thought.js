a455584f2870ae37e8193611e2575a807f802555
Lisas Sturegard <lisasturegard@hotmail.com>
2020-05-16 15:56:37 +0200

    Trying with taking out the default on hearts

--- models/thought.js
+++ models/thought.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose'
 
 const Thought = mongoose.model('Thought', {
   message: { type: String, requiered: true, minlength: 5, maxlength: 140 },
-  hearts: { type: Number, default: 0 },
+  hearts: { type: Number },
   createdAt: { type: Date, default: Date.now }
 })
 
