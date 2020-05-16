4b9994055e0211aca34c3defc1be9d23c202153a
jenniferot <56264114+Jenniferot@users.noreply.github.com>
2020-05-16 21:41:51 +0200

    started setting up backend

--- server.js
+++ server.js
@@ -18,10 +18,66 @@ const app = express()
 app.use(cors())
 app.use(bodyParser.json())
 
-// Start defining your routes here
-app.get('/', (req, res) => {
-  res.send('Hello world')
+//Model
+const Thought = mongoose.model("Thought", {
+  message: {
+    type: String,
+    required: true,
+    minlength: 5,
+    maxlength: 120
+  },
+  hearts: {
+    type: Number,
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  },
+  tag: {
+    type: String
+  }
 })
+
+// Start defining your routes here
+app.get('/', async (req, res) => {
+  const thoughts = await Thought.find()
+    .sort({ createdAt: "desc" })
+    .limit(20)
+  res.json(thoughts)
+  //res.send('Hello world')
+})
+
+app.post('/', async (req, res) => {
+  const thought = new Thought({
+    message: req.body.message,
+    hearts: 0
+  })
+  try {
+    const saved = await thought.save()
+    res.status(201).json(saved)
+  } catch (err) {
+    res
+      .status(400)
+      .json({ message: 'could not save thought', errors: err.errors })
+  }
+})
+
+app.post('/:id/like',
+  async (req, res) => {
+    try {
+      const thought = await Thought.findOneAndUpdate(
+        { _id: req.params.id },
+        { $inc: { hearts: 1 } },
+        { new: true }
+      )
+      res.json(thought)
+    } catch (err) {
+      res
+        .status(400)
+        .json({ message: 'could not update heart', error: err.errors })
+    }
+  })
 
 // Start the server
 app.listen(port, () => {
