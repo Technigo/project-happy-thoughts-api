15bbab9ab69b5a18d366e5170f88f2fa6e5371c3
thisen@gmail.com <thisen@gmail.com>
2020-05-17 15:43:15 +0200

    Created mongoose model

--- server.js
+++ server.js
@@ -2,10 +2,27 @@ import express from 'express'
 import bodyParser from 'body-parser'
 import cors from 'cors'
 import mongoose from 'mongoose'
+import { stringify } from 'querystring'
 
 const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/happyThoughts"
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
 mongoose.Promise = Promise
+
+const Thought = mongoose.model('Thought', {
+  text: {
+    type: String,
+    required: true,
+    minlength: 5,
+  },
+  like: {
+    type: Boolean,
+    default: false,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+})
 
 // Defines the port the app will run on. Defaults to 8080, but can be 
 // overridden when starting the server. For example:
@@ -20,8 +37,10 @@ app.use(bodyParser.json())
 
 // Start defining your routes here
 app.get('/', (req, res) => {
-  res.send('Hello world')
+  res.send('Root, short description here.')
 })
+
+// POST 
 
 // Start the server
 app.listen(port, () => {
