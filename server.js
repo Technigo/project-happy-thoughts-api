56e227da74bbed25eb18865c6c7577f9b0415532
thisen@gmail.com <thisen@gmail.com>
2020-05-17 20:11:39 +0200

    Added post route and process env reset databse

--- server.js
+++ server.js
@@ -7,46 +7,35 @@ const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/happyThoughts"
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
 mongoose.Promise = Promise
 
-// const Thought = mongoose.model('Thought', {
-//   text: {
-//     type: String,
-//     required: true,
-//     minlength: 5,
-//   },
-//   like: {
-//     type: Boolean,
-//     default: false,
-//   },
-//   createdAt: {
-//     type: Date,
-//     default: Date.now,
-//   },
-// })
-
-const Task = mongoose.model('Task', {
+const Thought = mongoose.model('Thought', {
   text: {
     type: String,
     required: true,
-    minlength: 5
+    minlength: 5,
   },
-  complete: {
+  like: {
     type: Boolean,
-    default: false
+    default: false,
   },
   createdAt: {
     type: Date,
-    default: Date.now
-  }
+    default: Date.now,
+  },
 })
 
-// Defines the port the app will run on. Defaults to 8080, but can be 
-// overridden when starting the server. For example:
-//
-//   PORT=9000 npm start
+if (process.env.RESET_DATABASE) {
+  console.log('Resetting database...');
+
+  const deleteDatabase = async () => {
+      await Thought.deleteMany();
+      console.log(`Deleting databse`)
+  };
+  deleteDatabase();
+}
+
 const port = process.env.PORT || 8080
 const app = express()
 
-// Add middlewares to enable cors and json body parsing
 app.use(cors())
 app.use(bodyParser.json())
 
@@ -55,13 +44,22 @@ app.get('/', (req, res) => {
 })
 
 
-app.get('/tasks', async (req, res) => {
-  const tasks = await Task.find().sort({createdAt: 'desc'}).limit(20).exec()
-  res.json(tasks)
-  res.send('hello')
+app.get('/thoughts', async (req, res) => {
+  const thoughts = await Thought.find().sort({createdAt: 'desc'}).limit(20).exec()
+  res.json(thoughts)
 })
 
-// POST 
+app.post('/thoughts', async (req, res) => {
+  const {text, like} = req.body
+  const thought = new Thought({ text, like })
+
+  try{
+    const savedThought = await thought.save()
+    res.status(200).json(savedThought)
+  } catch (err) {
+    res.status(400).json({ message: 'Could not save thought to the databse' , error:err.errors})
+  }
+})
 
 
 // Start the server
