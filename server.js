ff548dfb6ecb883be52dc2332d1e40d7a5ce0eb7
Lisa Hammarstrand <lisa@Lisas-Air.lan>
2020-05-17 20:19:04 +0200

    Added first endpoints

--- server.js
+++ server.js
@@ -7,10 +7,26 @@ const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/happyThoughts"
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
 mongoose.Promise = Promise
 
-// Defines the port the app will run on. Defaults to 8080, but can be 
-// overridden when starting the server. For example:
-//
-//   PORT=9000 npm start
+// Seed database to test endpoints
+
+// Hearts should not be assignable when adding a new thought, how to fix?
+const Thought = mongoose.model('/thought', {
+  message: {
+    type: String,
+    required: true,
+    minlength: 5,
+    maxlength: 140
+  },
+  hearts: {
+    type: Number,
+    default: 0,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
+})
+
 const port = process.env.PORT || 8080
 const app = express()
 
@@ -21,6 +37,28 @@ app.use(bodyParser.json())
 // Start defining your routes here
 app.get('/', (req, res) => {
   res.send('Hello world')
+})
+
+// Get thoughts, sort by createdAt descending order, limit to show 20
+app.get('/thoughts', async (req, res) => {
+  const thoughts = await Thought.find().sort({ createdAt: 'desc' }).limit(20).exec()
+  res.json(thoughts)
+})
+
+// Retreive the information sent by the client to our API endpoint
+app.post('/thoughts', async (req, res) => {
+  const { message, hearts } = req.body
+  // Use our mongoose model to create the database entry
+  const thought = new Thought({ message, hearts })
+
+  try {
+    //Success
+    const savedThought = await thought.save()
+    res.status(201).json(savedThought)
+    // Fail
+  } catch (err) {
+    res.status(400).json({ message: 'Could not save thought to database', error: err.errors })
+  }
 })
 
 // Start the server
