6bac25312e19bf5afcddbe3b2a2892e0ce66247f
annaelsamaria <hej@annagustafsson.se>
2020-05-17 19:56:18 +0200

    post happy thought works in postman

--- server.js
+++ server.js
@@ -2,10 +2,28 @@ import express from 'express'
 import bodyParser from 'body-parser'
 import cors from 'cors'
 import mongoose from 'mongoose'
+import { EventEmitter } from 'events'
 
 const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/happyThoughts"
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
 mongoose.Promise = Promise
+
+
+const Thought = mongoose.model('Thought', {
+  message: {
+    type: String,
+    required: true,
+    minlength: 5
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  },
+  heart: {
+    type: Number,
+    default: 0
+  }
+})
 
 // Defines the port the app will run on. Defaults to 8080, but can be 
 // overridden when starting the server. For example:
@@ -18,10 +36,37 @@ const app = express()
 app.use(cors())
 app.use(bodyParser.json())
 
-// Start defining your routes here
-app.get('/', (req, res) => {
-  res.send('Hello world')
+
+
+//This endpoint should return a maximum of 20 thoughts,
+//sorted by `createdAt` to show the most recent thoughts first.
+
+app.get('/', async (req, res) => {
+  const thoughts = await Thought.find().sort({ createdAt: 'desc' }).limit(20).exec()
+  res.json(thoughts)
 })
+
+app.post('/', async (req, res) => {
+  const { message } = req.body
+  const thought = new Thought({ message })
+
+  try {
+    const savedThought = await thought.save()
+    res.status(201).json(savedThought)
+  } catch (err) {
+    res.status(400).json({ message: 'Could not save happy thought to database', error: err.errors })
+  }
+})
+
+app.post('/:thoughtId/like', async (req, res) => {
+  const heart = await Thought.findById(req.params.id)
+  if (heart) {
+    res.json(heart)
+  } else {
+    res.status(404).json({ error: 'Could not like message' })
+  }
+})
+
 
 // Start the server
 app.listen(port, () => {
