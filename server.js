c7aeb1f10448aed4c95104a45616c2608a08ba76
Olle Alm <olle.alm@me.com>
2020-05-17 17:37:46 +0200

    deplyoing

--- server.js
+++ server.js
@@ -29,11 +29,14 @@ app.use(bodyParser.json())
 
 
 app.get('/', async (req, res) => {
-  const { page } = req.query
+  const { page = 1, tag, likes = 0, sort, order } = req.query
   const limit = 20
-  const thoughts = await Thought.find().sort({ createdAt: 'desc' }).limit(limit).skip(limit * (page - 1))
+  // const query = { tag: tag }
+  // const thoughts = await Thought.find({ likes }).sort({ createdAt: 'desc' }).limit(limit).skip(limit * (page - 1))
+  const thoughts = await Thought.find({ likes: { $gte: likes } }).sort({ createdAt: 'desc' }).limit(limit).skip(limit * (page - 1))
   const total = (await Thought.find()).length
   const pages = Math.ceil(total / limit)
+
   if (thoughts.length > 0) {
     res.json({ pages, total, thoughts })
   } else {
@@ -61,23 +64,39 @@ app.post('/', async (req, res) => {
 app.post('/:thoughtId/like', async (req, res) => {
   const { thoughtId } = req.params
   try {
-    const thought = await Thought.findByIdAndUpdate(thoughtId, { $inc: { likes: 1 } }, { useFindAndModify: false })
+    const thought = await Thought.findByIdAndUpdate(
+      thoughtId,
+      { $inc: { likes: 1 } },
+      { useFindAndModify: false },
+    )
     console.log(thought)
     res.status(200)
       .json(thought)
   } catch (err) {
     res.status(400).json({ message: 'Could not like post', error: err })
   }
+})
+
+app.post('/:thoughtId/delete', async (req, res) => {
+
+})
+
+app.post('/:thoughtId/edit', async (req, res) => {
 
 })
 
 
 
-// Delete
+// deploy to heruko and db 
+
 // filtering and sorting
 // choose to sort by oldest first, or only show thoughts which have a lot of hearts
+
 // edit with time limit
 
+// Delete
+
+// fork frontend
 
 
 // Start the server
