e975703bb9e6d54e47d8a442fafbd47ea0ff8860
palhamel <pal.hamel@gmail.com>
2020-05-17 16:20:45 +0200

    Initial mongoose model setup

--- server.js
+++ server.js
@@ -7,6 +7,40 @@ const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/happyThoughts"
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
 mongoose.Promise = Promise
 
+
+// Mongoose model setup:
+
+const Thought = mongoose.model("Thought", {
+
+  message: {
+    type: String,
+    required: true,
+    minlength: 5,
+    maxlength: 140,
+  },
+
+
+ // strict? not assignable  
+  heart: {
+    type: Number,
+    default: 0,
+  },
+ // strict? not assignable  
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+
+})
+
+
+
+
+
+
+
+
+
 // Defines the port the app will run on. Defaults to 8080, but can be 
 // overridden when starting the server. For example:
 //
