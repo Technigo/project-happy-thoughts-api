e05f597800c0eb3416728a4b4841a0cadd5de0d2
Your Name <malin.marie@hotmail.se>
2020-05-18 19:18:28 +0200

    sort

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.use(bodyParser.json())
 
 app.get('/thoughts', async (req, res) => {
   try {
-    const thoughts = await Thought.find().limit(20)
+    const thoughts = await Thought.find().limit(20).sort({ createdAt: -1 })
     res.status(200).json(thoughts)
   } catch(err) {
     res.status(404).send(err)
