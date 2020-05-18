7559eed4b69fb24e1c8fefd240583b2d0cc22f6e
palhamel <pal.hamel@gmail.com>
2020-05-18 15:34:13 +0200

    Add reset database via process.env

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ import bodyParser from "body-parser";
 import cors from "cors";
 import mongoose from "mongoose";
 
+// Error message:
 const ERR_CANNOT_FIND_THOUGHTS = 'No thoughts found';
 
 // Mongoose & Database setup:
@@ -10,41 +11,44 @@ const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/happyThoughts";
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true });
 mongoose.Promise = Promise;
 
-// Validation and error checking
-// TODO: Check if mongodb is up
 
-
-
+// -----------------------------------------------
 // Mongoose model setup:
 const Thought = mongoose.model("Thought", {
   message: {
     type: String,
     required: true,
     minlength: 5,
-    maxlength: 140,
+    maxlength: 140
   },
   // strict? not assignable
-  heart: {
+  hearts: {
     type: Number,
-    default: 0,
+    default: 0
     // required: true,
   },
   // strict? not assignable
   createdAt: {
     type: Date,
-    default: Date.now,
+    default: Date.now
   },
 });
 
+// -----------------------------------------------
+// To reset database (and then populate db if needed):
+// $ RESET_DATABASE=true npm run dev
+// Seed DATABASE using Async
+if (process.env.RESET_DATABASE) {
+  console.log("Message: Resetting database");
 
-// Seed database?
+  const seedDatabase = async () => {
+    await Thought.deleteMany();
+    // await booksData.forEach((book) => new Book(book).save());
+  };
+  seedDatabase();
+};
 
-/*
-
-
-*/
-
-
+// -----------------------------------------------
 // Defines the port the app will run on. Defaults to 8080, but can be
 // overridden when starting the server. For example:
 //
@@ -65,29 +69,23 @@ app.use((req, res, next) => {
   }
 });
 
-
-
-
-
+// -----------------------------------------------
 // Start defining your routes here
-/* app.get('/', (req, res) => {
-  res.send('Hello super world')
-}) */
+// 1: GET the Thoughts, 2: POST a Thought, 3: POST a Like on a Thought
 
-// GET the Thoughts:
+// 1: GET the Thoughts
 app.get("/", async (req, res) => {
   const thoughts = await Thought.find().sort({ createdAt: "desc" }).limit(20).exec();
   if (thoughts) {
     res.status(200).json(thoughts)
   } else {
-    res.status(404).json({ message: ERR_CANNOT_FIND_THOUGHTS })
+    res.status(404).json({ message: ERR_CANNOT_FIND_THOUGHTS, errors: err.errors  })
   }
 });
 
-
-
-
-// POST the Thoughts:
+// 2: POST a Thought
+// Validate user input
+// success / failed via try/catch
 
 app.post("/", async (req, res) => {
   // retrieve message sent by client to our API endpoint:
@@ -95,11 +93,9 @@ app.post("/", async (req, res) => {
   // use our mongoose model to create the database entry:
   const thought = new Thought({ message });
   try {
-    // success posting:
     const savedThought = await thought.save();
     res.status(201).json(savedThought);
   } catch (err) {
-    // failed posting:
     res.status(400).json({
         message: "Could not save thought to Database",
         error: err.errors,
@@ -107,15 +103,31 @@ app.post("/", async (req, res) => {
   }
 });
 
+// 3: POST a Like on a Thought
+// create app.post '/' for heart/like on thought:
+// using POST - as I already have post in my Happy project as method.
+// success / failed via try/catch
 
-
-// TODO: create app.post '/' for heart/like on thought:
+app.post('/:thoughtId/like', async (req, res) => {
+  const { thoughtId } = req.params;
+  try {
+    await Thought.updateOne({ '_id': thoughtId }, { '$inc': { 'hearts': 1 } });
+    res.status(201).json();
+  } catch (err) {
+    res.status(404).json({ message: `Could not like ${thoughtId}`, error: err.errors });
+  }
+})
 
 // TODO:
 // Validation of user input when POSTing a thought
 // Handling error's
 // Sending back error codes: 400..
 
+// Validation and error checking
+// Check if mongodb is up
+
+
+// -----------------------------------------------
 // Start the server
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
