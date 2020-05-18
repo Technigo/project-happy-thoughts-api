9885118d7d0982abd63af6b7920d3158f899761c
Frida Eriksson <Frida.Eriksson@silf.se>
2020-05-18 13:37:12 +0200

    created comment model

---
+++ models/comment.js
@@ -0,0 +1,15 @@
+import mongoose from 'mongoose'
+
+const Schema = mongoose.Schema
+
+const CommentSchema = new Schema({
+  comment: {
+    type: String
+  },
+  message: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Thought'
+  }
+})
+
+module.exports = new mongoose.model('Comment', ThoughtSchema)
