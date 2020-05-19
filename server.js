db46b38d4dc0a1ad1407cad1f07f96bbfb0149d8
Ingrid Hammargren <ingrid.hammargren@gmail.com>
2020-05-19 09:00:39 +0200

    created mongoose model, a get and a post endpoint

--- server.js
+++ server.js
@@ -7,6 +7,24 @@ const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/happyThoughts"
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
 mongoose.Promise = Promise
 
+const Tought = mongoose.model('Tought', {
+  text: {
+    type: String,
+    required: true,
+    minlength: 5,
+    maxlength: 140
+  },
+  hearts: {
+    type: Number,
+    default: 0
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
+
+})
+
 // Defines the port the app will run on. Defaults to 8080, but can be 
 // overridden when starting the server. For example:
 //
@@ -22,6 +40,28 @@ app.use(bodyParser.json())
 app.get('/', (req, res) => {
   res.send('Hello world')
 })
+
+// Get the 20 most recent toughts
+app.get('/toughts', async (req, res) => {
+  const toughts = await Tought.find().sort({ createdAt: 'desc' }).limit(20).exec()
+  res.json(toughts)
+})
+
+app.post('/toughts', async (req, res) => {
+  // retrieve the information sent from the client to out API endpoint
+  const { text, complete } = req.body
+  // use our mongoose model to create the database entry
+  const tought = new Tought({ text, complete })
+  try {
+    // sucess
+    const savedTought = await tought.save()
+    res.status(201).json(savedTought)
+  } catch (err) {
+    res.status(400).json({ message: 'Could not save tought to the Database', error: err.errors })
+
+  }
+})
+
 
 // Start the server
 app.listen(port, () => {
