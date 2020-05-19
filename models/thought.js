1830ec0223aee57448b32da9e84c24a98e6d6530
Lisas Sturegard <lisasturegard@hotmail.com>
2020-05-19 12:21:15 +0200

    Testing again

--- models/thought.js
+++ models/thought.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose'
 
 const Thought = mongoose.model('Thought', {
   message: { type: String, requiered: true, minlength: 5, maxlength: 140 },
-  hearts: { type: Number, requiered: true, default: 0},
+  hearts: { type: Number, requiered: true},
   createdAt: { type: Date, default: Date.now }
 }) 
 
