0d44fa362db4a210f7f7d2268ca91cee9b08ef97
elsareutersward <elsa.reutersward@gmail.com>
2020-05-20 13:51:52 +0200

    small changes for deploy

--- server.js
+++ server.js
@@ -14,9 +14,13 @@ const Thought = mongoose.model('Thought', {
     minlength: 5,
     maxlength: 140
   },
-  liked: {
-    type: Boolean,
-    default: false
+  name: {
+    type: String,
+    default: 'Anonymous'
+  },
+  hearts: {
+    type: Number,
+    default: 0
   },
   createdAt: { 
     type: Date, 
@@ -24,31 +28,56 @@ const Thought = mongoose.model('Thought', {
   }
 });
 
-const port = process.env.PORT || 8080
+if (process.env.RESET_DATABASE) {
+  const seedDatabase = async () => {
+    await Thought.deleteMany();
+    await Thought.forEach((thought) => {
+      new Thought(thought).save();
+    });
+  };
+  seedDatabase();
+};
+
+const port = process.env.PORT || 8080;
 const app = express();
 
 app.use(cors());
 app.use(bodyParser.json());
+app.use((req, res, next) => {
+  if (mongoose.connection.readyState === 1) {
+    next();
+  } else {
+    res.status(503).json({ error: 'Service unavailable' });
+  };
+});
 
 app.get('/', async (req, res) => {
-  const thoughts = await Thought.find().sort({createdAt: 'desc'}).limit(20).exec();
+  const thoughts = await Thought.find()
+    .sort({createdAt: 'desc'})
+    .limit(20);
   res.json(thoughts);
 });
 
 app.post('/', async (req, res) => {
   try {
-    const { message } = req.body
-    const thought = await new Thought({ message }).save();
-    res.status(201).json(thought);
+    const { message, name } = req.body;
+    const thought = await new Thought({ message, name }).save();
+    res.status(200).json(thought);
   } catch (err) {
     res.status(400).json({error: 'Could not save thought to the Database', errors:err.errors});
   }
 });
 
-app.post('/:thoughtID/like', async (req, res) => {
+app.put('/:thoughtID/like', async (req, res) => {
+  try {
+    await Thought.updateOne({ _id: _id }, { $inc : {hearts: 1} }).save();
+    res.status(201).json();
+  } catch (err) {
+    res.status(400).json({error: 'Could not save like to the Database', errors:err.errors});
+  }
+});
 
-})
 
 app.listen(port, () => {
-  console.log(`Server running on http://localhost:${port}`)
+  console.log(`Server running on http://localhost:${port}`);
 });
