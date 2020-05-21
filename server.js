2d28f881baa79bbc03ed1c90826329dc6e9708d2
ebbabw <ebbabergholtz@gmail.com>
2020-05-21 13:19:14 +0200

    First draft

--- server.js
+++ server.js
@@ -7,6 +7,23 @@ const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/happyThoughts"
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
 mongoose.Promise = Promise
 
+const Thought = mongoose.model('Thought', {
+  message: { 
+  type: String,
+  required: true,
+  minlength: 5,
+  maxlength: 140, 
+  },
+  heart: { 
+  type: Number, 
+  default: 0 
+  },
+  createdAt: { 
+  type: Date, 
+  default: () => new Date()
+  }
+})
+
 // Defines the port the app will run on. Defaults to 8080, but can be 
 // overridden when starting the server. For example:
 //
@@ -18,9 +35,38 @@ const app = express()
 app.use(cors())
 app.use(bodyParser.json())
 
+app.use((req, res, next) => {
+  if (mongoose.connection.readyState === 1) {
+    next()
+  } else {
+    res.status(503).json({ error: 'Service unavailable' })
+  }
+})
+
 // Start defining your routes here
 app.get('/', (req, res) => {
   res.send('Hello world')
+})
+
+app.get('/thoughts', async (req, res) => {
+  const thoughts = await Thought.find().sort({createdAt: 'desc'}).limit(20).exec()
+  res.json(thoughts)
+})
+
+app.post('/thoughts', async (req, res) => {
+  // Retrieve the information send by the client to your API endpoint
+  const { message} = req.body
+
+  // Use our mongoose model to create the database entry
+  const thought = new Thought({message});
+
+  try {
+    // Success
+    const saveThought = await thought.save()
+    res.status(201).json(saveThought)
+    } catch (err) {
+      res.status(400).json({message: 'could not save this thought to the Database' error:err.errors})
+    }
 })
 
 // Start the server
