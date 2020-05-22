ff2a432d19d1a907d4012b2de586ed3482bc9b73
Louise Eriksson <epost.louise@gmail.com>
2020-05-22 12:55:09 +0200

    cleaned up the code / deleted comments

--- server.js
+++ server.js
@@ -7,23 +7,94 @@ const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/happyThoughts"
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
 mongoose.Promise = Promise
 
-// Defines the port the app will run on. Defaults to 8080, but can be 
-// overridden when starting the server. For example:
-//
-//   PORT=9000 npm start
+const Thought = mongoose.model('Thought', {
+  message: {
+    type: String,
+    required: true,
+    minlength: 5,
+    maxlength: 140
+  },
+  hearts: {
+    type: Number,
+    default: 0,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
+})
+
+// if (process.env.RESET_DATABASE) {
+//   console.log('Resetting database')
+
+//   const deleteDatabase = async () => {
+//     await Thought.deleteMany()
+//     console.log(`Deleting database`)
+//   };
+//   deleteDatabase()
+// }
+
 const port = process.env.PORT || 8080
 const app = express()
 
-// Add middlewares to enable cors and json body parsing
 app.use(cors())
 app.use(bodyParser.json())
 
-// Start defining your routes here
+
+// message, heart, createdAt
+// get/ 20 thoughts sorted by created at
+//### `POST /`
+
+// This endpoint expects a JSON body with the thought `message`, like this: `{ "message": "Express is great!" }` . If the input is valid (more on that below), the thought should be saved, and the response should include the saved thought object, including its `_id`.
+
+// ### `POST /:thoughtId/like`
+
+// This endpoint doesn't require a JSON body. Given a valid thought id in the url, the API should find that thought, and update its `hearts` property to add one heart.
+
 app.get('/', (req, res) => {
   res.send('Hello world')
 })
 
-// Start the server
+// app.post('/thoughts', (req, res) => {
+//   const thought = new Thought(req.body)
+//   await note.save
+// })
+
+app.get('/thoughts', async (req, res) => {
+  try {
+    const thoughts = await Thought.find().sort({ createdAt: 'desc' }).limit(20).exec()
+    res.json(thoughts)
+  }
+  catch {
+    res.status(404).json({ error: "Can not find thoughts" })
+  }
+})
+
+
+app.post('/thoughts', async (req, res) => {
+  try {
+    const { message } = req.body
+    const thought = new Thought({ message })
+    const savedThought = await thought.save()
+    res.status(201).json(savedThought)
+  }
+  catch (err) {
+    res.status(400).json({ message: 'Could not save thought', error: err.errors })
+  }
+})
+
+app.post('/:thoughtId/like', async (req, res) => {
+  const { thoughtId } = req.params
+
+  try {
+    const likeThought = await Thought.updateOne({ _id: thoughtId }, { $inc: { hearts: 1 } })
+    res.status(201).json(likeThought)
+  }
+  catch (err) {
+    res.status(400).json({ message: 'Could not find Id', error: err.errors })
+  }
+})
+
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`)
 })
