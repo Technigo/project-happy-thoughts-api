ca86e1725a401d2997c45969dd8ee6af7ad5f7f3
Siri <siri.gyllander@gmail.com>
2020-05-22 22:13:06 +0200

    the newer version without models folder

--- server.js
+++ server.js
@@ -15,13 +15,69 @@ const port = process.env.PORT || 8080
 const app = express()
 
 // Add middlewares to enable cors and json body parsing
-app.use(cors())
-app.use(bodyParser.json())
+app.use(cors());
+app.use(bodyParser.json());
 
-// Start defining your routes here
-app.get('/', (req, res) => {
-  res.send('Hello world')
-})
+const thoughts = mongoose.model('thoughts', { 
+  message: {
+        type: String,
+        required: true,
+        minlenght: [2, "At least 2 characters"],
+    },
+  hearts: {
+       type: Number,
+       default: 0
+   },
+   createdAt:{
+        type: Date,
+        default: Date.now
+    }
+  });
+  // Start defining your routes here;
+
+  app.get("/", async (req, res) => {
+    const resPerPage = 20; // results per page
+    const { page } = req.query || 1; // Page
+    Thought.find((err, thoughts) => {
+      if (err) {
+        console.log(err);
+        res.status(404).json({ error: "Not found" });
+      } else {
+        res.json(thoughts);
+      }
+    })
+      .skip(resPerPage * page - resPerPage)
+      .sort({ createdAt: "desc" })
+      .limit(resPerPage);
+  });
+  
+  app.post("/", async (req, res) => {
+    try {
+      const thought = new Thought({ message: req.body.message });
+      await thought.save();
+      res.json(thought);
+    } catch (err) {
+      res
+        .status(400)
+        .json({ errors: err.errors, message: "Unable to add new thought" });
+    }
+  });
+  
+  app.post("/:thoughtId/like", async (req, res) => {
+    const { thoughtId } = req.params;
+  
+    try {
+      const like = await Thought.findById(thoughtId);
+      like.hearts += 1;
+      like.save();
+      res.status(201).json(like);
+    } catch (err) {
+      res.status(400).json({
+        message: "unable to add like, no thought available",
+        errors: err.errors
+      });
+    }
+  });
 
 // Start the server
 app.listen(port, () => {
