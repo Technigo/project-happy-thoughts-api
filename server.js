7c6b70c67764c194ef6bef9a836c6eb955be8550
aasa72 <asa.andersson10@comhem.se>
2020-06-07 22:56:33 +0200

    changed db name

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ import bodyParser from 'body-parser'
 import cors from 'cors'
 import mongoose from 'mongoose'
 
-const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/happyThought"
+const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/happyThought2"
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
 mongoose.Promise = Promise
 
