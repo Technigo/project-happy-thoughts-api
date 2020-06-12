819a93d9f9593848e07b3a74b82e587465445823
aasa72 <asa.andersson10@comhem.se>
2020-06-12 12:48:02 +0200

    changed mongoURL in attempt to hook up frontend and backend

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ import bodyParser from 'body-parser'
 import cors from 'cors'
 import mongoose from 'mongoose'
 
-const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/happyThought2"
+const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/happythought"
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
 mongoose.Promise = Promise
 
