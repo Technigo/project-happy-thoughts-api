e113620dc56159fdda2a31c8c3c6855d42436a21
Max Forssén <max.forssen@mentimeter.com>
2020-12-29 14:28:57 +0100

    GET endpoint, POST endpoint done

--- server.js
+++ server.js
@@ -2,10 +2,28 @@ import express from 'express'
 import bodyParser from 'body-parser'
 import cors from 'cors'
 import mongoose from 'mongoose'
+import { restart } from 'nodemon'
 
 const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/happyThoughts"
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
 mongoose.Promise = Promise
+
+const Thought = new mongoose.model('Thought', {
+  message: {
+    type: String,
+    required: true,
+    minlength: 5,
+    maxlength: 140
+  },
+  hearts: {
+    type: Number,
+    default: 0
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
+})
 
 // Defines the port the app will run on. Defaults to 8080, but can be 
 // overridden when starting the server. For example:
@@ -19,8 +37,21 @@ app.use(cors())
 app.use(bodyParser.json())
 
 // Start defining your routes here
-app.get('/', (req, res) => {
-  res.send('Hello world')
+app.get('/thoughts', async (req, res) => {
+  const thoughts = await Thought.find().sort({createdAt: 'desc'}).limit(20).exec()
+  res.json(thoughts)
+})
+
+app.post('/thoughts', async (req, res) => {
+  const {message, hearts} = req.body
+  const thoughts = new Thought({message, hearts})
+
+  try {
+    const saveThought = await thoughts.save()
+    res.status(201).json(saveThought)
+  } catch (err) {
+    res.status(400).json({message: 'could not save task to db', error:err.errors})
+  }
 })
 
 // Start the server
