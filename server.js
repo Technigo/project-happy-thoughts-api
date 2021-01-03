525336340ebae64c536d99fb4248310b4c23d276
Vanessa Sue Smith <sue27.vanessa@gmail.com>
2021-01-03 14:44:31 +0100

    Got basic endpoints in place, only tested locally

--- server.js
+++ server.js
@@ -1,29 +1,81 @@
-import express from 'express'
-import bodyParser from 'body-parser'
-import cors from 'cors'
-import mongoose from 'mongoose'
+import express from 'express';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+import mongoose from 'mongoose';
 
-const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/happyThoughts"
-mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
-mongoose.Promise = Promise
+const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/happyThoughts";
+mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.Promise = Promise;
 
-// Defines the port the app will run on. Defaults to 8080, but can be 
-// overridden when starting the server. For example:
-//
-//   PORT=9000 npm start
-const port = process.env.PORT || 8080
-const app = express()
+// Defines the port the app will run on
+const port = process.env.PORT || 8080;
+const app = express();
 
 // Add middlewares to enable cors and json body parsing
-app.use(cors())
-app.use(bodyParser.json())
+app.use(cors());
+app.use(bodyParser.json());
 
-// Start defining your routes here
+// mongoose model for Thought
+const Thought = mongoose.model('Thought', {
+  message: {
+    type: String,
+    required: true,
+    minLength: 5,
+    maxLength: 140
+  },
+  hearts: {
+    type: Number,
+    default: 0
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+// MAYBE: Add a Reset database function, in case we'd like to restart with a clean slate?
+
+// ENDPOINTS
 app.get('/', (req, res) => {
-  res.send('Hello world')
+  res.send('Hello world, welcome to Happy Thoughts API - Vanessa version <3');
 })
+
+// GET /thoughts : Endpoint showing the 20 most recent thoughts------------------------------------------------
+app.get('/thoughts', async (req, res) => {
+  const thoughts = await Thought.find().sort({ createdAt: 'desc' }).limit(20);
+  res.json(thoughts);
+});
+
+// POST /thoughts : Endpoint to add a new Thought to the database----------------------------------------------
+app.post('/thoughts', async (req, res) => {
+  // Retrieve message sent by the client in the POST request body
+  const message = req.body.message;
+
+  // use mongoose model to create a new Thought using the message we got from the client
+  const thought = new Thought({ message });
+
+  try {
+    // Success case: Add a new instance of Thought to the database by calling save() on it
+    const savedThought = await thought.save();
+    res.status(200).json(savedThought);
+  } catch (err) {
+    res.status(400).json({ message: "Could not save Thought to the database", error: err.errors });
+  }
+});
+
+// POST /thoughts/:thoughtId/like : Endpoint to add a heart "like" to a specific thought------------------------
+app.post('/thoughts/:thoughtId/like', async (req, res) => {
+  const thoughtId = req.params.thoughtId;
+
+  try {
+    const thoughtLiked = await Thought.updateOne({ _id: thoughtId }, { $inc: { hearts: 1 } });
+    res.json(thoughtLiked);
+  } catch (err) {
+    res.status(400).json({ message: "Could not find that Thought", error: err.errors });
+  }
+});
 
 // Start the server
 app.listen(port, () => {
-  console.log(`Server running on http://localhost:${port}`)
-})
+  console.log(`Server running on http://localhost:${port}`);
+});
