176b97184ed0d365a6fb8aab36c7713887c972d8
Gabbi-89 <gabriellabolin@hotmail.com>
2021-01-03 15:31:59 +0100

    First two endpoints created

--- server.js
+++ server.js
@@ -7,23 +7,67 @@ const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/happyThoughts"
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
 mongoose.Promise = Promise
 
+const Thought = mongoose.model('Thought', {
+  message: {
+    type: String,
+    required: true,
+    minlength: 5,
+    maxlength: 140
+  },
+  hearts: {
+    type: Number,
+    default: 0,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now()
+  }
+});
+
 // Defines the port the app will run on. Defaults to 8080, but can be 
 // overridden when starting the server. For example:
 //
 //   PORT=9000 npm start
-const port = process.env.PORT || 8080
-const app = express()
+const port = process.env.PORT || 8080;
+const app = express();
 
 // Add middlewares to enable cors and json body parsing
-app.use(cors())
-app.use(bodyParser.json())
+app.use(cors());
+app.use(bodyParser.json());
 
 // Start defining your routes here
 app.get('/', (req, res) => {
   res.send('Hello world')
-})
+});
+
+// Endpoint created to get a list of the 20 latest thoughts
+app.get('/thoughts', async (req, res) => {
+  const thoughts = await Thought.find().sort({ createdAt: 'desc' }).limit(20).exec();
+  res.json(thoughts);
+});
+
+// Endpoint for posting thoughts and adding it to the database
+app.post('/thoughts', async (req, res) => {
+  // Retrieves the information sent by the client to our API endpoint
+  const { message } = req.body;
+
+  // Use our mongoose model to create the database entry
+  const thought = new Thought({ message });
+
+  try {
+    // Sucess case
+    const savedThought = await thought.save();
+    res.status(200).json(savedThought);
+  } catch (err) {
+    // Bad request
+    res.status(400).json({
+      message: 'Could not save thought to the Database',
+      error: err.errors
+    });
+  }
+});
 
 // Start the server
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`)
-})
+});
