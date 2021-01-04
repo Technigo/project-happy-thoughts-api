41ca3b146a288286ddb81d2522486e6e828b8cda
Annika Melin <melin.anki>
2021-01-04 14:05:43 +0100

    added Thought model and get, post and post like endpoints

--- server.js
+++ server.js
@@ -7,11 +7,28 @@ const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/happyThoughts"
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
 mongoose.Promise = Promise
 
+const Thought = mongoose.model('Thought', {
+  message: {
+    type: String,
+    required: [true, 'Message required]'],
+    minLength: [5, 'Too short message, minimum 5 characters'],
+    maxLength: [140, 'Too long message, maximum 140 characters']
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  },
+  hearts: {
+    type: Number,
+    default: 0
+  }
+})
+
 // Defines the port the app will run on. Defaults to 8080, but can be 
 // overridden when starting the server. For example:
 //
 //   PORT=9000 npm start
-const port = process.env.PORT || 8080
+const port = process.env.PORT || 8081
 const app = express()
 
 // Add middlewares to enable cors and json body parsing
@@ -20,7 +37,31 @@ app.use(bodyParser.json())
 
 // Start defining your routes here
 app.get('/', (req, res) => {
-  res.send('Hello world')
+  res.send('Happy thoughts API')
+})
+
+app.get('/thoughts', async (req, res) => {
+  const thoughts = await Thought.find().sort({ created: 'desc' }).limit(20).exec()
+  res.json(thoughts)
+})
+
+app.post('/thoughts', async (req, res) => {
+  const newThought = new Thought({ message: req.body.message })
+  try {
+    const savedThought = await newThought.save()
+    res.status(201).json(savedThought)
+  } catch (err) {
+    res.status(400).json({ message: 'Could not save message', error: err.errors })
+  }
+})
+
+app.post('/thoughts/:id/like', async (req, res) => {
+  try {
+    const updatedThought = await Thought.updateOne({ _id: req.params.id }, { $inc: { 'hearts': 1 } }, { new: true })
+    res.status(201).json(updatedThought)
+  } catch (err) {
+    res.status(404).json({ message: 'Could not post like', error: err.errors })
+  }
 })
 
 // Start the server
