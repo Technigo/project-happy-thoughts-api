d3a73613bde3477a57776ebc94ed9a00093ff77d
Katsiez <katarinajovanovic.a@gmail.com>
2021-01-05 20:14:14 +0100

    first commit, followed the codealong, created get and post for thoughts

--- server.js
+++ server.js
@@ -7,11 +7,24 @@ const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/happyThoughts"
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
 mongoose.Promise = Promise
 
-// Defines the port the app will run on. Defaults to 8080, but can be 
-// overridden when starting the server. For example:
-//
-//   PORT=9000 npm start
-const port = process.env.PORT || 8080
+const Thought = mongoose.model('Thought', {
+  message:{
+    type: String,
+    required: true,
+    minlength: 5,
+    maxlength: 140
+  },
+  hearts:{
+    type: Number,
+    default: 0,
+  },
+  createdAt:{
+    type: Date,
+    default: Date.now
+  }
+})
+
+const port = process.env.PORT || 3200
 const app = express()
 
 // Add middlewares to enable cors and json body parsing
@@ -21,6 +34,24 @@ app.use(bodyParser.json())
 // Start defining your routes here
 app.get('/', (req, res) => {
   res.send('Hello world')
+})
+
+//GET AND POST THOUGHTS
+app.get('/thoughts', async (req, res) => {
+  const thoughts = await Thought.find().sort({createdAt: "desc"}).limit(20).exec()
+  res.json(thoughts)
+})
+
+app.post('/thoughts', async(req, res) => {
+  const {message, hearts} = req.body
+  const thought = new Thought({message, hearts})
+
+  try {
+    const savedThought = await thought.save()
+    res.status(201).json(savedThought)
+  } catch (err) {
+    res.status(400).json({message: 'Could not save thought to the database', error: err.errors})
+  }
 })
 
 // Start the server
