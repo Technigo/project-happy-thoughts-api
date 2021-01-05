a45ae67326ebf77a0c7fbf7113f12d500fb1f90c
Sofia Sousa <sofiavazsousa@gmail.com>
2021-01-05 16:58:55 +0100

    Change url to like

--- server.js
+++ server.js
@@ -72,7 +72,7 @@ app.post('/thoughts', async (req,res) => {
   };
 });
  
-app.post('/thoughts/:thoughtId/like', async (req, res) => { 
+app.post('/thoughts/:id/like', async (req, res) => { 
   const thoughtId = req.params.id;
 
   try { 
