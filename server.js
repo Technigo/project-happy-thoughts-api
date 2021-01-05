e628253ea8b5d3b3c11f088612548e8a3485e2ac
Karin Nordkvist <kontakt@karinnordkvist.se>
2021-01-05 11:45:44 +0100

    Path to send thought added and path to list thoughts

--- server.js
+++ server.js
@@ -1,29 +1,53 @@
-import express from 'express'
-import bodyParser from 'body-parser'
-import cors from 'cors'
-import mongoose from 'mongoose'
+import express from 'express';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+import mongoose from 'mongoose';
 
-const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/happyThoughts"
-mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
-mongoose.Promise = Promise
+const mongoUrl = process.env.MONGO_URL || 'mongodb://localhost/happyThoughts';
+mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.Promise = Promise;
 
-// Defines the port the app will run on. Defaults to 8080, but can be 
-// overridden when starting the server. For example:
-//
-//   PORT=9000 npm start
-const port = process.env.PORT || 8080
-const app = express()
+const port = process.env.PORT || 8080;
+const app = express();
 
-// Add middlewares to enable cors and json body parsing
-app.use(cors())
-app.use(bodyParser.json())
+// Middlewares ------------------------------------
+app.use(cors());
+app.use(bodyParser.json());
 
-// Start defining your routes here
+// Models ------------------------------------
+const Thought = mongoose.model('Thought', {
+  message: {
+    type: String,
+    required: true,
+  },
+  hearts: {
+    type: Number,
+    default: 0,
+  },
+  createdAt: {
+    type: Date,
+    default: () => new Date(),
+  },
+});
+
+// Routes ------------------------------------
 app.get('/', (req, res) => {
-  res.send('Hello world')
-})
+  res.send('Hello world');
+});
+
+app.post('/thoughts', async (req, res) => {
+  console.log(req.body);
+  const thought = new Thought({ message: req.body.message });
+  await thought.save();
+  res.json(thought);
+});
+
+app.get('/thoughts', async (req, res) => {
+  const thoughts = await Thought.find();
+  res.json(thoughts);
+});
 
 // Start the server
 app.listen(port, () => {
-  console.log(`Server running on http://localhost:${port}`)
-})
+  console.log(`Server running on http://localhost:${port}`);
+});
