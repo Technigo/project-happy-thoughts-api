a372f47f240c709e4022a8d366dfd27f50542a1c
Karan Mann <62376840+karanmann@users.noreply.github.com>
2021-01-05 23:42:54 +0100

    POST and GET functioning. Checked on MongoDB Compass and Postman respectively

--- server.js
+++ server.js
@@ -7,11 +7,28 @@ const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/happyThoughts"
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
 mongoose.Promise = Promise
 
+const Thought = mongoose.model('Thought', {
+  message: {
+    type: String,
+    required: [true, 'Message Required'],
+    minLength: [5, 'Message too short, minimum 5 characters'],
+    maxLength: [140, 'Message too long, maximum 140 characters']
+  },
+  hearts: {
+    type:Number,
+    default: 0
+  },
+  createdAt: {
+    type:Date,
+    default: () => Date.now()
+  }  
+})
+
 // Defines the port the app will run on. Defaults to 8080, but can be 
 // overridden when starting the server. For example:
 //
 //   PORT=9000 npm start
-const port = process.env.PORT || 8080
+const port = process.env.PORT || 9000
 const app = express()
 
 // Add middlewares to enable cors and json body parsing
@@ -20,9 +37,23 @@ app.use(bodyParser.json())
 
 // Start defining your routes here
 app.get('/', (req, res) => {
-  res.send('Hello world')
+  res.send('Happy Thoughts API')
 })
 
+app.get('thoughts', async (req, res) => {
+  const thoughts = await Thought.find().sort({createdAt: 'Date'}).limit(20).exec()
+  res.json(thoughts)
+})
+
+app.post('/thoughts', async (req,res) => {
+  const newThought = new Thought({message: req.body.message})
+  try {
+    const savedThought = await newThought.save()
+    res.status(201).json(savedThought)
+  } catch (err) {
+    res.status(404).json({message : 'Could not save message', error: err.errors})
+  }
+})
 // Start the server
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`)
