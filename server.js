543cd47691e102ba61a40021adbd967ffbb48e80
Ingela Löfgren <ingela.lofgren@gmail.com>
2021-01-05 13:13:25 +0100

    Created get and post

--- server.js
+++ server.js
@@ -7,6 +7,23 @@ const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/happyThoughts"
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
 mongoose.Promise = Promise
 
+const Thought = mongoose.model('Thought', {
+  message: {
+    type: String,
+    required: true,
+    minlength: 5,
+    maxlength: 140
+  },
+  hearts: {
+    type: Boolean,
+    default: 0,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
+})
+
 // Defines the port the app will run on. Defaults to 8080, but can be 
 // overridden when starting the server. For example:
 //
@@ -21,6 +38,24 @@ app.use(bodyParser.json())
 // Start defining your routes here
 app.get('/', (req, res) => {
   res.send('Hello world')
+})
+
+app.get('/thoughts', async (req, res) => {
+  const thoughts = await Thought.find().sort({createdAt: 'desc'}).limit(20).exec()
+  res.json(thoughts)
+})
+
+app.post('/thoughts', async (req, res) => {
+  const {message} = req.body
+  const thoughts = new Thought({message})
+
+  try{
+    //Success
+    const savedTask = await thoughts.save()
+    res.status(201).json(savedTask)
+  } catch (err) {
+    res.status(400).json({message: 'Could not save to the Database', error: err.errors})
+    }
 })
 
 // Start the server
