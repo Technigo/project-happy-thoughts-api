1449a1c7510e29ff9f4c4c00a8c50073ca152cc6
Sara Stjernberg <sara_stjernberg@hotmail.com>
2021-01-05 16:42:34 +0100

    added post requests

--- server.js
+++ server.js
@@ -18,9 +18,56 @@ const app = express()
 app.use(cors())
 app.use(bodyParser.json())
 
+const Thought = new mongoose.model('Thought', {
+  message: {
+    type: String,
+    required: true,
+    minlength: 5,
+    maxlength: 140
+  },
+  hearts: {
+    type: Number,
+    default: 0
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now()
+  }
+})
+
 // Start defining your routes here
 app.get('/', (req, res) => {
-  res.send('Hello world')
+  res.send('This is an API for "Happy Thoughts"')
+})
+
+app.get('/thoughts', async (req, res) => {
+  const thoughts = await Thought.find().sort({ createdAt: 'desc' }).limit(20).exec()
+  res.json(thoughts)
+})
+
+app.post('/thoughts', async (req, res) => {
+  //Retrieve the innformation sent by the client to our API endpoint
+  const { message } = req.body
+
+  //Use the mongoose model to create the database entry
+  const thought = new Thought({ message })
+
+  try {
+    //Success
+    const savedThought = await thought.save()
+    res.status(201).json(savedThought)
+  } catch (err) {
+    res.status(400).json({ message: 'Could not save thought to the datbase', error: err.errors })
+  }
+})
+
+app.post('/thoughts/:id/like', async (req, res) => {
+  try {
+    const updateThought = await Thought.updateOne({ _id: req.params.id }, { $inc: { 'hearts': 1 } }, { new: true })
+    res.status(201).json(updateThought)
+  } catch (err) {
+    res.status(404).json({ message: 'Could not like the thought.', error: err.errors })
+  }
 })
 
 // Start the server
