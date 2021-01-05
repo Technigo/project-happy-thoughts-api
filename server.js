433758f8b4276588d4c9fb2979e430c8989c0fbd
AnnaHellkvist <51534839+AnnaHellkvist@users.noreply.github.com>
2021-01-05 15:32:26 +0100

    added thought model and the 3 endpoints

--- server.js
+++ server.js
@@ -18,6 +18,70 @@ const app = express()
 app.use(cors())
 app.use(bodyParser.json())
 
+
+// Build a model with properties message, heart and createdAt
+const Thought = mongoose.model('Thought', {
+  message: {
+    type: String,
+    required: true,
+    minlength: 5,
+    maxlength: 40
+  },
+  hearts: {
+    type: Number,
+    default: 0
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
+})
+
+// Endpoint to GET and list the 20 latest thoughts
+app.get('/thoughts', async (req, res) => {
+  const thoughts = await Thought.find().sort({ createdAt: 'desc' }).limit(20).exec();
+  res.json(thoughts) //sends thoughts back in the form of json
+})
+
+// Endpoint to POST a thought to the database
+app.post('/thoughts', async (req, res) => {
+  // retrieve information sent by the client to our API endpoint
+  const { message, hearts } = req.body;
+
+  // Use our mongoose model to create the database entry
+  const thought = new Thought({ message, hearts });
+
+  try {
+    // Success 
+    const savedThought = await thought.save();
+    res.status(200).json(savedThought);
+  } catch (err) {
+    res.status(400).json({
+      message: 'Could not save thought to database',
+      error: err.errors
+    })
+  }
+})
+
+// Endpoint to POST likes to a choosen thought
+app.post('/thoughts/:thoughtId/like', async (req, res) => {
+  const { thoughtsId } = req.params;
+
+
+  try {
+    await Thought.updateOne({ _id: +thoughtsId }, { $inc: { hearts: 1 } });
+    res.status(200).json();
+  } catch (err) {
+    res.status(400).json({
+      message: 'Could not save like, thought not found',
+      error: err.errors
+    })
+  }
+
+})
+
+
+
 // Start defining your routes here
 app.get('/', (req, res) => {
   res.send('Hello world')
