829fd5cc9da5e21ce4097fb1b9ea0c42a21b48d6
Liza Minicangit config --global user.email liza.minican@gmail.com <liza.minican@gmail.com>
2021-01-05 22:17:36 +0100

    created get endpoint to retrive 20 thoughts max and post a thought endpoint with validation errors

--- server.js
+++ server.js
@@ -7,10 +7,28 @@ const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/happyThoughts"
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
 mongoose.Promise = Promise
 
-// Defines the port the app will run on. Defaults to 8080, but can be 
-// overridden when starting the server. For example:
-//
-//   PORT=9000 npm start
+//creating model where we specify all the rules the client should follow to add data correctly 
+const Thought = mongoose.model('Thought', {
+  message: {
+    type: String,
+    required: true,
+    minlength: 5,
+    maxlength: 140
+  },
+  createdAt: {
+    type: Date,
+    //provides the date at the moment when the request is sent and can't be modifed by user
+    //default: () => new Date()
+    default: Date.now
+  },
+  hearts: {
+    //this number will be 0 by default even if the client sends another quantity
+    type: Number,
+    default: 0
+  }  
+}) 
+// new Thought({message: "hey hey", hearts:8}).save();
+
 const port = process.env.PORT || 8080
 const app = express()
 
@@ -22,6 +40,38 @@ app.use(bodyParser.json())
 app.get('/', (req, res) => {
   res.send('Hello world')
 })
+
+app.get('/thoughts', async (req, res) => {
+  const thoughts = await Thought.find().sort({createdAt:'desc'}).limit(20).exec();
+  res.json(thoughts);
+});
+
+//posting an object to the database, if posted incorrectly the client receives errors
+app.post('/thoughts', async (req, res) => {
+  //Promises for learning purpose
+  // new Thought(req.body).save()
+  // .then((thought) =>{
+  //   res.status(200).json(thought)
+  // })
+  // .catch((err) => {
+  //   res.status(400).json({message:'Could not save thought', errors:err.errors});
+  // })
+
+  //Try catch
+  try {
+    //Success case
+
+    //retrive the information sent by the client to our API endpoint
+    const { message } = req.body
+    //use mongoose model to create database entry and save it
+    const thought = new Thought({ message });
+    const savedThought = await thought.save();
+    res.status(200).json(savedThought) 
+  } catch(err) {
+    //bad request sending the status to the server and the message
+    res.status(400).json({message:'Could not save thought to the Database', errors:err.errors});
+  }
+});
 
 // Start the server
 app.listen(port, () => {
