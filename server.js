567713494658029aace268ba71bb427d3b527340
Anna <hasselstromanna@gmail.com>
2021-01-06 11:17:06 +0100

    Add comments

--- server.js
+++ server.js
@@ -42,36 +42,38 @@ app.get('/', (req, res) => {
   res.send('Hello world')
 })
 
-// Save new message to database
+// Retrieve info from client to our API endpoint. Create and save new message 
+// to database using our mongoose model
 app.post('/messages', async (req, res) => {
   try {
-    const message = new Message({ message: req.body.message })
-    await message.save()
-    res.status(200).json(message)
+    const message = new Message({ message: req.body.message }); // only returns/stores message - 
+    // prevents reassigning of other properties.
+    await message.save();
+    res.status(200).json(message);
   } catch (err) {
-    res.status(400).json({ message: 'Could not save message to database', errors: err.errors })
+    res.status(400).json({ message: 'Could not save message to database', error: err.errors })
   }
 });
 
-// Return all messages to client
+// Return all messages to client. Use find, sort, limit and exec to order response.
 app.get('/messages', async (req, res) => {
   //const queryParameters = req.query;
-  const allMessages = await Message.find(req.query).sort({ createdAt: 'desc' }).limit(20);
+  const allMessages = await Message.find(req.query).sort({ createdAt: 'desc' }).limit(20).exec();
   if (allMessages) {
     res.status(200).json(allMessages)
   } else {
-    res.status(404).json({ error: 'Messages not found'})
+    res.status(404).json({ error: 'Messages not found' })
   }
-})
+});
 
-// Save likes to database, returns to client
+// Save likes to database, return to client. Use updateOne() to add likes.
 app.post('/messages/:messageId/like', async (req, res) => {
   try {
     const { messageId } = req.params;
     await Message.updateOne({ _id: messageId }, { $inc: { hearts: +1 } });
     res.status(200).json();
   } catch (err) {
-    res.status(400).json({ message: 'Cannot save to database', errors: err.errors })
+    res.status(400).json({ message: 'Cannot save to database', error: err.errors })
   }
 });
 
