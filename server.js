e1c927382da83273bc720d455b861773f83f6dcc
Henrike Wiemker <65461017+HenrikeW@users.noreply.github.com>
2021-01-06 16:20:40 +0100

    created endpoint to like a thought

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
-import express from 'express'
-import bodyParser from 'body-parser'
-import cors from 'cors'
-import mongoose from 'mongoose'
+import express from 'express';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+import mongoose from 'mongoose';
 
 const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/happyThoughts"
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
@@ -22,29 +22,31 @@ const Thought = mongoose.model('Thought', {
     type: Date,
     default: () => new Date
   }
-})
+});
 
 // Defines the port the app will run on. Defaults to 8080, but can be 
 // overridden when starting the server. For example:
 //
 //   PORT=9000 npm start
 const port = process.env.PORT || 8080
-const app = express()
+const app = express();
 
 // Add middlewares to enable cors and json body parsing
-app.use(cors())
-app.use(bodyParser.json())
+app.use(cors());
+app.use(bodyParser.json());
 
 // Start defining your routes here
 app.get('/', (req, res) => {
   res.send('Hello world')
 });
 
+// Get a list of the latest 20 thoughts
 app.get('/thoughts', async (req, res) => {
   const thoughts = await Thought.find().sort({createdAt: 'desc'}).limit(20).exec();
   res.json(thoughts);
 });
 
+// Create a new thought
 app.post('/thoughts', async (req,res) => {
   const { message } = req.body;
   const thought = new Thought({ message });
@@ -57,10 +59,11 @@ app.post('/thoughts', async (req,res) => {
   }
 });
 
-app.post('/thoughts/:id/like', async (req, res) => {
+// update the number of likes on a thought using a PUT request
+app.put('/thoughts/:id/like', async (req, res) => {
   try {
     const thought = await Thought.findById(req.params.id);
-    thought.hearts = thought.hearts + 1; // need to fix this!
+    await Thought.updateOne({ _id: thought._id}, {hearts: thought.hearts + 1})
     res.status(200).json(thought);
   } catch (err) {
     res.status(400).json({ message: 'Could not find the thought you want to like', error: err.errors });
@@ -70,4 +73,4 @@ app.post('/thoughts/:id/like', async (req, res) => {
 // Start the server
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`)
-})
+});
