e9c82725fff3f3ff1578f852a2894232286a88ff
Andrea <andrea88@live.se>
2021-01-06 08:56:38 +0100

    updated like

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
-import express from 'express'
-import bodyParser from 'body-parser'
-import cors from 'cors'
-import mongoose from 'mongoose'
+import express from "express"
+import bodyParser from "body-parser"
+import cors from "cors"
+import mongoose from "mongoose"
 
 // CODE FOR DATABASE
 const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/happyThoughts"
@@ -9,96 +9,105 @@ mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
 mongoose.Promise = Promise
 
 // THOUGHTS MODEL
-const Thought = mongoose.model('Thought', {
+const Thought = mongoose.model("Thought", {
   message: {
-    type: String, 
+    type: String,
     required: true,
     minlength: 5,
-    maxlength: 140
+    maxlength: 140,
   },
   hearts: {
     type: Number,
-    default: 0
+    default: 0,
   },
   name: {
-    type: String, 
-    default: 'Anonymous'
+    type: String,
+    default: "Anonymous",
   },
   createdAt: {
-    type: Date, 
-    default: () => new Date() //Date.now?
-  }
+    type: Date,
+    default: () => new Date(), //Date.now?
+  },
 })
 
 // SERVER
 const port = process.env.PORT || 8080
 const app = express()
-const listEndpoints = require('express-list-endpoints')
 
 // MIDDLEWARES
 app.use(cors())
 app.use(bodyParser.json())
+const listEndpoints = require("express-list-endpoints")
 
 // ERROR HANDLING WHEN DATABASE IS DOWN OR OUT OF REACH
-app.use((res, req, next) => {
+app.use((req, res, next) => {
   if (mongoose.connection.readyState === 1) {
     next()
   } else {
-    res.status(503).json({ error: 'Service unavailable' })
+    res.status(503).json({ error: "Service unavailable" })
   }
 })
 
 // ROOT ENDPOINT
-app.get('/', (req, res) => {
+app.get("/", (req, res) => {
   res.send(listEndpoints(app))
 })
 
 // ENDPOINT GET THOUGHTS
-app.get('/thoughts', async (req, res) => {
+app.get("/thoughts", async (req, res) => {
   try {
     //Success
-   const thoughts = await Thought.find()
-  .sort({createdAt: 'desc'})
-  .limit(20)
-  res.status(200).json(thoughts)  
+    const thoughts = await Thought.find().sort({ createdAt: "desc" }).limit(20)
+    res.status(200).json(thoughts)
   } catch (err) {
-    res.status(400).json({ message: 'Could not get thoughts', error: err.errors})
+    res
+      .status(400)
+      .json({ message: "Could not get thoughts", error: err.errors })
   }
 })
 
 // ENDPOINT POST THOUGHTS
-app.post('/thoughts', async (req, res ) => {
+app.post("/thoughts", async (req, res) => {
   // Retrieve the information sent by client to out API endpoint
-  const {message, name} = req.body
+  const { message, name } = req.body
 
   // Use mongoose model to create the database entry
-  const thoughts = new Thought({message, name})
+  const thoughts = new Thought({ message, name })
 
-    try {
-      //Success
-      const savedThought = await thoughts.save()
-      res.status(201).json(savedThought)
-    } catch (err) {
-      res.status(400).json({message: 'Could not save thought to the Database', error: err.errors})
-    }
+  try {
+    //Success
+    const savedThought = await thoughts.save()
+    res.status(201).json(savedThought)
+  } catch (err) {
+    res
+      .status(400)
+      .json({
+        message: "Could not save thought to the Database",
+        error: err.errors,
+      })
+  }
 })
 
 // ENDPOINT POST THOUGHTS/:thoughtId/like
-app.post('/thoughts/:id/like', async (req, res) => {
-  const {thoughtId} = req.params
-  const like = req.body 
+app.post("/thoughts/:id/like", async (req, res) => {
+  const { id } = req.params
+  const like = req.body
 
   try {
     //Success
     await thoughts.updateOne(
-      {_id: thoughtId}, 
-      { $inc: { hearts: 1}},
-      await new Like({ like }).save()
-    
+      { _id: id },
+      { $inc: { hearts: 1 } },
+       await new Like({ like }).save()
     )
     res.status(201).send()
   } catch (err) {
-    res.status(404).json({message: 'Could not save your like to the database', error: err.errors})
+    res
+      .status(404)
+      .json({
+        message: "Could not save your like to the database",
+        error: err.errors,
+      })
   }
 })
 
