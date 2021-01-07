27fca6f7448c9147f3d36f3c7764f6ff4356ca9e
Marjaana Tienaho <marjaanatienaho@Marjaanas-MacBook-Air.local>
2021-01-07 02:21:23 +0100

    Added post and get endpoints

--- server.js
+++ server.js
@@ -7,6 +7,31 @@ const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/happyThoughts"
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
 mongoose.Promise = Promise
 
+const Thoughts = mongoose.model('Thoughts', {
+  message: {
+    type: String,
+    required: true,
+    minlength: 5,
+    maxlength: 140
+  },
+
+  hearts: {
+    type: Number,
+    default: 0
+  }, 
+  
+  createdAt: {
+    type: Date,
+    default: Date.now
+
+  },
+  username:{
+    type: String,
+    minlength: 1
+
+  }
+})
+
 // Defines the port the app will run on. Defaults to 8080, but can be 
 // overridden when starting the server. For example:
 //
@@ -20,7 +45,35 @@ app.use(bodyParser.json())
 
 // Start defining your routes here
 app.get('/', (req, res) => {
-  res.send('Hello world')
+  res.send('Welcome to Happy Thoughts API  :)  // See this API live here: ');
+
+})
+//GET /thoughts : Endpoint that returns maximum of 20 thoughts, sorted by createdAt to show the most recent thoughts first.
+app.get('/thoughts', async (req, res) => {
+
+  try{
+    const thoughts = await Thoughts.find()
+    .sort({createdAt:'desc'})
+    .limit(20)
+    .exec()
+    
+    res.json(thoughts)
+  }
+  catch (err) {
+    res.status(400).json({ message:"Error when trying to get thoughts", error: err.errors })
+  }
+})
+
+// POST /thoughts : Endpoint to add a new Thought to the database
+app.post('/thoughts', async (req, res) => {
+  const {message} = req.body
+  const thought = new Thoughts({message})
+  try {
+    const newThought = await thought.save()
+    res.status(200).json(newThought)
+  }catch(err) {
+    res.status(400).json({ message:"Error when trying to save thought", error: err.errors })
+  }
 })
 
 // Start the server
