26d9c0a0ed36e927f424ac9d2bdccc5a234c6c33
destinyleia <destinylindgrenc@gmail.com>
2021-01-07 19:28:39 +0100

    updates, still not working

--- server.js
+++ server.js
@@ -7,10 +7,28 @@ const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/happyThoughts"
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
 mongoose.Promise = Promise
 
-// Defines the port the app will run on. Defaults to 8080, but can be 
-// overridden when starting the server. For example:
-//
-//   PORT=9000 npm start
+//The Mogoose model 
+const Thought = mongoose.model('Thought', {
+  message: {
+    type: String,
+    required: true,
+    minLength: 5,
+    maxLength: 140
+  },
+
+  hearts: {
+    type: Number,
+    default: 0
+  },
+
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
+})
+
+
+// Defines the port the app will run on. Defaults to 8080
 const port = process.env.PORT || 8080
 const app = express()
 
@@ -22,6 +40,36 @@ app.use(bodyParser.json())
 app.get('/', (req, res) => {
   res.send('Hello world')
 })
+
+//GET request 
+app.get('/thoughts', async (req, res) => {
+  const thoughts = await Thought.find()
+    .sort({ createdAt: 'desc' })
+    .limit(20)
+    .exec()
+
+  res.status(200).json(thoughts);
+})
+
+//POST request
+app.post('/thoughts', async (req, res) => {
+  const { message } = req.body
+  const thought = new Thought({ message })
+
+  //The try statement allows you to define a block of code to be tested for errors while it is being executed.
+  try {
+    const postThought = await thought.save()
+    res.status(200).json(postThought)
+  }
+  catch (error) {
+    res.status(400).json({
+      success: false,
+      message: 'Could not post thought',
+      errors: error.errors
+    })
+  }
+})
+
 
 // Start the server
 app.listen(port, () => {
