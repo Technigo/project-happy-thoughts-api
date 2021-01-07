13bffa061019cdabe68003d27316b862ac2b3a6b
LillanEriksson <nailsbylillan@hotmail.com>
2021-01-07 18:37:18 +0100

    Added routes to get and post

--- server.js
+++ server.js
@@ -1,29 +1,93 @@
-import express from 'express'
-import bodyParser from 'body-parser'
-import cors from 'cors'
-import mongoose from 'mongoose'
+import express from 'express';
+import bodyParser, { json } from 'body-parser';
+import cors from 'cors';
+import mongoose from 'mongoose';
 
-const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/happyThoughts"
-mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
-mongoose.Promise = Promise
+const mongoUrl = process.env.MONGO_URL || 'mongodb://localhost/happyThoughts';
+mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.Promise = Promise;
 
-// Defines the port the app will run on. Defaults to 8080, but can be 
+// Defines the port the app will run on. Defaults to 8080, but can be
 // overridden when starting the server. For example:
 //
 //   PORT=9000 npm start
-const port = process.env.PORT || 8080
-const app = express()
+const port = process.env.PORT || 8080;
+const app = express();
 
 // Add middlewares to enable cors and json body parsing
-app.use(cors())
-app.use(bodyParser.json())
+app.use(cors());
+app.use(bodyParser.json());
+
+// Middleware to handle server connection errors
+app.use((req, res, next) => {
+	try {
+		if (mongoose.connection.readyState === 1) {
+			next();
+		} else {
+			res.status(503).json({ error: 'Service unavailable' });
+		}
+	} catch (error) {
+		res.status(400).json({ error: 'Error! Could not access the server.' });
+	}
+});
+
+//Mongoose model
+const Thought = new mongoose.model('Thought', {
+	message: {
+		type: String,
+		required: true,
+		minlength: 5,
+		maxlength: 140,
+	},
+	hearts: {
+		type: Number,
+		default: 0,
+	},
+	createdAt: {
+		type: Date,
+		default: Date.now,
+	},
+});
 
 // Start defining your routes here
-app.get('/', (req, res) => {
-  res.send('Hello world')
-})
+
+app.get('/', async (req, res) => {
+	try {
+		const thoughts = await Thought.find()
+			.sort({ createdAt: 'desc' })
+			.limit(20)
+			.exec();
+
+		res.json(thoughts);
+	} catch (err) {
+		res.status(400).json({
+			success: false,
+			message: 'Could not get thoughts',
+			errors: err.errors,
+		});
+	}
+});
+
+//Post a new thought
+
+app.post('/', async (req, res) => {
+	const { message } = res.body;
+	const newThought = await new Thought({ message });
+	try {
+		const savedThought = await newThought.save();
+		res.status(201).json(savedThought);
+	} catch (err) {
+		res.status(400).json({
+			success: false,
+			message: 'Could not save new thought to the database',
+			errors: err.errors,
+		});
+	}
+});
+
+//Like a post
 
 // Start the server
 app.listen(port, () => {
-  console.log(`Server running on http://localhost:${port}`)
-})
+	console.log(`Server running on http://localhost:${port}`);
+});
