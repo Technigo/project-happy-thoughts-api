00a5c8611ef203801939ecf84a26ce12892f4848
Sandra Shumanteva Kaevska <sandra@shumantevi.mk>
2021-01-09 00:30:41 +0100

    trying to fix the heroku problem

--- server.js
+++ server.js
@@ -7,6 +7,16 @@ const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/happyThoughts"
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
 mongoose.Promise = Promise
 
+// Defines the port the app will run on. Defaults to 8080, but can be 
+// overridden when starting the server. For example:
+//
+//   PORT=9000 npm start
+const port = process.env.PORT || 8080
+const app = express()
+
+// Add middlewares to enable cors and json body parsing
+app.use(cors())
+app.use(bodyParser.json())
 
 const Thought = new mongoose.model('Thought', {
   message: {
@@ -24,18 +34,6 @@ const Thought = new mongoose.model('Thought', {
     default: Date.now
   }
 })
-
-// Defines the port the app will run on. Defaults to 8080, but can be 
-// overridden when starting the server. For example:
-//
-//   PORT=9000 npm start
-const port = process.env.PORT || 8080
-const app = express()
-
-// Add middlewares to enable cors and json body parsing
-app.use(cors())
-app.use(bodyParser.json())
-
 
 // Start defining your routes here
 app.get('/', (req, res) => {
