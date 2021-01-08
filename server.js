47580c24fb81c51706822419d2f4316be564e63a
Sandra Shumanteva Kaevska <sandra@shumantevi.mk>
2021-01-08 23:53:53 +0100

    Blue Level

--- server.js
+++ server.js
@@ -7,6 +7,24 @@ const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/happyThoughts"
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
 mongoose.Promise = Promise
 
+
+const Thought = new mongoose.model('Thought', {
+  message: {
+    type: String,
+    required: true,
+    minlength: 5,
+    maxlength: 140
+  },
+  hearts: {
+    type: Number,
+    default: 0
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
+})
+
 // Defines the port the app will run on. Defaults to 8080, but can be 
 // overridden when starting the server. For example:
 //
@@ -18,10 +36,40 @@ const app = express()
 app.use(cors())
 app.use(bodyParser.json())
 
+
 // Start defining your routes here
 app.get('/', (req, res) => {
-  res.send('Hello world')
+  res.send('Happy Thoughts Sandra')
 })
+
+app.get('/thoughts', async (req, res) => {
+  const thoughts = await Thought.find().sort({ createdAt: 'desc' }).limit(20).exec()
+  res.json(thoughts)
+})
+
+app.post('/thoughts', async (req, res) => {
+  const { message } = req.body
+  const thought = new Thought({ message })
+
+  try {
+    const savedThought = await thought.save()
+    res.status(200).json(savedThought)
+  } catch (err) {
+    res.status(400).json({ message: 'Could not save thought to Database', err: err.errors })
+  }
+})
+
+app.post('/thoughts/:thoughtId/like', async (req, res) => {
+  const thoughtId = req.params.thoughtId;
+
+  try {
+    const thoughtLiked = await Thought.updateOne({ _id: thoughtId }, { $inc: { hearts: 1 } });
+    res.json(thoughtLiked);
+  } catch (err) {
+    res.status(400).json({ message: "Thought not found", error: err.errors });
+  }
+})
+
 
 // Start the server
 app.listen(port, () => {
