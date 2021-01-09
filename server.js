c66735fbea3641753a8559bc39600853984c8cd9
Hanna Skeppe <hannaskeppe@gmail.com>
2021-01-09 01:14:59 +0100

    Added functionality to sort posts. Modified my endpoints and error-messages.

--- server.js
+++ server.js
@@ -4,7 +4,8 @@ import cors from 'cors'
 import mongoose from 'mongoose'
 import Thought from './models/thought'
 
-const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/hanna-happyThoughts" // The name of the database when using Mongo DB Compass.
+// The name of the database when using Mongo DB Compass:
+const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/hanna-happyThoughts" 
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
 mongoose.Promise = Promise
 
@@ -14,6 +15,11 @@ mongoose.Promise = Promise
 //   PORT=9000 npm start
 const port = process.env.PORT || 8080
 const app = express()
+
+const endpoints = require('express-list-endpoints')
+
+const POST_LIKE_ERROR = 'Error: could not add like on message with id:'
+const GET_ENDPOINTS_ERROR = 'Error: No endpoints found'
 
 // MIDDLEWARES (to enable cors and json body parsing)
 app.use(cors())
@@ -28,12 +34,39 @@ if (process.env.RESET_DATABASE) {
   seedDatabase()
 }
 
-// ////ROUTES (ENDPOINTS)////
+//////ROUTES (ENDPOINTS)////
 
-//GET: 20 THOUGHTS IN DESC. ORDER SORTED ON TIMESTAMP
-  app.get('/thoughts', async (req, res) => {
-    const thoughts = await Thought.find().sort({ createdAt: 'desc' }).limit(20)
-    res.status(200).json(thoughts)
+//GET: All available endpoints with express-list-endpoints
+app.get('/', (req, res) => {
+  if (res) {
+    res.status(200).send(endpoints(app));
+  } else {
+    res.status(404).send({ error: GET_ENDPOINTS_ERROR });
+  }
+});
+
+// GET 20 THOUGHTS IN DESC. ORDER SORTED ON TIMESTAMP (default)
+// SORT THOUGHTS:
+// - most likes: /thoughts/?sort=likes
+// - oldest first / asc: /thoughts/?sort=oldest
+// - newest first / desc (default): /thoughts/?sort=newest
+app.get('/thoughts', async (req, res) => {
+  const { sort } = req.query
+
+  const sortedThoughts = (sort) => {
+    if (sort === 'liked') {
+      return { hearts: 'desc' }
+    } else if (sort === 'oldest') {
+      return { createdAt: 'asc' }
+    } else {
+      return { createdAt: 'desc' }
+    }
+  }
+
+  const thoughts = await Thought.find()
+    .sort(sortedThoughts(sort))
+    .limit(20)
+    res.status(200).json(thoughts) // No error message because I want an empty string if no posts.
 })
 
 //POST: ADD A THOUGHT
@@ -41,34 +74,30 @@ app.post('/thoughts', async (req, res) => {
   const { message } = req.body
   try {
     const newThought = await new Thought({ message }).save()
-    res.status(200).json(newThought)
+    res.status(201).json(newThought)
   } catch (err) {
     console.log(err)
     res.status(404).json({
-      error: err.errors.message.message
+      error: err.errors.message.message // displays error message specified in the model
     })
   }
 })
 
 //POST: LIKE A THOUGHT 
-//(OR PUT? Because I modify something that already exists in the database)
-// the heart already has the value of 0, I'm just changing the value so: PUT?
-// se lecture @40 mins about PUT/POST regarding likes
 app.post('/thoughts/:id/like', async (req, res) => {
   const { id } = req.params
-  //updateOne or findOneAndUpdate?
+  //increment operator $inc to increase hearts (see mongoose docs).
   try {
-    await Thought.updateOne({ _id: id}, { $inc: { hearts: 1} })
-    res.status(200).json(`Thought with id: ${id} got one more like`)
+    await Thought.updateOne({ _id: id }, { $inc: { hearts: 1 } })
+    res.status(201).json(`Thought with id: ${id} got one more like`)
+    //or: res.status(200).send(); ???
   } catch (err) {
     res.status(404).json({
-      message: `Error: could not find a message with id: ${id} to like`,
+      message: `${POST_LIKE_ERROR} ${id}`,
       error: err.errors
     })
   }
 });
-
-//use increment operator: $inc (lecture 18/5 @52 min) look up mongoose docs.
 
 //DELETE A THOUGHT??? --> if I modify my front-end I could implement that there also. 
 //Or maybe not because then anyone could delete all messages and that is not desirable.
