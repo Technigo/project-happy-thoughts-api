8e254daf12366c12cde48b099a1a11ccf2fe50f2
Sara Sundblom <sundblomsara@gmail.com>
2021-01-12 20:39:17 +0100

    Copied all changes from other repo, accidentally made a non-forked version.. (https://github.com/ssundblom/project-happy-thoughts-api)

--- server.js
+++ server.js
@@ -3,14 +3,32 @@ import bodyParser from 'body-parser'
 import cors from 'cors'
 import mongoose from 'mongoose'
 
+const ERR_CANNOT_FIND_ID = ('Sorry, cant find that thought 😕')
+
 const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/happyThoughts"
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
 mongoose.Promise = Promise
+mongoose.set('useFindAndModify', false)
 
-// Defines the port the app will run on. Defaults to 8080, but can be 
-// overridden when starting the server. For example:
-//
-//   PORT=9000 npm start
+
+const Thought = mongoose.model('Thought', {
+  message: {
+    type: String,
+    required: true,
+    minlength: 5,
+    maxlength: 140
+  },
+  hearts: {
+    type: Number,
+    default: 0
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
+})
+
+
 const port = process.env.PORT || 8080
 const app = express()
 
@@ -20,7 +38,34 @@ app.use(bodyParser.json())
 
 // Start defining your routes here
 app.get('/', (req, res) => {
-  res.send('Hello world')
+  res.send('Hello happy happy world 🥳')
+})
+
+app.get('/thoughts', async (req, res) => {
+  const thought = await Thought.find().sort({ createdAt: 'desc' }).limit(20).exec()
+  res.json(thought)
+})
+
+app.post('/thoughts', async (req, res) => {
+  const { message } = req.body
+  const thought = new Thought({ message })
+
+  try {
+    const savedThought = await thought.save()
+    res.status(200).json(savedThought)
+  } catch (err) {
+    res.status(400).json({ message: 'Could not save thought' })
+  }
+})
+
+app.put('/thoughts/:thoughtId/like', async (req, res) => {
+  const { thoughtId } = req.params
+  const thought = await Thought.findOneAndUpdate({ _id: thoughtId }, {$inc: {'hearts' : 1 }}, { new: true })
+  if (thought) {
+    res.status(200).json(thought)
+  } else {
+    res.status(400).json({ message: ERR_CANNOT_FIND_ID })
+  }
 })
 
 // Start the server
