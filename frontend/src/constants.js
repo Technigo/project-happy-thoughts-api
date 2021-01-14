9cab51416832440b318a3baa84ac991c4d4722a1
Trang Tara <trangnusi@gmail.com>
2021-01-14 12:46:44 +0100

    add frontend

---
+++ frontend/src/constants.js
@@ -0,0 +1,5 @@
+// export const API_URL = "https://happy-thoughts-technigo.herokuapp.com/thoughts";
+
+// export const API_URL = "http://localhost:8080/thoughts";
+
+export const API_URL = "https://whatmakesyouhappy.herokuapp.com/thoughts"
