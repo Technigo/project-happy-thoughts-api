eb8423ddef267cc7f1671adc116b9e83b4969163
Claudia Kjerrgren <ckjerrgren@gmail.com>
2021-01-27 22:50:42 +0100

    finished endpoints

--- server.js
+++ server.js
@@ -8,21 +8,21 @@ mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
 mongoose.Promise = Promise
 
 const Thought = mongoose.model('Thought', {
-    text: {
+    message: {
       type: String,
       required: true,
       minlength: 5,
       maxlength: 140,
     },
-    heart: {
+    hearts: {
       type: Number,
-      default: () => 0
+      default: 0
     },
     createdAt: {
       type: Date,
-      default: Date.now,
+      default: () => new Date 
     },
-});
+})
 
 
 //   Defines the port the app will run on 
@@ -38,24 +38,37 @@ app.get('/', (req, res) => {
   res.send('Hello Claudia')
 })
 
-// endpoint returning 20 thoughts in descending order from created date.
+// 1. GET endpoint returning 20 thoughts in descending order from created date.
 app.get('/thoughts', async (req, res) => {
-  const thoughts = await Thought.find().sort({createdAt: 'desc'}).limit(20).exec();
-  res.json(thoughts);
-});
+  const thoughts = await Thought.find().sort({createdAt: 'desc'}).limit(20).exec()
+  res.json(thoughts)
+})
 
+// 2. POST to database 
 app.post('/thoughts', async (req, res) => {
   //Retrive the information sent by the client to our API endpoint 
-  const {text, complete} = req.body;
-
-  // Use our mongoose model to create the database entry.
-  const thought = new Thought({text, complete});
-
-  try{
-
-  }catch (err){
-
+  try {
+    const thought = await new Thought ({ message: req.body.message }).save()
+    res.status(200).json(thought)
+  } catch (error) {
+    res.status(400).json({ message: "Your message is too long/ short", errors:error.errors  })
   }
+})
+
+
+
+// 3. POST hearts (for likes)
+app.post('/thoughts/:thoughtId/heart', async (req, res) => {
+  const { thoughtId } = req.params
+  try {
+    await Thought.updateOne({ _id: thoughtId }, { $inc: { hearts: 1 } })
+    //confirmation message / or just like.. ?
+  } catch (error) {
+    res.status(404).json({
+      message: "Nothing to like", error: error.errors
+    })
+  }
+
 })
 
 // Start the server
