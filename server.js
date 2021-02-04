95e8a4d0ea2b9fb82f5fb524bab4437591b9b27c
destinyleia <destinylindgrenc@gmail.com>
2021-02-04 17:28:54 +0100

    updates

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ import bodyParser from 'body-parser'
 import cors from 'cors'
 import mongoose from 'mongoose'
 
-const mongoUrl = process.env.MONGO_URL || "git"
+const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/happyThoughts"
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
 mongoose.Promise = Promise
 
