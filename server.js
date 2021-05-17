0d122ac471a01854f26bd23892674e4d912f6829
Sofarz <sofia.wallen@gmail.com>
2021-05-17 19:40:40 +0200

    Created thoughtSchema, and Thought model. Created endpoint to post a thought to the database and one to get thoughts from the database.

--- server.js
+++ server.js
@@ -1,10 +1,9 @@
 import express from 'express'
-import bodyParser from 'body-parser'
 import cors from 'cors'
 import mongoose from 'mongoose'
 
 const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/happyThoughts"
-mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true })
 mongoose.Promise = Promise
 
 // Defines the port the app will run on. Defaults to 8080, but can be 
@@ -14,13 +13,47 @@ mongoose.Promise = Promise
 const port = process.env.PORT || 8080
 const app = express()
 
+const thoughtSchema = new mongoose.Schema({
+  message: {
+    type: String, 
+    required: [true, 'Message is required!'],
+    minlength: [5, 'Thought must be a minimum 5 characters!'],
+    maxlength: [140, 'Thought must be a maximum 140 characters!'],
+    trim: true
+  }, 
+  hearts: {
+    type: Number, 
+    default: 0
+  }, 
+  createdAt: {
+    type: Date, 
+    default: Date.now
+  }
+})
+
+const Thought = mongoose.model('Thought', thoughtSchema)
+
 // Add middlewares to enable cors and json body parsing
 app.use(cors())
-app.use(bodyParser.json())
+app.use(express.json())
 
 // Start defining your routes here
 app.get('/', (req, res) => {
   res.send('Hello world')
+})
+
+app.get('/thoughts', async (req, res) => {
+  const newThought = await Thought.find().sort({ createdAt: 'desc' }).limit(20)
+  res.json({ length: newThought.length, data: newThought })
+})
+
+app.post('/thoughts', async (req, res) => {
+  try {
+    const newThought = await new Thought(req.body).save()
+    res.json(newThought)
+  } catch (error) {
+      res.status(400).json(error)
+  }
 })
 
 // Start the server
