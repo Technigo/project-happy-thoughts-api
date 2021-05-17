607ac55049ee9088be98ee4cbf14f5269521a3c8
Tobias Hagberg <tbs.hagberg@gmail.com>
2021-05-17 17:31:54 +0200

    Established schema, model and enpoints for GET, POST. Tried to set up DB via .env and Atlas

--- server.js
+++ server.js
@@ -1,26 +1,62 @@
 import express from 'express'
-import bodyParser from 'body-parser'
 import cors from 'cors'
 import mongoose from 'mongoose'
+import dotenv from 'dotenv'
+
+dotenv.config()
 
 const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/happyThoughts"
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
 mongoose.Promise = Promise
 
-// Defines the port the app will run on. Defaults to 8080, but can be 
-// overridden when starting the server. For example:
-//
-//   PORT=9000 npm start
+
 const port = process.env.PORT || 8080
 const app = express()
 
-// Add middlewares to enable cors and json body parsing
-app.use(cors())
-app.use(bodyParser.json())
 
-// Start defining your routes here
+app.use(cors())
+app.use(express.json())
+
+
+const thoughtSchema = new mongoose.Schema({
+  message: {
+    type: String,
+    required: true,
+    trim: true,
+    minlength: 5,
+    maxlength: 140
+  },
+  hearts: {
+    type: Number,
+    default: 0
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
+})
+
+
+const Thought = mongoose.model('Thought', thoughtSchema);
+
+
 app.get('/', (req, res) => {
   res.send('Hello world')
+})
+
+app.get('/thoughts', async (req, res) => {
+  const tasks = await Thought.find().sort({createdAt: 'desc'}).limit(20).exec()
+  })
+
+app.post('/thoughts', async (req, res) => {
+  const thought = new Thought({ text, complete })
+    
+  try {
+  const newThought = await thought.save()
+  res.status(201).json(thought)
+  } catch (err) {
+  res.status(400).json({message: 'Could not save your thought to the database', error: err.errors})
+  } 
 })
 
 // Start the server
