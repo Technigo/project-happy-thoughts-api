51126f79a3a953db7e10b25d85905d16d1404450
Your Name <isabellamarkskog@live.se>
2021-05-17 14:34:30 +0200

    Added packages

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 import express from 'express';
 import cors from 'cors';
 import mongoose from 'mongoose';
+import listEndpoints from 'express-list-endpoints';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/happyThoughts";
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true });
@@ -34,17 +38,21 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-// app.use((req, res, next) => {
-//   if (mongoose.connection.readyState === 1) {
-//     next();
-//   } else {
-//     res.status(503).json({ error: 'Service unavailable' });
-//   }
-// });
+app.use((req, res, next) => {
+  if (mongoose.connection.readyState === 1) {
+    next();
+  } else {
+    res.status(503).json({ error: 'Service unavailable' });
+  }
+});
 
 // Routes 
 app.get('/', (req, res) => {
-  res.send('Hello world');
+  try {
+    res.send(listEndpoints(app));
+  } catch (err) {
+    res.status(404).send({ error: "Page not found" });
+  }
 });
 
 app.get('/thoughts', async (req, res) => {
