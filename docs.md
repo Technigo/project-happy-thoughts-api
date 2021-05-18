6d50e4f9fc84fba6b307e782e2e155b8374a13ca
Mir Albertioni <fethullah.dincer@gmail.com>
2021-05-18 12:26:47 +0200

    done

---
+++ docs.md
@@ -0,0 +1,10 @@
+npm install
+npm install @babel/helper-compilation-targets --save-dev
+
+RESET_DB=true npm run dev
+
+npx kill-port 8080
+
+
+Webpage with queries
+https://docs.mongodb.com/manual/reference/operator/query/gt/
--- server.js
+++ server.js
@@ -1,26 +1,61 @@
 import express from 'express'
-import bodyParser from 'body-parser'
 import cors from 'cors'
 import mongoose from 'mongoose'
 
 const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/happyThoughts"
-mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true })
 mongoose.Promise = Promise
 
-// Defines the port the app will run on. Defaults to 8080, but can be 
-// overridden when starting the server. For example:
-//
-//   PORT=9000 npm start
 const port = process.env.PORT || 8080
 const app = express()
 
+const thoughtSchema = new mongoose.Schema({
+  message: {
+    type: String,
+    required: [true, "Message is required"], // Du kan göra så på samtliga, text på unique etc etc där du kan skriva ett meddelande
+    unique: true,  // /^[0-9]+$/ Detta betyder att vi endast accepterar siffror
+    //match: /^[^0-9]+$/,   // /^[^0-9]+$/ Detta betyder att vår input inte får innehålla siffror. Oavsett om vi bara skriver siffror eller text+siffra
+    validate: {
+      validator: (value) => {
+        return /^[^0-9]+$/.test(value); // Vi gör så att ifall vi använder siffror så får vi ett meddelande som vi skrivit nedan att nummer ej tillåtna 
+      },
+      message: "Numbers are not allowed"
+    },
+    minlength: 5,
+    maxlength: 140
+  },  
+  hearts: {
+    type: Number,
+    default:0
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
+})
+
+const Thought = mongoose.model('Thought', thoughtSchema);
+
 // Add middlewares to enable cors and json body parsing
 app.use(cors())
-app.use(bodyParser.json())
+app.use(express.json())
 
 // Start defining your routes here
 app.get('/', (req, res) => {
   res.send('Hello world')
+})
+
+app.post('/thoughts', async (req,res) => {
+  try {
+    const newThought = await new Thought(req.body).save(); // req.body är samma sak som {message: req.body.message}
+    res.json(newThought);
+  } catch (error) {
+    if(error.code===11000){
+      res.status(400).json({ error: 'Duplicated valye', field: error.keyValue })
+    }
+    res.status(400).json(error)
+
+  }
 })
 
 // Start the server
