f43ae22a903348dbeb4ad2b725bfcab364f5a07b
Ekaterina Klimenko <klimenko3007@gmail.com>
2021-05-18 22:20:44 +0200

    Updated packages, created model, and post and get APIs

--- server.js
+++ server.js
@@ -1,5 +1,4 @@
 import express from 'express'
-import bodyParser from 'body-parser'
 import cors from 'cors'
 import mongoose from 'mongoose'
 
@@ -7,20 +6,69 @@ const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/happyThoughts"
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
 mongoose.Promise = Promise
 
-// Defines the port the app will run on. Defaults to 8080, but can be 
-// overridden when starting the server. For example:
-//
-//   PORT=9000 npm start
+const thoughtSchema = mongoose.Schema({
+  message: {
+    type: String,
+    required: true,
+    minlength: 5,
+    maxlength: 140,
+    trim: true,
+    validate: {
+      validator: (value) => {
+        return !(/\b(fuck)\b/.test(value))
+      },
+      message: "Cursing words are not allowed"
+    }
+  },
+  hearts: {
+    type: Number,
+    default: 0
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
+})
+
+const Thought = mongoose.model('Thought', thoughtSchema)
+
 const port = process.env.PORT || 8080
 const app = express()
 
-// Add middlewares to enable cors and json body parsing
 app.use(cors())
-app.use(bodyParser.json())
+app.use(express.json())
 
-// Start defining your routes here
 app.get('/', (req, res) => {
   res.send('Hello world')
+})
+
+app.post('/thoughts', async (req, res) => {
+  const { message } = req.body
+  try {
+    const thought = new Thought({ message })
+    await thought.save()
+    res.json(thought)
+  } catch (error) {
+    res.status(400).json({ error })
+  }
+})
+
+app.get('/thoughts', async (req, res) => {
+  try {
+    const thoughts = await Thought.aggregate([
+      {
+        $sort: {
+          createdAt: -1
+        }
+      },
+      { 
+        $limit: 20 
+      }
+    ])
+    res.json(thoughts)
+  } catch (error) {
+    res.status(400).json(error)
+  }
 })
 
 // Start the server
