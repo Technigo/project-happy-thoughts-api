216a08e5705c28a0f3e5d726742e00d82d036054
unknown <tezzinthecity@gmail.com>
2021-05-19 16:09:48 +0200

    remove typo in like endpoint

--- server.js
+++ server.js
@@ -71,7 +71,7 @@ app.post('/thoughts', async (req, res) => {
 }
 })
 
-app.post('/thoughts/:thoughtId/likes', async (req, res) => {
+app.post('/thoughts/:thoughtId/like', async (req, res) => {
   const { thoughtId } = req.params;
 
   try {
