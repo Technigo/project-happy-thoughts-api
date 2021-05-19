365c8c6c388d5281f45987d75e555dfd5dc2a8b4
jess-stack <72085164+jess-stack@users.noreply.github.com>
2021-05-19 15:59:00 +0200

    added get, post, patch and delete

--- server.js
+++ server.js
@@ -1,30 +1,159 @@
-import express from 'express'
-import bodyParser from 'body-parser'
-import cors from 'cors'
-import mongoose from 'mongoose'
+/* eslint-disable no-nested-ternary */
+/* eslint-disable linebreak-style */
+import express from 'express';
+import cors from 'cors';
+import mongoose from 'mongoose';
 
-const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/happyThoughts"
-mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
-mongoose.Promise = Promise
+const mongoUrl = process.env.MONGO_URL || 'mongodb://localhost/happyThoughts';
+mongoose.connect(mongoUrl, { 
+  useNewUrlParser: true, 
+  useUnifiedTopology: true, 
+  useCreateIndex: true 
+});
+mongoose.Promise = Promise;
 
-// Defines the port the app will run on. Defaults to 8080, but can be 
-// overridden when starting the server. For example:
-//
-//   PORT=9000 npm start
-const port = process.env.PORT || 8080
-const app = express()
+const port = process.env.PORT || 8080;
+const app = express();
+
+const thoughtsSchema = new mongoose.Schema({
+  message: {
+    type: String,
+    required: [true, 'You have to write a message'],
+    minlength: [5, 'Your message must be at least 5 characters'],
+    maxlength: [140, 'Your message can not be longer than 140 characters']
+  },
+  name: {
+    type: String,
+    default: 'Anonymous',
+    maxlength: 20
+  },
+  hearts: {
+    type: Number,
+    default: 0
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+const Thought = mongoose.model('Thought', thoughtsSchema);
 
 // Add middlewares to enable cors and json body parsing
-app.use(cors())
-app.use(bodyParser.json())
+app.use(cors());
+app.use(express.json());
 
-// Start defining your routes here
+// Routes
 app.get('/', (req, res) => {
   res.send('Hello world')
+});
+
+app.get('/thoughts', async (req, res) => {
+  const { page, sort } = req.query;
+
+  // Show 20 results per page
+  const pageResults = (page) => {
+    return ((page - 1) * 20)
+  };
+
+  // Sort thoughts on either most liked, newest or oldest
+  const sortThoughts = (sort) => {
+    if (sort === "hearts") {
+      return { hearts: -1 };
+    } else if (sort === "oldest") {
+      return { createdAt: 1 };
+    } else {
+      return { createdAt: -1 };
+    }
+  };
+
+  // const sortThoughts = sort === 'newest' ? { createdAt: 1 } : { createdAt: -1 };
+
+  const allThoughts = await Thought.find()
+    .sort(sortThoughts(sort))
+    .limit(20)
+    .skip(pageResults(page))
+    .exec()
+
+  try {
+    return allThoughts.length > 0
+      ? res.json(allThoughts)
+      : res.json({ result: 'No thoughts!' });
+  } catch (error) {
+    res.status(400).json({ error: 'Something went wrong', details: error });
+  }
+});
+
+// Post request for messages
+app.post('/thoughts', async (req, res) => {
+  const { message, name } = req.body;
+
+  try {
+    const newThought = await new Thought({ message, name: name || 'Anonymous' }).save();
+    res.json(newThought);
+  } catch (error) {
+    res.status(400).json(error);
+  }
+});
+
+// Delete thought
+app.delete('/thoughts/:id', async (req, res) => {
+  const { id } = req.params;
+  
+  try {
+    const deletedThought = await Thought.findByIdAndDelete(id);
+    if (deletedThought) {
+      res.json(deletedThought)
+    } else {
+      res.status(404).json({ message: 'Not found!' })
+    }
+  } catch (error) {
+    res.status(400).json({ message: 'Invalid request', error });
+  }
+});
+
+// Update thought with patch
+app.patch('/thoughts/:id', async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const updatedThought = await Thought.findByIdAndUpdate(
+      id, 
+      req.body,
+      { new: true }
+    );
+    if (updatedThought) {
+      res.json(updatedThought);
+    } else {
+      res.status(404).json({ message: 'Not found!' });
+    }
+  } catch (error) {
+    res.status(400).json({ message: 'Invalid request', error });
+  }
 })
+
+// // Post request for likes
+app.post('thoughts/:id/like', async (req, res) => {
+  const { id } = req.params;
+  const likeThought = await Thought.findByIdAndUpdate(
+    id, 
+    { $inc: { hearts: 1 } },
+    { new: true } 
+  );
+
+  try {
+    if (likeThought) {
+      res.json(likeThought);
+    } else {
+      res.status(404).json({ message: 'Not found!' });
+    }
+  } catch (error) {
+    res.status(400).json({ message: 'Could not find that thought', details: error });
+  }
+});
 
 // Start the server
 app.listen(port, () => {
   // eslint-disable-next-line
   console.log(`Server running on http://localhost:${port}`)
-})
+});
