c56dafc7da0f0d090a22a71b9dd8b2fbe3407f48
Irina <jekabsone.irina@gmail.com>
2021-05-20 20:59:45 +0200

    delete, post and get endpoints

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import cors from 'cors'
 import mongoose from 'mongoose'
+import listEndpoints from 'express-list-endpoints'
 
 const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/happyThoughts"
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true})
@@ -37,21 +38,70 @@ app.use(cors())
 app.use(express.json())
 
 
-app.get('/thoughts', (req, res) => {
-  res.send('Hello world')
+app.get('/', (req, res) => {
+  res.send(listEndpoints(app))
 })
 
+//Endpoint to fetch a thoughts list
+app.get('/thoughts', async (req, res) => {
+  try {
+    const allThoughts = await Thought.find().sort({createdAt: -1 }).limit(20)//.skip(20).limit(20) if you want to show another 20
+    res.json(allThoughts)
+  } catch (error) {
+    res.status(400).json({ message: 'Something went wrong', error })
+  }
+})
+
+//Endpoint to create a new Thought
 app.post('/thoughts', async (req, res) => {
   try {
-    //Creating a new Thought
     const newThought = await new Thought(req.body).save()// same as ({ message: req.body.message })
     res.json(newThought)
   } catch (error) {
     if (error.code === 11000) {
-      res.status(400).json({ error: 'Duplicated value', fields: error.keyValue })
+      res.status(400).json({ message: 'Duplicated value', fields: error.keyValue })
     } 
     res.status(400).json(error)
   }
+})
+
+//An endpoint to delete a single Thought
+app.delete('/thoughts/:id', async (req, res) => {
+  const { id } = req.params
+
+  try {
+    const deletedThought = await Thought.findByIdAndDelete(id)
+    if (deletedThought) {
+      res.json(deletedThought)
+    } else {
+      res.status(404).json({ message: 'Not found' })
+    }
+
+  } catch (error) {
+    res.status(400).json({ message: 'Invalid request', error })
+  }
+})
+
+//An enpoint to update likes
+app.post('/thoughts/:id/likes', async (req, res) => {
+  const { id } = req.params
+
+  try {
+    const updatedThought = await Thought.findByIdAndUpdate(
+      id,
+      { 
+        $inc: { //$inc is a special mongoose query selector used to update a number value
+          hearts: 1 
+        } 
+      },
+      { 
+        new: true
+      } ) //{ new: true } is responsible for sending back an object with already updated values
+    res.json(updatedThought)
+  } catch (error) {
+    res.status(400).json({ message: 'Invalid request', error })
+  }
+
 })
 
 // Start the server
