abd9857728a2eeaa9f91e4d3dfa0b5f7dbcc5a81
“[ida <[ida.t.sylven@gmail.com]>
2021-05-21 00:00:15 +0200

    Finalised endpoints and made clarifying comments

--- server.js
+++ server.js
@@ -1,29 +1,121 @@
 import express from 'express'
-import bodyParser from 'body-parser'
 import cors from 'cors'
 import mongoose from 'mongoose'
+import listEndpoints from 'express-list-endpoints'
 
 const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/happyThoughts"
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
 mongoose.Promise = Promise
 
-// Defines the port the app will run on. Defaults to 8080, but can be 
-// overridden when starting the server. For example:
-//
-//   PORT=9000 npm start
+// Defining the port the app will run on
 const port = process.env.PORT || 8080
 const app = express()
 
-// Add middlewares to enable cors and json body parsing
+// Middlewares to enable cors and json body parsing
 app.use(cors())
-app.use(bodyParser.json())
+app.use(express.json())
 
-// Start defining your routes here
-app.get('/', (req, res) => {
-  res.send('Hello world')
+const thoughtSchema = new mongoose.Schema({
+  message: {
+    type: String,
+    required: [true, "A message is required, please type a happy thought :)"],
+    minlenght: [5, "Sorry, your message needs to have at least 5 characters"],
+    maxlenght: [140, "Sorry, you message is unfortunately exceeding the limit of 140 characters"]
+  },
+  hearts: {
+    type: Number,
+    default: 0
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
 })
 
-// Start the server
+const Thought = mongoose.model('Thought', thoughtSchema)
+
+// ROUTES
+app.get('/', (req, res) => {
+  res.send(listEndpoints(app))
+})
+
+// Endpoint to get the most recent 20 thoughts in descending order
+app.post('/thoughts', async (req, res) => {
+  const allThoughts = await Thought.find().sort({ createdAt: -1 }).limit(20)
+  res.json(allThoughts)
+})
+
+// Endpoint to post a new thought 
+app.post('/thoughts', async (req, res) => {
+  try {
+    const newThought = await new Thought(req.body).save()
+    res.status(200).json(newThought)
+  } catch (error) {
+    res.status(400).json({ message: 'Invalid request', error })
+  }
+})
+
+// Endpoint to update likes/hearts 
+app.post('/thoughts/:id/likes', async (req, res) => {
+  const { id } = req.params
+
+  try {
+    const updatedThought = await Thought.findOneAndUpdate({ _id: id }, { $inc: { hearts: 1 } }, { new: true })
+    if (updatedThought) {
+      res.json(updatedThought)
+    } else {
+      res.status(404).json({ message: 'Not found' })
+    }
+  } catch (error) {
+    res.status(400).json({ message: 'Invalid request', error })
+  }
+})
+
+// Endpoint to delete a thought
+app.delete('/thoughts/:id', async (req, res) => {
+  const { id } = req.params
+
+  try {
+    const deletedThought = await Thought.deleteOne({ _id: id })
+    res.json(deletedThought)
+  } catch (error) {
+    res.status(400).json({ message: 'Invalid request', error })
+  }
+})
+
+// Endpoint to replace a thought
+app.put('/thoughts/:id', async (req, res) => {
+  const { id } = req.params
+
+  try {
+    const replacedThought = await Thought.findOneAndReplace({ _id: id }, req.body, { new: true })
+    if (replacedThought) {
+      res.json(replacedThought)
+    } else {
+      res.status(404).json({ message: 'Not found' })
+    }
+  } catch (error) {
+    res.status(400).json({ message: 'Invalid request', error })
+  }
+})
+
+// Endpoint to update a thought
+app.patch('/thoughts/:id', async (req, res) => {
+  const { id } = req.params
+
+  try {
+    const updatedThought = await Thought.findByIdAndUpdate(id, req.body, { new: true })
+    if (updatedThought) {
+      res.json(updatedThought)
+    } else {
+      res.status(404).json({ message: 'Not found' })
+    }
+  } catch (error) {
+    res.status(400).json({ message: 'Invalid request', error })
+  }
+})
+
+// Starting the server
 app.listen(port, () => {
   // eslint-disable-next-line
   console.log(`Server running on http://localhost:${port}`)
