b8ebe93da37341e65abb7a6ffc59f8b8a8a1d922
Sofarz <sofia.wallen@gmail.com>
2021-05-20 19:55:15 +0200

    testing

--- server.js
+++ server.js
@@ -46,7 +46,7 @@ app.get('/', (req, res) => {
 // GET endpoint to retrieve all thoughts from the database. 
 // Sorted by date from newest to oldest. Returning a maximum of 20 thoughts.
 app.get('/thoughts', async (req, res) => {
-  const newThought = await Thought.find().sort({ createdAt: 'desc' }).limit(20).exec()
+  const newThought = await Thought.find().limit(20).sort({ createdAt: 'desc' })
   res.json(newThought)
 })
 
