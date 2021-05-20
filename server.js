2359129dd3efe91d9591f3be5ee0e6f7b5c0beee
Eleonor Frankenberg <elle@Eleonors-MacBook-Air.local>
2021-05-20 11:15:36 +0200

    made a get req to get all the recent posted 20 thoughts

--- server.js
+++ server.js
@@ -1,16 +1,14 @@
 import express from 'express'
 import cors from 'cors'
 import mongoose from 'mongoose'
-import dotenv from 'dotenv';
+import dotenv from 'dotenv'
+import listEndpoints from 'express-list-endpoints'
 
 dotenv.config();
 
 const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/happyThoughts"
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
 mongoose.Promise = Promise
-
-const port = process.env.PORT || 8080
-const app = express()
 
 const thoughtSchema = new mongoose.Schema({
  message: {
@@ -32,12 +30,33 @@ const thoughtSchema = new mongoose.Schema({
 
 const Thought = mongoose.model('Thought', thoughtSchema)
 
+const port = process.env.PORT || 8080
+const app = express()
+
+
 app.use(cors())
 app.use(express.json())
 
 // Start defining your routes here
 app.get('/', (req, res) => {
-  res.send('Hello world')
+  try {
+    res.send(listEndpoints(app))
+  } catch (error) {
+    res.status(404).json({ error: 'Page not found' })
+  }
+})
+
+app.get('/thoughts', async (req, res) => {
+  try {
+    const recentThoughts = await Thought.find().sort({ createdAt: -1 }).limit(20)
+    if(recentThoughts) {
+      res.json(allRecentThoughts)
+    } else {
+      res.status(404).json({ error: 'Could not find any thoughts' })
+    }  
+  } catch (error) {
+    res.status(404).json({ error: 'Not found' })
+  }
 })
 
 
@@ -46,19 +65,11 @@ app.post('/thoughts', async (req, res) => {
     const newThought = await new Thought(req.body).save()
     res.json(newThought)
   } catch (error) {
-    res.status(400).json(error)
+    res.status(400).json({ message: 'Could not save', error })
   }
 })
 
-app.get('/thoughts', async (req, res) => {
 
-  try {
-    const allReasentThoughts = await Thought.find().sort({ createdAt: -1 }).limit(20)
-    res.json(allReasentThoughts)
-  } catch (error) {
-
-  }
-})
 
 
 
