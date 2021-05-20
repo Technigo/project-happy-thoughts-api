88094067427a905a6a9235314a013bc6d239b12b
Maria Sparre <you@example.com>
2021-05-20 10:33:09 +0200

    DELETE route added

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ dotenv.config()
 
 // const mongoUrl = process.env.MONGO_URL || "mongodb://localhost:27017/happyThoughts"
 const mongoUrl = process.env.MONGO_URL || `mongodb+srv://${process.env.USER_ID}:${process.env.API_KEY}@cluster0.bvwog.mongodb.net/happyThoughts?retryWrites=true&w=majority`
-mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(mongoUrl, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true })
 mongoose.Promise = Promise
 
 const port = process.env.PORT || 8081
@@ -18,8 +18,11 @@ const thoughtSchema = new mongoose.Schema({
   message: {
     type: String,
     required: true,
+    unique: true,
     minlength: 5, 
-    maxlength: 140
+    maxlength: 140,
+    // enum: ['']
+    //match: regex
   },
   hearts: {
     type: Number,
@@ -31,14 +34,14 @@ const thoughtSchema = new mongoose.Schema({
   }
 })
 
-//Create model for mongo database 
+//Model for mongo database 
 const Thought = mongoose.model('Thought', thoughtSchema)
 
 // Add middlewares to enable cors and json body parsing
 app.use(cors())
 app.use(express.json())
 
-// Start defining your routes here
+//GET route
 app.get('/', (req, res) => {
   res.send(listEndpoints)
 })
@@ -46,15 +49,61 @@ app.get('/thoughts', async (req, res) => {
   const thoughts = await Thought.find().sort({createdAt: 'desc'}).limit(20).exec()
   res.json(thoughts)
 })
+
+// POST route
 app.post('/thoughts', async (req, res) => {
   try {
     const newThought = await new Thought ({
       message: req.body}).save() 
       res.status(201).json(newThought)
   } catch(error) {
-  res.status(400).json({ message: 'Could not save message', errors: error.errors})
+    if (error.code === 11000) {
+      res.status(400).json({ error: 'Duplicated value', fields: error.keyValue })
+    }
+  res.status(400).json({ message: 'Could not send message', error})
   }
 })
+// app.post('/thoughts/:id/likes', async (req, res) => {
+//   const { id } = req.params
+
+//   try{
+//     const updatedThought = await Thought.findByIdAndUpdate(id, { $inc: { hearts: 1 } })
+//   } catch (error) {
+   
+//   }
+// })
+//DELETE route
+app.delete('thoughts/:id', async (req, res) => {
+  const { id } = req.params
+
+  try {
+    const deleteThought = await Thought.findOneAndDelete({ _id: id })
+    if (deleteThought) {
+  } else {
+    res.status(404).json({ message: 'Not found'})
+  }
+  } catch (error){
+    res.status(400).json({ message: 'Invalid request', error})
+  }
+})
+// PATCH route
+app.patch('/thoughts/:id', async (req, res) => {
+  const { id } = req.params
+
+  try{
+    const updatedThought = await Thought.findByIdAndUpdate(id, { name: req.body.name })
+    if (updatedThought) {
+      res.json(updatedThought)
+    } else {
+      res.status(404).json({ message: 'Not found'})    
+    }
+  } catch (error) {
+    res.status(400).json({ message: 'Invalid request', error})
+  }
+
+})
+
+
 
 // Start the server
 app.listen(port, () => {
