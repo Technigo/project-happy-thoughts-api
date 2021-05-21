0cb1c4b56ec064b573814d42b9e8c9486ed4c9e3
liaback <felicia.liaback@gmail.com>
2021-05-21 13:36:38 +0200

    create get request to show thoughts, sort and limit to 20 posts

--- server.js
+++ server.js
@@ -47,13 +47,34 @@ app.get("/", (req, res) => {
   res.send("Hello world");
 });
 
-// POST request - save new thought to db
+// GET request - show thoughts, sort latest post on top, limit to 20 messages
+app.get('/thoughts', async (req,res) => {
+  const allThoughts = await Thought.find().sort( { createdAt: -1 } ).limit(20)
+  res.json(allThoughts)
+})
+
+// POST request - save new thought to db - add message: req.body again (not to manipulat hearts or date)
 app.post("/thoughts", async (req, res) => {
   try {
     const newThought = await new Thought(req.body).save();
     res.json(newThought);
   } catch (error) {
     res.status(400).json(error);
+  }
+});
+
+// POST request - update hearts to the db
+app.post("/thoughts/:id/likes", async (req, res) => {
+  const { id } = req.params;
+  try {
+    const updatedThought = await Thought.findOneAndUpdate( {_id: id}, { $inc: { hearts: 1 }}, { new: true});
+    if (updatedThought) {
+      res.json(updatedThought);
+    } else {
+      res.status(404).json({ message: "Not found" });
+    }
+  } catch (error) {
+    res.status(400).json({ message: "Invalid request", error });
   }
 });
 
@@ -73,11 +94,30 @@ app.delete("/thoughts/:id", async (req, res) => {
 });
 
 // Change in the frontend where we use POST for increasing hearts...
-// PATCH request - updates entity 
+// PATCH request - updates entity
 app.patch("/thoughts/:id", async (req, res) => {
   const { id } = req.params;
   try {
-    const updatedThought = await Thought.findByIdAndUpdate(id, { message: req.body.message }, { new: true });
+    const updatedThought = await Thought.findByIdAndUpdate(id, req.body, { new: true, });
+    if (updatedThought) {
+      res.json(updatedThought);
+    } else {
+      res.status(404).json({ message: "Not found" });
+    }
+  } catch (error) {
+    res.status(400).json({ message: "Invalied requeset", error });
+  }
+});
+
+// PUT request - replace entity
+app.put("/thoughts/:id", async (req, res) => {
+  const { id } = req.params;
+  try {
+    const updatedThought = await Thought.findOneAndReplace(
+      { _id: id },
+      req.body,
+      { new: true }
+    );
     if (updatedThought) {
       res.json(updatedThought);
     } else {
