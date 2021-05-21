da560cd52dbdec00f2e90bd9d4759a98661c36f2
liaback <felicia.liaback@gmail.com>
2021-05-21 08:50:56 +0200

    create schema, model and post request, require message and add error message if not true

--- server.js
+++ server.js
@@ -1,30 +1,51 @@
-import express from 'express'
-import bodyParser from 'body-parser'
-import cors from 'cors'
-import mongoose from 'mongoose'
+import express from "express";
+import cors from "cors";
+import mongoose from "mongoose";
 
-const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/happyThoughts"
-mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
-mongoose.Promise = Promise
+const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/happyThoughts";
+mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.Promise = Promise;
 
-// Defines the port the app will run on. Defaults to 8080, but can be 
-// overridden when starting the server. For example:
-//
-//   PORT=9000 npm start
-const port = process.env.PORT || 8080
-const app = express()
+const port = process.env.PORT || 8080;
+const app = express();
 
-// Add middlewares to enable cors and json body parsing
-app.use(cors())
-app.use(bodyParser.json())
+//schema
+const thoughtSchema = mongoose.Schema({
+  message: {
+    type: String,
+    required: true
+  },
+  hearts: {
+    type: Number,
+    default:0
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now // can be written like () => Date.now()
+  }
+});
 
-// Start defining your routes here
-app.get('/', (req, res) => {
-  res.send('Hello world')
+//model
+const Thought = mongoose.model('Thought', thoughtSchema)
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Hello world");
+});
+
+// post request
+app.post('/thoughts', async (req,res) => {
+  try {
+    const newThought = await new Thought(req.body).save();
+    res.json(newThought);
+  } catch (error) {
+    res.status(400).json(error)
+}
 })
 
-// Start the server
 app.listen(port, () => {
   // eslint-disable-next-line
-  console.log(`Server running on http://localhost:${port}`)
-})
+  console.log(`Server running on http://localhost:${port}`);
+});
