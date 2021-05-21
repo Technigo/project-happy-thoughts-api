37331afcff72f156797a88a67db4501cbb3bceab
Anna Lindgren <annalindgren@MacBook-Pro-som-tillhor-Anna.local>
2021-05-21 16:42:59 +0200

    added PUT and PATCH endpoints for changing a thought

--- server.js
+++ server.js
@@ -52,19 +52,75 @@ app.get('/', (req, res) => {
   res.send('Hello world')
 })
 
-// POST request for new thought
+// POST endpoint for new thought
 app.post('/thoughts', async (req, res) => {
   // const { message } = req.body
   // console.log(req.body)
   // Specify all of the objects in schema
   try {
-    const newThought = await new Thought(req.body).save()
+    const newThought = await new Thought({ message: req.body.message }).save()
     res.json(newThought)
   } catch (error) {
     if (error.code === 11000) {
       res.status(400).json({ error: 'Duplicated value', fields: error.keyValue })
     }
     res.status(400).json(error)
+  }
+})
+
+// DELETE endpoint to find and delete a thought
+app.delete('/thoughts/:id', async (req, res) => {
+  const { id } = req.params
+
+  try {
+    // V.1 - Only delete
+    // const deletedThought = await Thought.deleteOne({ _id: id })
+    // res.json(deletedThought)
+
+    // V2 - find and delete
+    const deletedThought = await Thought.findOneAndDelete({ _id: id })
+    if (deletedThought) {
+      res.json(deletedThought)
+    } else {
+      res.status(404).json({ message: 'Not found' })
+    }
+  } catch (error) {
+    res.status(400).json({ message: 'Invalid request', error })
+  }
+})
+
+// PATCH endpoint for updating a thought
+app.patch('/thoughts/:id', async (req, res) => {
+  const { id } = req.params
+
+  try {
+    // find id but what do we want to update? 
+    // eslint-disable-next-line max-len
+    const updatedThought = await Thought.findByIdAndUpdate(id, { message: req.body.message }, { new: true })
+    if (updatedThought) {
+      res.json(updatedThought)
+    } else {
+      res.status(404).json({ message: 'Not found' })
+    }
+  } catch (error) {
+    res.status(400).json({ message: 'Invalid request', error })
+  }
+})
+
+// PUT endpoint for updating a thought
+app.put('/thoughts/:id', async (req, res) => {
+  const { id } = req.params
+
+  try {
+    // eslint-disable-next-line max-len
+    const updatedThought = await Thought.findOneAndReplace({ _id: id }, { message: req.body.message }, { new: true })
+    if (updatedThought) {
+      res.json(updatedThought)
+    } else {
+      res.status(404).json({ message: 'Not found' })
+    }
+  } catch (error) {
+    res.status(400).json({ message: 'Invalid request', error })
   }
 })
 
