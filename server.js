7aaacdb6ecfe0049790e25fab1ba416474f43206
Tobias Hagberg <tbs.hagberg@gmail.com>
2021-05-21 09:18:01 +0200

    built post for updating like by id

--- server.js
+++ server.js
@@ -61,6 +61,21 @@ app.post('/thoughts', async (req, res) => {
   } 
 })
 
+app.post('/thoughts/:thoughtId/like', async (req, res) => {
+  const { id } = req.params
+
+  try {
+    const updatedThought = await Thought.findByIdAndUpdate(id, { $inc: { hearts: 1 } }, {new: true})
+    if (updatedThought) {
+      res.json(updatedThought)
+    } else {
+      res.status(404).json({ message: 'Not found' })
+    }
+  } catch (error) {
+    res.status(400),json({ message: 'Invalid request', error })
+  }
+})
+
 // Start the server
 app.listen(port, () => {
   // eslint-disable-next-line
