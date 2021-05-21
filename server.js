f2a6e5a80ac4383bca45d5f2f024e62907b2dd5a
esteficodes <estefaniaquevedolusby@gmail.com>
2021-05-21 17:46:31 +0200

    corrected typos

--- server.js
+++ server.js
@@ -1,41 +1,24 @@
 import express from 'express'
 import cors from 'cors'
 import mongoose from 'mongoose'
-
-
-
-
+import listEndpoints from 'express-list-endpoints'
 
 const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/happyThoughts"
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true })
 mongoose.Promise = Promise
 
-// Defines the port the app will run on. Defaults to 8080, but can be 
-// overridden when starting the server
-
 const port = process.env.PORT || 9000
 const app = express()
 
+//CREATES SCHEMA
 const thoughtSchema  = new mongoose.Schema({
   message: {
     type: String,
     required: [true, "Ding, dong! Message is required!"],
     minLength: [5, 'Hey, the minimum length is 5 characters'],
     maxLength: [140, 'Hey, the maximum length is 140 characters'],
-    //unique: true,
-    //trim: true,
-     // enum: ['Technigo is cool', 'Technigo is great', 'That was the time of my life'],
-    // match: /^[^0-9]+$/,
-    validate: {
-      validator: (value) => {
-        return /^[^0-9]+$/.test(value)
-      },
-      message: "Numbers are not allowed"
-    },
-    username: {
-      type: String,
-      maxLength: 15,
-      default: 'Anonymous'
+    unique: true,
+    trim: true
     },
   hearts: {
     type: Number,
@@ -43,16 +26,19 @@ const thoughtSchema  = new mongoose.Schema({
   },
   createdAt: {
     type: Date,
-    default: Date.now
+    default: Date.now //also () => Date.now()
   }
-}
 })
-
+//CREATES SINGLE THOUGHT MODEL
 const Thougt = mongoose.model('Thought', thoughtSchema)
 
 // Add middlewares to enable cors and json body parsing
 app.use(cors())
 app.use(express.json())
+
+app.get('/', (req, res) => {
+  res.send(listEndpoints(app))
+})
 
 app.use((req, res, next) => {
   if (mongoose.connection.readyState === 1) {
@@ -66,49 +52,69 @@ app.use((req, res, next) => {
 
 // DEFINING ROUTES AND ENDPOINTS
 app.get('/', (req, res) => {
-  res.send("Hola Mundo! This is Estefania's Happy Coding Thoughts API <3")
+  res.send("Hola Mundo! This is Estefania's Happy Coding Thoughts API <3")//replaced by listEndpoints
 })
 
-//GET /thoughts: this endpoint will show the 20 most recent thoughts 
-app.get('/thougts', async (req, res) => {
-  const thoughts = await Thought.find().sort({ createdAt: 'desc' }).limit(20).exec()
+//GET /thoughts: ENDOPINT TO SHOW 20 RECENT POSTS
+app.get('/thoughts', async (req, res) => {
+  try {
+  const thoughts = await Thought.find().sort({ createdAt: -1 }).limit(20)
   res.json(thoughts)
+  } catch (error) {
+    res.status(400).json({ message: 'Ouch, something went wrong', error })
+  }
 })
 
 //ENDPOINT TO POST A THOUGHT
 app.post('/thoughts', async (req, res) => {
   try {
     const newThought = await new Thought(req.body).save()
-    res.status(200).json(newThought)
+    res.json(newThought)
   }  catch (error)  {
-       res.status(400).json({ 
-         message: 'Could not save to database', error
-         })
+    if (error.code === 11000) {
+       res.status(400).json({ message: 'Could not save to database', error })
      }
+     res.status(400).json(error)
+  }
 })
 
-//ENDPOINT TO ADD LIKES/HEARTS
-app.post('/thoughts/:thoughtsId/like', async (req, res) => {
+//ENDPOINT TO DELETE A THOUGHT
+app.delete('/thoughts/:thoughtId', async (req, res) => {
   const { thoughtId } = req.params
 
   try {
-    const like = await Thought.findOneAndUpdate(
-      {_id: thoughtId },
-      { $inc: { hearts: 1 } },
-      { new: true }
-    )
-    if (like) {
-    res.status(200).json(like)
+    const deletedThought = await Thought.findByIdAndDelete(id)
+    if (deletedThought) {
+      res.json(deletedThought)
     } else {
-      res.status(404).json({
-      message: 'Message was not found'
-    })
+      res.status(404).json({ message: 'Not found' })
     }
+
   } catch (error) {
-    res.status(400).json({
-      message: 'Invalid request', error
-    })
+    res.status(400).json({ message: 'Invalid request', error })
   }
+})
+
+//ENDPOINT TO ADD LIKES/HEARTS
+app.post('/thoughts/:thoughtId/likes', async (req, res) => {
+  const { thoughtId } = req.params
+
+  try {
+    const likedThought = await Thought.findByIdAndUpdate(
+      thoughtId,
+      { 
+        $inc: {
+           hearts: 1 
+        }
+      }, //$inc is a mogoose query selector that updates a number value
+      { 
+        new: true // { new:true } is responsible for sending back an object with updated values
+      } )
+    res.json(likedThought)
+  } catch (error) {
+    res.status(400).json({ message: 'Invalid request', error })
+  }
+
 })
 
 // Start the server
