1dfd55b6635ac0e980a9e954435aef0807fe562b
Tobias Hagberg <tbs.hagberg@gmail.com>
2021-05-21 09:28:31 +0200

    problems on update heart

--- server.js
+++ server.js
@@ -65,7 +65,7 @@ app.post('/thoughts/:thoughtId/like', async (req, res) => {
   const { id } = req.params
 
   try {
-    const updatedThought = await Thought.findByIdAndUpdate(id, { $inc: { hearts: 1 } }, {new: true})
+    const updatedThought = await Thought.findByIdAndUpdate(id, { $inc: { hearts: 1 } })
     if (updatedThought) {
       res.json(updatedThought)
     } else {
