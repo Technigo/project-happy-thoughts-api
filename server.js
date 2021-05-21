ca1c8d8506f4e13cb47abc4dd4b8422b6efeea3c
liaback <felicia.liaback@gmail.com>
2021-05-21 11:04:30 +0200

    create patch request to update entity

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 import express from "express";
 import cors from "cors";
-import mongoose from "mongoose";
+import mongoose, { get } from "mongoose";
 
 const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/happyThoughts";
 mongoose.connect(mongoUrl, {
@@ -47,7 +47,7 @@ app.get("/", (req, res) => {
   res.send("Hello world");
 });
 
-// post request - save new thought to db
+// POST request - save new thought to db
 app.post("/thoughts", async (req, res) => {
   try {
     const newThought = await new Thought(req.body).save();
@@ -57,7 +57,7 @@ app.post("/thoughts", async (req, res) => {
   }
 });
 
-// delte request - delete thought from db
+// DELETE request - delete thought from db
 app.delete("/thoughts/:id", async (req, res) => {
   const { id } = req.params;
   try {
@@ -65,10 +65,26 @@ app.delete("/thoughts/:id", async (req, res) => {
     if (deletedThought) {
       res.json(deletedThought);
     } else {
-      res.status(404).json({ message: "not found" });
+      res.status(404).json({ message: "Not found" });
     }
   } catch (error) {
     res.status(400).json({ message: "Invalid request", error });
+  }
+});
+
+// Change in the frontend where we use POST for increasing hearts...
+// PATCH request - updates entity 
+app.patch("/thoughts/:id", async (req, res) => {
+  const { id } = req.params;
+  try {
+    const updatedThought = await Thought.findByIdAndUpdate(id, { message: req.body.message }, { new: true });
+    if (updatedThought) {
+      res.json(updatedThought);
+    } else {
+      res.status(404).json({ message: "Not found" });
+    }
+  } catch (error) {
+    res.status(400).json({ message: "Invalied requeset", error });
   }
 });
 
@@ -76,6 +92,3 @@ app.listen(port, () => {
   // eslint-disable-next-line
   console.log(`Server running on http://localhost:${port}`);
 });
-
-//Editing the thoughts, delet, query them with GET request
-// delet, patch, put, get
