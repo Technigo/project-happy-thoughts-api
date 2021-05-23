7f0ebb5595788f177f9a9362dff84355a389ff59
Anna Lindgren <annalindgren@MacBook-Pro-som-tillhor-Anna.local>
2021-05-23 17:40:36 +0200

    updated readme

--- README.md
+++ README.md
@@ -1,13 +1,25 @@
 # Project Happy Thoughts API
 
-Replace this readme with your own information about your project.
+This project i practised using Express and MongoDB to build an API which includes GET request endpoints to return data and POST endpoints to create data. 
+I have learned:
+*How to build a full API which includes handling of user input
+*How to include error handling to return good validation errors
+*How to build an API which works well with an existing frontend
 
-Start by briefly describing the assignment in a sentence or two. Keep it short and to the point.
+## The assignment
+I started out by creating a thoughtSchema to define the structure of the document. I added default values and validators for the posted thought. 
 
-## The problem
+ Path | Description |
+| --- | --- |
+|GET "/thoughts" | Endpoint to get all thoughts. I used .sort() and .limit() to get the newest thoughts and limit to 20 thoughts. |
+| POST  "/thoughts" | Endpoint to post thoughts which expects JSON body with a thought message. I added validation to require a message, minimum message length of 5 characters and maximum length of 140 characters. If message does not meet the validation requirements, appropriate error message will be given.|
+| POST "/thoughts/:id/likes" | Endpoint to increase heart likes on a specific thought id. |
 
-Describe how you approached to problem, and what tools and techniques you used to solve it. How did you plan? What technologies did you use? If you had more time, what would be next?
+I also have a DELETE endpoint, which I do not use at the moment. When given more time I would like to use this endpoint where in the frontend you could delete a thought with a specific id. That is why i have choosen to keep this endpoint to continue my work. 
+
+When given more time I would like to implement a delete button in the frontend to delete a thought. I would also like to add the functionality of the user to enter their name by adding a new property on the thought model.
 
 ## View it live
 
-Every project should be deployed somewhere. Be sure to include the link to the deployed project so that the viewer can click around and see what it's all about.
+https://anna-happy-thoughts.herokuapp.com/
+https://no-bad-thoughts-inc.netlify.app/
--- server.js
+++ server.js
@@ -12,11 +12,6 @@ const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/happyThoughts"
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true })
 mongoose.Promise = Promise
 
-// Date.now without ()
-// if () it is going to be set in the moment we run our app and not going to change
-// Schema is only run one time when application starts
-// enum always has to be array
-// match takes only regex
 const thougthSchema = new mongoose.Schema({
   message: {
     type: String,
@@ -65,9 +60,6 @@ app.get('/thoughts/:id', async (req, res) => {
 
 // POST endpoint for new thought
 app.post('/thoughts', async (req, res) => {
-  // const { message } = req.body
-  // console.log(req.body)
-  // Specify all of the objects in schema
   try {
     const newThought = await new Thought({ message: req.body.message }).save()
     res.json(newThought)
@@ -114,49 +106,9 @@ app.delete('/thoughts/:id', async (req, res) => {
   const { id } = req.params
 
   try {
-    // V.1 - Only delete
-    // const deletedThought = await Thought.deleteOne({ _id: id })
-    // res.json(deletedThought)
-
-    // V2 - find and delete
     const deletedThought = await Thought.findOneAndDelete({ _id: id })
     if (deletedThought) {
       res.json(deletedThought)
-    } else {
-      res.status(404).json({ message: 'Not found' })
-    }
-  } catch (error) {
-    res.status(400).json({ message: 'Invalid request', error })
-  }
-})
-
-// PATCH endpoint for updating a thought
-app.patch('/thoughts/:id', async (req, res) => {
-  const { id } = req.params
-
-  try {
-    // find id but what do we want to update? 
-    // eslint-disable-next-line max-len
-    const updatedThought = await Thought.findByIdAndUpdate(id, { message: req.body.message }, { new: true })
-    if (updatedThought) {
-      res.json(updatedThought)
-    } else {
-      res.status(404).json({ message: 'Not found' })
-    }
-  } catch (error) {
-    res.status(400).json({ message: 'Invalid request', error })
-  }
-})
-
-// PUT endpoint for updating a thought
-app.put('/thoughts/:id', async (req, res) => {
-  const { id } = req.params
-
-  try {
-    // eslint-disable-next-line max-len
-    const updatedThought = await Thought.findOneAndReplace({ _id: id }, { message: req.body.message }, { new: true })
-    if (updatedThought) {
-      res.json(updatedThought)
     } else {
       res.status(404).json({ message: 'Not found' })
     }
