382dd63dda4ab46d0f0ad1a3da22a5e0dacd81c2
Polkapetunian <70851837+Polkapetunian@users.noreply.github.com>
2021-05-23 10:53:14 +0200

    added  thoughts endpoint with GET and POST request possibilities

--- server.js
+++ server.js
@@ -1,11 +1,27 @@
 import express from 'express'
-import bodyParser from 'body-parser'
 import cors from 'cors'
 import mongoose from 'mongoose'
 
 const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/happyThoughts"
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
 mongoose.Promise = Promise
+
+const Thought = mongoose.model('Thought', {
+  message: {
+    type: String,
+    required: true,
+    minlength: 5,
+    maxlength: 140
+  },
+  hearts: {
+    type: Number,
+    default: 0
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
+})
 
 // Defines the port the app will run on. Defaults to 8080, but can be 
 // overridden when starting the server. For example:
@@ -16,11 +32,34 @@ const app = express()
 
 // Add middlewares to enable cors and json body parsing
 app.use(cors())
-app.use(bodyParser.json())
+app.use(express.json())
 
 // Start defining your routes here
-app.get('/', (req, res) => {
+app.get('/', (_, res) => {
   res.send('Hello world')
+})
+
+//Returns 20 sorted thoughts
+app.get('/thoughts', async (_, res) => {
+  const thoughts = await Thought.find().sort({createdAt: 'desc'}).limit(20).exec()
+  res.json(thoughts)
+})
+
+//Adds and saves a thought to the database
+app.post('/thoughts', async (req, res) => {
+  const { message } = req.body
+  const thought = new Thought({message})
+  try {
+    const savedThought = await thought.save()
+    res.status(201).json(savedThought)
+  } catch (err) {
+    res.status(400).json({message: 'Could not save the thought to the database.', error: err.errors})
+  }
+})
+
+//Adds a like (a heart) to a specifik thought
+app.post('/thougts/:thoughtId/like', async (req, res) => {
+  const addHeart = await 
 })
 
 // Start the server
