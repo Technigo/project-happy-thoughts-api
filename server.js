dbaca808197fec1b07c5bea2fd8a243d8ad5e112
Polkapetunian <70851837+Polkapetunian@users.noreply.github.com>
2021-05-24 09:35:42 +0200

    changed a line

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ import cors from 'cors'
 import mongoose from 'mongoose'
 import dotenv from 'dotenv'
 
-dotenv. config()
+dotenv.config()
 
 const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/happyThoughts"
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
