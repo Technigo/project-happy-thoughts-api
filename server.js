ddbfc164b1e91ba3df7ccbd10372bd8ef266b428
DANIELA <DANIELA.ZACARIAS@OUTLOOK.COM>
2021-05-24 09:14:02 -0500

    Added POST request to increase amount of hearts

--- server.js
+++ server.js
@@ -66,6 +66,34 @@ app.post('/thoughts', async (req, res) => {
   }
 })
 
+//POST request to increase amount of hearts
+app.post('/thoughts/:id/likes', async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const updatedThought = await Thought.findOneAndUpdate(
+      {
+        _id: id
+      },
+      { 
+        $inc: {
+          hearts: 1
+        }
+      },
+      {
+        new: true
+      }
+    );
+    if (updatedThought) {
+      res.json(updatedThought);
+    } else {
+      res.status(404).json({ message: 'Not found' })
+    }
+  } catch (error) {
+    res.status(400).json({ message: 'Invalid request', error });
+  }
+});
+
 app.delete('/thoughts/:id', async (req, res) => {
   const { id } = req.params
 
