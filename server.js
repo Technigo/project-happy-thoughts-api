bda14529b902a3719f5a3c3955274088b751d019
DANIELA <DANIELA.ZACARIAS@OUTLOOK.COM>
2021-05-24 07:02:59 -0500

    Created schema, model and newThought

--- server.js
+++ server.js
@@ -1,26 +1,49 @@
 import express from 'express'
-import bodyParser from 'body-parser'
 import cors from 'cors'
 import mongoose from 'mongoose'
 
 const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/happyThoughts"
-mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex:true })
 mongoose.Promise = Promise
 
-// Defines the port the app will run on. Defaults to 8080, but can be 
-// overridden when starting the server. For example:
-//
-//   PORT=9000 npm start
 const port = process.env.PORT || 8080
 const app = express()
 
 // Add middlewares to enable cors and json body parsing
 app.use(cors())
-app.use(bodyParser.json())
+app.use(express.json())
+
+const thoughtSchema = new mongoose.Schema({
+  message: {
+    type:String,
+    required:true,
+    unique:true
+  },
+  hearts: {
+    type: Number,
+    default: 0
+  },
+  createdAt: {
+    type: Date,
+  default: Date.now
+  }
+})
+
+const Thought = mongoose.model('Thought', thoughtSchema)
 
 // Start defining your routes here
 app.get('/', (req, res) => {
-  res.send('Hello world')
+  res.send('Happy thoughts')
+})
+
+//Creating a new thought based on our model
+app.post('/thoughts', async (req, res) => {
+  try {
+  const newThought = await new Thought(req.body).save()
+  res.json(newThought)
+  } catch (error) {
+    res.status(400).json(error)
+  }
 })
 
 // Start the server
