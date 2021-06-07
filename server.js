8c9c4719296376193535f9c7ae82d5f4a8628a63
Elaine Bergstrom <Elainebergstrom26@gmail.com>
2021-06-08 01:29:57 +0200

    Schema complete for though, validation added, delete added

--- server.js
+++ server.js
@@ -1,10 +1,9 @@
 import express from 'express'
-import bodyParser from 'body-parser'
 import cors from 'cors'
 import mongoose from 'mongoose'
 
 const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/happyThoughts"
-mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true })
 mongoose.Promise = Promise
 
 
@@ -12,9 +11,30 @@ const port = process.env.PORT || 8080
 const app = express()
 
 const thoughtSchema = new mongoose.Schema({
-  message: String,
-  hearts: Number,
-  createdAt: Date
+  message: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+    // enum:["Alfie is best dog", "frasse is cool too"]
+    // match: /^[^0-9]+$/,
+    validate: {
+      validator: (value) => {
+        return /^[^0-9]+$/.test(value);
+      },
+      message: "Numbers are not allowed"
+    },
+    minlength: 5,
+    maxlength: 140
+  },
+  hearts: {
+    type: Number,
+    default: 0 //should have this default value always
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
 });
 
 const Thought = mongoose.model('Thought', thoughtSchema);
@@ -28,12 +48,26 @@ app.get('/', (req, res) => {
 });
 
 app.post('/thoughts', async (req, res) => {
-  const savedThought = await new Thought({
-    message: req.body.message,
-    hearts: 0,
-    createdAt: Date.now()
-  }).save();
-  res.json(savedThought);
+  try {
+    const newThought = await new Thought(req.body).save();
+    res.json(newThought);
+  } catch (error) {
+    if (error.code === 11000) {
+      res.status(400).json({ error: 'Duplicated value', fields: error.keyValue })
+    }
+  res.status(400).json(error);
+  }
+});
+
+app.delete('/thoughts/:id', async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const deletedThought = await Thought.deleteOne({ _id: id });
+    res.json(deletedThought);
+  } catch (error) {
+    res.status(400).json({ message: 'Invalid request', error });
+  }
 });
 
 // Start the server
