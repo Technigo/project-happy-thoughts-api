0a1cf8233c0dfea178a28ed088769f1911c586ad
Elaine Bergstrom <Elainebergstrom26@gmail.com>
2021-06-09 23:23:41 +0200

    Added get thoughts endpoint, post likes endpoitn

--- server.js
+++ server.js
@@ -1,77 +1,130 @@
-import express from 'express'
-import cors from 'cors'
-import mongoose from 'mongoose'
+import express from "express";
+import cors from "cors";
+import mongoose from "mongoose";
 
-const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/happyThoughts"
-mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true })
-mongoose.Promise = Promise
+const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/happyThoughts";
+mongoose.connect(mongoUrl, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+});
+mongoose.Promise = Promise;
 
-
-const port = process.env.PORT || 8080
-const app = express()
+const port = process.env.PORT || 8080;
+const app = express();
 
 const thoughtSchema = new mongoose.Schema({
   message: {
     type: String,
-    required: true,
+    required: [true, "Message Required"],
     unique: true,
-    trim: true,
-    // enum:["Alfie is best dog", "frasse is cool too"]
-    // match: /^[^0-9]+$/,
-    validate: {
-      validator: (value) => {
-        return /^[^0-9]+$/.test(value);
-      },
-      message: "Numbers are not allowed"
-    },
-    minlength: 5,
-    maxlength: 140
+    trim: [true],
+    minlength: [5, "Must be a minimum of 5 characters."],
+    maxlength: [140, "Must have a maximum of 140 characters!"],
   },
   hearts: {
     type: Number,
-    default: 0 //should have this default value always
+    default: 0, //should have this default value always
   },
   createdAt: {
     type: Date,
-    default: Date.now
-  }
+    default: Date.now,
+  },
 });
 
-const Thought = mongoose.model('Thought', thoughtSchema);
+const Thought = mongoose.model("Thought", thoughtSchema);
 
-app.use(cors())
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
 
 // Routes
-app.get('/', (req, res) => {
-  res.send('Hello world')
+app.get("/", (req, res) => {
+  res.send("Hello world");
 });
 
-app.post('/thoughts', async (req, res) => {
-  try {
-    const newThought = await new Thought(req.body).save();
+app.get("/thoughts", async (req, res)  => {
+    const allThoughts= await Thought.find().sort({ createdAt: -1 });
+    res.json(allThoughts);
+});
+
+app.post("/thoughts", async (req, res) => {
+  try { 
+    const newThought = await new Thought(req.body).save().limit(20)
     res.json(newThought);
   } catch (error) {
     if (error.code === 11000) {
-      res.status(400).json({ error: 'Duplicated value', fields: error.keyValue })
+      res
+        .status(400)
+        .json({ error: "Duplicated value", fields: error.keyValue });
     }
-  res.status(400).json(error);
+    res.status(400).json(error);
   }
 });
 
-app.delete('/thoughts/:id', async (req, res) => {
+app.post("/thoughts/:id/likes", async (req, res) => {
   const { id } = req.params;
 
   try {
-    const deletedThought = await Thought.deleteOne({ _id: id });
-    res.json(deletedThought);
+    const updatedThought = await Thought.findByIdAndUpdate(
+      {
+        _id: id,
+      },
+      {
+        $inc: {
+          hearts: 1,
+        },
+      },
+      {
+        new: true,
+      }
+    );
+    if (updatedThought) {
+      res.json(updatedThought);
+    } else {
+      res.status(404).json({ message: "Not found" });
+    }
   } catch (error) {
-    res.status(400).json({ message: 'Invalid request', error });
+    res.status(400).json({ message: "Invalid request", error });
   }
 });
+
+app.delete("/thoughts/:id", async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    //v1 - only delte
+    // const deletedThought = await Thought.deleteOne({ _id: id });
+    // res.json(deletedThought);
+
+    //v2 - find and delete
+    const deletedThought = await Thought.findOneAndDelete({ _id: id });
+    if (deletedThought) {
+      res.json(deletedThought);
+    } else {
+      res.status(404).json({ message: "Not found" });
+    }
+  } catch (error) {
+    res.status(400).json({ message: "Invalid request", error });
+  }
+});
+
+// app.patch('/thoughts/:id', async (req, res) => {
+//   const { id } = req.params;
+
+//   try{
+//     const  updatedThought = await Thought.findByIdAndUpdate(id, { message: req.body.message }, { new: true });
+//     if (updatedThought){
+//       res.json(updatedThought);
+//     } else {
+//       res.status(404).json({ message: 'Not found' });
+//     }
+//   } catch (error) {
+//     res.status(400).json({ message: 'Invalid request', error });
+//   }
+// });
 
 // Start the server
 app.listen(port, () => {
   // eslint-disable-next-line
-  console.log(`Server running on http://localhost:${port}`)
-})
+  console.log(`Server running on http://localhost:${port}`);
+});
