7d9270ced40c6d3fad866334ff89756c96b0bda3
Nazanin Adampira <Nazi.pira88@gmail.com>
2021-06-23 19:44:28 +0200

    cors fix

--- server.js
+++ server.js
@@ -31,7 +31,7 @@ app.use(cors({ origin: true }));
 app.use(bodyParser.json());
 
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Headers", "*");
   res.header("Access-Control-Allow-Credentials", true);
   next();
