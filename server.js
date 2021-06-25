eaec73263210f15c71797d58cf9c1fb8f72bd4e1
matilda svensson <info@matildasvensson.se>
2021-06-26 00:20:31 +0200

    trying to fix

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 import express from 'express'
-// import bodyParser from 'body-parser'
+import bodyParser from 'body-parser'
 import cors from 'cors'
 import mongoose from 'mongoose'
 
@@ -49,15 +49,26 @@ app.get('/thoughts', async (req, res) => {
 
 // Endpoint to post thoughts
 app.post('/thoughts', async (req, res) => {
-  const { message } = req.body
-  const thought = new Thought({ message })
-  try {
-    const postedThought = await thought.save()
-    res.status(400).json(postedThought)
+  // const { message } = req.body
+  // const thought = new Thought({ message })
+  // try {
+  //   const postedThought = await thought.save()
+  //   res.json(postedThought)
+  // } catch (error) {
+  //   res.status(400).json({ message: 'Could not post thought', error: err.errors })
+  // }
+  try { 
+    const newThought = await new Thought(req.body).save().limit(20)
+    res.json(newThought);
   } catch (error) {
-  res.status(400).json({ message: 'Could not post thought', error: err.errors })
+    if (error.code === 11000) {
+      res
+        .status(400)
+        .json({ error: "Duplicated value", fields: error.keyValue });
+    }
+    res.status(400).json(error);
   }
-})
+});
 
 // endpoint to delete a thought
 app.delete('/thoughts/:id', async (req, res) => {
