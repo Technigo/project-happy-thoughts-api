628008a81ac3b01edc252ae25f18b1d671e03d5f
Darya <darya.lapata@gmail.com>
2022-01-01 19:44:28 +0100

    set up

--- README.md
+++ README.md
@@ -10,4 +10,12 @@ Describe how you approached to problem, and what tools and techniques you used t
 
 ## View it live
 
-Every project should be deployed somewhere. Be sure to include the link to the deployed project so that the viewer can click around and see what it's all about.
+## **🔵 Blue Level (Minimum Requirements)**
+
+- Your API should implement the routes exactly as documented above.
+- Your `GET /thoughts` endpoint should only return 20 results, ordered by `createdAt` in descending order.
+- Your API should validate user input and return appropriate errors if the input is invalid.
+- In the `POST /thoughts` endpoint to create a new thought, if the input was invalid and the API is returning errors, it should set the response status to `400` (bad request).
+- The endpoint to add hearts to a thought should return an appropriate error if the thought was not found.
+
+
--- server.js
+++ server.js
@@ -6,6 +6,22 @@ const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/happyThoughts"
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
 mongoose.Promise = Promise
 
+const Thought = mongoose.model('Thought', {
+  message: {
+    type: String,
+    required: true,
+    minlength: 5,
+    maxlenght: 140
+  },
+  hearts: {
+    type: Number,
+    default: 0
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
+})
 // Defines the port the app will run on. Defaults to 8080, but can be 
 // overridden when starting the server. For example:
 //
@@ -20,7 +36,26 @@ app.use(express.json())
 // Start defining your routes here
 app.get('/', (req, res) => {
   res.send('Hello world')
-})
+});
+
+app.get('/thoughts', async (req, res) => {
+  const thoughts = await Thought.find().sort({ createdAt: 'desc' }).limit(20).exec();
+  res.json(thoughts)
+});
+
+app.post('/thoughts', async (req, res) => {
+  // Retrieve the information sent by the client to our API endpoint
+  const { message } = req.body;
+  // use mongoose model to create the database entry
+  const thought = new Thought({ message });
+  try {
+    // Success case
+    const savedThought = await thought.save();
+    res.status(201).json(savedThought)
+  } catch (err) {
+    res.status(404).json({ message: "Could not save thought to the database", error: err.errors });
+  }
+});
 
 // Start the server
 app.listen(port, () => {
