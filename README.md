faa289d9d9122020a023e22682fd16798946d945
Priscila Alfaro <priscila24n@hotmail.com>
2022-01-03 14:48:17 +0100

    add 3 endpoints with validation

--- README.md
+++ README.md
@@ -1,13 +1,22 @@
+# Week 19 - Technigo Bootcamp
+
 # Project Happy Thoughts API
+This week's project is to use your new skills with Express and Mongodb to build an API which includes both GET request endpoints to return data and POST endpoints to create data.
+In order to replace the API we built, you're going to need to build a `Thought` Mongoose model which has properties for the `message` string, a `heart` property for tracking the number of likes, and a `createdAt` property to store when the thought was added.
 
-Replace this readme with your own information about your project.
+Then, you'll need to add 3 endpoints:
 
-Start by briefly describing the assignment in a sentence or two. Keep it short and to the point.
+### `GET /thoughts`
+This endpoint should return a maximum of 20 thoughts, sorted by `createdAt` to show the most recent thoughts first.
+
+### `POST /thoughts`
+This endpoint expects a JSON body with the thought `message`, like this: `{ "message": "Express is great!" }`. If the input is valid (more on that below), the thought should be saved, and the response should include the saved thought object, including its `_id`.
+
+### `POST thoughts/:thoughtId/like`
+This endpoint doesn't require a JSON body. Given a valid thought id in the URL, the API should find that thought, and update its `hearts` property to add one heart.
 
 ## The problem
-
-Describe how you approached to problem, and what tools and techniques you used to solve it. How did you plan? What technologies did you use? If you had more time, what would be next?
+To develop this week approach I followed the videos about how to use sort and limit operations from database. I applied mongoose validation to some parts of the schema and also default value for date.
 
 ## View it live
-
-Every project should be deployed somewhere. Be sure to include the link to the deployed project so that the viewer can click around and see what it's all about.
+See: 
--- server.js
+++ server.js
@@ -1,28 +1,87 @@
 import express from 'express'
 import cors from 'cors'
 import mongoose from 'mongoose'
+import dotenv from 'dotenv'
 
-const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/happyThoughts"
-mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
+dotenv.config();
+
+const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/happyThoughts" // name of the database
+
+mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true }).then(() => {
+  // eslint-disable-next-line no-console
+  console.log(`database connected`)
+}, (onreject) => {
+  // eslint-disable-next-line no-console
+  console.log(onreject);
+})
 mongoose.Promise = Promise
 
-// Defines the port the app will run on. Defaults to 8080, but can be 
-// overridden when starting the server. For example:
-//
-//   PORT=9000 npm start
-const port = process.env.PORT || 8080
+const port = process.env.PORT || 5000
 const app = express()
 
 // Add middlewares to enable cors and json body parsing
 app.use(cors())
 app.use(express.json())
 
-// Start defining your routes here
-app.get('/', (req, res) => {
-  res.send('Hello world')
+const Thougth = mongoose.model("Thougth", {
+  message: {
+    type: String,
+    required: true,
+    minlength: 5,
+    maxlength: 140
+  },
+  heart: {
+    type: Number,
+    default: 0,
+    max: [1, 'Must be just 1 per time, got {VALUE}']
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
 })
+// Start defining your routes here
+app.get('/thoughts', async (req, res) => {
+  try {
+    const thought = await Thougth.find().sort({ createdAt: 'desc' }).limit(20)
+    res.status(200).json(thought);
+  } catch (e) {
+    res.status(400).json({ message: "could not get thought", errors: e.errors })
+  }
+});
 
-// Start the server
+app.post('/thoughts', async (req, res) => {
+  try {
+    const thought = await new Thougth(req.body).save();
+    res.status(200).json(thought);
+  } catch (e) {
+    res.status(400).json({ message: "could not save thought", errors: e.errors })
+  }
+});
+
+app.post('/thoughts/:thoughtId/like', async (req, res) => {
+  const { thoughtId } = req.params;
+  try {
+    const thought = await Thougth.findById(thoughtId)
+    if (thought === null || !thought) {
+      res.status(404).json({ message: 'Thought does not exist', success: "false" });
+    } else {
+      const heartUpdated = thought.heart + 1;
+      const thoughtUpdated = await Thougth.updateOne({ _id: thoughtId },
+        { $set: {
+          heart: heartUpdated
+        } })
+      if (thoughtUpdated.nModified > 0) {
+        res.status(200).json({ message: thought.message, heart: heartUpdated, success: "true" });
+      } else {
+        res.status(404).json({ message: 'nop updated', success: "false" });
+      }
+    }
+  } catch (e) {
+    res.status(400).json({ message: "could not save like", errors: e.errors })
+  }
+});
+
 app.listen(port, () => {
   // eslint-disable-next-line
   console.log(`Server running on http://localhost:${port}`)
