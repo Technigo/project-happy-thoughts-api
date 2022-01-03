43a643d8d4e60bb33a6053bdfdb25c97974ade34
HedvigM <hedvig@mejstedt.se>
2022-01-03 18:44:03 +0100

    coding along with monday lesson

--- server.js
+++ server.js
@@ -1,29 +1,59 @@
-import express from 'express'
-import cors from 'cors'
-import mongoose from 'mongoose'
+import express from 'express';
+import cors from 'cors';
+import mongoose from 'mongoose';
 
-const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/happyThoughts"
-mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
-mongoose.Promise = Promise
+const mongoUrl = process.env.MONGO_URL || 'mongodb://localhost/happyThoughts';
+mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.Promise = Promise;
 
-// Defines the port the app will run on. Defaults to 8080, but can be 
-// overridden when starting the server. For example:
-//
-//   PORT=9000 npm start
-const port = process.env.PORT || 8080
-const app = express()
+const port = process.env.PORT || 8080;
+const app = express();
+
+const Member = mongoose.model('Member', {
+  name: {
+    type: String,
+    required: false,
+    unique: false,
+    /* enum: ['Jennie', 'Matilda', 'Karin', 'Maksymilian'], */
+  },
+  description: {
+    type: String,
+    minlength: 5,
+    maxlength: 10,
+    trim: true,
+  },
+  score: {
+    type: Number,
+    default: 0,
+  },
+  createdAt: {
+    type: Date,
+    default: () => Date.now(),
+  },
+});
 
 // Add middlewares to enable cors and json body parsing
-app.use(cors())
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
 
 // Start defining your routes here
 app.get('/', (req, res) => {
-  res.send('Hello world')
-})
+  res.send('Hello world');
+});
+
+app.post('/members', async (req, res) => {
+  const { name, description } = req.body;
+  try {
+    // den enda anledningen till att man skulle vilja spara den nya medlemmen till en variabel är om man vill sända vidare infon till the frontend. kanske som en konfirmation.
+    const newMember = await new Member({ name, description }).save();
+    res.status(201).json({ response: newMember, success: true });
+  } catch (error) {
+    res.status(400).json({ response: error, success: false });
+  }
+});
 
 // Start the server
 app.listen(port, () => {
   // eslint-disable-next-line
-  console.log(`Server running on http://localhost:${port}`)
-})
+  console.log(`Server running on http://localhost:${port}`);
+});
