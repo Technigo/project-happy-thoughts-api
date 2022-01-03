b6687f118bfe809932c6cc8054cedacfda262bcc
Sabrina Zancotti <zancotti@gmail.com>
2022-01-03 15:42:23 +0100

    Changed into a schema and added enum for the category

--- server.js
+++ server.js
@@ -1,21 +1,23 @@
 import express from "express";
 import cors from "cors";
-import mongoose from "mongoose";
+import mongoose, { Schema } from "mongoose";
 
 const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/happyThoughts";
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true });
 mongoose.Promise = Promise;
 
-const category = {
-  foodThoughts: "Food thoughts",
-  homeThoughts: "Home thoughts",
-  workThoughts: "Work thoughts",
-};
+//   PORT=9000 npm start
+const port = process.env.PORT || 8080;
+const app = express();
 
-const Thought = mongoose.model("Thought", {
+// v1 Schema method:
+const ThoughtSchema = new mongoose.Schema({
   message: { type: String, required: true, minlength: 5, maxlength: 140 },
   hearts: { type: Number, default: 0 },
-  category: String,
+  category: {
+    type: String,
+    enum: ["Food thoughts", "Home thoughts", "Work thoughts"],
+  },
   name: String,
   createdAt: {
     type: Date,
@@ -23,12 +25,23 @@ const Thought = mongoose.model("Thought", {
   },
 });
 
+const Thought = mongoose.model("Thought", ThoughtSchema);
+
+// v2 Moongose method:
+// const Thought = mongoose.model("Thought", {
+//   message: { type: String, required: true, minlength: 5, maxlength: 140 },
+//   hearts: { type: Number, default: 0 },
+//   category: String,
+//   name: String,
+//   createdAt: {
+//     type: Date,
+//     default: () => new Date(),
+//   },
+// });
+
 // Defines the port the app will run on. Defaults to 8080, but can be
 // overridden when starting the server. For example:
 //
-//   PORT=9000 npm start
-const port = process.env.PORT || 8080;
-const app = express();
 
 // Add middlewares to enable cors and json body parsing
 app.use(cors());
@@ -46,7 +59,8 @@ app.post("/thoughts", async (req, res) => {
     res.json(thought);
   } catch (error) {
     res.status(400).json({
-      response: "The text needs to be betweeen 5-140 characters.",
+      response:
+        "The text needs to be betweeen 5-140 characters or you have used an invalid category.",
       success: false,
     });
   }
@@ -54,9 +68,8 @@ app.post("/thoughts", async (req, res) => {
 
 app.get("/thoughts", async (req, res) => {
   let queryCategory = req.query.category;
-  let cat = category[queryCategory];
 
-  if (!cat) {
+  if (!queryCategory) {
     const thoughts = await Thought.find().sort({ createdAt: -1 });
     res.json(thoughts.slice(0, 20));
   } else {
