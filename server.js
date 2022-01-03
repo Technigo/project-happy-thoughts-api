ba6c9ed2c1e5b8d1f53f1e60619499144e252b0c
Jessica Nygren <jessicanygren@C02D54BJMD6R.lan>
2022-01-03 15:40:26 +0100

    model, schema and endpoints for get thought, post thought and increase likes

--- server.js
+++ server.js
@@ -6,11 +6,33 @@ const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/happyThoughts"
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
 mongoose.Promise = Promise
 
+const ThoughtSchema = new mongoose.Schema({
+  text: {
+    type: String,
+    required: true,
+    minLenght: 5,
+    maxlength: 140,
+    trim: true
+  },
+  createdAt: {
+    type: Number,
+    default: () => Date.now()
+  },
+  likes: {
+    type: Number,
+    default: 0
+  }
+})
+
+const Thought = mongoose.model('Thought', ThoughtSchema) 
+
+
+
 // Defines the port the app will run on. Defaults to 8080, but can be 
 // overridden when starting the server. For example:
 //
 //   PORT=9000 npm start
-const port = process.env.PORT || 8080
+const port = process.env.PORT || 9000
 const app = express()
 
 // Add middlewares to enable cors and json body parsing
@@ -20,6 +42,42 @@ app.use(express.json())
 // Start defining your routes here
 app.get('/', (req, res) => {
   res.send('Hello world')
+})
+
+app.get('/thoughts', async (req, res) => {
+  const thoughts = await Thought.find().sort({createdAt: 'desc'}).limit(20).exec()
+  res.json(thoughts)
+})
+
+app.post('/thoughts', async (req, res) => {
+  const { text } = req.body
+
+  const thought = new Thought({text})
+
+  try {
+    const newThought = await thought.save()
+    res.status(201).json({ response: newThought, sucess: true })
+  } catch (err) {
+     res.status(400).json({ message: 'Could not save thought to the Database', success: false })
+  }
+})
+
+app.post('/thoughts/:id/likes', async (req, res) => {
+  const { id } = req.params
+
+  try {
+    const thought = await Thought.findByIdAndUpdate(
+      id,
+      { $inc: 
+        { likes: 1 } },
+      { 
+        new: true 
+      }
+    )
+    res.status(200).json(thought)
+  } catch (err) {
+    res.status(400).json({ message: 'Could not find thought', errors: err.errors })
+  }
 })
 
 // Start the server
