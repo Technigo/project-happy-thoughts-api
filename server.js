51d78435b65f3ef776db000cf3625f877552409f
fatigr26 <fatigr2692@gmail.com>
2022-01-04 10:33:28 +0100

    added mongoose querie

--- server.js
+++ server.js
@@ -1,15 +1,15 @@
-import express from 'express'
-import cors from 'cors'
-import mongoose from 'mongoose'
+import express from "express";
+import cors from "cors";
+import mongoose from "mongoose";
 
-// mongoURL is the address of my database in my local machine 
-const mongoUrl = process.env.MONGO_URL || "mongodb://127.0.0.1/happyThoughts"
-mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
-mongoose.Promise = Promise
+// mongoURL is the address of my database in my local machine
+const mongoUrl = process.env.MONGO_URL || "mongodb://127.0.0.1/happyThoughts";
+mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.Promise = Promise;
 
 // Defines the port the app will run on. Defaults to 8080, but can be overridden when starting the server. For example: PORT=9000 npm start
-const port = process.env.PORT || 8080
-const app = express()
+const port = process.env.PORT || 8080;
+const app = express();
 
 // Schema: part of the Model
 const MemberSchema = new mongoose.Schema({
@@ -17,7 +17,7 @@ const MemberSchema = new mongoose.Schema({
     type: String,
     required: true, //it requires a new memeber name, otherwise, mongo throws an error
     unique: true, // mongo double checks if that name already exists or not
-    enum: ['Jennie', 'Matilda' , 'Karin', 'Maksymilian'],
+    enum: ["Jennie", "Matilda", "Karin", "Maksymilian"],
   },
   description: {
     type: String,
@@ -33,33 +33,30 @@ const MemberSchema = new mongoose.Schema({
     type: Number,
     default: () => Date.now(),
   },
-})
+});
 
 // Model
-const Member = mongoose.model('Member', MemberSchema)
-
+const Member = mongoose.model("Member", MemberSchema);
 
 // Add middlewares to enable cors and json body parsing
-app.use(cors())
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
 
 // Start defining your routes here
-app.get('/', (req, res) => {
-  res.send('Hello Technigo! Here we go!')
-})
-
+app.get("/", (req, res) => {
+  res.send("Hello Technigo! Here we go!");
+});
 
 // v1: post request using async await
-app.post('/members', async (req, res) => {
+app.post("/members", async (req, res) => {
   const { name, description } = req.body;
 
-try {
-  const newMember = await new Member({ name, description }).save();
-  res.status(201).json({ response: newMember, success: true });
-
-} catch (error) {
-  res.status(400).json({ response: error, success: false });
-}
+  try {
+    const newMember = await new Member({ name, description }).save();
+    res.status(201).json({ response: newMember, success: true });
+  } catch (error) {
+    res.status(400).json({ response: error, success: false });
+  }
 });
 
 // v2: post request using promises
@@ -77,7 +74,6 @@ try {
 //   })
 // });
 
-
 // v3: mongoose callback
 // app.post('/members', (req, res) => {
 //   const { name, description } = req.body;
@@ -94,16 +90,32 @@ try {
 //   })
 // })
 
+// updating existing data (post or patch)
+app.post("/members/:id/score", async (req, res) => {
+  const { id } = req.params;
+  
+  try {
+    const updatedMember = await Member.findByIdAndUpdate( //mongoose queries: findByIdAndUpdate: it saves internally so we don't need to save
+      id,
+      {
+        $inc: {
+          score: 1,
+        },
+      },
 
-app.patch('/members/:id/score', (req, res) => {
+      {
+        new: true,
+      }
+    );
 
-
-
+    res.status(200).json({ response: updatedMember, success: true });
+  } catch (error) {
+    res.status(400).json({ response: error, success: false });
+  }
 });
-
 
 // Start the server
 app.listen(port, () => {
   // eslint-disable-next-line
-  console.log(`Server running on http://localhost:${port}`)
-})
+  console.log(`Server running on http://localhost:${port}`);
+});
