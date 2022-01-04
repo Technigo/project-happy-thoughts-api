42ab2d7de40c1ce52a14b2ac2854939a326027bc
Mattsson717 <83518778+Mattsson717@users.noreply.github.com>
2022-01-04 14:32:17 +0100

    started codealong with yesterdays lecture

--- server.js
+++ server.js
@@ -1,29 +1,92 @@
-import express from 'express'
-import cors from 'cors'
-import mongoose from 'mongoose'
+import express from "express";
+import cors from "cors";
+import mongoose from "mongoose";
 
-const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/happyThoughts"
-mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
-mongoose.Promise = Promise
+const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/happyThoughts";
+mongoose.connect(mongoUrl, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: true,
+});
+mongoose.Promise = Promise;
 
-// Defines the port the app will run on. Defaults to 8080, but can be 
-// overridden when starting the server. For example:
-//
-//   PORT=9000 npm start
-const port = process.env.PORT || 8080
-const app = express()
+const port = process.env.PORT || 8080;
+const app = express();
+
+// scheema : name. description etc
+// This way, when you are going to reuse your schema
+// Enum = only allowed values
+const ThoughtSchema = new mongoose.Schema({
+  text: {
+    type: String,
+    required: true,
+    minlength: 5,
+    maxlength: 140,
+    trim: true,
+  },
+  like: {
+    type: Number,
+    default: 0,
+  },
+  createdAt: {
+    type: Number,
+    default: Date.now,
+  },
+});
+//Mongoose model with Schema
+const Thought = mongoose.model("Thought", ThoughtSchema);
 
 // Add middlewares to enable cors and json body parsing
-app.use(cors())
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
 
 // Start defining your routes here
-app.get('/', (req, res) => {
-  res.send('Hello world')
-})
+app.get("/", (req, res) => {
+  res.send("Cowabunga");
+});
+
+//Endpoint that return thoughts in descending order
+app.get("/thoughts", async (req, res) => {
+  const thoughts = await Thought.find()
+    .sort({ createdAt: "desc" })
+    .limit(20)
+    .exec();
+  res.json(thoughts);
+});
+
+app.post("/thoughts", async (req, res) => {
+  // Retrieve the information sent by the client to our API endpoint
+  const { text } = req.body;
+
+  try {
+    const newThought = await new Thought({ text }).save();
+    res.status(201).json({ response: savedThought, success: true });
+  } catch (error) {
+    res.status(400).json({ response: error, success: false });
+  }
+});
+
+// Post method for adding likes/hearts
+app.post("/thoughts/:id/likes", async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const updatedThought = await Thought.findByIdAndUpdate(
+      id,
+      {
+        $inc: { likes: 1 },
+      },
+      { new: true }
+    );
+    res.status(200).json({ response: updatedThought, succes: true });
+  } catch (error) {
+    res.status(400).json({ response: error, success: false });
+  }
+});
 
 // Start the server
 app.listen(port, () => {
   // eslint-disable-next-line
-  console.log(`Server running on http://localhost:${port}`)
-})
+  console.log(`Server running on http://localhost:${port}`);
+});
