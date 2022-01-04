fb836ce638d6aa245084a753db03f82d0badbda7
isomoth <ic.gonzalez35@hotmail.com>
2022-01-04 21:43:01 +0100

    added try-catch block to GET thoughts request

--- server.js
+++ server.js
@@ -49,11 +49,15 @@ app.get('/', (req, res) => {
 
 // Sort thoughts by createdAt and return the most recent thoughts first (max. 20 of them)
 app.get('/thoughts', async (req, res) => {
-  const thoughts = await Thought.find()
-    .sort({ createdAt: 'desc' })
-    .limit(20)
-    .exec();
-  res.json(thoughts);
+  try {
+    const thoughts = await Thought.find()
+      .sort({ createdAt: 'desc' })
+      .limit(20)
+      .exec();
+    res.json(thoughts);
+  } catch (error) {
+    res.status(400).json({ response: error, success: false });
+  }
 });
 
 // Retrieve the info sent by the client to our API endpoint
