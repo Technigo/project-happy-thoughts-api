06e22ee8ee0c0132d98a6ebb246b8115d311fef5
Rosanna Dahlberg <wernerrosanna86@gmail.com>
2022-01-05 15:00:49 +0100

    Added code from monday lesson

--- server.js
+++ server.js
@@ -1,29 +1,116 @@
-import express from 'express'
-import cors from 'cors'
-import mongoose from 'mongoose'
+import express from 'express';
+import cors from 'cors';
+import mongoose from 'mongoose';
 
-const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/happyThoughts"
-mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
-mongoose.Promise = Promise
+const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/HappyApi";
+mongoose.connect(mongoUrl, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true
+});
+mongoose.Promise = Promise;
 
-// Defines the port the app will run on. Defaults to 8080, but can be 
+//mongo is a framework for Mongoose
+
+// Defines the port the app will run on. Defaults to 8080, but can be
 // overridden when starting the server. For example:
 //
 //   PORT=9000 npm start
-const port = process.env.PORT || 8080
-const app = express()
+const port = process.env.PORT || 8080;
+const app = express();
 
 // Add middlewares to enable cors and json body parsing
-app.use(cors())
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
+
+
+const thoughtSchema = new mongoose.Schema({//call the function the same as the model (it's a method)
+	//here we specify the types
+  message: {
+    type: String,
+    required: [true, "Message is required!"],
+    minlength: 5,
+    maxlength: 140,
+ 
+  },
+  hearts: {
+    type: Number,
+    default: 0,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+
+const HappyThought = mongoose.model('HappyThought', thoughtSchema);
+
 
 // Start defining your routes here
 app.get('/', (req, res) => {
-  res.send('Hello world')
+  res.send('Welcome to the Happy Thoughts API')
 })
+
+
+//GET: This endpoint should return a maximum of 20 messages, sorted by createdAt to show the most recent messages first. */
+
+app.get("/thoughts", async (req, res) => {
+  const message = await AllUsermessages.find()
+    .sort({ createdAt: "desc" })
+    .limit(20)
+    .exec();
+
+  try {
+    res.json(message);
+  } catch (error) {
+    res.status(400).json({ response: error, success: false });
+  }
+});
+
+
+// v1 - async await
+app.post('/thoughts', async (req, res) => {
+	const { message } = req.body;
+
+	try {
+    //sucessfull request
+		const newHappyThought = await new Member({ message }).save();
+		res.status(201).json({ response: newHappyThought, success: true });
+	} catch (error) {
+    //No good request
+		res.status(400).json({ response: error, success: false });
+	}
+});
+
+
+//endpoint to increase likes
+app.post('/thoughts/:thoughtId/like', async (req, res) => {
+  const { thoughtId } = req.params
+
+	try {
+		const updatedMember = await Member.findByIdAndUpdate(
+			// Argument 1 - id
+			id,
+			// Argument 2 - properties to change
+			{
+				$inc: {
+					score: 1,
+				},
+			},
+			// Argument 3 - options (not mandatory)
+			{
+				new: true,
+			}
+		);
+		res.status(200).json({ response: updatedMember, success: true });
+	} catch (error) {
+		res.status(400).json({ response: error, success: false });
+	}
+});
 
 // Start the server
 app.listen(port, () => {
-  // eslint-disable-next-line
-  console.log(`Server running on http://localhost:${port}`)
-})
+	// eslint-disable-next-line
+	console.log(`Server running on http://localhost:${port}`);
+});
