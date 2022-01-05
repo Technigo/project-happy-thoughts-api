24c1d0c935465b30298ed6c1738c6b923f3193c6
madeleinesvensson <madeleinesvensson13@gmail.com>
2022-01-05 11:52:23 +0100

    twesing for frontend again

--- server.js
+++ server.js
@@ -54,12 +54,11 @@ app.get("/", (req, res) => {
 });
 
 app.get("/thoughts", async (req, res) => {
-  const thoughts = await Thought.find()
+  const thoughts = await Thought.find({})
     .sort({ createdAt: "desc" })
     .limit(20)
     .exec();
-  // res.json(thoughts);
-  res.status(200).json({ response: thoughts, success: true });
+  res.json(thoughts);
 });
 
 app.post("/thoughts", async (req, res) => {
