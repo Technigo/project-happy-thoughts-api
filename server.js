b87038bb4800e7c644b520366603571c74a522fd
Skrosen <76884712+Skrosen@users.noreply.github.com>
2022-01-06 12:38:57 +0100

    added enpoints for get /thoughts, post /thoughts. Created Thought mongoose model

--- server.js
+++ server.js
@@ -1,29 +1,76 @@
-import express from 'express'
-import cors from 'cors'
-import mongoose from 'mongoose'
+import express from "express";
+import cors from "cors";
+import mongoose, { now } from "mongoose";
 
-const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/happyThoughts"
-mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
-mongoose.Promise = Promise
+const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/happyThoughts";
+mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.Promise = Promise;
 
-// Defines the port the app will run on. Defaults to 8080, but can be 
+const Thought = mongoose.model("Thought", {
+	message: {
+		type: String,
+		required: true,
+		minlength: 5,
+		maxlength: 140,
+	},
+	hearts: {
+		type: Number,
+		default: 0,
+	},
+	createdAt: {
+		type: Date,
+		default: Date.now(),
+	},
+});
+// Defines the port the app will run on. Defaults to 8080, but can be
 // overridden when starting the server. For example:
 //
 //   PORT=9000 npm start
-const port = process.env.PORT || 8080
-const app = express()
+const port = process.env.PORT || 8080;
+const app = express();
 
 // Add middlewares to enable cors and json body parsing
-app.use(cors())
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
 
 // Start defining your routes here
-app.get('/', (req, res) => {
-  res.send('Hello world')
-})
+app.get("/", (req, res) => {
+	res.send("Hello world");
+});
+
+app.get("/thoughts", async (req, res) => {
+	const thoughts = await Thought.find()
+		.sort({ createdAt: "desc" })
+		.limit(20)
+		.exec();
+	try {
+		res.status(200).json(thoughts);
+	} catch (err) {
+		res.status(400).json({
+			message: "Not able to execute request",
+			error: err.errors,
+		});
+	}
+});
+
+app.post("/thoughts", async (req, res) => {
+	const { message } = req.body;
+	const thought = await new Thought({ message });
+	try {
+		const savedThought = await thought.save();
+		res.status(201).json(savedThought);
+	} catch (err) {
+		res.status(400).json({
+			message: "Not able to execute request",
+			error: err.errors,
+		});
+	}
+});
+
+app.post("thoughts/:thoughtId/like", (req, res) => {});
 
 // Start the server
 app.listen(port, () => {
-  // eslint-disable-next-line
-  console.log(`Server running on http://localhost:${port}`)
-})
+	// eslint-disable-next-line
+	console.log(`Server running on http://localhost:${port}`);
+});
