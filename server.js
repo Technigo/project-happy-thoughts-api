1170a4c8d99573ba114753f4c03db419dc7b88f9
jakobxlindstrom <jake@lindstroem.se>
2022-01-06 20:36:10 +0100

    try catch

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import cors from 'cors'
 import mongoose from 'mongoose'
+import listEndpoints from 'express-list-endpoints'
 
 const mongoUrl = process.env.MONGO_URL || 'mongodb://localhost/happyThoughts'
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
@@ -27,7 +28,7 @@ const HappyThoughtSchema = new mongoose.Schema({
   },
 })
 
-const Happy = mongoose.model('Happy', HappyThoughtSchema)
+const Thought = mongoose.model('Thought', HappyThoughtSchema)
 
 // Add middlewares to enable cors and json body parsing
 app.use(cors())
@@ -35,22 +36,28 @@ app.use(express.json())
 
 // Start defining your routes here
 app.get('/', (req, res) => {
-  res.send('Hello peeps here are some happy thoughts')
+  res.send(listEndpoints(app))
+  // res.send('Hello peeps here are some happy thoughts')
 })
 
 app.get('/thoughts', async (req, res) => {
-  const thoughts = await Happy.find()
+  const thoughts = await Thought.find()
     .sort({ createdAt: 'desc' })
     .limit(20)
     .exec()
+  try {
+    res.status(200).json({ response: thoughts, success: true })
+  } catch (error) {
+    res.status(400).json({ response: error, success: false })
+  }
   res.json(thoughts)
 })
 
 app.post('/thoughts', async (req, res) => {
-  const { message, tags, name } = req.body
+  const { message } = req.body
   try {
-    const newThought = await new Thought({ message, tags, name }).save()
-    res.status(201).json({ response: newThought, success: true })
+    const newThought = await new Thought({ message }).save()
+    res.status(200).json({ response: newThought, success: true })
   } catch (error) {
     res.status(400).json({ response: error, success: false })
   }
@@ -59,7 +66,7 @@ app.post('/thoughts', async (req, res) => {
 app.post('/thoughts/:thoughtId/like', async (req, res) => {
   const { thoughtId } = req.params
   try {
-    const updatedThought = await Happy.findByIdAndUpdate(
+    const updatedThought = await Thought.findByIdAndUpdate(
       thoughtId,
       {
         $inc: { hearts: 1 },
