9c472988c02c11675e0da0af65972c505fde27ad
Rephili <72921631+Rephili@users.noreply.github.com>
2022-01-06 15:51:29 +0100

    endpoints for the project

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import cors from 'cors'
 import mongoose from 'mongoose'
+import listEndpoints from 'express-list-endpoints';
 
 const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/happyThoughts"
 mongoose.set('useFindAndModify', false);
@@ -15,74 +16,81 @@ mongoose.Promise = Promise
 const port = process.env.PORT || 8080
 const app = express()
 
-const MemberSchema = new mongoose.Schema({
-  name: {
-  type: String,
-  required: true,
-  unique: true, 
-  enum: ['Jennie', 'Matilda', 'Karin', 'Maksymilian'],
+const Thought = mongoose.model('Thought', {
+  message: {
+    type: String,
+    required: true,
+    minlength: 5,
+    maxlength: 140,
+    trim: true
   },
-  description: {
-  type: String,
-  minlength: 5,
-  maxlength: 10,
-  trim: true,
-  },
-  score: { 
-  type: Number,
-  default: 0
+  hearts: {
+    type: Number,
+    default: 0
   },
   createdAt: {
-  type: Number,
-  default: () => Date.now()
+    type: Date,
+    default: () => Date.now()
   }
-})   
-
-const Member = mongoose.model('Member', MemberSchema)
+});
 
 // Add middlewares to enable cors and json body parsing
 app.use(cors())
 app.use(express.json())
 
-// Get request to get members
-app.get('/members', async (req, res) => {
-  const members = await Member.find({}).sort({ createdAt: 1 }).limit(2)
-  res.status(200).json({ response: members, success: true })
+// Start defining your routes here
+app.get('/', (req, res) => {
+  res.send('Go to /endpoints to see all routes')
 })
 
-// Post request for new members
-app.post('/members', async (req, res) => {
-  const { name, description } = req.body;
+// Get endpoints
+app.get('/endpoints', (req, res) => {
+  res.send(listEndpoints(app));
+});
 
-try {
-  const newMember = await new Member({ name, description }).save()
-  res.status(201).json({ response: newMember, success: true })
-} catch (error) {
-res.status(400).json({ response: error, success: false })
-}
+// Get request to get a list of the 20 most recent thoughts
+app.get('/thoughts', async (req, res) => {
+  const thoughts = await Thought.find({}).sort({ createdAt: -1 }).limit(2)
+  res.status(200).json({ response: thoughts, success: true })
 })
 
-// Post request for increasing score
-app.post('/members/:id/score', async (req, res) => {
-  const { id } = req.params
+// Post request for new thoughts
+app.post('/thoughts', async (req, res) => {
+  const { message } = req.body;
 
   try {
-  const updatedMember = await Member.findByIdAndUpdate(
-    id, 
-    {
-     $inc: { 
-       score: 1, 
-      },
-    },
-    {
-      new: true
-    }
-    )
-  res.status(200).json({ response: updatedMember, success: true })
+    const newThought = await new Thought({ message }).save();
+    res.status(201).json({ response: newThought, success: true });
   } catch (error) {
-  res.status(400).json({ response: error, success: false })
+    res.status(400).json({ response: error, success: false });
   }
-})
+});
+
+// Post request for increasing the amount of hearts (likes) by 1
+app.post('/thoughts/:thoughtId/like', async (req, res) => {
+  const { thoughtId } = req.params
+
+  try {
+    const updatedThought = await Thought.findByIdAndUpdate(
+      thoughtId,
+      {
+        $inc: {
+          hearts: 1
+        }
+      },
+      {
+        new: true
+      }
+    );
+    if (updatedThought) {
+      res.status(200).json({ response: updatedThought, success: true });
+    } else {
+      res.status(404).json({ response: 'No thought found with this id', success: false });
+    }
+  } catch (error) {
+    res.status(400).json({ response: error, success: false });
+  }
+});
 
 // Start the server
 app.listen(port, () => {
