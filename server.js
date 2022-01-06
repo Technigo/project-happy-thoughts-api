6c30f50eed965d72f69773f134b85ba4df8e7e2b
molbimien <43146046+molbimien@users.noreply.github.com>
2022-01-06 18:06:02 +0100

    updated post endpoint for thought and removed id

--- server.js
+++ server.js
@@ -58,11 +58,11 @@ app.get('/thoughts', async (req, res) => {
 // and the response should include the saved thought object, including its `_id`.
 
 app.post('/thoughts', async (req, res) => {
-  const { thoughtId, message } = req.body
+  const { message } = req.body
 
   try {
-    const newThought = await new Thought({ thoughtId, message }).save()
-    res.status(201).json({ response: newThought, success: true })
+    const thought = await new Thought({ message }).save()
+    res.status(201).json({ response: thought, success: true })
   } catch (error) {
     res.status(400).json({ response: error, success: false })
   }
