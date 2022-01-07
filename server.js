9c59a281c7a697c609a92d30b599ee2da161b614
Linnea <linnea.iseb@gmail.com>
2022-01-07 10:22:33 +0100

    Fixing

--- server.js
+++ server.js
@@ -50,10 +50,10 @@ app.get('/thoughts', async (req, res) => {
 });
 
 app.post('/thoughts', async (req, res) => {
-  const { thought } = req.body;
+  const { message } = req.body;
 
   try {
-    const newThought = await new Thought({ thought }).save();
+    const newThought = await new Thought({ message }).save();
     res.status(201).json({ response: newThought, success: true });
   } catch (error) {
     res.status(400).json({ response: error, success: false });
