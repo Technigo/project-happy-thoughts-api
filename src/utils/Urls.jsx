02b05bee6e84c534da483793198f11c014ac4603
Skrosen <76884712+Skrosen@users.noreply.github.com>
2022-01-07 09:35:03 +0100

    erased frontend because i realised i have that on another repository that i can update easily

--- src/utils/Urls.jsx
+++
@@ -1,4 +0,0 @@
-export const API_URL =
-	"https://happy-thoughts-api-technigo.herokuapp.com/thoughts";
-export const LIKES_URL = (messageId) =>
-	`https://happy-thoughts-api-technigo.herokuapp.com/thoughts/${messageId}/like`;
