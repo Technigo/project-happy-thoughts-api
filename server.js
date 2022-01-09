b544f4d50d9a9d33332d1df61c4e1bab5e7dd83e
Jessica Nygren <jessicanygren@C02D54BJMD6R.lan>
2022-01-09 19:01:10 +0100

    list endpoints

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import cors from 'cors'
 import mongoose from 'mongoose'
+import listEndpoints from 'express-list-endpoints'
 
 const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/happyThoughts"
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
@@ -39,9 +40,9 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 
-// Start defining your routes here
+// List all endpoints
 app.get('/', (req, res) => {
-  res.send('Hello world')
+  res.send(listEndpoints(app))
 })
 
 app.get('/thoughts', async (req, res) => {
