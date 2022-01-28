2b1bbd4bd59767ba6a010c283159df9f38a5aabc
JessicaNordahl <jessica.nordahl@hotmail.com>
2022-01-28 14:09:03 +0100

    created endpoint with limitation, and delete. added comments

--- server.js
+++ server.js
@@ -6,14 +6,10 @@ const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/happyThoughts"
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
 mongoose.Promise = Promise
 
-// Defines the port the app will run on. Defaults to 8080, but can be 
-// overridden when starting the server. For example:
-//
-//   PORT=9000 npm start
 const port = process.env.PORT || 8080
 const app = express()
 
-//Writing the schema separately, to be able to not repeat it. It can be combined like this
+//Writing the schema for thoughts in happy-thoughts
 const ThoughtSchema = new mongoose.Schema({      
   message: {
     type: String,
@@ -33,27 +29,45 @@ const ThoughtSchema = new mongoose.Schema({
   }
 })
 
-const Thought = mongoose.model('Thought', ThoughtSchema)
-
+const Thought = mongoose.model('Thought', ThoughtSchema) //This connects the model to the schema above
 
 // Add middlewares to enable cors and json body parsing
 app.use(cors())
 app.use(express.json())
 
-// Start defining your routes here
+// Start defining routes here
 app.get('/', (req, res) => {
   res.send('Hello happy world! To see the API live, go to: https://project-happy-thoughts-jessica-nordahl.netlify.app/')
 })
 
-//how we handle the body from frontend/request
+// Endpoint to return maximum 20 thoughts
+app.get('/thoughts', async (req, res) => {
+	try {
+		const thoughtsList = await Thought.find({})
+			.sort({ createdAt: 'desc' })
+			.limit(20);
+		res.status(200).json(thoughtsList);    
+  } catch (error) {
+		// If above code is unsuccessful, status code = bad request:
+		res.status(400).json({ 
+      response: error, 
+      success: false });
+	}
+});  
+
+// Endpoint o add a new thought
 app.post('/thoughts', async (req, res) => {
   const { message } = req.body
 
   try {
     const newThought = await new Thought ({ message }).save() //asynchronous code with await
-    res.status(201).json({ response: newThought, success: true }) //status created
+    res.status(201).json({ 
+      response: newThought, 
+      success: true }) //status created
   } catch (error) {
-    res.status(400).json({ response: error, success: false }) 
+    res.status(400).json({ 
+      response: error, 
+      success: false }) 
   }
 })
 
@@ -72,9 +86,13 @@ app.post('/thoughts/:thoughtsId/like', async (req, res)=> {
   { new: true }
   )
 if (updatedLike) {
-  res.status(201).json({ response: success, success: true })
+  res.status(201).json({ 
+    response: success, 
+    success: true })
 } else {
-  res.status(404).json({ response: error, success: false });
+  res.status(404).json({ 
+    response: error, 
+    success: false });
 }
 } catch (error){ 
     res.status(400).json({ 
@@ -83,6 +101,29 @@ if (updatedLike) {
       success: false });
   }
 })
+
+// Endpoint to delete a specific thought/message
+app.delete("/thoughts/:thoughtId/delete", async (req, res) => {
+  const { thoughtId } = req.params;
+
+  try {
+    const deletedThought = await Thought.findByIdAndDelete(thoughtId);
+    if (deletedThought) {
+      res.status(200).json({ 
+        response: deletedThought, 
+        success: true });
+    } else {
+      res.status(404).json({ 
+        response: "Message not found!", 
+        success: false });
+    }
+  } catch (error) {
+    res.status(400).json({ 
+      message: "Bad request", 
+      response: error, 
+      success: false });
+  }
+});
 
 // Start the server
 app.listen(port, () => {
