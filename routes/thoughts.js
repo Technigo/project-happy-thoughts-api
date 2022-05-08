75eca11369c3b83b6bde7602c6fdf1c1adaaef70
Rawisara Saman <rawiisara@gmail.com>
2022-05-08 22:58:53 +0200

    posts methods updated

--- routes/thoughts.js
+++ routes/thoughts.js
@@ -5,9 +5,13 @@ const router = express.Router()
 
 router.get('/', async (req, res) => {
     const thoughts = await Thought.find()
+    .sort({ createdAt: "desc" })
+    .limit(20)
     res.json(thoughts)
  })
 
+
+//I wanted to try out different syntax for promises (I could have used async and await here as well for consistency.)
 router.post('/', (req, res) => {
     const thought = new Thought({
         message: req.body.message,
@@ -16,24 +20,49 @@ router.post('/', (req, res) => {
     .then(data => {
         res.status(200).json(data)
     })
-    .catch(() => {
-        res.json(500).json({message: "There is an unknown issue for posting a thought. Please try again"})
+    .catch(err => {
+        res.json(500).json({message: "There is an unknown issue for posting a thought. Please try again", errors: err.errors})
     })
 })
 
 //https://happy-thoughts-technigo.herokuapp.com/thoughts/THOUGHT_ID/like
 
-// router.post('/:thoughtId/like', async (req, res) => {
-//     const addLike =  await new Thought.findById(req.params.postId)({
-//         heart: req.body.heart,
-//     })
-//     thought.save()
-//     .then(data => {
-//         res.status(200).json(data)
-//     })
-//     .catch(() => {
-//         res.json(500).json({message: "There is an unknown issue for posting a thought. Please try again"})
-//     })
-// })
+router.post('/thoughts/:thoughtId/like', async (req, res) => {
+    try {
+      const likedThought = await Thought.findByIdAndUpdate(
+        { _id: req.params.thoughtId },
+        { $inc: { hearts: 1 } },
+        { new: true }
+      )
+      res.status(200).json(likedThought)
+    } catch (err) {
+      res.status(400).json({ message: 'Could not find thought', errors: err.errors })
+    }
+  })
+
+
+//Update and delete
+
+//   router.delete('/:postId', async (req, res) => {
+//     try {
+//        const removedPost = await Post.remove({_id: req.params.postId})
+//        res.json(removedPost)
+//      } catch (err) {
+//          res.json({message: err})
+//      }
+//  })
+ 
+//  router.patch('/:postId', async (req, res) => {
+//      try {
+//          const updatedPost = await Post.updateOne(
+//              {_id: req.params.postId}, 
+//              {$set: {title: req.body.title}}
+//              )
+//              res.json(updatedPost)
+//      } catch (err) {
+//          res.json({message: err})
+//      }
+//  })
+
 
 module.exports = router
