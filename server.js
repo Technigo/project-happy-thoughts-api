e3b7cfb308cf0aa98cdb93119a213ca120a920b5
Mimmisen <mimmi.fordaluddin@hotmail.com>
2022-05-16 20:37:17 +0200

    did two post req for the messages and likes, some error handeling

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 import express from "express";
 import cors from "cors";
 import mongoose from "mongoose";
+import { type } from "express/lib/response";
 
-const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/project-mongo";
+// import listEndpoints from "express-list-endpoints";
+
+const mongoUrl =
+  process.env.MONGO_URL || "mongodb://localhost/happyThoughts-api";
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true });
 mongoose.Promise = Promise;
 
@@ -12,13 +16,98 @@ mongoose.Promise = Promise;
 const port = process.env.PORT || 8080;
 const app = express();
 
+const HappyThoughtSchema = new mongoose.Schema({
+  message: {
+    type: String,
+    required: true,
+    minlength: 5,
+    maxlength: 140,
+    trim: true,
+  },
+  heart: {
+    type: Number,
+    default: 0,
+  },
+  createdAt: {
+    type: Date,
+    default: () => new Date(),
+    // type: Number,
+    // default: Date.now,
+  },
+});
+
+const HappyThought = mongoose.model("HappyThought", HappyThoughtSchema);
+
 // Add middlewares to enable cors and json body parsing
 app.use(cors());
 app.use(express.json());
+app.use((req, res, next) => {
+  if (mongoose.connection.readyState === 1) {
+    next();
+  } else {
+    res.status(503).json({ error: "Service unavailable" });
+  }
+});
 
 // Start defining your routes here
+// add the get thoughts <-------
 app.get("/", (req, res) => {
   res.send("Hello Technigo!");
+});
+
+app.post("/thoughts", async (req, res) => {
+  const { message } = req.body;
+
+  try {
+    const newThought = await new HappyThought({ message }).save();
+    res.status(201).json({ response: newThought, success: true });
+  } catch (error) {
+    res.status(400).json({ response: error, success: false });
+  }
+
+  // promises version
+  // app.post("/thoughts", (req, res) => {
+  //   const { message } = req.body;
+
+  //   new newThought({message}).save()
+  //     .then(data => {
+  //       res.status(201).json({response: data, success: true})
+  //     })
+  //     .catch(error => {
+  //       res.status(400).json({ response: error, success: false})
+  //     })
+
+  // mongoose callback
+  // app.post("/thoughts", (req, res) => {
+  // const { message } = req.body;
+
+  // new newThought({message}).save((error, data) => {
+  //   if (error) {
+  //     res.status(400).json({ response: error, success: false})
+  //   } else {
+  //     res.status(201).json({response: data, success: true})
+  //   }
+  // })
+});
+
+app.post("/thoughts/:thoughtId/like", async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const updateThought = await HappyThought.findByIdAndUpdate(
+      id,
+      {
+        $inc: { heart: 1 },
+      },
+      {
+        //options, to update directly
+        new: true,
+      }
+    );
+    res.status(200).json({ response: updateThought, success: true });
+  } catch (error) {
+    res.status(400).json({ response: error, success: false });
+  }
 });
 
 // Start the server
