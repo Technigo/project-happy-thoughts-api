ff12324d1ef81c01bdec624409026cc09cee4b3a
Marianne Ardin <55622908+MarianneArdin@users.noreply.github.com>
2022-05-16 16:15:25 +0200

    after codealong

--- README.md
+++ README.md
@@ -1,10 +1,8 @@
 # Project Happy Thoughts API
 
-Replace this readme with your own information about your project.
+This project is a new version of an earlier project: https://github.com/MarianneArdin/project-happy-thoughts but now using MongoDB and Mongoose for the backend.
 
-Start by briefly describing the assignment in a sentence or two. Keep it short and to the point.
-
-## The problem
+## The challenge
 
 Describe how you approached to problem, and what tools and techniques you used to solve it. How did you plan? What technologies did you use? If you had more time, what would be next?
 
--- server.js
+++ server.js
@@ -1,8 +1,10 @@
-import express from "express";
-import cors from "cors";
-import mongoose from "mongoose";
+import express from 'express';
+import cors from 'cors';
+import mongoose from 'mongoose';
+import { MemoryRouter } from 'react-router-dom';
 
-const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/project-mongo";
+const mongoUrl =
+	process.env.MONGO_URL || 'mongodb://localhost/project-mongo-wk19';
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true });
 mongoose.Promise = Promise;
 
@@ -16,12 +18,114 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+//From Daniel live 16/5
+
+const TechnigoMemberSchema = new mongoose.Schema({
+	name: {
+		//Most important
+		type: String,
+		required: true,
+		unique: true,
+		enum: [
+			'Lovisa',
+			'Marianne',
+			'Emma',
+			'Suki',
+			'Anki',
+			'Tiina',
+			'Kristiina',
+			'Dorothea',
+			'Mimmi',
+		],
+	},
+	description: {
+		type: String,
+		minlength: 5,
+		maxlength: 140,
+		//deletes whitespace from beginning and end of string
+		trim: true,
+	},
+	score: {
+		type: Number,
+		default: 0,
+	},
+	createdAt: {
+		type: Number,
+		default: Date.now,
+	},
+});
+
+{
+	/* <Button onClick={handleClick()} />; */
+}
+
+const TechnigoMember = mongoose.model('TechnigoMember', TechnigoMemberSchema);
+
+//POST requests v1 INDUSTRY STANDARD
+app.post('/members', async (req, res) => {
+	const { name, description } = req.body;
+	try {
+		const newMember = await new TechnigoMember({
+			name: name,
+			description: description,
+		}).save();
+		res.status(200).json({ response: newMember, success: true });
+	} catch (error) {
+		res.status(400).json({ response: error, success: false });
+	}
+});
+
+//POST v2 w promises
+// app.post('/members', (req, res) => {
+// 	const { name, description } = req.body;
+// 	console.log(req.body);
+
+// 	new TechnigoMember({ name: name, description: description })
+// 		.save()
+// 		.then((data) => {
+// 			res.status(201).json({ response: data, success: true });
+// 		})
+// 		.catch((error) => {
+// 			res.status(400).json({ response: error, success: false });
+// 		});
+// });
+
+// POST v 3 kommer inte funka utan att kommentera ut V1 2. Mongoose specific
+// app.post('/members', (req, res) => {
+// 	const { name, description } = req.body;
+// 	new TechnigoMember({ name: name, description: description }).save(
+// 		(error, data) => {
+// 			if (error) {
+// 				res.status(400).json({ response: error, success: false });
+// 			} else {
+// 				res.status(201).json({ response: data, success: true });
+// 			}
+// 		}
+// 	);
+// });
+
+// POST -> creating
+// PUT -> replaces
+// PATCH -> changes
+
+app.post('/members/:id/score', async (req, res) => {
+	const { id } = req.params;
+	try {
+		const memberToUpdate = await TechnigoMember.findByIdAndUpdate(id, {
+			$inc: { score: 1 },
+		});
+		res.status(200).json({ response: memberToUpdate, success: true });
+	} catch (error) {
+		res.status(400).json({ response: error, success: false });
+	}
+});
+
 // Start defining your routes here
-app.get("/", (req, res) => {
-  res.send("Hello Technigo!");
+app.get('/', (req, res) => {
+	res.send('Hello Technigo!');
 });
 
 // Start the server
 app.listen(port, () => {
-  console.log(`Server running on http://localhost:${port}`);
+	console.log(`Server running on http://localhost:${port}`);
 });
