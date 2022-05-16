3d78df70d04c94997e435f58f0fc3170ee46e115
Kyparn <93533136+Kyparn@users.noreply.github.com>
2022-05-16 13:18:32 +0200

    C:/Program Files/Git/thoughts

--- server.js
+++ server.js
@@ -1,34 +1,68 @@
 import express from 'express'
 import cors from 'cors'
 import mongoose from 'mongoose'
+import listEndpoints from 'express-list-endpoints'
 
-const mongoUrl = process.env.MONGO_URL || 'mongodb://localhost/project-happy'
+const mongoUrl =
+  process.env.MONGO_URL || 'mongodb://localhost/project-happyhappyThoughts'
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
 mongoose.Promise = Promise
 
-const Note = mongoose.model('Note', {
-  text: String,
-  createdAt: {
-    type: Date,
-    default: () => new Date(),
-  },
-})
-// Defines the port the app will run on. Defaults to 8080, but can be overridden
-// when starting the server. Example command to overwrite PORT env variable value:
-// PORT=9000 npm start
 const port = process.env.PORT || 8080
 const app = express()
 
 // Add middlewares to enable cors and json body parsing
+
 app.use(cors())
 app.use(express.json())
 
+app.use((req, res, next) => {
+  if (mongoose.connection.readyState === 1) {
+    next()
+  } else {
+    res.status(503).json({ error: 'Service not online' })
+  }
+})
+
+const ThoughtSchema = new mongoose.Schema({
+  message: {
+    type: String,
+    minlength: 5,
+    maxlength: 140,
+    required: true,
+  },
+  hearts: {
+    type: Number,
+    default: 0,
+  },
+  createAt: {
+    type: Date,
+    default: () => new Date(),
+  },
+})
+
+const Thought = mongoose.model('Thought', ThoughtSchema)
+
 // Start defining your routes here
 app.get('/', (req, res) => {
-  res.send('Hello Technigo!')
+  res.send(listEndpoints(app))
 })
-app.post('/happy', async (req, res) => {
-const note = New Note (req.body)
+
+app.get('/thoughts', async (req, res) => {
+  const thoughts = await Thought.find()
+    .sort({ createAt: 'desc' })
+    .limit(20)
+    .exec()
+  res.json(thoughts)
+})
+app.post('/thoughts', async (req, res) => {
+  const { message } = req.body
+  try {
+    const newThought = await new Thought({ message }).save()
+    res.status(200).json({ respone: newThought, success: true })
+  } catch (error) {
+    res.status(400).json({ respone: error, success: false })
+  }
 })
 // Start the server
 app.listen(port, () => {
