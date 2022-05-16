359827b7ba7ef470913a3a41729b24f7d2bd77d9
idanaslund <idanaslund@users.noreply.github.com>
2022-05-16 15:50:50 +0200

    happy thoughts server

--- server.js
+++ server.js
@@ -1,40 +1,41 @@
 import express from "express";
 import cors from "cors";
 import mongoose from "mongoose";
+import listEndpoints from "express-list-endpoints"
 
-const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/project-mongo";
+const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/happy-thoughts-api";
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true });
 mongoose.Promise = Promise;
 
-const TechnigoMemberSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true,
-    unique: true,
-    enum:["Karin", "Petra", "Matilda", "Poya", "Daniel"]
-  },
+// const TechnigoMemberSchema = new mongoose.Schema({
+//   name: {
+//     type: String,
+//     required: true,
+//     unique: true,
+//     enum:["Karin", "Petra", "Matilda", "Poya", "Daniel"]
+//   },
 
-  description: {
-    type: String,
-    minlength: 4,
-    maxlength: 30,
-    // Deletes whitespace from beginning and end of a string (but no spaces between words)
-    trim: true
-  }, 
+//   description: {
+//     type: String,
+//     minlength: 4,
+//     maxlength: 30,
+//     // Deletes whitespace from beginning and end of a string (but no spaces between words)
+//     trim: true
+//   }, 
 
-  score: {
-    type: Number,
-    default: 0,
-  },
+//   score: {
+//     type: Number,
+//     default: 0,
+//   },
   
-  createdAt: {
-    type: Date,
-    // Anonymous function for the function to run for every new POST and not only when the application starts
-    default: () => new Date()
-  } 
-})
+//   createdAt: {
+//     type: Date,
+//     // Anonymous function for the function to run for every new POST and not only when the application starts
+//     default: () => new Date()
+//   } 
+// })
 
-const TechnigoMember = mongoose.model('TechnigoMember', TechnigoMemberSchema)
+// const TechnigoMember = mongoose.model('TechnigoMember', TechnigoMemberSchema)
 
 // Codealong with VAN
 // const Task = mongoose.model('Task', {
@@ -53,6 +54,36 @@ const TechnigoMember = mongoose.model('TechnigoMember', TechnigoMemberSchema)
 //   } 
 // })
 
+
+
+
+
+const HappyThoughtsSchema = new mongoose.Schema({
+
+  message: {
+    type: String,
+    minlength: 5,
+    maxlength: 140,
+    required: true,
+    // Deletes whitespace from beginning and end of a string (but no spaces between words)
+    trim: true
+  }, 
+
+  heart: {
+    type: Number,
+    default: 0,
+  },
+  
+  createdAt: {
+    type: Date,
+    // Anonymous function for the function to run for every new POST and not only when the application starts
+    default: () => new Date()
+  } 
+})
+
+const HappyThoughts = mongoose.model('HappyThoughts', HappyThoughtsSchema)
+
+
 // Defines the port the app will run on. Defaults to 8080, but can be overridden
 // when starting the server. Example command to overwrite PORT env variable value:
 // PORT=9000 npm start
@@ -63,34 +94,78 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.use((req, res, next) => {
+  if (mongoose.connection.readyState === 1) {
+    next()
+  } else {
+    res.status(503).json({ error: "Service unavailable" })
+  }
+})
+
 // Start defining your routes here
 app.get("/", (req, res) => {
-  res.send("Hello Technigo!");
+  res.send(listEndpoints(app));
 });
 
-// Async await POST - prefered verion
-app.post('/members', async (req, res) => {
-  const { name, description } = req.body
-  console.log(req.body)
+app.get('/thoughts', async (req,res) => {
+
+  const allThoughts = await HappyThoughts.find({}).sort({createdAt: 'desc'}).limit(20).exec()
+  res.status(200).json(allThoughts)
+  
+})
+
+
+
+app.post('/thoughts', async (req, res) => {
+  const { message } = req.body
+  
   try {
-    const newMember = await new TechnigoMember({name: name, description: description}).save()
+    const newMessage = await new HappyThoughts({message: message}).save()
     // status 201 = created
-    res.status(201).json({response: newMember, success: true})
+    res.status(201).json({response: newMessage, success: true})
   } catch (error) {
     res.status(400).json({response: error, success: false})
   }
 })
 
-// Does not work
-app.post('members/:id/score', async (req, res) => {
+app.post('thoughts/:id/heart', async (req, res) => {
   const { id } = req.params
   try {
-    const memberToUpdate = await TechnigoMember.findByIdAndUpdate(id, {$inc: {score: 1}})
-    res.status(200).json({response: `Member ${memberToUpdate.name} has been updated`, success: true})
+    const heartToUpdate = await HappyThoughts.findByIdAndUpdate(id, {$inc: {score: 1}})
+    res.status(200).json({response: 'You have liked this message', success: true})
   } catch (error) {
     res.status(400).json({response: error, success: false})
   }
 })
+
+
+
+
+
+
+// Async await POST - prefered verion
+// app.post('/members', async (req, res) => {
+//   const { name, description } = req.body
+//   console.log(req.body)
+//   try {
+//     const newMember = await new TechnigoMember({name: name, description: description}).save()
+//     // status 201 = created
+//     res.status(201).json({response: newMember, success: true})
+//   } catch (error) {
+//     res.status(400).json({response: error, success: false})
+//   }
+// })
+
+// Does not work
+// app.post('members/:id/score', async (req, res) => {
+//   const { id } = req.params
+//   try {
+//     const memberToUpdate = await TechnigoMember.findByIdAndUpdate(id, {$inc: {score: 1}})
+//     res.status(200).json({response: `Member ${memberToUpdate.name} has been updated`, success: true})
+//   } catch (error) {
+//     res.status(400).json({response: error, success: false})
+//   }
+// })
 
 // POST with promises
 // app.post('/members',  (req, res) => {
