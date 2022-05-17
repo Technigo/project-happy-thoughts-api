4ce01ef01307f2683678e48b3cc48366967b7b98
Ida Halling <idahalling@Idas-MacBook-Pro.local>
2022-05-17 14:00:21 +0200

    first set up routes POST message, Like

--- server.js
+++ server.js
@@ -2,7 +2,8 @@ import express from "express";
 import cors from "cors";
 import mongoose from "mongoose";
 
-const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/project-mongo";
+const mongoUrl =
+  process.env.MONGO_URL || "mongodb://localhost/project-mongoMongo";
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true });
 mongoose.Promise = Promise;
 
@@ -15,6 +16,56 @@ const app = express();
 // Add middlewares to enable cors and json body parsing
 app.use(cors());
 app.use(express.json());
+
+const HappyThoughtSchema = new mongoose.Schema({
+  message: {
+    type: String,
+    required: true,
+    minlength: 5,
+    maxlength: 140,
+    trim: true,
+  },
+  likes: {
+    type: Number,
+    default: 0,
+  },
+  createdAt: {
+    type: Date,
+    // anonumys function calls date every time a post is created
+    // default: () => new Date(),
+    default: Date.now,
+  },
+});
+
+const HappyThought = mongoose.model("HappyThought", HappyThoughtSchema);
+
+app.post("/message", async (req, res) => {
+  const { message } = req.body;
+
+  try {
+    const newMessage = await new HappyThought({
+      message: message,
+    }).save();
+    res.status(200).json({ response: newMessage, success: true });
+  } catch (error) {
+    res.status(400).json({ response: error, success: false });
+  }
+});
+
+app.post("/message/:id/likes", async (req, res) => {
+  const { id } = req.params;
+  try {
+    const messageToUpdate = await HappyThought.findByIdAndUpdate(id, {
+      $inc: { likes: 1 },
+    });
+    res.status(200).json({
+      respons: `Message '${messageToUpdate.message}' has been updated`,
+      success: true,
+    });
+  } catch (error) {
+    res.status(400).json({ respons: error, success: false });
+  }
+});
 
 // Start defining your routes here
 app.get("/", (req, res) => {
