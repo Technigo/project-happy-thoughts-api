5e5013030ac92697a17dcb6ccf039450cc68884f
Isabell Wästfelt <isabell@iMac-som-tillhor-Isabell.local>
2022-05-17 10:24:17 +0200

    made a mongoosemodel

--- server.js
+++ server.js
@@ -8,15 +8,29 @@ const mongoUrl =
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
 mongoose.Promise = Promise
 
-// Defines the port the app will run on. Defaults to 8080, but can be overridden
-// when starting the server. Example command to overwrite PORT env variable value:
-// PORT=9000 npm start
 const port = process.env.PORT || 8080
 const app = express()
 
-// Add middlewares to enable cors and json body parsing
 app.use(cors())
 app.use(express.json())
+
+const ThoughtCollection = new mongoose.Collection({
+  message: {
+    type: String,
+    required: [true, 'A message is required.'],
+    minLength: [5, 'The message must be at least 5 characters.'],
+    maxLength: [140, 'The message can have a maximum of 140 characters.'],
+    trim: true,
+  },
+  hearts: {
+    type: Number,
+    default: 0,
+  },
+  createdAt: {
+    typ: Date,
+    default: () => new Date(),
+  },
+})
 
 // Start defining your routes here
 app.get('/', (req, res) => {
