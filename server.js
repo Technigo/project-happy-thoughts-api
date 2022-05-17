b87a1946fc7f2698b4e3cde006b4ea7dae4060ff
SofiePellegrini <sofie.pellegrini@outlook.com>
2022-05-17 21:47:56 +0200

    first endpoint

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
-import express from "express";
-import cors from "cors";
-import mongoose from "mongoose";
+import express from 'express';
+import cors from 'cors';
+import mongoose from 'mongoose';
+import allEndpoints from 'express-list-endpoints';
 
-const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/project-mongo";
+const mongoUrl = process.env.MONGO_URL || 'mongodb://localhost/project-mongo';
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true });
 mongoose.Promise = Promise;
 
@@ -16,9 +17,52 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+const ThoughtSchema = new mongoose.Schema({
+  message: {
+    type: String,
+    required: true,
+    minLength: 5,
+    maxLength: 140,
+    trim: true,
+  },
+  hearts: {
+    type: Number,
+  },
+  createdAt: {
+    type: Date,
+    default: () => new Date(),
+  },
+});
+
+const Thought = mongoose.model('Thought', ThoughtSchema);
+
 // Start defining your routes here
-app.get("/", (req, res) => {
-  res.send("Hello Technigo!");
+app.get('/', (req, res) => {
+  res.send({
+    'Hello!': "This is Sofie's Happy Thoughts API.",
+    'See all endpoints here': '/endpoints',
+    'And here you can watch the API live':
+      'https://graceful-lily-1fde84.netlify.app/',
+  });
+});
+
+app.get('/endpoints', (req, res) => {
+  res.send(allEndpoints(app));
+});
+
+app.post('/thoughts', async (req, res) => {
+  const { message } = req.body;
+  try {
+    const newThought = await new Thought({ message: message }).save();
+    res.status(201).json(newThought);
+  } catch (err) {
+    res.status(400).json({
+      success: false,
+      status_code: 400,
+      message: 'Sorry! Could not save your thought.',
+      error: err.errors,
+    });
+  }
 });
 
 // Start the server
