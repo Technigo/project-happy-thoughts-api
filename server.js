559451387d7670943bc68bfa5b2675318f22731d
Amanda Tange <amandatange@hotmail.com>
2022-05-17 08:54:52 +0100

    fixed connection issue ECONNREFUSED by manually starting MongoDB in taskmanager, now the connection doesn't crash

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ import express from "express";
 import cors from "cors";
 import mongoose from "mongoose";
 
-const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/project-mongo";
+const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/project-happy-thoughts-api";
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true });
 mongoose.Promise = Promise;
 
@@ -36,6 +36,17 @@ const HappyThoughtSchema = new mongoose.Schema({
 
 const HappyThought = mongoose.model("HappyThought", HappyThoughtSchema);
 
+/////////////////////
+// Start defining your routes here
+app.get("/", (req, res) => {
+  res.send({
+    Routes: [
+      `GET /thoughts This endpoint should return a maximum of 20 thoughts, sorted by 'createdAt' to show the most recent thoughts first.`,
+      `POST /thoughts This endpoint expects a JSON body with the thought 'message', like this: { "message": "Express is great!" }. If the input is valid (more on that below), the thought should be saved, and the response should include the saved thought object, including its _id`,
+      `POST thoughts/:thoughtId/like This endpoint doesn't require a JSON body. Given a valid thought id in the URL, the API should find that thought, and update its 'hearts' property to add one heart.`
+    ]
+  });
+});
 
 
 app.post("/thoughts", async (req, res) => {
@@ -47,7 +58,7 @@ app.post("/thoughts", async (req, res) => {
     
     res.status(201).json({response: newThought, success: true});
   } catch(error) {
-
+    console.log("fail")
     res.status(400).json({response: error, success: false});
   }
 
@@ -58,15 +69,29 @@ app.post("/thoughts/:id/hearts", async (req, res) => {
   const { id } = req.params;
   try {
     const thoughtToUpdate = await HappyThought.findByIdAndUpdate(id, {$inc: {hearts: 1}});
-    res.status(200).json({response: `Thought ${thoughtToUpdate.message} has been liked`, succes: true});
+    res.status(200).json({response: `Thought '${thoughtToUpdate.message}' has been liked`, succes: true});
   } catch (error) {
     res.status(400).json({response: error, succes: false});
   }
 });
 
-app.get("/thoughts", (req, res) => {
+app.get("/thoughts", async (req, res) => {
   // const { id, name } = req.query;
-  let thoughts = await HappyThought.find();
+  const thoughts = await HappyThought.find();
+  // console.log(thoughts);
+  // res.status(200).json("hej")
+
+  // try {
+  //   const thoughts = await HappyThought.find().sort({ createdAt: "desc" }).limit(20).exec()
+  //   res.status(200).json(thoughts)
+  // } catch (err) {
+  //   res.status(400).json({
+  //     success: false,
+  //     status_code: 400,
+  //     message: "Bad request, could not fetch thoughts.",
+  //     error: err.errors
+  //   })
+  // }
 
   // if (id) {
 	// 	authors = authors.filter(item => item._id.toString() === id);
@@ -78,18 +103,13 @@ app.get("/thoughts", (req, res) => {
   //   )
   // };
 
-  if (thoughts.length > 0) {
+  if (thoughts) {
     res.json(thoughts);
   } else {
     res.status(404).json({ error: "No happy thoughts found" })
   } 
 });
 
-/////////////////////
-// Start defining your routes here
-app.get("/", (req, res) => {
-  res.send("Hello Technigo!");
-});
 
 // Start the server
 app.listen(port, () => {
