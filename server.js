3ef0f83c6f14dde8c3e5dbe2fa59c3acc4c42ffc
idanaslund <idanaslund@users.noreply.github.com>
2022-05-17 17:58:17 +0200

    moved code

--- server.js
+++ server.js
@@ -58,6 +58,27 @@ mongoose.Promise = Promise;
 
 
 
+
+
+// Defines the port the app will run on. Defaults to 8080, but can be overridden
+// when starting the server. Example command to overwrite PORT env variable value:
+// PORT=9000 npm start
+const port = process.env.PORT || 8080;
+const app = express();
+
+// Add middlewares to enable cors and json body parsing
+app.use(cors());
+app.use(express.json());
+
+app.use((req, res, next) => {
+  if (mongoose.connection.readyState === 1) {
+    next()
+  } else {
+    res.status(503).json({ error: "Service unavailable" })
+  }
+})
+
+
 const HappyThoughtsSchema = new mongoose.Schema({
 
   message: {
@@ -83,24 +104,6 @@ const HappyThoughtsSchema = new mongoose.Schema({
 
 const HappyThoughts = mongoose.model('HappyThoughts', HappyThoughtsSchema)
 
-
-// Defines the port the app will run on. Defaults to 8080, but can be overridden
-// when starting the server. Example command to overwrite PORT env variable value:
-// PORT=9000 npm start
-const port = process.env.PORT || 8080;
-const app = express();
-
-// Add middlewares to enable cors and json body parsing
-app.use(cors());
-app.use(express.json());
-
-app.use((req, res, next) => {
-  if (mongoose.connection.readyState === 1) {
-    next()
-  } else {
-    res.status(503).json({ error: "Service unavailable" })
-  }
-})
 
 // Start defining your routes here
 app.get("/", (req, res) => {
