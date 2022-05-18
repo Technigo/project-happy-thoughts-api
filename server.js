2db21f238bb22d0a98226349cce1cd2dbc785141
frida axelsson <axelsson.frida@gmail.com>
2022-05-18 16:28:03 +0200

    added model and started on routes

--- server.js
+++ server.js
@@ -6,6 +6,23 @@ const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/project-mongo";
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true });
 mongoose.Promise = Promise;
 
+const thought = mongoose.model("Thought", {
+  message: {
+    type: String,
+    required: true,
+    minlength: 5,
+    maxlength: 140,
+  },
+  complete: {
+    type: Boolean,
+    default: false,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
 // Defines the port the app will run on. Defaults to 8080, but can be overridden
 // when starting the server. Example command to overwrite PORT env variable value:
 // PORT=9000 npm start
@@ -21,6 +38,26 @@ app.get("/", (req, res) => {
   res.send("Hello Technigo!");
 });
 
+app.get("/thought", async (req, res) => {
+  const thoughts = await find().sort({ createdAt: "desc" }).limit(20).exec();
+  res.json(thoughts);
+});
+
+app.post("/thought", async (res, req) => {
+  const { message, complete } = req.body;
+  const thought = new thought({ message, complete });
+  try {
+    const savedThought = await thought.save();
+    res.status(200).json(savedThought);
+  } catch (err) {
+    res
+      .status(400)
+      .json({
+        message: "Could not save task to the Database",
+        error: err.errors,
+      });
+  }
+});
 // Start the server
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
