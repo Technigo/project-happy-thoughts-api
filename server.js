5f1d3b505871001c83036e78e46eee380b533ee8
Suki Nhung Phan <nhungphan@Joel-Lov.local>
2022-05-18 16:49:57 +0300

    initial setup

--- server.js
+++ server.js
@@ -16,12 +16,45 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+// Data model
+const Thought = mongoose.model('Thought', {
+  hearts: Number,
+  message: String,
+  createdAt: {
+    type: Date,
+    default: () => new Date()
+  }
+})
+
 // Start defining your routes here
 app.get("/", (req, res) => {
   res.send("Hello Technigo!");
 });
 
+// POST new thought
+app.post('/thought', async (req, res) => {
+ 
+    const newThought = new Thought({ message: req.body.message, hearts: 0 });
+    await newThought.save();
+ 
+    res.status(200).json(newThought)
+
+})
+
+// GET all thoughts
+app.get("/thoughts", async (req,res) => {
+    const thoughts = await Thought.find();
+   res.status(200).json(thoughts)
+
+})
+
 // Start the server
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
 });
+
+
+//Get / thoughts: return at least 20 thoughts, sort by createdAt
+//Post / thoughts: json body ({ "message": "Express is great!" })
+// the thought should be saved, and the response should include the saved thought object, including its _id.
+// Update thought's likes: POST thoughts/:thoughtId/like
