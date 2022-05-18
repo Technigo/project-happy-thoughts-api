9b2c2773e2690998e0568c0a2078409a116e337c
Lisa Bergström <lisa.bergstrom@live.se>
2022-05-18 14:29:37 +0200

    added code

--- server.js
+++ server.js
@@ -9,16 +9,80 @@ mongoose.Promise = Promise;
 // Defines the port the app will run on. Defaults to 8080, but can be overridden
 // when starting the server. Example command to overwrite PORT env variable value:
 // PORT=9000 npm start
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 8090;
 const app = express();
 
 // Add middlewares to enable cors and json body parsing
 app.use(cors());
 app.use(express.json());
 
+const HappyThoughtsSchema = new mongoose.Schema({
+  message: {
+    type: String,
+    required: true,
+    minlength: 5,
+    maxlength: 140,
+    trim: true,
+  },
+  hearts: {
+    type: Number,
+    default: 0,
+  },
+  createAt: {
+    type: Date,
+    default: () => new Date(),
+  },
+});
+
+const HappyThoughts = mongoose.model("HappyThoughts", HappyThoughtsSchema);
+
 // Start defining your routes here
 app.get("/", (req, res) => {
   res.send("Hello Technigo!");
+});
+
+app.get("/thoughts", async (req, res) => {
+  try {
+    const thoughts = await HappyThoughts.find().sort({ createdAt: "desc" });
+    res.status(200).json(thoughts);
+  } catch (err) {
+    res.status(400).json({
+      message: "Could not get thoughts",
+      error: err.errors,
+      success: false,
+    });
+  }
+});
+
+app.post("/thoughts", async (req, res) => {
+  const { message } = req.body;
+
+  try {
+    const newHappyThought = await new HappyThoughts({ message }).save();
+    res.status(200).json(newHappyThought);
+  } catch (err) {
+    res.status(400).json({
+      message: "Could not save the Happy Thoughts",
+      error: err.errors,
+      success: false,
+    });
+  }
+});
+app.post("thoughts/:thougtsID/like", async (req, res) => {
+  const { thoughtID } = req.params;
+
+  try {
+    const likedThought = await HappyThoughts.findByIdAndUpdate(thoughtID, {
+      $inc: { hearts: 1 },
+    });
+    res.status(200).json(likedThought);
+  } catch (err) {
+    res.status(400).json({
+      message: "Could not find and update this post",
+      error: err.errors,
+      success: false,
+    });
+  }
 });
 
 // Start the server
