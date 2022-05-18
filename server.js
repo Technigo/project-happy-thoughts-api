8ea37732c70dcd522f28153d6965504536da8786
Jessica Sandler <jessica.sandler@hotmail.com>
2022-05-18 18:48:07 +0200

    Add schema, model and routes

--- server.js
+++ server.js
@@ -1,27 +1,123 @@
 import express from "express";
 import cors from "cors";
 import mongoose from "mongoose";
+import allEndpoints from "express-list-endpoints";
+import dotenv from 'dotenv';
 
-const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/project-mongo";
+dotenv.config()
+
+const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/project-happy-thoughts";
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true });
 mongoose.Promise = Promise;
 
-// Defines the port the app will run on. Defaults to 8080, but can be overridden
-// when starting the server. Example command to overwrite PORT env variable value:
-// PORT=9000 npm start
+const { Schema } = mongoose;
+
+const thoughtSchema = new Schema({
+  message: {
+    type: String,
+    required: true,
+    minlength: 5,
+    maxlength: 140,
+    trim: true
+  },
+  hearts: {
+    type: Number,
+    default: 0
+  },
+  createdAt: {
+    type: Date,
+    default: () => new Date()
+  }
+})
+
+const Thought = mongoose.model('Thought', thoughtSchema);
+
+new Thought({ message: "Ullabella" }).save();
+
 const port = process.env.PORT || 8080;
 const app = express();
 
-// Add middlewares to enable cors and json body parsing
 app.use(cors());
 app.use(express.json());
 
-// Start defining your routes here
+app.use((req, res, next) => {
+  if (mongoose.connection.readyState === 1) {
+    next()
+  } else {
+    res.status(503).json({ error: 'Service unavailable' })
+  }
+})
+
+// Update documentation
+
 app.get("/", (req, res) => {
-  res.send("Hello Technigo!");
+  const documentation = {
+    "About": "This is the Happy Thoughts API",
+    "Routes": [
+      {
+        "/": "Documentation",
+        "/endpoints": "All endpoints",
+        "/thoughts": "Get the 20 latest thoughts",
+        "/thoughts": "Add new thought",
+        "/thoughts/:thoughtId/like": "Like a thought",
+      }
+    ]
+  }
+
+  res.send(documentation);
 });
 
-// Start the server
+app.get('/thoughts', async (req, res) => {
+  try {
+    const thoughts = await Thought.find()
+      .sort({ createdAt: 'desc' })
+      .limit(20)
+      .exec();
+    res.status(200).json(thoughts);
+  } catch (err) {
+    res.status(400).json({
+      error: err.errors
+    })
+  }
+
+})
+
+app.post('/thoughts', async (req, res) => {
+  const { message } = req.body;
+
+  try {
+    const thoughtToAdd = await new Thought({ message }).save();
+    res.status(201).json(thoughtToAdd);
+  } catch (err) {
+    res.status(400).json({
+      message: 'Could not save thought',
+      errors: err.errors
+    })
+  }
+})
+
+
+app.post('/thoughts/:thoughtId/like', async (req, res) => {
+  const { thoughtId } = req.params
+
+  try {
+    const likedThought = await Thought.findByIdAndUpdate(
+      thoughtId,
+      { $inc: { hearts: 1 } }
+    );
+    res.status(200).json(likedThought)
+  } catch (err) {
+    res.status(400).json({
+      message: 'Bad request. Could not find thought with requested id.',
+      errors: err.errors
+    })
+  }
+})
+
+app.get('/endpoints', (req, res) => {
+  res.send(allEndpoints(app));
+});
+
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
 });
