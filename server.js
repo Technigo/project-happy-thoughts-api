11b5d46c39ddbaaf0ea4005f3531fdae36db630e
TereseClaesson <terese.claesson@gmail.com>
2022-05-18 10:00:43 +0200

    testing out different code and started the project

--- server.js
+++ server.js
@@ -16,6 +16,49 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+//Testing, difference between schema and model?
+//Schema
+const TechnigoMemberSchema = new mongoose.Schema({
+  name: {
+    //most important one
+    type: String,
+    required: true,
+    unique: true,
+    enum: ["Karin", "Petra", "Matilda","Poya", "Daniel"]
+  }, 
+  description: {
+    type: String,
+    minlength: 4,
+    maxlength: 30,
+    // deletes whitespace from beginning and the end of a string
+    trim: true
+  },
+  Score: {
+    type: Number,
+    default: 0
+  },
+  createdAt: {
+    type: Date,
+    default: ()=> new Date()
+  } 
+})
+
+//model
+const TechnigoMember = mongoose.model("TechnigoMember", TechnigoMemberSchema)
+
+// POST request
+app.post("/members", async(req, res)=> {
+  const { name, description } = req.body
+  console.log(req.body)
+  try {
+    const newMember = await new TechnigoMember({ name: name, description: description}).save()
+    res.status(200).json({response: newMember, sucess: true})
+  } catch(error) {
+
+    res.status(400).json({response: error, sucess: false})
+  }
+})
+
 // Start defining your routes here
 app.get("/", (req, res) => {
   res.send("Hello Technigo!");
