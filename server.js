2ac96124a5f4040421d05e45d509211db9e65e99
frida axelsson <axelsson.frida@gmail.com>
2022-05-19 12:43:04 +0200

    problem solving

--- server.js
+++ server.js
@@ -6,58 +6,68 @@ const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/project-mongo";
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true });
 mongoose.Promise = Promise;
 
-const thought = mongoose.model("Thought", {
+const port = process.env.PORT || 8080;
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+const ThoughtsSchema = new mongoose.Schema({
   message: {
     type: String,
     required: true,
     minlength: 5,
     maxlength: 140,
   },
-  complete: {
-    type: Boolean,
-    default: false,
+  hearts: {
+    type: Number,
+    default: 0,
   },
   createdAt: {
     type: Date,
-    default: Date.now,
+    default: () => new Date(),
   },
 });
 
-// Defines the port the app will run on. Defaults to 8080, but can be overridden
-// when starting the server. Example command to overwrite PORT env variable value:
-// PORT=9000 npm start
-const port = process.env.PORT || 8080;
-const app = express();
-
-// Add middlewares to enable cors and json body parsing
-app.use(cors());
-app.use(express.json());
+const thought = mongoose.model("thought", ThoughtsSchema);
 
 // Start defining your routes here
 app.get("/", (req, res) => {
   res.send("Hello Technigo!");
 });
 
-app.get("/thought", async (req, res) => {
-  const thoughts = await find().sort({ createdAt: "desc" }).limit(20).exec();
-  res.json(thoughts);
-});
-
-app.post("/thought", async (res, req) => {
-  const { message, complete } = req.body;
-  const thought = new thought({ message, complete });
+app.get("/thoughts", async (req, res) => {
   try {
-    const savedThought = await thought.save();
-    res.status(200).json(savedThought);
+    const thoughts = await thought
+      .find()
+      .sort({ createdAt: "desc" })
+      .limit(20)
+      .exec();
+    res.status(200).json(thoughts);
   } catch (err) {
-    res
-      .status(400)
-      .json({
-        message: "Could not save task to the Database",
-        error: err.errors,
-      });
+    res.status(400).json({
+      success: false,
+      error: err.errors,
+    });
   }
 });
+
+app.post("/thoughts", async (res, req) => {
+  const { message } = req.body;
+  try {
+    const newThought = await new thought({ message: message }).save();
+    res.status(201).json({
+      response: newThought,
+      success: true,
+    });
+  } catch (error) {
+    res.status(400).json({
+      response: error,
+      success: false,
+    });
+  }
+});
+
 // Start the server
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
