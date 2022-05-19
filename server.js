7f99573c80ffb2b5f717d31d86861d75b4b3c88f
Emma Högberg <emmahogberg88@gmail.com>
2022-05-19 13:32:53 +0200

    Change name for like to hearts

--- server.js
+++ server.js
@@ -70,10 +70,10 @@ app.get('/thoughts', async (req, res) => {
 })
 
 app.post('/thoughts', async (req, res) => {
-	const { message, likes } = req.body
+	const { message, hearts } = req.body
 	console.log(req.body)
 	try {
-		const newThought = await new HappyThought({ message: message, likes: likes }).save()
+		const newThought = await new HappyThought({ message: message, hearts: hearts }).save()
 		res.status(201).json({ response: newThought, success: true })
 	} catch (error) {
 		res.status(400).json({ response: error, success: false })
