104736213a9e39ebe96befd56075113602eb12e7
Sherin Susan Thomas <sherinsusanthomas95@gmail.com>
2022-05-19 10:39:59 +0200

    created API for happy  thouhts

--- server.js
+++ server.js
@@ -15,10 +15,83 @@ const app = express();
 // Add middlewares to enable cors and json body parsing
 app.use(cors());
 app.use(express.json());
+///
+const ThoughtSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    default: "unknown",
+    //to delete the whitespace
+    trim: true,
+  },
+  hearts: {
+    type: Number,
+    default: 0,
+  },
+  message: {
+    type: String,
+    required: true,
+    minlength: 5,
+    maxlength: 140,
+    trim: true,
+  },
+  createdAt: {
+    type: Date,
+    default: () => new Date(),
+  },
+});
 
+const Thoughts = mongoose.model("Thoughts", ThoughtSchema);
 // Start defining your routes here
 app.get("/", (req, res) => {
-  res.send("Hello Technigo!");
+  res.send("Welcome to Happy Thoughts API");
+});
+app.get("/thoughts", async (req, res) => {
+  try {
+    const thoughts = await Thoughts.find()
+      .sort({ createdAt: "desc" })
+      .limit(20)
+      .exec();
+    res.status(200).json(thoughts);
+  } catch (err) {
+    res.status(400).json({
+      success: false,
+      response: "Bad request, couldnot fetch thoughts, try again!.",
+    });
+  }
+});
+//POST request
+app.post("/thoughts", async (req, res) => {
+  const { name, message } = req.body;
+
+  try {
+    const newThoughts = await new Thoughts({
+      message,
+      name: "unknown",
+    }).save();
+    res.status(201).json({ response: newThoughts, success: true });
+  } catch (error) {
+    res.status(400).json({
+      success: false,
+      response: "Bad request, couldnot save new thoughts.",
+    });
+  }
+});
+app.post("/thoughts/:thoughtId/like", async (req, res) => {
+  const { thoughtId } = req.params;
+
+  try {
+    const thoughtsToLike = await Thoughts.findByIdAndUpdate(thoughtId, {
+      $inc: { hearts: 1 },
+    });
+    res.status(201).json(thoughtsToLike);
+  } catch (err) {
+    res.status(400).json({
+      success: false,
+      status_code: 400,
+      message: "Bad request, could not find and update the thought.Try again!",
+      error: err.errors,
+    });
+  }
 });
 
 // Start the server
