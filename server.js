260740457d0bae265eaea0a109c764146a5a5d52
joannaringqvist <joanna.ringqvist@gmail.com>
2022-05-19 15:19:40 +0200

    Created the api for happy thoughts

--- server.js
+++ server.js
@@ -2,13 +2,10 @@ import express from "express";
 import cors from "cors";
 import mongoose from "mongoose";
 
-const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/project-mongo";
+const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/project-happy-thoughts-api";
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true });
 mongoose.Promise = Promise;
 
-// Defines the port the app will run on. Defaults to 8080, but can be overridden
-// when starting the server. Example command to overwrite PORT env variable value:
-// PORT=9000 npm start
 const port = process.env.PORT || 8080;
 const app = express();
 
@@ -16,9 +13,63 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-// Start defining your routes here
+const HappyThoughtSchema = new mongoose.Schema({
+  message: {
+    type: String,
+    required: true,
+    minLength: 5,
+    maxLength: 140
+  },
+  like: {
+    type: Number,
+    default: 0
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+const HappyThought = mongoose.model("HappyThought", HappyThoughtSchema);
+
 app.get("/", (req, res) => {
-  res.send("Hello Technigo!");
+  res.send("Hello and welcome to Happy thoughts!");
+});
+
+app.get('/thoughts', async (req, res) => {
+  const thoughts = await HappyThought.find().sort({createdAt: 'desc'}).limit(20).exec();
+  res.json(thoughts);
+});
+
+app.post('/thoughts', async (req, res) => {
+  const { message } = req.body;
+  try {
+    const newThought = new HappyThought({message: message});
+      await newThought.save();
+      res.status(201).json({
+        response: newThought,
+        success: true
+      });
+    } catch(err) {
+      res.status(400).json({
+        response: err,
+        message: 'Could not save the thought', 
+        errors: err.errors,
+        success: false,
+        status: 400
+      })
+    }
+});
+
+app.post('/thoughts/:id/like', async (req, res) => {
+  const { id } = req.params;
+  try {
+    const happyThoughtToUpdate = await HappyThought.findByIdAndUpdate(id, { $inc: {like: 1}})
+    console.log(happyThoughtToUpdate.message);
+    res.status(200).json({response: `Happy Thought "${happyThoughtToUpdate.message}" is liked`, success: true});
+  } catch (error) {
+    res.status(400).json({response: error, success: false})
+  }
 });
 
 // Start the server
