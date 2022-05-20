0d0a83032cdc8aaab12dff8d55980312fcc1a96d
bostrombundock <sbostrom@ymail.com>
2022-05-20 08:48:19 +0200

    added new endpoints

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 import express from "express";
 import cors from "cors";
 import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config ()
 
 const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/project-mongo";
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true });
@@ -15,6 +18,51 @@ const app = express();
 // Add middlewares to enable cors and json body parsing
 app.use(cors());
 app.use(express.json());
+
+
+const ThoughtSchema = new mongoose.Schema({
+  message: {
+    type: String,
+    required: true,
+    minlength: 5,
+    maxlength: 140
+  },
+
+  hearts: {
+    type: Number,
+    default: 0
+  },
+
+  createdAt: {
+    type: Date,
+    default: ()=> new Date()
+  },
+})
+
+const Thought = mongoose.model('Thought', ThoughtSchema);
+
+
+app.post("/thougths", async (req, res) => {
+
+  const { message } = req.body;
+ try {
+   const newThought = await new Thought({message: message}).save()
+
+  res.status(200).json({
+    respons: newThought,
+    success:true
+  })
+  } catch(error) {
+    res.status(400).json({
+      response: error,
+      sucess:false,
+
+    })
+
+ }
+
+
+});
 
 // Start defining your routes here
 app.get("/", (req, res) => {
