4633138ec52d3e06226b137fa21d60d4e74e8ecb
Justine Zwiazek <justine.zwiazek@mentimeter.com>
2022-05-21 09:17:09 +0200

    codealong with Daniel

--- server.js
+++ server.js
@@ -1,24 +1,125 @@
 import express from "express";
 import cors from "cors";
 import mongoose from "mongoose";
+import listEndpoints from "express-list-endpoints";
 
-const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/project-mongo";
+const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/happyThoughts";
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true });
 mongoose.Promise = Promise;
 
-// Defines the port the app will run on. Defaults to 8080, but can be overridden
-// when starting the server. Example command to overwrite PORT env variable value:
-// PORT=9000 npm start
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 8081;
 const app = express();
+
+// Coding along with Damien:
+// const Note = mongoose.model("Note", {
+//   text: "String",
+//   createdAt: {
+//     type: "Date",
+//     default: () => new Date(),
+//   },
+// });
+
+// Codealong Lice Session with Daniel:
+////////////////////////////////////////
+// Mongoose schema:
+const ThoughtSchema = new mongoose.Schema({
+  message: {
+    // the most important is type
+    type: String,
+    required: true,
+    minlength: 5,
+    maxlength: 140,
+    // trim deletes whitespace but only from the begining and the end of the desctiption space
+    trim: true,
+    // Enum takes an array of values that will be accepted, i.e. for names
+    // enum: ["Karin", "Petra"],
+  },
+  hearts: {
+    type: Number,
+    default: 0,
+  },
+  createdAt: {
+    type: Date,
+    default: () => new Date(),
+  },
+  tag: {
+    type: String,
+    required: true,
+    enum: ["Hobby", "Relationships", "Work", "Self care", "Food", "Nature"],
+  },
+  name: {
+    type: String,
+    trim: true,
+    required: true,
+    maxlength: 50,
+  },
+});
+
+// Mongoose model:
+const Thought = mongoose.model("Thought", ThoughtSchema);
 
 // Add middlewares to enable cors and json body parsing
 app.use(cors());
 app.use(express.json());
 
-// Start defining your routes here
+// Endpoints
 app.get("/", (req, res) => {
-  res.send("Hello Technigo!");
+  res.send(listEndpoints(app));
+});
+
+// Start defining your routes here
+
+// // Post req Codealong with Damien
+// app.post("/happy", async (req, res) => {
+//   const note = new Note(req.body);
+//   await note.save();
+//   res.json(note);
+// });
+
+// Post req codealong with Daniel
+
+//Get all thoughts in descending order, maximum of 20
+app.get("/thoughts", async (req, res) => {
+  const thoughts = await Thought.find()
+    .sort({ createdAt: "desc" })
+    .limit(20)
+    .exec();
+  res.json(thoughts);
+});
+
+// Post a new thought
+// We have three paramaters as default value, so we will need to take those in
+app.post("/thoughts", async (req, res) => {
+  const { message, tag, name } = req.body;
+
+  try {
+    const newThought = await new Thought({ message, tag, name }).save();
+    res.status(201).json({ response: newThought, success: true });
+  } catch (error) {
+    res.status(400).json({ response: error, success: false });
+  }
+});
+
+//Update number of likes using thought id
+app.post("/thoughts/:thoughtId/like", async (req, res) => {
+  const { thoughtId } = req.params;
+
+  try {
+    const updatedLikes = await Thought.findByIdAndUpdate(
+      thoughtId,
+      {
+        $inc: {
+          hearts: 1,
+        },
+      },
+      {
+        new: true,
+      }
+    );
+    res.status(200).json({ response: updatedLikes, success: true });
+  } catch (error) {
+    res.status(400).json({ response: error, success: false });
+  }
 });
 
 // Start the server
