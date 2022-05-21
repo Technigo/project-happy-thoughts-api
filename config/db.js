64fd67b5d85a30ac448a9ea4c88f13edc53ed2c9
Arianna Allahyar <anallahyar@gmail.com>
2022-05-21 19:20:52 +0200

    Connection to database created

---
+++ config/db.js
@@ -0,0 +1,16 @@
+import mongoose from "mongoose";
+
+const connectDB = () => {
+  try {
+    const conn = mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
+  } catch (err) {
+    console.error(err);
+    process.exit(1);
+  }
+};
+
+export default connectDB;
