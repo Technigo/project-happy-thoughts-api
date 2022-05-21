ae58ca9dbd72f3e588dba1ffdd195db9f35f4848
Jenfi72 <jennyfiskaare@Jennys-MacBook-Air.local>
2022-05-21 12:38:18 +0200

    test2

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/project-mongo";
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true });
 mongoose.Promise = Promise;
 
-// Defines the port the app will run on. Defaults to 8080, but can be overridden
+// Defines the port the app will run on. Defaults to 8080, but can be overridden 
 // when starting the server. Example command to overwrite PORT env variable value:
 // PORT=9000 npm start
 const port = process.env.PORT || 8080;
