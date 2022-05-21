98b205c7479ac23bea1ba4cc458748bf38694470
Joanna Lodell <joannalodell@c77-218-53-198.bredband.tele2.se>
2022-05-21 12:01:08 +0200

    adapted to this weeks project

--- server.js
+++ server.js
@@ -1,191 +1,97 @@
 import express from "express";
 import cors from "cors";
 import mongoose from "mongoose";
+import listEndpoints from "express-list-endpoints";
 
-const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/project-mongo";
+const mongoUrl = 
+  process.env.MONGO_URL || "mongodb://localhost/project-happy-thoughts-api";
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true });
 mongoose.Promise = Promise;
 
-// Defines the port the app will run on. Defaults to 8080, but can be overridden
-// when starting the server. Example command to overwrite PORT env variable value:
-// PORT=9000 npm start
 const port = process.env.PORT || 8080;
 const app = express();
 
-// Add middlewares to enable cors and json body parsing
 app.use(cors());
 app.use(express.json());
-////////////////////
 
-const TechnigoMemberSchema = new mongoose.Schema({
-  name: {
-    // most important one
+const ThoughtSchema = new mongoose.Schema({
+  message: {
     type: String,
     required: true,
-    unique: true,
-    enum:["Karin", "Petra", "Matilda", "Poya", "Daniel"]
+    minlength: 5,
+    maxlength: 140,
+    trim: true,
   },
-  description: {
-    type: String,
-    minlength: 4,
-    maxlength: 30,
-    // deletes whitespace from beginning and end of a string               lala lala        
-    trim: true
-  },
-  score: {
+  hearts: {
     type: Number,
-    default: 0
+    default: 0,
   },
   createdAt: {
     type: Date,
-    default: ()=> new Date()
-  }
-});
-// mongoose.Model
-// mongoose.model
-
-// const array = [];
-// Array
-
-const TechnigoMember = mongoose.model("TechnigoMember", TechnigoMemberSchema);
-
-//// POST request V1
-app.post("/members", async (req, res) => {
-
-  const { name, description } = req.body;
-  console.log(req.body);
-  try {
-    const newMember = await new TechnigoMember({name: name, description: description}).save();
-    
-    res.status(201).json({response: newMember, success: true});
-  } catch(error) {
-
-    res.status(400).json({response: error, success: false});
-  }
-
+    default: () =>
+      new Date(Date.now()).toLocaleString('sv-SE', {
+        timeZone: 'Europe/Stockholm',
+      }),
+  },
 });
 
-//// POST with promises
-// app.post("/members", (req, res) => {
+const Thought = mongoose.model("Thought", ThoughtSchema);
 
-//   const { name, description } = req.body;
-//   console.log(req.body);
-
-// new TechnigoMember({name: name, description: description}).save()
-//   .then(data => {
-//     res.status(201).json({response: data, success: true});
-//   }).catch(error => {
-//     res.status(400).json({response: error, success: false});
-//   });
-// });
-
-//// V3
-// app.post("/members", (req, res) => {
-
-//   const { name, description } = req.body;
-
-//   new TechnigoMember({name: name, description: description}).save((error, data) => {
-//     if (error) {
-//       res.status(400).json({response: error, success: false});
-//     } else {
-//       res.status(201).json({response: data, success: true});
-//     }
-//   });
-// });
-
-//// POST -> creating
-///PUT -> replaces 
-////PATCH -> changes
-
-app.post("/members/:id/score", async (req, res) => {
-  const { id } = req.params;
-  try {
-    const memberToUpdate = await TechnigoMember.findByIdAndUpdate(id, {$inc: {score: 1}});
-    res.status(200).json({response: `Member ${memberToUpdate.name} has been updated`, succes: true});
-  } catch (error) {
-    res.status(400).json({response: error, succes: false});
-  }
-});
-//////////////////
-//// Wednesday
-
-app.get("/members", async (req, res) => {
-  // const members = await TechnigoMember.find({});
-
-  // V1 -mongoose
-  // const {page, perPage} = req.query; 
-  // try {
-  //   const members = await TechnigoMember.find({}).sort({createdAt: 1}) //1 ascending -1 descending //Sort the mongoose way
-  //   .skip((page - 1) * perPage).limit(perPage);
-
-  //   res.status(200).json({success: true, response: members});
-  // } catch (error) {
-  //   res.status(400).json({success: false, response: error});
-  // }
-  
-/// V2 Mongo
-const { page, perPage, numPage = +page, numPerPage = +perPage } = req.query; 
-  try {
-    const members = await TechnigoMember.aggregate([
-      {
-        $sort: {
-          createdAt: -1
-        }
-      },
-      {
-        $skip: (numPage -1) * numPerPage
-      },
-      {
-        $limit: numPerPage
-      }
-    ]);
-    // .skip((page - 1) * perPage).limit(perPage);
-
-    res.status(200).json({success: true, response: members});
-  } catch (error) {
-    res.status(400).json({success: false, response: error});
-  }
-  
-});
-app.delete("/members/:id", async (req, res) => {
-  const { id } = req.params;
-  
-  try {
-    // const deleted = await TechnigoMember.deleteOne({_id: id}); //Gives us only true/that we deleted something 
-    const deleted = await TechnigoMember.findOneAndDelete({_id: id}); //Gives us who we deleted - This one is encouraged to use. 
-    if(deleted) {
-      res.status(200).json({success: true, response: deleted})
-    } else {
-      res.status(404).json({succes: false, response: "Not found"});
-    }
-  } catch (error) {
-    res.status(400).json({succes: false, response: error});
-  }
-
-});
-
-/////////
-
-
-app.patch("/members/:id", async (req, res) => {
-  const { id } = req.params;
-  const { updatedName } = req.body;
-
-  try {
-    const memberToUpdate = await TechnigoMember.findByIdAndUpdate({_id: id}, {name: updatedName});
-    if(memberToUpdate) {
-      res.status(200).json({success: true, response: memberToUpdate});
-    } else {
-      res.status(404).json({succes: false, response: "Not found"});
-    }
-  } catch (error) {
-    res.status(400).json({succes: false, response: error});
-  }
-});
-
-// Start defining your routes here
 app.get("/", (req, res) => {
-  res.send("Hello Technigo!");
+  res.send(listEndpoints(app))
+});
+
+app.get("/thoughts", async (req, res) => {
+  try {
+    const nthoughts = await Thought.find().sort({ createdAt: 'desc' }).limit(20)
+    res.status(200).json(thoughts)
+  } catch (err) {
+    res.status(400).json({
+      succes: false,
+      status_code: 400, 
+      message: "Bad request, could not fetch thoughts.",
+      error: err.errors,
+    });
+  }
+});
+
+app.post("/thoughts", async (req, res) => {
+  const { message } = req.body;
+
+  try {
+    const newThought = await new Thought({
+      message: message, 
+    }).save();
+    res.status(200).json({ response: newThought, success: true});
+  } catch(err) {
+    res.status(400).json({
+      success: false,
+      status_code: 400,
+      message: "Bad request, could not post your thought.",
+      error: err.errors, 
+    });
+  }
+});
+
+app.post("/thoughts/:thoughtId/like", async (req, res) => {
+  const { thoughtId } = req.params;
+
+  try {
+    const thoughtToLike = await Thought.findByIdAndUpdate(thoughtId, {
+      $inc: {hearts: 1},
+    });
+    res.status(200).json({
+      response: `Like ${thoughtToLike.message} has been updated`, 
+      succes: true
+    });
+  } catch (err) {
+    res.status(400).json({
+      succes: false,
+      status_code: 400, 
+      message: "Bad request, could not find and update this thought.",
+      error: err.errors,
+    });
+  }
 });
 
 // Start the server
