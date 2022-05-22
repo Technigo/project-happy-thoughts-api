fe0eedf3064fe0c50a31c187a86923313b6778af
Arianna Allahyar <anallahyar@gmail.com>
2022-05-22 19:58:55 +0200

    Connection to db with environmental variables fixed

--- config/db.js
+++ config/db.js
@@ -1,8 +1,8 @@
 import mongoose from "mongoose";
 
-const connectDB = () => {
+const connectDB = async () => {
   try {
-    const conn = mongoose.connect(process.env.MONGO_URI, {
+    const conn = await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 import express from "express";
+import {} from "dotenv/config";
 import cors from "cors";
 import connectDB from "./config/db";
+import routes from "./routes/thoughtsRoutes";
 
 const port = process.env.PORT || 8080;
 
@@ -10,9 +12,8 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
-app.get("/", (req, res) => {
-  res.send("Hello Technigo!");
-});
+app.use("/thoughts", routes);
 
 app.listen(port, () => console.log(`Server running on http://localhost:${port}`));
