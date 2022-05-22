64eef2a1a404bceebb223badb53e1950458abecd
Therese Nyman <theresenyman@Thereses-MacBook-Air.local>
2022-05-22 13:51:51 +0200

    all endpoints, schema and mongoose model

--- README.md
+++ README.md
@@ -1,12 +1,6 @@
 # Project Happy Thoughts API
-
-Replace this readme with your own information about your project.
-
-Start by briefly describing the assignment in a sentence or two. Keep it short and to the point.
-
-## The problem
-
-Describe how you approached to problem, and what tools and techniques you used to solve it. How did you plan? What technologies did you use? If you had more time, what would be next?
+The task is to build an API which includes both GET request endpoints to return data 
+and POST endpoints to create data. Use this API and connect it to an earlier project, Happy Thoughts.
 
 ## View it live
 
--- server.js
+++ server.js
@@ -2,26 +2,99 @@ import express from "express";
 import cors from "cors";
 import mongoose from "mongoose";
 
-const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/project-mongo";
+import getEndpoints from "express-list-endpoints";
+
+const mongoUrl =
+  process.env.MONGO_URL || "mongodb://localhost/project-happythought-API";
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true });
 mongoose.Promise = Promise;
 
-// Defines the port the app will run on. Defaults to 8080, but can be overridden
-// when starting the server. Example command to overwrite PORT env variable value:
-// PORT=9000 npm start
 const port = process.env.PORT || 8080;
 const app = express();
 
-// Add middlewares to enable cors and json body parsing
 app.use(cors());
 app.use(express.json());
 
-// Start defining your routes here
-app.get("/", (req, res) => {
-  res.send("Hello Technigo!");
+//SCHEMA
+const HappyThoughtSchema = new mongoose.Schema({
+  message: {
+    type: String,
+    required: true,
+    minlength: 5,
+    maxlength: 140,
+    trim: true,
+  },
+  hearts: {
+    type: Number,
+    default: 0,
+  },
+  createdAt: {
+    type: String,
+    default: () =>
+      new Date(Date.now()).toLocaleString("sv-SE", {
+        timeZone: "Europe/Stockholm",
+      }),
+  },
 });
 
-// Start the server
+//MONGOOSE MODEL
+const Thought = mongoose.model("Thought", HappyThoughtSchema);
+
+//ROUTES
+//overview on first page
+app.get("/", (req, res) => {
+  res.send(getEndpoints(app));
+});
+
+//post thought to database
+app.post("/thoughts", async (req, res) => {
+  const { message } = req.body;
+  console.log(req.body);
+  try {
+    const newThought = await new Thought({
+      message: message,
+    }).save();
+
+    res.status(201).json({ response: newThought, success: true });
+  } catch (error) {
+    res.status(400).json({ response: error, success: false });
+  }
+});
+
+//update likes on thought
+app.post("/thoughts/:_id/like", async (req, res) => {
+  const { _id } = req.params;
+  try {
+    const updateLikes = await Thought.findByIdAndUpdate(_id, {
+      $inc: { hearts: 1 },
+    });
+    res.status(200).json({
+      response: `Message ${updateLikes.message} has been updated with a like`,
+      success: true,
+    });
+  } catch (error) {
+    res.status(400).json({ response: error, success: false });
+  }
+});
+
+//show a maximum of 20 thoughts sorted on created date, in descending order
+app.get("/thoughts", async (req, res) => {
+  try {
+    const thoughts = await Thought.find()
+      .sort({ createdAt: "desc" })
+      .limit(20)
+      .exec();
+    res.status(200).json(thoughts);
+  } catch (error) {
+    res.status(400).json({
+      response: error,
+      success: false,
+      message: "Could not fetch thoughts",
+    });
+  }
+});
+
+//Start the server
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
 });
