9902f1d8c31fae80cf3b14ddde350e3681b3307f
ViktorSvenssonN <viktor.tommy.svensson@gmail.com>
2022-11-28 18:14:03 +0100

    tried codealong with Van

--- server.js
+++ server.js
@@ -6,6 +6,22 @@ const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/project-mongo";
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true });
 mongoose.Promise = Promise;
 
+const Task =mongoose.model('Task', {
+  text: {
+    type: String,
+    required: true,
+    minlength:1
+  },
+  complete: {
+    type: Boolean,
+    default: false
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
+})
+
 // Defines the port the app will run on. Defaults to 8080, but can be overridden
 // when starting the server. Example command to overwrite PORT env variable value:
 // PORT=9000 npm start
@@ -20,6 +36,24 @@ app.use(express.json());
 app.get("/", (req, res) => {
   res.send("Hello Technigo!");
 });
+
+app.get("/tasks", async (req, res) => {
+  const tasks = await Task.find().sort({createdAt: 'desc'}).limit(25).exec();
+  res.json(tasks);
+})
+
+app.post("/tasks", async (req, res) => {
+  const {text, complete} = req.body;
+  const task = new Task({text, complete});
+
+  try {
+    const savedTask = await task.save()
+    res.status(201).json(savedTask);
+  }catch (err){
+    res.status(400).json({message: 'Could not save task to database', error:err.errors });
+  }
+})
+
 
 // Start the server
 app.listen(port, () => {
