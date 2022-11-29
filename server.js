87c169cd909f14a300c0568ee8c278f581da1a0b
fannycardell <fannycardell@gmail.com>
2022-11-29 18:46:58 +0100

    Added tuesday session code along

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-import express from "express";
+import express, { response } from "express";
 import cors from "cors";
 import mongoose from "mongoose";
 
@@ -20,7 +20,94 @@ app.use(express.json());
 app.get("/", (req, res) => {
   res.send("Hello Technigo!");
 });
+////////////////
+const TechnigoMemberSchema = new mongoose.Schema({
+  name: {
+    // most important one
+    type: String,
+    // forces to provide this value
+    required: true,
+    // new name will have to be different than all others in the DB
+    unique: true,
+    // all the allowed values. Right not are only these 5 allowed in the database.  
+    // Better exampels temperature messurements ex farhenheit, celcius 
+    // or in a gallery that ONLY have picaso and van gogh
+    
+    enum: ["Matilda", "Poya", "Petra", "Hanna", "Daniel"]
+  },
+  description: {   //Like the main body of the texts in HT min 4 max 140
+    type: String,
+    minlength: 4,
+    maxlength: 30,
+    // removes unnecessary whitespaces
+    trim: true
+  },
+  score: {
+    type: Number,
+    // initial value, if none other is specified
+    default: 0
+  },
+  createdAt: {
+    type: Date,
+    // new Date() will execute once - when we start the server
+    // default: new Date()
+    // onClick = { someFunction()}
+    // Immediatly Invoked  Function expression IIFE - in the moment that you declare the function it is called/executed right away
+    // (() => new Date())()
+    // (function functionName () {new Date()})()
+    default: () => new Date()
+  }
+});
 
+const TechnigoMember = mongoose.model("TechnigoMember", TechnigoMemberSchema);
+
+app.post("/members", async (req, res) => {
+  const {name, description} = req.body;
+  console.log(req.body);
+  try {
+    const newMember = await new TechnigoMember({name: name, description: description}).save();
+    res.status(201).json({success: true, response: newMember});
+  } catch(error) {
+    res.status(400).json({success: false, response: error});
+  }
+});
+
+// V2 POST with promises
+// app.post("/members", (req, res) => {
+//  const {name, description} = req.body;
+//    const newMember = new TechnigoMember({name: name, description: description}).save()
+//    .then(data => {
+ //       res.status(201).json({success: true, response: data});
+//   }).catch(error => {
+ //       res.status(400).json({success: false, response: error});
+ //   });
+// });
+
+// V3 POST mongoose syntax
+// app.post("/members", (req, res) => {
+//   const {name, description} = req.body;
+//     const newMember = new TechnigoMember({name: name, description: description}).save((error, data) => {
+//       if(error) {
+//         res.status(400).json({success: false, response: error});
+//       } else {
+//         res.status(201).json({success: true, response: data});
+//       } 
+//     });
+// });
+
+// POST => create stuff
+// PUT => replace in DB -> one PErson switch with another
+// PATCH => change/modify stuff
+app.patch("/members/:id/score", async (req, res) => {
+   const { id } = req.params;
+   try {
+    const memberToUpdate = await TechnigoMember.findByIdAndUpdate(id, {$inc: {score: 1}});
+    res.status(200).json({success: true, response: `Member ${memberToUpdate.name} has their score updated`});
+   } catch (error) {
+    res.status(400).json({success: false, response: error});
+   }
+});
+///////////////
 // Start the server
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
