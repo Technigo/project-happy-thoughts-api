5898e1f3084f305539df38eb70b5a51963c8a44a
Sailornina <sylcardozo.sc@gmail.com>
2022-11-29 15:46:26 +0100

    Listendpoint was added. ThoughtsSchema was created

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 import express from "express";
 import cors from "cors";
 import mongoose from "mongoose";
+import listEndpoints from "express-list-endpoints";
 
-const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/project-mongo";
+const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/happy-thoughts";
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true });
 mongoose.Promise = Promise;
 
@@ -16,9 +17,45 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-// Start defining your routes here
+const ThoughtsSchema = new mongoose.Schema({
+  message: {
+    type: String,
+    minlength: 5,
+    maxlength: 140,
+  },
+  hearts: {
+    type: Number,
+    default: 0,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+})
+
+const Thought = mongoose.model("Thought", ThoughtsSchema);
+
+app.get("/endpoints", (req, res) => {
+  res.send(listEndpoints(app))
+});
+
 app.get("/", (req, res) => {
-  res.send("Hello Technigo!");
+  const HappyThoughtsApi = {
+    Welcome: 'Hi! This is the happy-thoughts-API from https://happy-thoughts-antonella.netlify.app/',
+    Routes: [{
+      "/thoughts": 'Get all Happy Thoughts.'
+    }]
+  }
+  res.send(HappyThoughtsApi);
+});
+
+app.get("/thoughts", async (req, res) => {
+  try {
+    const thoughts = await Thought.find()
+    res.status(200).json(thoughts)
+  } catch (error) {
+    res.status(400).json({ message: "Failed to load thoughts" })
+  }
 });
 
 // Start the server
