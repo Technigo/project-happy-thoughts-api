4ec3236fe7162728f8716d38e765669df3ae5a5c
Kneedler <sarahklindelow@gmail.com>
2022-11-29 16:10:39 +0100

    GET, POST and PATCH working in postman

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-import express from "express";
+import express, { response } from "express";
 import cors from "cors";
 import mongoose from "mongoose";
 
@@ -6,9 +6,6 @@ const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/project-mongo";
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true });
 mongoose.Promise = Promise;
 
-// Defines the port the app will run on. Defaults to 8080, but can be overridden
-// when starting the server. Example command to overwrite PORT env variable value:
-// PORT=9000 npm start
 const port = process.env.PORT || 8080;
 const app = express();
 
@@ -16,9 +13,55 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-// Start defining your routes here
 app.get("/", (req, res) => {
   res.send("Hello Technigo!");
+});
+
+const ThoughtSchema = new mongoose.Schema({
+  text: {
+    type: String,
+    required: true,
+    minlength: 5,
+    maxlength: 140,
+    trim: true
+  },
+  likes: {
+    type: Number,
+    default: 0
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+const Thought = mongoose.model("Thought", ThoughtSchema);
+
+// Start defining your routes here
+
+app.get('/thoughts', async (req, res) => {
+  const thoughts = await Thought.find().sort({createdAt: 'desc'}).limit(20).exec();
+  res.json(thoughts);
+});
+
+app.post("/thoughts", async (req, res) => {
+  const { text } = req.body;
+  try {
+    const newThought = await new Thought({text: text}).save();
+    res.status(201).json({success: true, response: newThought});
+  } catch (error) {
+    res.status(400).json({success: false, response: error, message: 'could not save thought to the database' })
+  }
+});
+
+app.patch("/thoughts/:id/likes", async (req, res) => {
+  const { id } = req.params;
+  try {
+   const thoughtToUpdate = await Thought.findByIdAndUpdate(id, {$inc: {likes: 1}});
+   res.status(200).json({success: true, response: `Thought ${thoughtToUpdate.text} has their score updated`});
+  } catch (error) {
+   res.status(400).json({success: false, response: error});
+  }
 });
 
 // Start the server
