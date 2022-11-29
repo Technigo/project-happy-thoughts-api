407e803776ee279a160e954eb92f47b0f896f0db
Elin <elin.segelov@gmail.com>
2022-11-29 15:20:08 +0100

    Set up .env variable

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 import express from "express";
 import cors from "cors";
 import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/project-happyThoughts";
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true });
