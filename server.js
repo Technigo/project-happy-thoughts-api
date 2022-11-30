369d96c1863ef52108e2fc51673604be9e475d32
Jessica Hansson <jessica.hansson@hotmail.se>
2022-11-30 10:54:21 +0100

    First setup

--- server.js
+++ server.js
@@ -1,10 +1,32 @@
 import express from "express";
 import cors from "cors";
 import mongoose from "mongoose";
+import listEndpoints from 'express-list-endpoints'
 
 const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/project-mongo";
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true });
 mongoose.Promise = Promise;
+
+
+const ThoughtSchema  =  new mongoose.Schema({
+    message:{
+        type: String,
+        required : true,
+        minlength: 5,
+        maxlength: 140,
+        trim: true, 
+    },
+    hearts: {
+        type: Number,
+        default: 0
+    }, 
+    createdAt: {
+        type: Date, 
+        default: () => new Date()
+    }
+})
+
+const Thought = mongoose.model('Thought', ThoughtSchema)
 
 // Defines the port the app will run on. Defaults to 8080, but can be overridden
 // when starting the server. Example command to overwrite PORT env variable value:
@@ -16,11 +38,57 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-// Start defining your routes here
-app.get("/", (req, res) => {
-  res.send("Hello Technigo!");
-});
+// ROUTES
+// Lists all endpoints available
+app.get('/', (req, res) => {
+    res.send(listEndpoints(app))
+  })
 
+  //Get all thoughts in descending order, maximum of 20
+app.get('/thoughts', async (req, res) => {
+    const thoughts = await Thought.find().sort({createdAt: 'desc'}).limit(20).exec();
+    res.json(thoughts);
+})
+
+//Create new thought
+app.post('/thoughts', async (req, res) => {
+    // Retrieve the information sent by the client to our API endpoint
+    const { message } = req.body;
+
+    // Use our mongoose model to create the database entry
+    const thought = new Thought({ message })
+
+    try {
+        const newThought = await thought.save();
+        res.status(201).json({ 
+         response: newThought, 
+         success: true 
+        })
+    } catch (error) {
+        res.status(400).json({
+         response: error, 
+         success: false
+        });
+    }
+})
+
+// Update like by thougt id
+app.post("/thoughts/:id/hearts", async (req, res) => {
+    const { id } = req.params;
+    try {
+        const heartsUpdated = await Thought.findByIdAndUpdate(id, {$inc: {hearts: 1}});
+        res.status(200).json({
+         response: heartsUpdated,
+         success: true
+    });
+    } catch (error) {
+        res.status(400).json({
+         message: "Couldn't find no post with that ID",
+         response: error,
+         success: false,  
+    });
+    }
+ });
 // Start the server
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
