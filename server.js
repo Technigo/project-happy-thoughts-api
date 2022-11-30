cbaf24719bc5d4f34de87f1d2342b8d04bb29c4a
malmen237 <linda.malm237@gmail.com>
2022-11-30 10:42:05 +0100

    First commit.

--- server.js
+++ server.js
@@ -6,9 +6,6 @@ const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/project-mongo";
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true });
 mongoose.Promise = Promise;
 
-// Defines the port the app will run on. Defaults to 8080, but can be overridden
-// when starting the server. Example command to overwrite PORT env variable value:
-// PORT=9000 npm start
 const port = process.env.PORT || 8080;
 const app = express();
 
@@ -16,12 +13,67 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.use((req, res, next) => {
+  if (mongoose.connection.readyState === 1) {
+    next()
+  } else {
+    res.status(503).json({ error: "Service unavaliable" })
+  }
+});
+
+// MongoDB
+const MessageSchema = new mongoose.Schema({
+  message: {
+    type: String,
+    minlength: 1,
+    maxlength: 140,
+    required: true
+  },
+  like: {
+    type: Number,
+    default: 0
+  },
+  CreatedAt: {
+    type: Date,
+    default: () => new Date()
+  }
+})
+
+const Message = mongoose.model("Message", MessageSchema);
+
 // Start defining your routes here
 app.get("/", (req, res) => {
   res.send("Hello Technigo!");
+});
+
+app.post("/messages", async (req, res) => {
+  const {message} = req.body;
+  try {
+    const newMessage = await new Message({message: message}).save();
+    res.status(201).json({
+      success: true, 
+      response: newMessage
+    });
+  } catch(error) {
+    res.status(400).json({
+      success: false,
+      response: error
+    });
+  }
+})
+
+app.patch("/messages/:id/like", async (req, res) => {
+  const { id } = req.params;
+  try {
+   await Message.findByIdAndUpdate(id, {$inc: {like: 1}});
+   res.status(200).json({success: true, response: "Message Liked"});
+  } catch (error) {
+   res.status(400).json({success: false, response: error});
+  }
 });
 
 // Start the server
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
 });
+
