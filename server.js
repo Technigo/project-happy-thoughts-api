1d92395b10a438b420b3eb41a8eda99fb4d19465
Maria Westling <maria_westling@hotmail.com>
2022-11-30 13:54:31 +0100

    initial commit, can add thoughts and counter works

--- server.js
+++ server.js
@@ -21,6 +21,50 @@ app.get("/", (req, res) => {
   res.send("Hello Technigo!");
 });
 
+const HappyThoughtSchema = new mongoose.Schema({
+  thought: {
+    type: String,
+    minlenght: 4,
+    maxlength: 140,
+  },
+  counter: {
+    type: Number,
+    default: 0
+  },
+  createdAt: {
+    type: Date,
+    default: () => new Date()
+  }
+})
+
+// app.get("/thoughts", async (req, res) => {
+
+// })
+
+const HappyThought = mongoose.model("HappyThought", HappyThoughtSchema)
+
+app.post("/thoughts", async (req, res) => {
+  const { thought } = req.body
+  console.log(req.body)
+  try {
+    const newThought = await new HappyThought({ thought: thought }).save()
+    res.status(201).json({ success: true, response: newThought })
+  } catch (error) {
+    res.status(400).json({ success: false, response: error })
+  }
+})
+
+app.patch("/thoughts/:id/counter", async (req, res) => {
+  const { id } = req.params
+  try {
+    const thoughtToAddLike = await HappyThought.findByIdAndUpdate(id, { $inc: { counter: 1 } })
+    res.status(200).json({ success: true, response: `One more like for ${thoughtToAddLike.thought}` })
+  } catch (error) {
+    res.status(400).json({ success: false, response: error })
+  }
+})
+
+
 // Start the server
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
