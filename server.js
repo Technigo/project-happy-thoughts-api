d46fec0e1b5f9daa35efbce663cd8954048ecdbe
uohman <ulrikalovisa@gmail.com>
2022-11-30 12:07:53 +0200

    Added a Thoughts model, a get endpoint and a post endpoint

--- server.js
+++ server.js
@@ -2,10 +2,26 @@ import express from "express";
 import cors from "cors";
 import mongoose from "mongoose";
 
-const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/project-mongo";
+const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/project-happy-thoughts";
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true });
 mongoose.Promise = Promise;
 
+const Thought = mongoose.model('Thought', {
+  text: {
+    type: String,
+    required: true,
+    minlength: 5
+  },
+  complete: {
+    type: Boolean,
+    default: false
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
+
+});
 // Defines the port the app will run on. Defaults to 8080, but can be overridden
 // when starting the server. Example command to overwrite PORT env variable value:
 // PORT=9000 npm start
@@ -18,7 +34,36 @@ app.use(express.json());
 
 // Start defining your routes here
 app.get("/", (req, res) => {
-  res.send("Hello Technigo!");
+  res.send({
+    Welcome: 
+    "Welcome to this Happy thoughts API.❤️",
+    Routes: [
+      { 
+        "/thought": "Show all posted happy thoughts",
+      }
+    ],
+  });
+});
+
+app.get('/thoughts', async (req, res) => {
+  const thoughts = await Thought.find().sort({createdAt: 'desc'}).limit(20).exec();
+  res.json(thoughts);
+});
+
+app.post('/thoughts', async (req, res) => {
+  // Retrieve the information sent by the client to our API endpoint
+  const {text, complete} = req.body;
+
+  // Use our mongoose model to create the database entry
+  const thought = new Thought({text, complete});
+
+  try {
+    // Success
+    const savedThought = await  thought.save();
+    res.status(201).json(savedThought);
+  } catch (err) {
+    res.status(400).json({message: 'Could not save thought to the Database', error: err.errors});
+  }
 });
 
 // Start the server
