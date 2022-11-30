5411c8725242498c345bd1f43f4bf1a761f597f1
Fonnt <dballesterfont@gmail.com>
2022-11-30 14:48:58 +0100

    [db] Created Schema & Model / added get, post, patch endpoints

--- server.js
+++ server.js
@@ -21,6 +21,55 @@ app.get("/", (req, res) => {
   res.send("Hello Technigo!");
 });
 
+const ThoughtSchema = new mongoose.Schema({
+  message: {
+    type: String,
+    required: true,
+    minlength: 5,
+    maxlength: 140,
+    trim: true,
+  },
+  hearts: {
+    type: Number,
+    default: 0
+  },
+  createdAt: {
+    type: Date,
+    default: () => new Date()
+  }
+});
+
+const Thought = mongoose.model("Thought", ThoughtSchema);
+
+app.get("/thoughts", async (req, res) => {
+  try {
+    const thoughts = await Thought.find({}).sort({ createdAt: "desc" }).limit(20)
+    res.status(200).json({ success: true, response: thoughts })
+  } catch (err) {
+    res.status(400).json({ success: false, response: err })
+  }
+});
+
+app.post("/thoughts", async (req, res) => {
+  const { message } = req.body;
+  try {
+    const newThought = await new Thought({ message: message }).save();
+    res.status(201).json({ success: true, response: newThought });
+  } catch (err) {
+    res.status(400).json({ success: false, response: err })
+  }
+});
+
+app.patch("/thoughts/:thoughtId/like", async (req, res) => {
+  const { thoughtId } = req.params;
+  try {
+    const heartsToUpdate = await Thought.findByIdAndUpdate(thoughtId, { $inc: { hearts: 1 } });
+    res.status(200).json({ success: true, response: `The number of hearts has been updated. Total hearts: ${heartsToUpdate.hearts + 1}` });
+  } catch (err) {
+    res.status(400).json({ success: false, response: err });
+  }
+})
+
 // Start the server
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
