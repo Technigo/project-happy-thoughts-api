4b767ed2627adcaa3053aa07546ac06199c48edc
Josefine Engdahl <josefineengdahl@MacBook-Air-som-tillhor-Josefine.local>
2022-11-30 10:02:10 +0100

    added the endpoints

--- server.js
+++ server.js
@@ -6,6 +6,27 @@ const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/project-mongo";
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true });
 mongoose.Promise = Promise;
 
+const ThoughtsSchema = mongoose.Schema({
+message: {
+  type: String,
+  required: true,
+  minlength: 5,
+  maxlength: 140,
+  trim: true
+},
+hearts: {
+  type: Number,
+  default: 0,
+},
+
+createdAt: {
+  type: Date,
+  default: () => new Date()
+}
+})
+
+const Thought = mongoose.model("Thought", ThoughtsSchema)
+
 // Defines the port the app will run on. Defaults to 8080, but can be overridden
 // when starting the server. Example command to overwrite PORT env variable value:
 // PORT=9000 npm start
@@ -18,7 +39,60 @@ app.use(express.json());
 
 // Start defining your routes here
 app.get("/", (req, res) => {
-  res.send("Hello Technigo!");
+  res.send("This is my API for the project Happy Thoughts, view it live at https://steady-faun-1238b0.netlify.app");
+});
+
+
+// POST => create stuff
+// PUT => repacing something in the DB --> for example: one person switch with another
+// PATCH => change / modify stuff
+
+// ENDPOINT 1: RETURNING THE THOUGHTS
+app.get("/thoughts", async (req, res) => {
+  try {
+    const thoughts = await Thought.find().sort({ createdAt: "desc" }).limit(20);
+    res.json(thoughts);
+  } catch (err) {
+    res.status(400).json({
+      message: "Could not find this post",
+      response: err.errors,
+      success: false,
+    });
+  }
+});
+
+// ENDPOINT 2: POSTING THOUGHTS
+app.post("/thoughts", async (req, res) => {
+  const { message } = req.body
+  // console.log(req.body)
+  try {
+    const newThought =  await new Thought({ message }).save()
+    res.status(201).json({success: true, response: newThought})
+  } catch(err){
+    res.status(400).json({
+      message: "Could not post this thought",
+      response: err.errors,
+      success: false,
+    });
+  }
+})
+
+// ENDPOINT 3: LIKEING THOUGHTS
+app.patch("/thoughts/:id/like", async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const likedThought = await Thought.findByIdAndUpdate(id, {
+      $inc: { hearts: 1 },
+    });
+    res.status(200).json({success: true, response:`It's working! The post got ${likedThought.hearts} likes`});
+  } catch (err) {
+    res.status(400).json({
+      message: "Could not find and update this post",
+      response: err.errors,
+      success: false,
+    });
+  }
 });
 
 // Start the server
