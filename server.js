022cdcbab5ca2a3e9f37fd5249cc020ff78cf611
Julia Ostedt <julia.ostedt@gmail.com>
2022-11-30 10:40:46 +0100

    likes not working on FE, test to change from patch to post

--- server.js
+++ server.js
@@ -63,12 +63,12 @@ app.post("/thoughts", async (req, res) => {
   try {
     const newThought = await new Thought({message: message}).save();
     res.status(201).json({success: true, response: newThought})
-  }catch (err){
-    res.status(400).json({success: false, response: "Could not save message to the Database", error: err.errors})
+  }catch (error){
+    res.status(400).json({success: false, response: "Could not save message to the Database", error: error})
   }
 })
 
-app.patch("/thoughts/:thoughtId/like", async (req, res) => {
+app.post("/thoughts/:thoughtId/like", async (req, res) => {
    const {thoughtId} = req.params;
    try {
     const thoughtToUpdate = await Thought.findByIdAndUpdate(thoughtId, {$inc: {hearts: 1}});
@@ -78,94 +78,6 @@ app.patch("/thoughts/:thoughtId/like", async (req, res) => {
    }
 });
 
-////////////////
-/* const TechnigoMemberSchema = new mongoose.Schema({
-  name: {
-    // most important one
-    type: String,
-    // forces to provide this value
-    required: true,
-    // new name will have to be different than all others in the DB
-    unique: true,
-    // all the allowed values
-    enum: ["Matilda", "Poya", "Petra", "Hanna", "Daniel"]
-  },
-  description: {
-    type: String,
-    minlength: 4,
-    maxlength: 30,
-    // removes unnecessary whitespaces
-    trim: true
-  },
-  score: {
-    type: Number,
-    // initial value, if none other is specified
-    default: 0
-  },
-  createdAt: {
-    type: Date,
-    // new Date() will execute once - when we start the server
-    // default: new Date()
-    // onClick = { someFunction()}
-    // IIFE - in the moment that you declare the function it is called/executed right away
-    // (() => new Date())()
-    // (function functionName () {new Date()})()
-    default: () => new Date()
-  }
-});
-
-const TechnigoMember = mongoose.model("TechnigoMember", TechnigoMemberSchema); */
-
-// V1
-// app.post("/members", async (req, res) => {
-//   const {name, description} = req.body;
-//   console.log(req.body);
-//   try {
-//     const newMember = await new TechnigoMember({name: name, description: description}).save();
-//     res.status(201).json({success: true, response: newMember});
-//   } catch(error) {
-//     res.status(400).json({success: false, response: error});
-//   }
-// });
-
-
-
-// V2 POST with promises
-// app.post("/members", (req, res) => {
-//  const {name, description} = req.body;
-//  const newMember = new TechnigoMember({name: name, description: description}).save()
-//    .then(data => {
-//    res.status(201).json({success: true, response: data});
-//  }).catch(error => {
-//  res.status(400).json({success: false, response: error});
-//  });
-//});
-
-// V3 POST mongoose syntax
-// app.post("/members", (req, res) => {
-//   const {name, description} = req.body;
-//     const newMember = new TechnigoMember({name: name, description: description}).save((error, data) => {
-//       if(error) {
-//         res.status(400).json({success: false, response: error});
-//       } else {
-//         res.status(201).json({success: true, response: data});
-//       } 
-//     });
-// });
-
-// POST => create stuff
-// PUT => replace in DB -> one PErson switch with another
-// PATCH => change/modify stuff
-/* app.patch("/members/:id/score", async (req, res) => {
-   const { id } = req.params;
-   try {
-    const memberToUpdate = await TechnigoMember.findByIdAndUpdate(id, {$inc: {score: 1}});
-    res.status(200).json({success: true, response: `Member ${memberToUpdate.name} has their score updated`});
-   } catch (error) {
-    res.status(400).json({success: false, response: error});
-   }
-}); */
-///////////////
 // Start the server
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
