1123079a6a0d4b2e6882ee9e46c819e5778e2e7a
Maria <so.mariagloria@gmail.com>
2022-12-01 01:21:39 +0200

    Setting up mongoose Schema elements

--- server.js
+++ server.js
@@ -1,11 +1,16 @@
 import express from "express";
+import bodyParser from "body-parser";
 import cors from "cors";
 import mongoose from "mongoose";
+import dotenv from 'dotenv'
 
-const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/project-mongo";
+dotenv.config()
+
+const mongoUrl = process.env.MONGO_URL 
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true });
 mongoose.Promise = Promise;
 
+// var bodyParser = require('body-parser')
 // Defines the port the app will run on. Defaults to 8080, but can be overridden
 // when starting the server. Example command to overwrite PORT env variable value:
 // PORT=9000 npm start
@@ -20,6 +25,20 @@ app.use(express.json());
 app.get("/", (req, res) => {
   res.send("Hello Technigo!");
 });
+
+const person = new mongoose.Schema({ // allowing us to create a object, below the description/type
+  name: {
+    type: String, // type , madatory , most important
+    required : true, // providing a name mandatory
+    unique : true, // new name have to be diff than all others from the database
+    enum : ['Ben', 'Jerry', 'Donald Duck' , 'Hercules'] // takes an array of values
+  },
+  description : {
+    type: String,
+    minlength : 4,
+    maxlength : 30,
+  }
+  })
 
 // Start the server
 app.listen(port, () => {
