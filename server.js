cde4d8e7f65e43ee8ab58169ec6824897b37d272
Sailornina <sylcardozo.sc@gmail.com>
2022-11-30 16:36:14 +0100

    POST request was created

--- server.js
+++ server.js
@@ -7,9 +7,6 @@ const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/happy-thoughts";
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true });
 mongoose.Promise = Promise;
 
-// Defines the port the app will run on. Defaults to 8080, but can be overridden
-// when starting the server. Example command to overwrite PORT env variable value:
-// PORT=9000 npm start
 const port = process.env.PORT || 8080;
 const app = express();
 
@@ -51,10 +48,20 @@ app.get("/", (req, res) => {
 
 app.get("/thoughts", async (req, res) => {
   try {
-    const thoughts = await Thought.find()
+    const thoughts = await Thought.find().sort({createdAt: 'desc'}).limit(20).exec();
     res.status(200).json(thoughts)
   } catch (error) {
     res.status(400).json({ message: "Failed to load thoughts" })
+  }
+});
+
+app.post("/thoughts/", async (req, res) => {
+  const { message } = req.body
+  try {
+    const newThought = await Thought({ message }).save()
+    res.status(200).json({ newThought })
+  } catch (error) {
+    res.status(400).json({ error: "Thought cannot be sent" })
   }
 });
 
