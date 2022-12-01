17a4e1b0624ddeaf227533a3ebfb30518f656772
Maria <so.mariagloria@gmail.com>
2022-12-02 01:01:01 +0200

    post and patch created

--- server.js
+++ server.js
@@ -26,21 +26,91 @@ app.get("/", (req, res) => {
   res.send("Hello Technigo!");
 });
 
-const person = new mongoose.Schema({ // allowing us to create a object, below the description/type
+const ChatSchema = new mongoose.Schema({ // allowing us to create a object, below the description/type
   name: {
     type: String, // type , madatory , most important
-    required : true, // providing a name mandatory
-    unique : true, // new name have to be diff than all others from the database
-    enum : ['Ben', 'Jerry', 'Donald Duck' , 'Hercules'] // takes an array of values
+    required: true, // providing a name mandatory
+    unique: true, // new name have to be diff than all others from the database
+    enum: ["Ben", "Jerry", "Donald Duck", "Hercules"] // takes an array of values
   },
-  description : {
+  description: { // main block of happy thought
     type: String,
-    minlength : 4,
-    maxlength : 30,
+    minlength: 4,
+    maxlength: 30,
+    trim: true, // removes unnecessary whitespace
+  },
+  score: {
+    type: Number,
+    default: 0
+  }, 
+  createdAt: {
+    type: Date,               // on default you can do logic with anonymous function on Schemas () =>
+    default: () => new Date() // executing once we run the server. if you want to call it immediately,(for req info too): (()=> new Date)() or like this (function functioName () {new DAte()})()
   }
   })
 
-// Start the server
+  const Person = mongoose.model("Person", ChatSchema)
+
+  app.post("/chat", async (req, res) => {
+    const {name, description} = req.body // ex. "name": "Maria" => terminal gives output
+    console.log(req.body)
+    try {
+      const newInput = await new Person({ name: name , description: description}).save()
+      res.status(201).json({ success: true, response: newInput }) // 201 created success
+    } catch (error) {
+      res.status(400).json({ success: false, response: error })
+    }
+  })
+
+  app.patch("/chat/:id/score", async (req, res) => {
+    const { id } = req.params
+    try {
+      const update = await Person.findByIdAndUpdate(id, {$inc: {score: 1}})
+      res.status(200).json({ success: true, response: `Member ${update.name}`})
+    } catch (error) {
+      res.status(401).json({ success: false, response: error})
+    }
+  })
+
+  // Start the server
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
 });
+
+
+
+/********** Different options for syntax *******/
+
+// Mongoose syntax style
+/*app.post("/chat", (req, res) => {
+  const {name, description} = req.body // ex. "name": "Maria" => terminal gives output
+
+  const newInput = new Person({ name: name, description: description}).save((error, data) => {
+    if (error) {
+      res.status(400).json({ success: false, response: error })
+      console.log("No")
+    } else {
+      res.status(201).json({ success: true, response: data }) // 201 created success
+      console.log("")
+    }
+  })  
+})*/
+
+//Promises syntax
+/*app.post("/chat", (req, res) => {
+  const {name, description} = req.body
+  const newInput = new Person({ name: name , description: description}).save()
+  .then(data => {
+    res.status(201).json({ success: true, response: data })
+    console.log("Work")
+  })
+  .catch(error => {
+    res.status(400).json({ success: false, response: error})
+    console.log("Nope")
+  })
+})*/
+
+//POST => create stuff
+//PUT => replace
+//PATCH => modify/change
+
