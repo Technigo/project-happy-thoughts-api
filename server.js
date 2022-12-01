56cf93a1ac018d87c6ca328f4632596527dbd72b
Petra Eriksson <eriksson.petra@gmail.com>
2022-12-01 14:16:01 +0100

    changed a letter to see if it udates correctly in google

--- server.js
+++ server.js
@@ -39,7 +39,7 @@ app.use(express.json());
 
 // Start defining your routes here
 app.get("/", (req, res) => {
-  res.send("Hello Technigo!");
+  res.send("Hello Technigoo!");
 });
 
 app.get("/thoughts", async (req, res) => {
