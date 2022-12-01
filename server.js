2a484b659c1914503b1e3ededc7d76a2955c828f
Petra Eriksson <eriksson.petra@gmail.com>
2022-12-01 13:36:52 +0100

    created a model and endnodes with restrictions

--- server.js
+++ server.js
@@ -6,6 +6,27 @@ const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/project-mongo";
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true });
 mongoose.Promise = Promise;
 
+// creating the schema, the structur of our model
+const ThoughtSchema = new mongoose.Schema({
+  message: {
+    type: String,
+    required: true,
+    minlenght: 5,
+    maxlenght: 140,
+    trim: true,
+  },
+  heart: {
+    type: Number,
+    default: 0,
+  },
+  createdAt: {
+    type: Date,
+    default: () => new Date(),
+  },
+});
+
+const Thoughts = mongoose.model("Thoughts", ThoughtSchema);
+
 // Defines the port the app will run on. Defaults to 8080, but can be overridden
 // when starting the server. Example command to overwrite PORT env variable value:
 // PORT=9000 npm start
@@ -19,6 +40,42 @@ app.use(express.json());
 // Start defining your routes here
 app.get("/", (req, res) => {
   res.send("Hello Technigo!");
+});
+
+app.get("/thoughts", async (req, res) => {
+  const thoughts = await Thoughts.find()
+    .sort({ createdAt: "desc" })
+    .limit(20)
+    .exec();
+  res.json(thoughts);
+});
+
+app.post("/thoughts", async (req, res) => {
+  const { message } = req.body;
+  try {
+    const newThought = await new Thoughts({
+      message: message
+    }).save();
+    res.status(201).json({ sucess: true, resonse: newThought });
+  } catch (error) {
+    res.status(400).json({ success: false, response: error });
+  }
+});
+app.patch("/thoughts/:id/like", async (req, res) => {
+  const { id } = req.params;
+  try {
+    const heartToUpdate = await Thoughts.findByIdAndUpdate(id, {
+      $inc: { heart: 1 },
+    });
+    res
+      .status(200)
+      .json({
+        success: true,
+        response: `Thought ${heartToUpdate.message} has it's heart updated`,
+      });
+  } catch (error) {
+    res.status(400).json({ success: false, response: error });
+  }
 });
 
 // Start the server
