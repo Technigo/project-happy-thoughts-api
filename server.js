5371836f5bd0c716f8662504b9ceb7f0fa570641
ViktorSvenssonN <viktor.tommy.svensson@gmail.com>
2022-12-01 17:43:00 +0100

    added comments cleaned up the code

--- server.js
+++ server.js
@@ -6,6 +6,8 @@ const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/project-Happy-tho
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true });
 mongoose.Promise = Promise;
 
+// Thought "Schema" defines the structer of how the thoughts in the frontend is handled
+
 const ThoughtSchema = new mongoose.Schema({
  message: {
     type: String,
@@ -26,25 +28,30 @@ const ThoughtSchema = new mongoose.Schema({
 
 const Thought = mongoose.model("Thought", ThoughtSchema);
 
-// Defines the port the app will run on. Defaults to 8080, but can be overridden
-// when starting the server. Example command to overwrite PORT env variable value:
-// PORT=9000 npm start
 const port = process.env.PORT || 8080;
 const app = express();
 
-// Add middlewares to enable cors and json body parsing
 app.use(cors());
 app.use(express.json());
 
-// Start defining your routes here
 app.get("/", (req, res) => {
-  res.send("Hello Technigo!");
+  "Hello, this is a backend built for: https://dynamic-puffpuff-f7c490.netlify.app/"
+  res.send([
+    {"Hello":  "This is a backend built for: https://dynamic-puffpuff-f7c490.netlify.app/"},
+    {"/": "start page for backend api : https://project-happy-thoughts-api-nczig4g6cq-lz.a.run.app/"},
+    {"/thoughts": "Displays the latest 20 thoughts. Example: https://project-happy-thoughts-api-nczig4g6cq-lz.a.run.app/thoughts"},
+    {"/thoughts/:id/hearth": "creates a route where the user where we store how many likes the thought, ex: https://project-happy-thoughts-api-nczig4g6cq-lz.a.run.app/63863c0713c6c039df9e3b22/hearth"},
+  ]);
 });
+
+// Route where a the latest 20 thoughts is shown.
 
 app.get("/thoughts", async (req, res) => {
   const thought = await Thought.find().sort({createdAt: 'desc'}).limit(20).exec();
   res.json(thought);
 })
+
+// Route where a new thought is created, which is stored in the backend
 
 app.post("/thoughts", async (req, res) => {
   const {message, createdAt} = req.body;
@@ -57,7 +64,8 @@ app.post("/thoughts", async (req, res) => {
   }
 });
 
-// we want to updated the like
+// Route where we add +1 to hearth (like) on a thought
+
 app.patch("/thoughts/:id/hearth", async (req, res) => {
 const { id } = req.params; // *deconstucting*
 try {
@@ -68,8 +76,6 @@ try {
 }
 })
 
-
-// Start the server
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
 });
