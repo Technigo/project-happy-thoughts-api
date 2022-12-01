4594b4f88d9fad5ac0c79f8557d56ad17881bb82
Kristin <kristin@iMac.local>
2022-12-01 15:28:11 +0100

    Updated code from code along/live session to this weeks happy thought

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
-import express from "express";
+import express, { response } from "express";
 import cors from "cors";
 import mongoose from "mongoose";
 
-const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/project-mongo";
+const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/project-happythought";
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true });
 mongoose.Promise = Promise;
 
@@ -18,7 +18,79 @@ app.use(express.json());
 
 // Start defining your routes here
 app.get("/", (req, res) => {
-  res.send("Hello Technigo!");
+const navigation = {
+    guide: "Routes for this API",
+    Routes: [
+      {
+      "/thoughts": "Happy thoughts API"
+      },
+    ],
+  };
+  res.send(navigation);
+});
+
+const ThoughtSchema = new mongoose.Schema({
+  message: {
+    type: String,
+    required: true,
+    unique: true,
+    minlength: 2,
+    maxlength:30,
+    trim: true,
+  },
+  hearts: {
+    type: Number,
+    default: 0,
+  },
+  createdAt: {
+    type: Date,
+    default: () => new Date()
+  }
+});
+
+const Thought = mongoose.model("Thought", ThoughtSchema);
+
+app.post("/thoughts", async (req, res) => {
+  const { message } = req.body;
+    try {
+    const newThought = await new 
+    Thought({message}).save()
+    res.status(201).json({
+      success: true, 
+      response: newThought})
+    } catch(err) {
+      res.status(400).json({
+      message: "Could not post this thought",
+      response: err.errors,
+      success: false,
+    })}
+});
+
+app.get("/thoughts", async (req, res) => {
+    try {
+    const thoughts = await new 
+    Thought.find().limit(20).sort({ createdAt: "desc" })
+    res.status(200).json({
+    success: true,
+    response: thoughts
+    })
+  } catch (err) {
+    res.status(400).json({
+      message: "Could not find this post",
+      response: err.errors,
+      success: false,
+    });
+  }
+});
+
+app.patch("/members/:id/like", async (req, res) => {
+   const { id } = req.params;
+   try {
+    const thoughtToUpdate = await Thought.findByIdAndUpdate(id, {$inc: {heart: 1}});
+    res.status(200).json({success: true, response: `Thought ${thoughtToUpdate.hearts} has been updated`});
+   } catch (error) {
+    res.status(400).json({success: false, response: error});
+   }
 });
 
 // Start the server
