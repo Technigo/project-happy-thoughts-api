213a9b712ee43007e83f57a6598912621e456f9c
jessikalind <jessika.lind@me.com>
2022-12-01 10:47:42 +0100

    patch request working

--- server.js
+++ server.js
@@ -6,6 +6,22 @@ const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/project-mongo";
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true });
 mongoose.Promise = Promise;
 
+const Thought = mongoose.model('Thought', {
+  text: {
+    type: String,
+    minlength: 5
+  }, 
+  createdAt: {
+    type: Date,
+    default: () => new Date()
+    // default: (() => new Date())() -- IIFE - in the moment that you declare the function it is called/executed right away.
+  }, 
+  hearts: {
+    type: Number,
+    default: 0
+  }
+})
+
 // Defines the port the app will run on. Defaults to 8080, but can be overridden
 // when starting the server. Example command to overwrite PORT env variable value:
 // PORT=9000 npm start
@@ -20,6 +36,37 @@ app.use(express.json());
 app.get("/", (req, res) => {
   res.send("Hello Technigo!");
 });
+
+//Get-request
+app.get("/thoughts", async (req, res) => {
+  const thoughts = await Thought.find().sort({createdAt: 'desc'}).limit(20).exec()
+  res.json(thoughts)
+})
+
+//Post-request
+app.post("/thoughts", async (req, res) => {
+  const {text} = req.body
+  const thought = new Thought({text})
+
+  try {
+    const savedThought = await thought.save()
+    res.status(201).json({sucess: true, response: savedThought})
+  } catch (err) {
+    res.status(400).json({sucess: false, message: 'Could not save thought to the database', error: err.errors})
+  }
+})
+
+// PATCH-request
+app.patch("/thoughts/:id/hearts", async (req, res) => {
+  const { id } = req.params
+  const thoughtToUpdate = await Thought.findByIdAndUpdate(id, {$inc: {hearts: 1}})
+
+  try {
+    res.status(200).json({success: true, response: `Thought ${thoughtToUpdate.id} has their hearts updated`})
+  } catch (err) {
+    res.status(400).json({success: false, message: 'Could not save thought to the database', error: err.errors})
+  }
+})
 
 // Start the server
 app.listen(port, () => {
