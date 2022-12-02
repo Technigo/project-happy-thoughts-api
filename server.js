a7abfd82142bb69715623465ba1999c3c1f0ad33
Tara78 <70331037+Tara78@users.noreply.github.com>
2022-12-02 11:32:56 +0100

    .

--- server.js
+++ server.js
@@ -7,7 +7,6 @@ const mongoUrl = process.env.MONGO_URL || "mongodb://127.0.0.1/project-mongo";
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true });
 mongoose.Promise = Promise;
 
-// when starting the server. Example command to overwrite PORT env variable value:
 // PORT=9000 npm start
 const port = process.env.PORT || 8000;
 const app = express();
