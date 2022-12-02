445c7953540c2bf447a2a7a012d41dd30226b890
Kneedler <sarahklindelow@gmail.com>
2022-12-02 13:39:54 +0100

    route to delete and enter name

--- README.md
+++ README.md
@@ -1,13 +1,18 @@
 # Project Happy Thoughts API
 
+This is an API built with mongoose with the help of MongoDB. It's connected to the frontend here: https://sage-bavarois-1b13f0.netlify.app/
+
+It is possible to POST new happy thoughts, GET older happy thoughts and 'like' them via PATCH. 
+
 Replace this readme with your own information about your project.
 
 Start by briefly describing the assignment in a sentence or two. Keep it short and to the point.
 
 ## The problem
+The problem i came by was displaying the data in the frontend, it turned out to be some misspelling in the Schema and using a different property. I solved it by going through the code, both front and back, to understand the problem and solve it. It brings a good understaning of the code. I used postman, MongoDB compass and atlas, express, Node.js and mongoose. 
 
 Describe how you approached to problem, and what tools and techniques you used to solve it. How did you plan? What technologies did you use? If you had more time, what would be next?
 
 ## View it live
+https://project-happy-thoughts-api-wqvqkjwgmq-lz.a.run.app
 
-Every project should be deployed somewhere. Be sure to include the link to the deployed project so that the viewer can click around and see what it's all about.
--- server.js
+++ server.js
@@ -2,7 +2,6 @@ import express, { response } from "express";
 import cors from "cors";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
-import { restart } from "nodemon";
 
 dotenv.config()
 
@@ -11,6 +10,12 @@ mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true });
 mongoose.Promise = Promise;
 
 const ThoughtSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    maxlength: 30,
+    required: true,
+    trim: true
+  },
   message: {
     type: String,
     required: true,
@@ -37,20 +42,19 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-/* app.use((req, res, next) => {
+app.use((req, res, next) => {
   if (mongoose.connection.readyState === 1) {
     next()
   } else {
     res.status(503).json({ error: 'Service unavailable' })
   }
-}) */
+})
 
 // Start defining your routes here
 app.get("/", (req, res) => {
   res.send("Hello Technigo!");
 });
 
-//Pagination
 app.get('/thoughts', async (req, res) => {
   try {
   const thoughtList = await Thought.find().sort({createdAt: 'desc'}).limit(20).exec();
@@ -60,23 +64,22 @@ app.get('/thoughts', async (req, res) => {
 }
 });
 
+
+
 app.post("/thoughts", async (req, res) => {
-  const { message } = req.body;
+  const { name, message } = req.body;
   try {
-    const newThought = await new Thought({message: message}).save();
+    const newThought = await new Thought({name: name, message: message}).save();
     res.status(201).json({success: true, response: newThought});
   } catch (error) {
     res.status(400).json({success: false, response: error, message: 'could not save thought to the database' })
   }
 });
 
-/* const newThought = await new Thought({text: text , like: like, createdAt: createdAt }).save();
-const { text, like, createdAt } = req.body; */
-
 app.patch("/thoughts/:thoughtId/like", async (req, res) => {
   const { thoughtId } = req.params;
   try {
-   const thoughtToUpdate = await Thought.findByIdAndUpdate(thoughtId, {$inc: {hearts: 1}}/* , {new: true} */);
+   const thoughtToUpdate = await Thought.findByIdAndUpdate(thoughtId, {$inc: {hearts: 1}});
     if (thoughtToUpdate) {
    res.status(200).json({success: true, response: `Thought ${thoughtToUpdate.thoughtId} has been like'ed`});
   } else {
@@ -84,6 +87,20 @@ app.patch("/thoughts/:thoughtId/like", async (req, res) => {
   }
   } catch (error) {
    res.status(400).json({success: false, error: 'Invalid request' });
+  }
+});
+
+app.delete("/thoughts/:thoughtId", async (req, res) => {
+  const { thoughtId } = req.params;
+  try {
+    const deletedThought = await Thought.findOneAndDelete({_id: thoughtId});
+    if (deletedThought) {
+      res.status(200).json({success: true, response: deletedThought});
+    } else {
+      res.status(404).json({success: false, response: 'Thought not found'});
+    }
+  } catch (error) {
+    res.status(400).json({success: false, response: error});
   }
 });
 
