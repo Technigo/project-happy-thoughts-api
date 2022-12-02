f6b89937bd0553c29a7a3ed2f32466549b4b51d5
YoTesfam <102465267+YoTesfam@users.noreply.github.com>
2022-12-02 13:38:35 +0100

    Added POST and testing on google cloud

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 import express from "express";
 import cors from "cors";
 import mongoose from "mongoose";
+import dotenv from "dotenv"
 
+
+dotenv.config()
 const mongoUrl = process.env.MONGO_URL || `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PW}@cluster0.lmkbhok.mongodb.net/happythoughtsAPI?retryWrites=true&w=majority`;
 // "mongodb://localhost/project-mongo";
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true });
@@ -17,10 +20,48 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-// Start defining your routes here
-app.get("/", (req, res) => {
-  res.send("Hello Technigo!");
+const ThougtsSchema = new mongoose.Schema({
+  message: {
+    type: String,
+    required: true,
+    minlength: 4,
+    maxlength: 140,
+    trim: true
+  },
+  hearts: {
+    type: Number,
+    default: 0
+  },
+
+  createdAt: {
+    type: Date,
+    default: () => new Date()
+  }
 });
+
+const Thoughts = mongoose.model("Thoughts", ThougtsSchema);
+
+//*******ROUTES*********//
+
+// Displaying thoughs in descending order with a maximum of 20 thoughts.
+app.get("/thoughts", async (req, res) => {
+  const thoughts = await Thoughts.find().sort({createdAt: "desc"}).limit(20).exec();
+  res.json(thoughts);
+})
+
+app.post('/thoughts', async(req, res) =>{
+  // Collect the information sent by the client to our API 
+  const {message , hearts} = req.body;
+
+  // Use our mongoose model to create the database entry - to save
+  const thought = new Thoughts({message, hearts})
+  try{
+    const savedThought = await thought.save();
+  res.status(201).json(savedThought);
+  }catch(err){
+    res.status(400).json({message: 'Could not save thought to the database', errors: err.errors});
+  }
+})
 
 // Start the server
 app.listen(port, () => {
