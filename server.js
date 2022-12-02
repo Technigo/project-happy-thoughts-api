144db07e3ba436521f99d98c6350fdeff9a40bc7
Cecilia Råden <you@example.com>
2022-12-02 10:09:03 +0100

    cleaned code and commented the routes

--- server.js
+++ server.js
@@ -6,9 +6,6 @@ const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/project-mongo";
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true });
 mongoose.Promise = Promise;
 
-// Defines the port the app will run on. Defaults to 8080, but can be overridden
-// when starting the server. Example command to overwrite PORT env variable value:
-// PORT=9000 npm start
 const port = process.env.PORT || 8080;
 const app = express();
 
@@ -17,6 +14,7 @@ app.use(cors());
 app.use(express.json());
 
 
+// The schema which sets the rules for the messages, like-buttons and timestamp
 const ThoughtsSchema = new mongoose.Schema({
   message: {
     type: String,
@@ -37,15 +35,13 @@ const ThoughtsSchema = new mongoose.Schema({
 
 const Thoughts = mongoose.model("Thoughts", ThoughtsSchema);
 
-/* app.get("/", (req, res) => {
-  res.send("Cicci testar!");
-}); */
-
+// Fetching the thoughts and ordering them in a descending order, with a maximum of 20 thoughts.
 app.get("/thoughts", async (req, res) => {
   const thoughts = await Thoughts.find().sort({createdAt: "desc"}).limit(20).exec();
   res.json(thoughts);
 })
 
+// Posting the new thoughts
 app.post("/thoughts", async (req, res) => {
   const {message} = req.body;
   const thought = new Thoughts({message})
@@ -57,6 +53,7 @@ app.post("/thoughts", async (req, res) => {
   }
 })
 
+// The likes from the heart button, which increases by one everytime you click on them
 app.patch("/thoughts/:thoughtId/like", async (req, res) => {
 const { thoughtId } = req.params
 try {
@@ -67,17 +64,6 @@ res.status(200).json({success: true, response: `There are ${likesToUpdate.hearts
 }
 })
 
-/*
-app.patch("/members/:id/score", async (req, res) => {
-  const { id } = req.params;
-  try {
-   const memberToUpdate = await TechnigoMember.findByIdAndUpdate(id, {$inc: {score: 1}});
-   res.status(200).json({success: true, response: `Member ${memberToUpdate.name} has their score updated`});
-  } catch (error) {
-   res.status(400).json({success: false, response: error});
-  }
-});
-*/
 // Start the server
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
