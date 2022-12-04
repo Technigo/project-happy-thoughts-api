86ed92d9bc5d3c31363a2bec2a80a783981726f5
jessnmo <jessicamo92@gmail.com>
2022-12-04 18:16:11 +0100

    Update server.js

--- server.js
+++ server.js
@@ -2,8 +2,7 @@ import express from 'express';
 import cors from 'cors';
 import mongoose from 'mongoose';
 
-const mongoUrl =
-	process.env.MONGO_URL || 'mongodb://localhost/project-project-mongo';
+const mongoUrl = process.env.MONGO_URL || 'mongodb://localhost/project-mongo';
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true });
 mongoose.Promise = Promise;
 
