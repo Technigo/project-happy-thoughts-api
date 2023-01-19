7a60f6ae7f992605cc8cb6a055b218c528119f9a
jnbjensen <mail@jnbjensen.com>
2023-01-19 10:18:43 +0100

    Set cors origin to *

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-import express, { response } from "express";
+import express from "express";
 import cors from "cors";
 import mongoose from "mongoose";
 
@@ -14,7 +14,7 @@ const port = process.env.PORT || 8080;
 const app = express();
 
 // Add middlewares to enable cors and json body parsing
-app.use(cors());
+app.use(cors({origin: '*'}));
 app.use(express.json());
 
 // Creating Schema
