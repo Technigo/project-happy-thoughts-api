e459c7ad386fd9e5289d6a094cc62f385db90e17
Iracuzzi <arnau.monforte@gmail.com>
2023-01-22 22:30:31 +0100

    rerework

--- server.js
+++ server.js
@@ -1,100 +1,96 @@
-import express, { text } from "express";
+import express, { response } from "express";
 import cors from "cors";
 import mongoose from "mongoose";
-const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/project-happy-thoughts";
+
+const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/project-mongo";
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true });
 mongoose.Promise = Promise;
 
-
-const Thought = mongoose.model('Thought', {
-  message: {
-    type: String,
-    required: true,
-    minlength: 5,
-    maxlength: 140
-  },
-  hearts: {
-    type: Number,
-    default: 0
-
-  }, 
-  createdAt: {
-    type: Date,
-    default: Date.now
-  }
-
-});
-
-
 const port = process.env.PORT || 8080;
 const app = express();
+
+// Add middlewares to enable cors and json body parsing
 app.use(cors());
 app.use(express.json());
 
 
+
+const ThoughtSchema = new mongoose.Schema({
+  message: {
+    type: String,
+    required: true,
+    minlength: 5,
+    maxlength: 140,
+    trim: true
+  },
+  hearts: {
+    type: Number,
+    default: 0
+  },
+  createdAt: {
+    type: Date,
+    default: () => new Date()
+  }
+});
+
+const Thought = mongoose.model("Thought", ThoughtSchema);
+
+// Start defining your routes here
 app.get("/", (req, res) => {
-  res.send("Welcome!");
+  res.json({ 
+    Message: "Welcome to Happy Thoughts",
+    Routes: {
+      "/thoughts": "Is to GET and POST Thouhgts",
+      "/thoughts/:id/like": "a PATCH that will add Hearts to the Happy Thoughts"
+    }
+  });
 });
 
 app.get("/thoughts", async (req, res) => {
   try {
-    const thoughts = await Thought.find().sort({createdAt: 'desc'}).limit(20).exec();
-    res.json(thoughts);
-  }
-  catch (error) {
+    const thoughts = await Thought.find().sort({createdAt: -1}).limit(20).exec();
+    res.status(201).json({
+      success: true, 
+      response: thoughts
+    });
+  } catch(error) {
     res.status(400).json({
       success: false,
-      response: error,
+      response: error
     });
   }
- 
-});
+})
 
-app.post('/thoughts', async (req, res) => {
- const {message, complete} = req.body;
-
- const thought = new Thought({message, complete});
-
- try{
-  const savedThought = await thought.save();
-  res.status(201).json({success: true, response: savedThought});
- } catch (error){
-  res.status(400).json({success: false, message: "Could not save happy thought to db", error: err.errors});
-
- }
-});
-
-app.patch("/thoughts/:id/like", async (req, res) => {
-  const { id } = req.params;
-  try{
-  const heartsUpToDate = await Thought.findByIdAndUpdate(id, {$inc: {hearts: 1}})
-  res.status(200).json({success: true, response:`Thought ${heartsUpToDate.id} has been updated with a heart`})
-  } catch (error) {
-    res.status(400).json({success: false, response: "Thought-id not found"});
-  }
-});
-
-app.delete("/thoughts/:id", async (req, res) => {
-  const { id } = req.params;
+app.post("/thoughts", async (req, res) => {
+  const {message} = req.body;
   try {
-    const deletedThought = await Thought.findOneAndDelete({ _id: id });
-    if (deletedThought) {
-      res.status(200).json({
-        success: true,
-        response: deletedThought,
-        message: "thought is now deleted"
-      });
-    } else {
-      res.status(404).json({ success: false, response: "Thought id not found" });
-    }
-  } catch (error) {
+    const newThought = await new Thought({message: message}).save();
+    res.status(201).json({
+      success: true, 
+      response: newThought
+    });
+  } catch(error) {
     res.status(400).json({
       success: false,
-      response: error,
+      response: error
     });
   }
+})
+
+app.patch("/thoughts/:thoughtId/like", async (req, res) => {
+   const { id } = req.params;
+   try {
+    const heartsToUpdate = await Thought.findByIdAndUpdate(id, {$inc: {hearts: 1}});
+    res.status(200).json({
+      success: true, 
+      response: `Thought ${heartsToUpdate.thought} has their hearts updated`});
+   } catch (error) {
+    res.status(400).json({
+      success: false, 
+      response: 'Could not updated thought'});
+   }
 });
 
+// Start the server
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
-});
