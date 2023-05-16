61d4ce6dff89c847ad529743ec506eea8f5f12a9
cpz-manifold <carol.pinzon.ardila@gmail.com>
2023-05-16 18:46:46 +0200

    schema

--- server.js
+++ server.js
@@ -18,8 +18,27 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-// thoughts schema 
-// model 
+// Thoughts schema
+const ThoughtsSchema = new mongoose.Schema({
+  message: {
+    type: String,
+    required: true, 
+    minlength: 5,
+    maxlength: 140, 
+    trim: true 
+  }, 
+  hearts: {
+    type: Number, 
+    default: 0
+  }, 
+  createdAt: {
+    type: Date, 
+    default: () => new Date()
+  }
+});
+
+// Model
+const Thought = mongoose.model("Thought", ThoughtsSchema);
 
 
 // Routes
