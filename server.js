bde605a63693d9086f8914cc6dc7c18e0d3057aa
smirrebinx <48405465+smirrebinx@users.noreply.github.com>
2023-05-16 15:52:36 +0200

    Add app.post tasks

--- server.js
+++ server.js
@@ -2,9 +2,25 @@ import express from "express";
 import cors from "cors";
 import mongoose from "mongoose";
 
-const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/project-mongo";
+const mongoUrl = process.env.MONGO_URL || "mongodb://127.0.0.1:27017/project-happy-thoughts-api";
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true });
 mongoose.Promise = Promise;
+
+const Task = mongoose.model('Task', {
+  text: {
+    type: String,
+    required: true,
+    minlength: 5
+  }, 
+  complete: {
+    type: Boolean,
+    default: false
+  }, 
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
+});
 
 // Defines the port the app will run on. Defaults to 8080, but can be overridden
 // when starting the server. Example command to overwrite PORT env variable value:
@@ -19,6 +35,27 @@ app.use(express.json());
 // Start defining your routes here
 app.get("/", (req, res) => {
   res.send("Hello Technigo!");
+});
+
+app.get('/tasks', async (req, res) => {
+  const tasks = await Task.find().sort({createdAt: 'desc'}).limit(20).exec();
+  res.json(tasks);
+});
+
+app.post('/tasks', async (req, res) => {
+  // Retrieve the information sent by the client to our API endpoint
+  const {text, complete} = req.body;
+
+  // Use our mongoose model to create the database
+  const task = new Task({text, complete});
+
+  try {
+    // Success
+    const savedTask = await task.save();
+    res.status(201).json(savedTask);
+  } catch (err) {
+    res.status(400).json({message:'Could not save task to the Database', error: err.errors});
+  }
 });
 
 // Start the server
