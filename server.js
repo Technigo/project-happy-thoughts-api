1f9e6e61b400a6055d193d6acc5170b847f9cf3f
Ed Code <edvard.ekstrom@gmail.com>
2023-05-16 16:12:52 +0200

    functional

--- server.js
+++ server.js
@@ -2,7 +2,8 @@ import express from "express";
 import cors from "cors";
 import mongoose from "mongoose";
 
-const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/project-mongo";
+const mongoUrl =
+  process.env.MONGO_URL || "mongodb://localhost/project-happy-thoughts-api";
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true });
 mongoose.Promise = Promise;
 
@@ -16,9 +17,66 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+const Thought = new mongoose.model("Thought", {
+  message: {
+    type: String,
+    required: true,
+    minlength: 5,
+    maxlength: 150,
+  },
+  hearts: {
+    type: Number,
+    default: 0,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+  },
+});
+
 // Start defining your routes here
 app.get("/", (req, res) => {
   res.send("Hello Technigo!");
+});
+
+app.get("/thoughts", async (req, res) => {
+  const thoughts = await Thought.find()
+    .sort({ createdAt: "desc" })
+    .limit(20)
+    .exec();
+  res.json(thoughts);
+});
+
+app.post("/thoughts", async (req, res) => {
+  const { message } = req.body;
+  const thought = new Thought({ message });
+
+  try {
+    const savedThought = await thought.save();
+    res.status(201).json(savedThought);
+  } catch (err) {
+    res.status(400).json({
+      message: "Could not save thought to database",
+      error: err.errors,
+    });
+  }
+});
+
+app.post("/thoughts/:thoughtId/like", async (req, res) => {
+  const thoughtId = req.params.thoughtId;
+
+  try {
+    const hearted = await Thought.updateOne(
+      { _id: thoughtId },
+      { $inc: { hearts: 1 } }
+    );
+    res.status(201).json(hearted);
+  } catch (err) {
+    res.status(400).json({
+      message: "Could not update likes",
+      error: err.errors,
+    });
+  }
 });
 
 // Start the server
