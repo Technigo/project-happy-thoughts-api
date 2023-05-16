3f409fa0a635dac7ed53cb38706429e8d1c164eb
camcron <camillacronqvist@hotmail.com>
2023-05-16 21:43:57 +0200

    Added routes for GET and POST requests

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ import express from "express";
 import cors from "cors";
 import mongoose from "mongoose";
 
-const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/project-mongo";
+const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/project-happy-thoughts";
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true });
 mongoose.Promise = Promise;
 
@@ -18,7 +18,66 @@ app.use(express.json());
 
 // Start defining your routes here
 app.get("/", (req, res) => {
-  res.send("Hello Technigo!");
+  res.send("Happy Thoughts API!");
+});
+
+const { Schema } = mongoose;
+const thoughtsSchema = new Schema({
+  message: {
+    type: String,
+    required: true,
+    minlength: 5,
+    maxlength: 140,
+    trim: true
+  },
+  hearts: {
+    type: Number,
+    default: 0,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
+}); 
+
+const Thought = mongoose.model("thoughts", thoughtsSchema);
+
+app.get('/thoughts', async (req, res) => {
+  const thoughts = await Thought.find().sort({ createdAt: 'desc' }).limit(20).exec();
+  res.json(thoughts);
+});
+
+app.post("/thoughts", async (req, res)=>{
+  const {message} = req.body;
+    try{
+      const thought = await new Thought({message}).save();
+      res.status(201).json({
+       success: true,
+        response: thought,
+        message: "created successfully"
+      });
+    } catch (error) {
+      res.status(400).json({
+        success: false,
+        response: error,
+        message: "An error occurred when creating the thought"
+      });
+    }
+});
+
+app.post('/thoughts/:thoughtId/like', async (req, res) => {
+  const { thoughtId } = req.params;
+
+  try {
+    const updatedThought = await Thought.findByIdAndUpdate(thoughtId, { $inc: { hearts: 1 } }, { new: true });
+    if (updatedThought) {
+      res.json(updatedThought);
+    } else {
+      res.status(404).json({ message: 'The thought could not found' });
+    }
+  } catch (error) {
+    res.status(400).json({ message: 'Invalid request', error });
+  }
 });
 
 // Start the server
