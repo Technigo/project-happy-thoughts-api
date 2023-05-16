3f53aaa53f0537dc4004053831376b5e8b2270a3
JosefinRobertsson <josefinrobertsson@hotmail.com>
2023-05-16 21:51:55 +0200

    initial commit

--- server.js
+++ server.js
@@ -2,8 +2,10 @@ import express from "express";
 import cors from "cors";
 import mongoose from "mongoose";
 
-const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/project-mongo";
-mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true });
+// STOPPED AT 1:04:00
+
+const mongoUrl = process.env.MONGO_URL || "mongodb://127.0.0.1/project-happy-thoughts-api";
+mongoose.connect(mongoUrl, { useNewUrlParser : true, useUnifiedTopology: true })
 mongoose.Promise = Promise;
 
 // Defines the port the app will run on. Defaults to 8080, but can be overridden
@@ -12,14 +14,130 @@ mongoose.Promise = Promise;
 const port = process.env.PORT || 8080;
 const app = express();
 
+
 // Add middlewares to enable cors and json body parsing
 app.use(cors());
 app.use(express.json());
 
+const { Schema } = mongoose;
+
+
+const ThoughtSchema =new Schema({
+  message: {
+    type: String,
+    required: true,
+    minlength: 5,
+    maxlength: 140,
+    trim: true // removes whitespace before and after string
+    // unique: true // if you don't want to allow duplicates
+  },
+  heart: {
+    type: Number,
+    default: 0
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now // or new Date()?
+  }
+  /* To create a way to differentiate between categories. Enum is an array of all allowed values. 
+  category: {
+    type: String,
+    enum: ["love", "daily", "funny", "other"] */
+});
+
+const Thought = mongoose.model("Thought", ThoughtSchema)
+
+
+
 // Start defining your routes here
 app.get("/", (req, res) => {
-  res.send("Hello Technigo!");
+  res.send("happy thoughts");
 });
+
+// fetch from database
+app.get("/thoughts", async (req, res) => {
+  const thoughts = await Thought.find().sort({ createdAt: "desc" }).limit(20).exec();
+  res.json(thoughts);
+});
+
+// post to database
+app.post("/thoughts", async (req, res) => {
+  // retrieve information sent by client to our API endpoint
+  const { message, heart } = req.body;
+  try {
+    const savedThought = await new Thought({message, heart}).save();
+    res.status(201).json({
+      success: true,
+      response: savedThought,
+      message: "Thought successfully saved"
+    });
+  } catch (err) {
+    res.status(400).json({
+      success: false,
+      response:"Pardon, could not save", error: err.errors
+    });
+  }
+});
+
+app.patch("/thoughts/:id", async (req, res) => {
+  const { id } = req.params;
+  const { heart } = req.body;
+  try {
+    const savedThought = await Thought.findByIdAndUpdate(id, { heart: heart + 1 }, { new: true });
+    res.status(200).json({
+      success: true,
+      response: savedThought,
+      message: "Update successful"
+    });
+  } catch (err) {
+    res.status(400).json({
+      success: false,
+      response: "Pardon, could not update", error: err.errors
+    });
+  }
+});
+
+
+
+//POST to create
+//PATCH to update
+//PUT to replace
+
+/*
+//No need for async when dealing with promises. Async await is more flexible so preferrable, but promises are common in leqacy code.
+app.post("/thoughts",  (req, res) => {
+  const { message, heart } = req.body;
+  const thought = new Thought({ message, heart }).save()
+  .then(item => {
+    res.status(201).json({
+    success: true,
+    response: item,
+    message: "Thought successfully saved"
+  });
+}).catch(err => {
+  res.status(400).json({
+    success: false,
+    response: "Pardon, could not save", error: err.errors
+  });
+});
+}); */
+
+  
+  
+  /* From Vans codealong
+  // use our mongoose model to create the database entry
+  const thought = new Thought({ message });
+
+  try {
+    // success
+    const savedThought = await thought.save();
+    res.status(201).json(savedThought);
+  } catch (err) {
+    // bad request
+    res.status(400).json({ message: "Pardon, could not save", error: err.errors });
+  }
+}); */
+
 
 // Start the server
 app.listen(port, () => {
