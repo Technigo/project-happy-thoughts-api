59cd6541527de24b4c9c7643eb26b0e2d9472353
Eva Sjöholm <e.sjoholm.int@gmail.com>
2023-05-16 18:10:31 +0200

    trying to make URL work, not working

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ import express from "express";
 import cors from "cors";
 import mongoose from "mongoose";
 
-const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/EvaThoughtApi";
+const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/evathoughtapi";
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true });
 mongoose.Promise = Promise;
 
