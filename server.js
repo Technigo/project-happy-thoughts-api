2d30b13ff233f816e4f954bae533289095b7faca
AjmalZ <ajmal.zahir@hotmail.com>
2023-05-16 16:28:38 +0200

    created routs

--- server.js
+++ server.js
@@ -1,14 +1,34 @@
+// /////////////////////////////// IMPORT //////////////////////////////// //
 import express from "express";
 import cors from "cors";
 import mongoose from "mongoose";
-
-const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/project-mongo";
+// ////////////////////CONNECTION TO DISPLAY ///////////////////////////// //
+const mongoUrl = process.env.MONGO_URL || "mongodb://127.0.0.1:27017/project-happy-thoughts-api";;
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true });
 mongoose.Promise = Promise;
 
-// Defines the port the app will run on. Defaults to 8080, but can be overridden
-// when starting the server. Example command to overwrite PORT env variable value:
-// PORT=9000 npm start
+const ThoughtsSchema = new mongoose.Schema({
+  message: {
+    type: String,
+    required: true,
+    minlength: 5,
+    maxlength: 140,
+    trim: true
+  },
+
+  likes: {
+    type: Number,
+    default: 0
+  },
+
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+const Message = mongoose.model("Message", ThoughtsSchema);
+
 const port = process.env.PORT || 8080;
 const app = express();
 
@@ -16,12 +36,101 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-// Start defining your routes here
+// //////////////////////////// APP - ROUTES ////////////////////////////////////////// //
+// Startpage endpoint
 app.get("/", (req, res) => {
-  res.send("Hello Technigo!");
+  res.send({
+    "Hello": "Welcome to this Happy Thought API!",
+    "Endpoints": [
+      {
+        "/": "Startpage / Api Info"
+      },
+      {
+        "/thoughts": "GET. See 20 thoughts in descending order"
+      },
+      {
+        "/thoughts": "POST. Post your thoughts"
+      }
+    ]
+  });
 });
+
+app.get('/thoughts', async (req, res) => {
+  const thoughtsList = await Message.find().sort({ createdAt: 'desc' }).limit(20).exec();
+  try {
+    res.status(200).json({
+      success: true,
+      response: thoughtsList,
+      message: "get successfull"
+    });
+  }
+
+  catch (err) {
+    res.status(400).json({
+      success: false,
+      message: 'Could not save thought to database',
+      error: err.errors
+    });
+  }
+});
+
+app.post('/thoughts', async (req, res) => {
+  const { message, createdAt } = req.body;
+  try {
+    const savedThought = await new Message({ message, createdAt }).save()
+    res.status(200).json({
+      success: true,
+      response: savedThought,
+      message: "post successfull"
+    });
+  }
+
+  catch (err) {
+    res.status(400).json({
+      success: false,
+      response: err,
+      message: 'Could not save thought to database', error: err.errors
+    });
+  }
+});
+
+app.patch("/thoughts/:id/likes", async (req, res) => {
+  const { id } = req.params;
+  try {
+    const updateLikes = await Message.findByIdAndUpdate(id, { $inc: { likes: 1 } });
+    res.status(201).json({
+      success: true,
+      response: `Happy thought: ${updateLikes.message} has updated likes`
+    });
+
+  } catch (error) {
+    res.status(400).json({
+      success: false,
+      message: 'Could not save like to database',
+      response: error
+    });
+  }
+});
+
+
+
+
+// POST - create something
+// PATCH - update
+// PUT - replace
+// DELETE - delete
+
 
 // Start the server
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
 });
+
+
+
+
+
+
+
+
+
