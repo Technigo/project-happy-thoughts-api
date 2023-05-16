bdbb0e09d07f6b8e7cc1397fd8f54f6f991330e2
fannystenberg <ffannystenberg@gmail.com>
2023-05-16 19:16:28 +0200

    created Thought model and POST request

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ import express from "express";
 import cors from "cors";
 import mongoose from "mongoose";
 
-const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/project-mongo";
+const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/happy-thoughts";
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true });
 mongoose.Promise = Promise;
 
@@ -19,6 +19,45 @@ app.use(express.json());
 // Start defining your routes here
 app.get("/", (req, res) => {
   res.send("Hello Technigo!");
+});
+
+// The thought model
+const { Schema } = mongoose;
+const ThoughtSchema = new Schema({
+  message: {
+    type: String,
+    required: true,
+    minlength: 5,
+    maxlength: 140
+  },
+  hearts: {
+    type: Number,
+    default: 0
+  },
+  createdAt: {
+    type: Date,
+    default: new Date()
+  }
+});
+const Thought = mongoose.model("Thought", ThoughtSchema);
+
+// POST new thought
+app.post("/thoughts", async(req, res) => {
+  const { message } = req.body;
+  try {
+    const newThought = await Thought({message}).save();
+    res.status(201).json({
+      success: true,
+      response: newThought,
+      message: "new thought created successfully"
+    });
+  } catch(e) {
+    res.status(400).json({
+      success: false,
+      respone: e,
+      message: "error occured, could not create new thought"
+    });
+  }
 });
 
 // Start the server
