6e82654f8f59916a563c5ed15bef8bb73eda79e5
Hannah Ek <ekhannahek@gmail.com>
2023-05-17 22:14:02 +0200

    altered the id like

--- server.js
+++ server.js
@@ -83,7 +83,7 @@ app.post("/thoughts", async (req, res) =>{
     }
 });
 
-app.patch("/thoughts/id/:id/like", async (req, res) => {
+app.patch("/thoughts/:id/like", async (req, res) => {
   const { id } = req.params;
   try {
     const thought = await Thought.findByIdAndUpdate(id, { $inc: { heart: 1 } }, { new: true });
