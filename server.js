5a49a81124f349e629719931e74c9fa05e679805
Annika Lindberg <annika.edit.lindberg@gmail.com>
2023-05-17 08:28:25 +0200

    changed url-mongo

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ import cors from "cors";
 import mongoose from "mongoose";
 import listEndpoints from "express-list-endpoints";
 
-const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/project-mongo";
+const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/Happy-Thoughts-API";
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true });
 mongoose.Promise = Promise;
 
