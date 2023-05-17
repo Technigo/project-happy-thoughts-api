58a077dea3209af6ef60684d1609cccb493722a3
Vera Sjunnesson <verasjunnesson@MacBook-Air-som-tillhor-Ulla-4.local>
2023-05-17 11:56:21 +0200

    debugging/cors problem

--- server.js
+++ server.js
@@ -13,6 +13,7 @@ const port = process.env.PORT || 8080;
 const app = express();
 
 // Add middlewares to enable cors and json body parsing
+const cors = require("cors");
 app.use(cors());
 app.use(express.json());
 const listEndPoints = require('express-list-endpoints');
