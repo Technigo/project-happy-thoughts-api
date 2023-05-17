928b2ee90250e157e67fcb0315d43151c5c12992
Andreas Axelsson <pererikandreas@gmail.com>
2023-05-17 18:32:32 +0200

    added structure

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
+// //////////////////////////////////////////////////////////////////////// //
 // /////////////////////////////// IMPORT ///////////////////////////////// //
+// //////////////////////////////////////////////////////////////////////// //
 
-import express from "express";
+import express from "express"; 
 import cors from "cors";
 import mongoose from "mongoose";
 
+// //////////////////////////////////////////////////////////////////////// //
 // //////////////////// CONNECTION TO DISPLAY ///////////////////////////// //
+// //////////////////////////////////////////////////////////////////////// //
 
 const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/project-happy-thoughts-api";
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true });
@@ -16,9 +20,11 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+// //////////////////////////////////////////////////////////////////////// //
 // //////////////////////////// SCHEMA //////////////////////////////////// //
+// //////////////////////////////////////////////////////////////////////// //
 
-const ThoughtsSchema = new mongoose.Schema({
+const ThoughtsListSchema = new mongoose.Schema({
   message: {
     type: String,
     required: true,
@@ -32,13 +38,15 @@ const ThoughtsSchema = new mongoose.Schema({
   },
   createdAt: {
     type: Date,
-    default: new Date()
+    default: Date.now
   }
 }); 
 
-const Message = mongoose.model("Message", ThoughtsSchema);
+const ThoughtsList = mongoose.model("ThoughtsList", ThoughtsListSchema);
 
-// //////////////////////////// STARTPAGE //////////////////////////////////// //
+// //////////////////////////////////////////////////////////////////////// //
+// //////////////////////////// STARTPAGE ///////////////////////////////// //
+// //////////////////////////////////////////////////////////////////////// //
 
 app.get("/", (req, res) => {
   res.send({
@@ -51,62 +59,76 @@ app.get("/", (req, res) => {
     });
 });
 
+// ///////////////////////////////////////////////////////////////////// //
 // //////////////////////////// GET //////////////////////////////////// //
+// ///////////////////////////////////////////////////////////////////// //
 
 app.get("/thoughts", async (req, res) => {
-  const thoughtsList = await Message.find().sort({createdAt: 'desc'}).limit(20).exec();
+  const displayedThoughts = await ThoughtsList.find().sort({createdAt: 'desc'}).limit(20).exec();
     try {
       res.status(200).json({
         success: true, 
-        response: thoughtsList,
+        response: displayedThoughts,
         message: "Successfully found thoughts"
       });
   } catch(e) {
     res.status(400).json({
       success: false,
       response: e,
-      message: "Did not find thoughts", error:err.errors
+      message: "Did not find thoughts", 
+      error: e.errors
      });
   }
 });
 
+// ////////////////////////////////////////////////////////////////////// //
 // //////////////////////////// POST //////////////////////////////////// //
+// ////////////////////////////////////////////////////////////////////// //
 
 app.post('/thoughts', async (req, res) => {
   const { message, createdAt } = req.body;
   try {
-    const savedThought = await new Message({message, createdAt}).save()
+    const savedThought = await new ThoughtsList({ message, createdAt }).save();
     res.status(201).json({
       success: true, 
       message: "Post successfull", 
       response: savedThought
     });
   }
-  catch (err){
+  catch (e){
     res.status(400).json({
       success: false, 
-      response: err,
-      message: 'Could not save thought to database', error:err.errors });
+      response: e,
+      message: 'Could not save thought to database', 
+      error:err.errors 
+    });
   }
 });
 
-// //////////////////////////// PATCH //////////////////////////////////// //
+// /////////////////////////////////////////////////////////////////////// //
+// //////////////////////////// LIKES //////////////////////////////////// //
+// /////////////////////////////////////////////////////////////////////// //
 
 app.post("/thoughts/:thoughtId/like", async (req, res) => {
   const { thoughtId } = req.params;
   try {
-   const updateLikes = await Message.findByIdAndUpdate(id, {$inc: {likes: 1}});
+   const updateLikes = await ThoughtsList.findByIdAndUpdate(thoughtId, { $inc: { likes: 1 }});
    res.status(200).json({
     success: true, 
-    response: `Happy thought: ${updateLikes.message} has updated likes`
+    response: `Thought: ${updateLikes.message} has updated likes`
   });
-  } catch (error) {
+  } catch (e) {
    res.status(400).json({
     success: false, 
-    message: 'Could not save like to database',
-    response: error});
+    message: 'Could not save like to thought',
+    response: e
+  });
   }
 });
+
+// /////////////////////////////////////////////////////////////////////// //
+// //////////////////////////// START APP //////////////////////////////// //
+// /////////////////////////////////////////////////////////////////////// //
 
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
