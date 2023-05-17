c390b04910ab058eb6a2db55afbff077467f6a18
NinaW <120198299+NinaWald@users.noreply.github.com>
2023-05-17 15:32:55 +0200

    started with the code

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ import express from "express";
 import cors from "cors";
 import mongoose from "mongoose";
 
-const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/project-mongo";
+const mongoUrl = process.env.MONGO_URL || "mongodb://127.0.0.1:27017/project-happy-thoughts-api";
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true });
 mongoose.Promise = Promise;
 
@@ -16,10 +16,49 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+const { Schema } = mongoose;
+
+const HappyThoughtsSchema = new Schema({
+  text: {
+    type:String,
+    required: true,
+    minlength: 4,
+    maxlength: 140
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  },
+  description: {
+    type: String,
+    minlength: 4,
+    maxlength: 140
+  }
+})
+
+const HappyToughts = mongoose.model("HappyThoughts", HappyThoughtsSchema)
+
 // Start defining your routes here
 app.get("/", (req, res) => {
-  res.send("Hello Technigo!");
+  res.send("Hello from Nina!");
 });
+
+app.get('/thoughts', async (req, res) => {
+  const thoughts = await HappyToughts.find().sort({createdAt: 'desc'}).limit(20).exec();
+  res.json(thoughts)
+  });
+app.post('/thoughts', async (req, res) => {
+  const { text, description } = req.body;
+  const thought = new HappyToughts({ text, description });
+
+  try{
+    const savedThought = await thought.save()
+    res.status(201).json(savedThought)
+  } catch (err){
+    console.error(err);
+    res.status(400).json({message: 'Could not save thought', error:err.errors})
+  }
+})
 
 // Start the server
 app.listen(port, () => {
