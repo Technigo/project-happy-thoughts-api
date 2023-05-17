1c934a342119e6566c987f225b694e597b65ec0e
Hannah Ek <ekhannahek@gmail.com>
2023-05-17 21:02:01 +0200

    update

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import mongoose from "mongoose";
+import moment from "moment-timezone";
 
 const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/project-mongo";
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true });
