188eb17facc23b6b5aa070fa7817285e6e6f1a72
Amanda <amanda.sl747@gmail.com>
2023-05-18 10:35:16 +0200

    initial commit
    installed express-list-endpoints and mongodb, changed mongo_url

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ import express from "express";
 import cors from "cors";
 import mongoose from "mongoose";
 
-const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/project-mongo";
+const mongoUrl = process.env.MONGO_URL || "mongodb://127.0.0.1:27017/happy-thoughts";
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true });
 mongoose.Promise = Promise;
 
