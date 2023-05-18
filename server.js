a84c6397c7a1bab635a7a68ea8c906471f3617e3
smirrebinx <48405465+smirrebinx@users.noreply.github.com>
2023-05-18 13:00:47 +0200

    Add schema, model, get and post

--- server.js
+++ server.js
@@ -6,21 +6,29 @@ const mongoUrl = process.env.MONGO_URL || "mongodb://127.0.0.1:27017/project-hap
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true });
 mongoose.Promise = Promise;
 
-const Task = mongoose.model('Task', {
-  text: {
+// The schema defines the structure and rules for the data that will be stored in the MongoDB collection
+const ThoughtSchema = new mongoose.Schema({
+  message: {
     type: String,
+    minlength: 5, 
+    maxlength: 140,
     required: true,
-    minlength: 5
-  }, 
-  complete: {
-    type: Boolean,
-    default: false
-  }, 
+  },
+  hearts: {
+    type: Number, 
+    default: 0
+  },
   createdAt: {
     type: Date,
-    default: Date.now
+    default: new Date()
   }
-});
+})
+
+// The model for ThoughtSchema, created using mongoose.model(), is a wrapper around the schema that provides an interface 
+// for querying and manipulating the MongoDB collection. 
+// The model allows you to perform CRUD (Create, Read, Update, Delete) operations on the "Thoughts" collection
+
+const Thought = mongoose.model('Thought', ThoughtSchema);
 
 // Defines the port the app will run on. Defaults to 8080, but can be overridden
 // when starting the server. Example command to overwrite PORT env variable value:
@@ -33,30 +41,79 @@ app.use(cors());
 app.use(express.json());
 
 // Start defining your routes here
+// When a client sends an HTTP GET request to the root URL, 
+// the callback function (req, res) => {...} is executed
+
+// listEndpoints(app): This function is imported from the 
+// "express-list-endpoints" library. It takes the app object, 
+// which represents the Express application, as an argument. 
+// It inspects the application and returns an array of all the 
+// defined endpoints in the application, including their 
+// HTTP methods, paths, and middleware.
+
+// res.json(listEndpoints(app)) sends the list of endpoints in JSON format 
+// as the response to the client when a GET request is made to the root URL ("/"). 
+
 app.get("/", (req, res) => {
-  res.send("Hello Technigo!");
+  res.send(listEndpoints(app));
 });
 
-app.get('/tasks', async (req, res) => {
-  const tasks = await Task.find().sort({createdAt: 'desc'}).limit(20).exec();
-  res.json(tasks);
+// Retrieves the 20 most recent thoughts from the MongoDB database, 
+// sorting them in descending order by their creation dates, 
+// and assigns the result to the thoughts variable.
+
+// The ternary operator 'thoughts ? res.json(thoughts) : res.status(404).json({ error: "thoughts not found" })' 
+// is used to check if the thoughts variable is truthy. 
+// If thoughts is truthy (not null, undefined, 0, empty string, etc.), it executes res.json(thoughts), 
+// which sends the thoughts as a JSON response. 
+// Otherwise, if thoughts is falsy, it executes res.status(404).json({ error: "thoughts not found" }), 
+// which sends a JSON response with a 404 status code and an error message.
+
+app.get('/thoughts', async (req, res) => {
+  const thoughts = await Thought.find().sort({createdAt: 'desc'}).limit(20).exec();
+  thoughts ? res.json(thoughts) : res.status(400).json({ error: 'Posts not found'});
 });
 
-app.post('/tasks', async (req, res) => {
+
+// User input (thought) is validated and saved in database if the validation checks out
+app.post('/thoughts', async (req, res) => {
   // Retrieve the information sent by the client to our API endpoint
-  const {text, complete} = req.body;
+  const {message} = req.body;
 
-  // Use our mongoose model to create the database
-  const task = new Task({text, complete});
-
-  try {
-    // Success
-    const savedTask = await task.save();
-    res.status(201).json(savedTask);
-  } catch (err) {
-    res.status(400).json({message:'Could not save task to the Database', error: err.errors});
+    try {
+  const newThought = await new Thought({
+    message: message
+  }).save()
+  res.status(200).json({
+    success: true,
+    response: `Your thought was posted: ${newThought}`
+  })
+} catch (err) {
+    res.status(400).json({
+      success: false,
+      response:'Thought could not be posted', 
+      error: err.errors
+    });
   }
 });
+
+// Increase hearts by 1 if the thought is liked using the $inc operator. The 'hearts' is from the ThoughtSchema.
+app.post('/thoughts/:id/like', async (req, res) => {
+  const { id } = req.params;
+  try {
+    const updateHearts = await Thought.findByIdAndUpdate (id, { $inc: { hearts: 1 } });
+    res.status(200).json({
+      success: true,
+      response: `The post ${updateHearts.id} has been updated with a heart`
+    })
+  } catch (err) {
+    res.status(400).json({
+      success: false,
+      response: 'Hearts not updated',
+      error: err.errors
+    })
+  }
+})
 
 // Start the server
 app.listen(port, () => {
