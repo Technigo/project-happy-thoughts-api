32dc683458e35739f3d2c0e7d9b5153b710b2cfe
Bridget Mailley <bmailley42@gmail.com>
2023-05-18 12:46:41 +0200

    created happy thoughts endpoints

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ import express from "express";
 import cors from "cors";
 import mongoose from "mongoose";
 
-const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/project-mongo";
+const mongoUrl = process.env.MONGO_URL || "mongodb://127.0.0.1/happythoughts";
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true });
 mongoose.Promise = Promise;
 
@@ -20,6 +20,90 @@ app.use(express.json());
 app.get("/", (req, res) => {
   res.send("Hello Technigo!");
 });
+
+const { Schema } = mongoose;
+
+const thoughtSchema = new Schema({
+  message: {
+    type: String,
+    required: true,
+    minlength: 5,
+    maxlength: 140,
+    trim: true
+  },
+  hearts: {
+    type: Number,
+    default: 0
+  },
+  createdAt: {
+    type: Date,
+    default: new Date()
+  }
+});
+
+const Thought = mongoose.model("Thought", thoughtSchema)
+
+app.get('/thoughts', async (req, res) => {
+  const response = {
+    success: true,
+    body:{}
+  }
+
+  try {
+    const allThoughts = await Thought.find().sort({createdAt:'desc'}).limit(20);
+    if(allThoughts){
+      response.body = allThoughts
+      res.status(200).json(response)
+    } else {
+      response.success = false
+      response.body = {message: "No thoughts to be found."}
+      res.status(404).json(response)
+    }
+  } catch (error) {
+    response.success = false
+    response.body = {message: error}
+    res.status(500).json(response)
+  }
+})
+
+app.post('/thoughts', async (req, res) =>{
+  const response = {
+    success: true,
+    body:{}
+  }
+  const {message} = req.body;
+  try{
+    const thought = await new Thought({message}).save()
+    response.body = thought
+    res.status(201).json(response)
+  } catch (error) {
+    response.success = false
+    response.body = {message: error}
+    res.status(400).json(response)
+  }
+})
+
+app.post('/thoughts/:thoughtId/like', async (req, res) => {
+  const { thoughtId } = req.params
+  const response = {
+    success: true, 
+    body: {}
+  }
+  
+  try {
+    const likedThought = await Thought.findById(thoughtId);
+    const addHeart = likedThought.hearts + 1;
+    // console.log(likedThought)
+    const updateHearts = await Thought.findByIdAndUpdate(thoughtId, {hearts: addHeart})
+    // console.log(updateHearts)
+    response.body = updateHearts
+    res.status(201).json(response)
+  } catch (error) {
+    response.success = false
+    response.body = {message: error}
+    res.status(400).json(response)
+  }
+})
 
 // Start the server
 app.listen(port, () => {
