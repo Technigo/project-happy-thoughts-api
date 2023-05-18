3a198fc66761f992a2c9321ca20a3676866eb031
tess.aquilon <tess.aquilon@gmail.com>
2023-05-18 14:04:03 +0200

    get and post endpoints created

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ import express from "express";
 import cors from "cors";
 import mongoose from "mongoose";
 
-const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/project-mongo";
+const mongoUrl = process.env.MONGO_URL || "mongodb://127.0.0.1:27017/project-happy-thoughts-api";
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true });
 mongoose.Promise = Promise;
 
@@ -20,6 +20,53 @@ app.use(express.json());
 app.get("/", (req, res) => {
   res.send("Hello Technigo!");
 });
+
+const Thought = mongoose.model('Thought', {
+  message: {
+    type: String,
+    required: true,
+    minlength: 5,
+    maxlength: 140 
+  },
+  hearts: {
+    type: Number,
+    default: 0
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
+} )
+
+// GET thoughts
+// This endpoint should return a maximum of 20 thoughts, sorted by createdAt to show the most recent thoughts first.
+
+app.get("/thoughts", async (req, res) => {
+  const thoughts = await Thought.find().sort({createdAt: 'desc'}).limit(20).exec();
+  res.json(thoughts);
+});
+
+// POST thoughts
+// This endpoint should return a maximum of 20 thoughts, sorted by createdAt to show the most recent thoughts first.
+
+app.post("/thoughts", async (req, res) => {
+  const { message } = req.body;
+  const thought = new Thought({ message });
+  try {
+    // Success
+    const savedThought = await thought.save();
+    res.status(201).json(savedThought);
+  } catch(err) {
+      res.status(400).json({
+        message: 'Could not save thought to the Database', error: err.errors
+      });
+  }
+})
+
+// POST thoughts/:thoughtId/like
+// This endpoint doesn't require a JSON body. Given a valid thought id in the URL, the API should find that thought, and update its hearts property to add one heart
+
+
 
 // Start the server
 app.listen(port, () => {
