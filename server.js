6a8de67df94c850fd77d1d3260df0d85bac09b36
SandraMadeleine <sandramohlsson@yahoo.com>
2023-05-20 17:12:47 +0200

    Adding and testing code

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 import express from "express";
 import cors from "cors";
 import mongoose from "mongoose";
+import listEndpoints from "express-list-endpoints";
 
-const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/project-mongo";
+const mongoUrl = process.env.MONGO_URL || "mongodb://127.0.0.1:27107/project-mongo";
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true });
 mongoose.Promise = Promise;
 
@@ -16,9 +17,83 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-// Start defining your routes here
-app.get("/", (req, res) => {
-  res.send("Hello Technigo!");
+const ThoughtSchema = new mongoose.Schema({
+  message: {
+    type: String,
+    minlength: 3,
+    maxlength: 300,
+    required: true
+  },
+  hearts: {
+    type: Number,
+    default: 0
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+const Thought = mongoose.model('Thought', ThoughtSchema);
+
+// Root endpoint to list all endpoints
+app.get("/", (request, response) => {
+  response.json(listEndpoints(app));
+});
+
+// GET endpoint to retrieve 20 most recent thoughts
+app.get("/thoughts", async (request, response) => {
+  const thoughts = await Thought.find().sort({ createdAt: 'desc' }).limit(20).exec();
+  if (thoughts.length > 0) {
+    response.json(thoughts);
+  } else {
+    response.status(404).json({ error: "Thoughts not found" });
+  }
+});
+
+// POST endpoint to create a new thought
+app.post("/thoughts", async (request, response) => {
+  const { message } = request.body;
+  try {
+    const newThought = await new Thought({
+      message: message
+    }).save();
+    response.status(200).json({
+      success: true,
+      response: `This thought was posted: ${newThought}`
+    });
+  } catch (err) {
+    response.status(400).json({
+      success: false,
+      response: 'Thought could not be posted',
+      errors: err.errors
+    });
+  }
+});
+
+// POST endpoint to increase the number of hearts for a thought
+app.post('/thoughts/:id/like', async (request, response) => {
+  const { id } = request.params;
+  try {
+    const heartUpdate = await Thought.findByIdAndUpdate(id, { $inc: { hearts: 1 } });
+    if (heartUpdate) {
+      response.status(200).json({
+        success: true,
+        response: `Heart-count was updated for post ${heartUpdate.id}`
+      });
+    } else {
+      response.status(404).json({
+        success: false,
+        response: 'Thought not found'
+      });
+    }
+  } catch (err) {
+    response.status(400).json({
+      success: false,
+      response: 'Heart-count could not update',
+      error: err.errors
+    });
+  }
 });
 
 // Start the server
