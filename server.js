8a0d6882a6df7d549d7d42cde593aa3939997d08
smirrebinx <48405465+smirrebinx@users.noreply.github.com>
2023-05-20 12:54:24 +0200

    Add import listEndpoints

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import mongoose from "mongoose";
+import listEndpoints from "express-list-endpoints";
 
 const mongoUrl = process.env.MONGO_URL || "mongodb://127.0.0.1:27017/project-happy-thoughts-api";
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true });
