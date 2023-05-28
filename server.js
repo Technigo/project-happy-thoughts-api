ea49f3e9798045b89fe3fc18d093442b7ca5a7ed
Michelle Wegler <48405465+smirrebinx@users.noreply.github.com>
2023-05-28 11:57:03 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,13 +1,36 @@
 import express from "express";
 import cors from "cors";
 import mongoose from "mongoose";
-import crypto from "crypto";
-import bcrypt from "bcrypt";
 import listEndpoints from "express-list-endpoints";
 
-const mongoUrl = process.env.MONGO_URL || "mongodb://127.0.0.1:27017/final-project-api";
+const mongoUrl = process.env.MONGO_URL || "mongodb://127.0.0.1:27017/project-happy-thoughts-api";
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true });
 mongoose.Promise = Promise;
+
+// The schema defines the structure and rules for the data that will be stored in the MongoDB collection
+const ThoughtSchema = new mongoose.Schema({
+  message: {
+    type: String,
+    minlength: 5, 
+    maxlength: 140,
+    required: true,
+    trim: true
+  },
+  hearts: {
+    type: Number, 
+    default: 0
+  },
+  createdAt: {
+    type: Date,
+    default: () => new Date()
+  }
+})
+
+// The model for ThoughtSchema, created using mongoose.model(), is a wrapper around the schema that provides an interface 
+// for querying and manipulating the MongoDB collection. 
+// The model allows you to perform CRUD (Create, Read, Update, Delete) operations on the "Thoughts" collection
+
+const Thought = mongoose.model('Thought', ThoughtSchema);
 
 // Defines the port the app will run on. Defaults to 8080, but can be overridden
 // when starting the server. Example command to overwrite PORT env variable value:
@@ -16,147 +39,90 @@ const port = process.env.PORT || 8080;
 const app = express();
 
 // Add middlewares to enable cors and json body parsing
-app.use(
-  cors({
-    origin: "http://localhost:8080", 
-    methods: ["GET", "POST"], // Specify the allowed methods
-    allowedHeaders: ["Content-Type", "Authorization"], // Specify the allowed headers
-  })
-); 
+app.use(cors());
 app.use(express.json());
 
 // Defines endpoint paths as constants to be able to only update the paths in one place if needed
 const PATHS = {
   root: "/",
-  register: "/register",
-  login: "/login",
-  secrets: "/secrets",
-  sessions: "/sessions"
+  thoughts: "/thoughts",
+  thoughtsById: "/thoughts/:_id/like"
 }
 
 // Start defining your routes here
-app.get("/", (req, res) => {
+// When a client sends an HTTP GET request to the root URL, 
+// the callback function (req, res) => {...} is executed
+
+// listEndpoints(app): This function is imported from the 
+// "express-list-endpoints" library. It takes the app object, 
+// which represents the Express application, as an argument. 
+// It inspects the application and returns an array of all the 
+// defined endpoints in the application, including their 
+// HTTP methods, paths, and middleware.
+
+// res.json(listEndpoints(app)) sends the list of endpoints in JSON format 
+// as the response to the client when a GET request is made to the root URL ("/"). 
+
+// GET
+app.get(PATHS.root, (req, res) => {
   res.send(listEndpoints(app));
 });
 
-const { Schema } = mongoose;
+// Retrieves the 20 most recent thoughts from the MongoDB database, 
+// sorting them in descending order by their creation dates, 
+// and assigns the result to the thoughts variable.
 
-const UserSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true,
-    unique: true
-  },
-  password: {
-    type: String,
-    required: true
-  },
-  accessToken: {
-    type: String,
-    default: () => crypto.randomBytes(128).toString("hex")
-  }
+// The ternary operator 'thoughts ? res.json(thoughts) : res.status(404).json({ error: "thoughts not found" })' 
+// is used to check if the thoughts variable is truthy. 
+// If thoughts is truthy (not null, undefined, 0, empty string, etc.), it executes res.json(thoughts), 
+// which sends the thoughts as a JSON response. 
+// Otherwise, if thoughts is falsy, it executes res.status(404).json({ error: "thoughts not found" }), 
+// which sends a JSON response with a 404 status code and an error message.
+
+// GET
+app.get(PATHS.thoughts, async (req, res) => {
+  const thoughts = await Thought.find().sort({createdAt: 'desc'}).limit(20).exec();
+  thoughts ? res.json(thoughts) : res.status(400).json({ error: 'Posts not found'});
 });
 
-const User = mongoose.model("User", UserSchema);
+// POST
+// User input (thought) is validated and saved in database if the validation checks out
+app.post(PATHS.thoughts, async (req, res) => {
+ 
+  // Retrieve the information sent by the client to our API endpoint
+  const {message} = req.body;
 
-// Registration
-app.post(PATHS.register, async (req, res) => {
-  const { name, password } = req.body;
+    try {
+  const newThought = await new Thought({
+    message: message
+  }).save()
 
-  try {
-    const salt = bcrypt.genSaltSync();
-    // Do not store plaintext passwords
-    const newUser = await new User({
-      name: name,
-      password: bcrypt.hashSync(password, salt)})
-    .save();
-    res.status(201).json({
-      success: true,
-      response: {
-        name: newUser.name,
-        id: newUser._id,
-        accessToken: newUser.accessToken
-      }
-    })
-  } catch (e) {
-    // Bad request
+  res.status(200).json({
+    success: true,
+    response: `Your thought was posted: ${newThought}`
+  })
+} catch (err) {
     res.status(400).json({
       success: false,
-      response: e,
-      message: 'Could not create user', 
-      errors: e.errors
-    });
-  }
-});
-// Login
-app.post(PATHS.login, async (req, res) => {
-  const { name, password } = req.body;
-  try {
-    const user = await User.findOne({name: name})
-    if (user && bcrypt.compareSync(password, user.password)) {
-      res.status(200).json({
-        success: true,
-        response: {
-          name: user.name,
-          id: user._id,
-          accessToken: user.accessToken
-        }
-      });
-    } else {
-      res.status(400).json({
-        success: false,
-        response: "Credentials do not match"
-      });
-    }
-  } catch (e) {
-    res.status(500).json({
-      success: false,
-      response: e
+      response:'Thought could not be posted', 
+      error: err.errors
     });
   }
 });
 
-// Authenticate the user
-const authenticateUser = async (req, res, next) => {
-  const accessToken = req.header("Authorization");
-  try {
-    const user = await User.findOne({accessToken: accessToken});
-    if (user) {
-      next();
-    } else {
-      res.status(401).json({
-        success: false,
-        response: "Please log in",
-        loggedOut: true
-      })
-    }
-  } catch (e) {
-    res.status(500).json({
-      success: false,
-      response: e
-    });
-  }
-}
 
-// Authenticate the user and return the secret message
-app.get(PATHS.secrets, async (req, res) => {
-  const accessToken = req.header("Authorization");
+// PATCH
+// Increase hearts by 1 if the thought is liked using the $inc operator. The 'hearts' is from the ThoughtSchema.
+app.patch(PATHS.thoughtsById, async (req, res) => {
+  const { _id } = req.params;
   try {
-    const user = await User.findOne({ accessToken: accessToken });
-    if (user) {
-      const secretMessage = "This is the secret page! Woop woop";
-      res.status(200).json({ secret: secretMessage });
-    } else {
-      res.status(401).json({
-        success: false,
-        response: "Please log in",
-        loggedOut: true,
-      });
-    }
-  } catch (e) {
-    res.status(500).json({
+    const updatedThought = await Thought.findByIdAndUpdate(_id, { $inc: { hearts: 1 } }, { new: true });
+   res.status(200).json(updatedThought);
+  } catch (err) {
+    res.status(400).json({
       success: false,
-      response: e,
+      response: 'Hearts not updated',
+      error: err.errors
     });
   }
 });
@@ -165,29 +131,3 @@ app.get(PATHS.secrets, async (req, res) => {
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
 });
-
-// Test in postman
-
-// Post: http://localhost:8080/register 
-// {
-//   "name": "enter new name",
-//   "password": "enter password"
-// }
-
-// Post: http://localhost:8080/login
-// {
-//     "name": "name",
-//     "password": "password"
-// }
-
-// Get   http://localhost:8080/secrets
-// Headers: Authorization
-// Enter accessToken in value
-
-// Post: http://localhost:8080/sessions
-// {
-//     "name": "name",
-//     "password": "password"
-// }
-
-
