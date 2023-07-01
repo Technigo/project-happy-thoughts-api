7b6971cee3698d2b87c1d5afaaef458e5deb4a53
Emma Engvall <emma.engvall@ica.se>
2023-07-01 21:24:27 +0200

    created HappyThoughtSchema

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ import express from "express";
 import cors from "cors";
 import mongoose from "mongoose";
 
-const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/project-mongo";
+const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/happy-thoughts-project";
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true });
 mongoose.Promise = Promise;
 
@@ -16,48 +16,87 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+const { Schema } = mongoose;
+const HappyThoughtSchema = new Schema ({
+  message: {
+    type: String,
+    required: true,
+    minlength: 5,
+    maxlength: 140,
+    trim: true
+  },
+  hearts: {
+    type: Number,
+    default: 0
+    
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
+})
+
+const Thought = mongoose.model("Thought", HappyThoughtSchema)
+
 // Start defining your routes here
 app.get("/", (req, res) => {
   res.send("Time for some happy thoughts!");
 });
 
-const { Schema } = mongoose;
-const FruitOrVegetableSchema = new Schema ({
-  name: {
-    type: String,
-    required: true,
-    unique: true
-  },
-  description: {
-    type: String,
-    minlength: 5,
-    maxlength: 140,
-    trim: true
-  },
-  createdAt:{
-    type: Date,
-    default: new Date()
-  },
-  kind: {
-    type: String,
-    enum:["fruit", "vegetable"]
+// GET thoughts
+app.get("/thoughts", async (req, res) => {
+  try {
+    const thoughtItem = await Thought.find().sort({
+    createdAt: 'desc' }).limit(20).exec()
+    res.status(200).json({
+      success: true,
+      response: thoughtItem,
+      message: "thought fetch successful"
+    })
+  } catch(e) {
+    res.status(400).json({
+      success: false,
+      response: e,
+      message: "thought not found"
+    })    
   }
 })
 
-const FruitOrVegetable = mongoose.model("FruitOrVegetable", FruitOrVegetableSchema)
-
-app.post("/fruit_or_vegetable", async (req, res) => {
-  const {kind, name, description} = req.body;
+// POST (create new message entries in the database)
+app.post("/thoughts", async (req, res) => {
+  const { message } = req.body
     try{
-      const foodItem = await new FruitOrVegetable({kind, name, description}).save();
+      const thoughtItem = await new Thought ({ message }).save()
       res.status(201).json({
         success: true,
-        response: foodItem
+        response: thoughtItem,
+        message: "new thought created successfully"
       })
     } catch (e) {
       res.status(400).json({
         success: false,
-        response: e
+        response: e,
+        message: "error occurred, could not create new thought"
+      })
+    }
+})
+
+// POST (update the number of likes for a message. It is preferred to use PATCH as method.)
+app.post("/thoughts/:thoughtId/like", async (req, res) => {
+  const { thoughtId } = req.params
+    try{
+      const likedItem = await Thought.findByIdAndUpdate (
+      { $inc: { hearts: 1 } }, { new: true })
+      res.status(201).json({
+        success: true,
+        response: likedItem,
+        message: "like of thought successful"
+      })
+    } catch (e) {
+      res.status(400).json({
+        success: false,
+        response: e,
+        message: "error occurred, could not like thought"
       })
     }
 })
