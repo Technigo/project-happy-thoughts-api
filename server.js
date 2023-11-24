6359378315b7e9b925854b4e07bc51b6cbe0c416
So Youn Choi <manghatuhyo@gmail.com>
2023-11-24 21:10:44 +0100

    err and validation

--- server.js
+++ server.js
@@ -2,21 +2,31 @@ import express from "express";
 import cors from "cors";
 import mongoose from "mongoose";
 
-const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/notes";
+const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/validationScreencast";
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true });
 mongoose.Promise = Promise;
 
-const Note = mongoose.model('Note', {
-  text: String,
-  createdAt: {
+// Create model for mongo database
+const Person = mongoose.model('Person', {
+  name:{
+    type: String,
+    required: true,
+    minlength: 2,
+    maxlength: 500 
+  }, 
+  height:{
+    type: Number,
+    required: true,
+    min: 5
+  },
+  birthdate:{
     type: Date,
-    default: () => new Date()
+    default: Date.now
   }
 })
 
-// Defines the port the app will run on. Defaults to 8080, but can be overridden
-// when starting the server. Example command to overwrite PORT env variable value:
-// PORT=9000 npm start
+new Person({name: "Van", height: 150}).save()
+
 const port = process.env.PORT || 8080;
 const app = express();
 
@@ -29,11 +39,17 @@ app.get("/", (req, res) => {
   res.send("Hello Technigo!");
 });
 
-app.post('/notes', async (req, res) => {
-  const { text } = req.body
-  const note = new Note({ text })
-  await note.save()
-  res.json(note)
+app.post('/people', async (req, res) => {
+  // Promises
+  new Person(req.body).save()
+  .then((person) => {
+    res.status(200).json(person)
+  })
+  .catch((err) => {
+    res.status(400).json({message: 'Could not save person', errors: err.errors})
+  })
+
+  
 })
 
 // Start the server
