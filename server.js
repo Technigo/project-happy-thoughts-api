3dce7865e0c40d0302cc56c23b0c2a96e9797e69
So Youn Choi <manghatuhyo@gmail.com>
2023-11-25 12:09:06 +0100

    post requests with mongo

--- server.js
+++ server.js
@@ -2,31 +2,28 @@ import express from "express";
 import cors from "cors";
 import mongoose from "mongoose";
 
-const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/validationScreencast";
+const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/post-codealong";
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true });
 mongoose.Promise = Promise;
 
-// Create model for mongo database
-const Person = mongoose.model('Person', {
-  name:{
+const Task = mongoose.model('Task', {
+  text: {
     type: String,
     required: true,
-    minlength: 2,
-    maxlength: 500 
-  }, 
-  height:{
-    type: Number,
-    required: true,
-    min: 5
+    minlength: 5
   },
-  birthdate:{
+  complete: {
+    type: Boolean,
+    default: false
+  },
+  createdAt: {
     type: Date,
     default: Date.now
   }
 })
-
-new Person({name: "Van", height: 150}).save()
-
+// Defines the port the app will run on. Defaults to 8080, but can be overridden
+// when starting the server. Example command to overwrite PORT env variable value:
+// PORT=9000 npm start
 const port = process.env.PORT || 8080;
 const app = express();
 
@@ -39,17 +36,25 @@ app.get("/", (req, res) => {
   res.send("Hello Technigo!");
 });
 
-app.post('/people', async (req, res) => {
-  // Promises
-  new Person(req.body).save()
-  .then((person) => {
-    res.status(200).json(person)
-  })
-  .catch((err) => {
-    res.status(400).json({message: 'Could not save person', errors: err.errors})
-  })
+app.get('/tasks', async (req, res) => {
+  const tasks = await Task.find().sort({createdAt: 'desc'}).limit(20).exec()
+  res.json(tasks)
+})
 
-  
+app.post('/tasks', async (req, res) => {
+  // Retrieve the information sent by the client to our API endpoint
+  const {text, complete} = req.body
+
+  // Use our mongoose model to create the database entry
+  const task = new Task({text, complete})
+
+  try {
+    // Success
+    const savedTask = await task.save()
+    res.status(201).json(savedTask)
+  } catch (err) {
+    res.status(400).json({message: 'Cound not save task to the Database', error:err.errors})
+  }
 })
 
 // Start the server
