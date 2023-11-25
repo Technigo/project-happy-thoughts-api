335fa9d50329376e5f58d3bb1c0f7ee7a70b7227
So Youn Choi <manghatuhyo@gmail.com>
2023-11-25 12:57:00 +0100

    user can upload message if they are using postman

--- server.js
+++ server.js
@@ -2,25 +2,20 @@ import express from "express";
 import cors from "cors";
 import mongoose from "mongoose";
 
-const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/post-codealong";
+const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/project-mongo";
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true });
 mongoose.Promise = Promise;
 
-const Task = mongoose.model('Task', {
-  text: {
-    type: String,
-    required: true,
-    minlength: 5
-  },
-  complete: {
-    type: Boolean,
-    default: false
-  },
+const messageSchema = new mongoose.Schema({
+  content: String,
   createdAt: {
     type: Date,
-    default: Date.now
+    default: Date.now,
   }
 })
+
+const Message = mongoose.model('Message', messageSchema)
+
 // Defines the port the app will run on. Defaults to 8080, but can be overridden
 // when starting the server. Example command to overwrite PORT env variable value:
 // PORT=9000 npm start
@@ -36,24 +31,27 @@ app.get("/", (req, res) => {
   res.send("Hello Technigo!");
 });
 
-app.get('/tasks', async (req, res) => {
-  const tasks = await Task.find().sort({createdAt: 'desc'}).limit(20).exec()
-  res.json(tasks)
+app.get('/messages', async (req, res) => {
+  try {
+    const messages = await Message.find().sort({ createdAt: 'desc' }).limit(20).exec()
+    res.json(messages)
+  } catch (error) {
+    res.status(500).json({ message: 'Error retrieving messages', error })
+  }
 })
 
-app.post('/tasks', async (req, res) => {
-  // Retrieve the information sent by the client to our API endpoint
-  const {text, complete} = req.body
-
-  // Use our mongoose model to create the database entry
-  const task = new Task({text, complete})
-
+app.post('/messages', async (req, res) => {
   try {
-    // Success
-    const savedTask = await task.save()
-    res.status(201).json(savedTask)
-  } catch (err) {
-    res.status(400).json({message: 'Cound not save task to the Database', error:err.errors})
+    const { content } = req.body
+    if (!content) {
+      res.status(400).json({ message: 'Content is required' })
+      return
+    }
+    const newMessage = new Message({ content })
+    await newMessage.save()
+    res.status(201).json(newMessage)
+  } catch (error) {
+    res.status(500).json({ message: error.message })
   }
 })
 
