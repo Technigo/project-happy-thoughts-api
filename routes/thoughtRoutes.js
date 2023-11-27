199efcc250a303b1994151454e2e97333999a10c
Emmy Dieden <emmydieden@Emmys-MacBook-Pro.local>
2023-11-27 20:12:38 +0100

    heart route working

--- routes/thoughtRoutes.js
+++ routes/thoughtRoutes.js
@@ -1,21 +1,60 @@
-import express from "express"
-const listEndPoints = require("express-list-endpoints")
-import { ThoughtModel } from "../models/Thought"
+import express from "express";
+const listEndPoints = require("express-list-endpoints");
+import { ThoughtModel } from "../models/Thought";
 
-const router = express.Router()
+const router = express.Router();
 
 // Route to list all the endpoints
 router.get("/", async (req, res) => {
-    const endpoints = listEndPoints(req.app)
-    res.json(endpoints)
-  })
+  const endpoints = listEndPoints(req.app);
+  res.json(endpoints);
+});
+
+router.get("/thoughts", async (req, res) => {
+  const thoughts = await ThoughtModel.find()
+    .sort({ createdAt: "desc" })
+    .limit(20)
+    .exec();
+  res.json(thoughts);
+});
 
 router.post("/thoughts", async (req, res) => {
-    const { message, hearts } = req.body
-    const newMessage = new ThoughtModel({message, hearts})
-    await newMessage.save()
-    res.json(newMessage)
+    //Retrieve the information sent by the client to our API endpoint
+  const { message } = req.body;
+
+  //Use our mongoose model to create the database entry
+  const newMessage = new ThoughtModel({ message });
+  try {
+    //Success: 
+  await newMessage.save();
+  res.status(201).json(newMessage);
+} catch (err) {
+    res.status(400).json({message: "Could not save the happy thought to the database", error: err.errors})
+}
+});
+
+router.post("/thoughts/:thoughtId/like", async (req, res) => {
+    const { thoughtId } = req.params;
+  
+    try {
+      // Find the thought by its ID
+      const thought = await ThoughtModel.findById(thoughtId);
+  
+      if (!thought) {
+        // If the thought with the provided ID is not found
+        return res.status(404).json({ message: "Thought not found" });
+      }
+  
+      // Increment the hearts/likes count
+      thought.hearts += 1;
+  
+      // Save the updated thought
+      await thought.save();
+  
+      res.json(thought);
+    } catch (err) {
+      res.status(500).json({ message: "Error while processing like", error: err.message });
+    }
 })
-  
-  
-  export default router
+
+export default router;
