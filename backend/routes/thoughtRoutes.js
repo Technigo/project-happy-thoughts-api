9d81aac146c12baeefe26a1b801c63d920611861
Andrej <72137459+andrkavr@users.noreply.github.com>
2023-11-29 10:51:44 +0100

    initial commit

---
+++ backend/routes/thoughtRoutes.js
@@ -0,0 +1,34 @@
+import express from "express";
+import listEndpoints from "express-list-endpoints";
+import thoughtRoutes from "../routes/";
+import { ThoughtModel } from "../models/thoughtModel";
+const router = express.Router();
+
+// Route to get available endpoints
+router.get("/", (req, res) => {
+  const endpoints = listEndpoints(router);
+  res.json({ endpoints });
+});
+
+// Route to get all titles from the database
+router.get("/thoughts", async (req, res) => {
+  await ThoughtModel.find()
+    .then((result) => {
+      res.json(result);
+    })
+    .catch((err) => res.json(err));
+});
+
+router.post("/add", async (req, res) => {
+  const title = req.body.title;
+  // ... (repeat for other properties)
+
+  await ThoughtModel.create({
+    title: title,
+    // ... (repeat for other properties)
+  })
+    .then((result) => res.json(result))
+    .catch((err) => res.json(err));
+});
+
+export default router;
