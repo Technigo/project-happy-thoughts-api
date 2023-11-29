97df7a0c15ef3c78ee66c4fe6d4eaef0bdb684b1
EvelynDelCarmen <hey@evelyndelcarmen.com>
2023-11-29 18:27:09 +0100

    added all the comments, to understand

--- models/Thought.js
+++ models/Thought.js
@@ -1,28 +1,36 @@
+// Import the mongoose library, which is an ODM (Object Data Modeling) library for MongoDB
 import mongoose from "mongoose";
 
+// Destructure the Schema object from the mongoose library
 const { Schema } = mongoose;
 
+// Define a Mongoose schema for the "thought" document in MongoDB
 export const thoughtSchema = new Schema({
+    // Define the "message" field with specific properties
     message: {
-        type: String,
-        required: true,
-        minlength: 5,
-        maxlength: 140,
-        trim: true,
+        type: String, // Data type of the field is String
+        required: true, // Field is required (must be present)
+        minlength: 5, // Minimum length of the string is 5 characters
+        maxlength: 140, // Maximum length of the string is 140 characters
+        trim: true, // Remove leading and trailing whitespaces from the string
     },
+    // Define the "hearts" field with specific properties
     hearts: {
-        type: Number,
-        default: 0,
+        type: Number, // Data type of the field is Number
+        default: 0, // Default value is 0 if not provided
     },
+    // Define the "createdAt" field with specific properties
     createdAt: {
-        type: Date,
-        default: Date.now,
+        type: Date, // Data type of the field is Date
+        default: Date.now, // Default value is the current date and time
     },
+    // Define the "complete" field with specific properties
     complete: {
-        type: Boolean,
-        default: false,
+        type: Boolean, // Data type of the field is Boolean
+        default: false, // Default value is false if not provided
     },
 });
 
+// Create a Mongoose model named "Thought" based on the defined schema
 export const ThoughtModel = mongoose.model("Thought", thoughtSchema);
 
--- server.js
+++ server.js
@@ -1,31 +1,47 @@
-import express from "express";
-import cors from "cors";
-import mongoose from "mongoose";
-import thoughtRoute from "./routes/thoughtRoute";
-import dotenv from "dotenv";
+import express from "express";// Express is a web framework for Node.js
+import cors from "cors"; // CORS (Cross-Origin Resource Sharing) middleware for Express
+import mongoose from "mongoose"; // Mongoose is an ODM (Object Data Modeling) library for MongoDB
+import thoughtRoute from "./routes/thoughtRoute";// Importing the routes defined in thoughtRoute.js
+import dotenv from "dotenv";// Load environment variables from a .env file
 
+// Load environment variables from a .env file
 dotenv.config();
+
+// Set a configuration option for Mongoose to allow more flexible queries
 mongoose.set("strictQuery", false);
 
+// Define the MongoDB connection URL, default to a local database
 const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/project-mongo";
+
+// Connect to the MongoDB database
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true });
+
+// Use native promises with Mongoose
 mongoose.Promise = Promise;
 
+// Define the port the server will run on, default to 8080
 const port = process.env.PORT || 8080;
+// Create an instance of the Express application
 const app = express();
 
+// Enable CORS middleware to handle cross-origin requests
 app.use(cors());
+// Parse incoming JSON requests
 app.use(express.json());
 
-// Use the thought routes
+// Use the thought routes defined in thoughtRoute.js
 app.use("/", thoughtRoute);
 
-// Error handling middleware
+// Error handling middleware - this will catch and handle errors in the application
 app.use((err, req, res, next) => {
+  // Log the error to the console
   console.error(err.stack);
+
+  // Send a 500 Internal Server Error response with a JSON object
   res.status(500).json({ error: "Internal Server Error" });
 });
 
+// Start the server and listen for incoming requests on the specified port
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
 });
