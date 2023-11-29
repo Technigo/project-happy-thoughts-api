29ec12e64ba9f1af351cb513887043bcc40b2272
Minadarabi <minaadabirii@gmail.com>
2023-11-29 09:19:59 -0800

    first commit

---
+++ models/Thoughts.js
@@ -0,0 +1,27 @@
+import mongoose from "mongoose";
+
+const { Schema } = mongoose;
+
+export const thoughtsSchema = new Schema({
+    message: {
+        type: String,
+        required: true,
+        minlength: 5,
+        maxlength: 140
+    },
+    hearts: {
+        type: Number,
+        default: 0,
+       
+    
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now(),
+        timestamps: true
+    }
+
+
+});
+
+export const ThoughtsModel = mongoose.model("thought" , thoughtsSchema)
