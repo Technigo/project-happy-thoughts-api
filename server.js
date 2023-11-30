82b47132d7daf86d9e926596edc27d6153c53a52
“mikaeloling” <mikael.oling@gmail.com>
2023-11-30 22:23:43 +0200

    server js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ import cors from "cors";
 import mongoose from "mongoose";
 import router from "./routes/thoughtsRouter.js";
 
-const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/project-mongo";
+const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/thoughts";
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true });
 mongoose.Promise = Promise;
 
@@ -16,11 +16,29 @@ const app = express();
 // Add middlewares to enable cors and json body parsing
 app.use(cors());
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 
 // Start defining your routes here
 app.get("/", (req, res) => {
-  res.send("Hello Technigo!");
+  res.json([
+    {
+      message: "Welcome to my thoughts API",
+      message: "This is a REST API for my thoughts",
+    },
+    {
+      "paths": [ "/thoughts", "/thoughts/:thoughtId/like" ],
+      "methods": [ "GET", "POST" ],
+      "description": "Get all thoughts or post a new thought",
+      "query": [ "message", "name" ],
+      "body": [ "message", "name" ],
+      "example": "/thoughts?message=Happy%20thoughts&name=Anonymous"
+    }
+      
+  ]);
 });
+
+
 
 app.use("/thoughts", router);
 
