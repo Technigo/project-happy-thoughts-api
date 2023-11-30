8d6121b5588c06ba6bad3f4bde3397cccefc21aa
Andrej <72137459+andrkavr@users.noreply.github.com>
2023-11-30 15:34:50 +0100

    changed timestamp 2

--- backend/models/Thought.js
+++ backend/models/Thought.js
@@ -2,17 +2,22 @@ import mongoose from "mongoose";
 
 const { Schema } = mongoose;
 
-export const thoughtSchema = new Schema({
-  title: {
-    type: String,
+export const thoughtSchema = new Schema(
+  {
+    title: {
+      type: String,
+    },
+    likes: {
+      type: Number,
+    },
+
+    //     author: {
+    //       type: String
+    //   }
   },
-  likes: {
-    type: Number,
-  },
-  timestamps: true,
-  //     author: {
-  //       type: String
-  //   }
-});
+  {
+    timestamps: true,
+  }
+);
 
 export const ThoughtModel = mongoose.model("thoughts", thoughtSchema);
