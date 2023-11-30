14a59d45b30c4fa2821be64455dbb9c41dbbf88b
Andrej <72137459+andrkavr@users.noreply.github.com>
2023-11-30 15:34:11 +0100

    changed timestamp 1

--- backend/models/User.js
+++ backend/models/User.js
@@ -2,30 +2,34 @@ import mongoose from "mongoose";
 
 const { Schema } = mongoose;
 
-export const userSchema = new Schema({
-  email: {
-        type: String,
-        unique: true,
-        required: true,
-  },
-  username: {
+export const userSchema = new Schema(
+  {
+    email: {
+      type: String,
+      unique: true,
+      required: true,
+    },
+    username: {
       type: String,
       unique: true,
       required: true,
       minlength: 5,
-  },
-  password: {
-    type: String,
-    required: true,
-},
-    accessToken: {
-        type: String,
-        default: () => crypto.randomBytes(128).toString("hex")
     },
+    password: {
+      type: String,
+      required: true,
+    },
+    accessToken: {
+      type: String,
+      default: () => crypto.randomBytes(128).toString("hex"),
+    },
+    // posts: {
+    //     type: String
+    // }
+  },
+  {
     timestamps: true,
-  // posts: {
-  //     type: String
-  // }
-});
+  }
+);
 
 export const UserModel = mongoose.model("users", userSchema);
