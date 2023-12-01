c582f500f3403fdae279b7fca15ebe2da3236c06
veronica astrom <v.aastrom@gmail.com>
2023-12-01 17:21:30 +0100

    connected to DB

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -3,6 +3,21 @@
 const Thought = require('../models/thoughtModel')
 
 
+// Error handling function
+const handleErrors = (res, error) => {
+    // Check if the error is a validation error
+    if (error.name === 'ValidationError') {
+        // Extract validation error messages
+        const validationErrors = Object.values(error.errors).map((e) => e.message);
+        // Respond with a 400 status code and include validation error details
+        res.status(400).json({ error: 'Validation Error', validationErrors });
+    } else {
+        // For other types of errors, log the error and respond with a 500 status code
+        console.error(error);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
+};
+
 // @desc Get thoughts
 // @route GET /thoughts
 // @access Public
@@ -16,8 +31,7 @@ const getThoughts = async (req, res) => {
         res.status(200).json(thoughts);
 
     } catch (error) {
-        console.error(error);
-        res.status(500).json({ error: 'Internal Server Error' });
+        handleErrors(res, error)
     }
 }
 
@@ -39,16 +53,7 @@ const createThought = async (req, res) => {
         return res.status(201).json(thought);
 
     } catch (error) {
-        // Check if the error is of type 'ValidationError'
-        if (error.name === 'ValidationError') {
-            // Validation error occurred
-            // Extract error messages from each validation error and map them
-            const validationErrors = Object.values(error.errors).map((e) => e.message);
-            return res.status(400).json({ error: 'Validation Error', validationErrors });
-        }
-        // If it's not a validation error, log the error and send a 500 Internal Server Error response
-        console.error(error);
-        return res.status(500).json({ error: 'Internal Server Error' });
+        handleErrors(res, error)
     }
 }
 
@@ -58,7 +63,7 @@ const createThought = async (req, res) => {
 
 const createLike = async (req, res) => {
     try {
-       
+
         // The $inc operator has the following form: { $inc: { <field1>: <amount1>, <field2>: <amount2>, ... } }
         const thought = await Thought.findByIdAndUpdate(
             req.params.thoughtId,
@@ -67,15 +72,14 @@ const createLike = async (req, res) => {
         );
 
         // Check if the thought with the specified ID exists
-        if (!thought) {
+        if (thought === null) {
             return res.status(400).json({ error: 'Thought was not found' });
         }
 
         return res.status(201).json(thought);
 
     } catch (error) {
-        console.error(error);
-        res.status(500).json({ error: 'Internal Server Error' });
+        handleErrors(res, error)
     }
 };
 
