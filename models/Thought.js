ed07db9f0747d6675762c5dc9dd643cb5f2731e0
Emmy Dieden <emmydieden@Emmys-MacBook-Pro.local>
2023-12-01 10:00:28 +0100

    updated readme

--- models/Thought.js
+++ models/Thought.js
@@ -1,24 +1,24 @@
-import mongoose from "mongoose"
+import mongoose from "mongoose";
 
-const { Schema } = mongoose
+const { Schema } = mongoose;
 
 export const thoughtSchema = new Schema({
-    message: {
-        type: String, 
-        required: true, 
-        minlength: 5, 
-        maxlength: 140
-    }, 
-    hearts: {
-        type: Number, 
-        default: 0, 
-        //Should not be assignable when creating a new thought
-    }, 
-    createdAt: {
-         type: Date, 
-         default: Date.now
-        //Should not be assignable when creating a new thought
-    }
-})
+  message: {
+    type: String,
+    required: true,
+    minlength: 5,
+    maxlength: 140,
+  },
+  hearts: {
+    type: Number,
+    default: 0,
+    //Should not be assignable when creating a new thought
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+    //Should not be assignable when creating a new thought
+  },
+});
 
-export const ThoughtModel = mongoose.model("Thought", thoughtSchema)
+export const ThoughtModel = mongoose.model("Thought", thoughtSchema);
--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 import express from "express";
 import cors from "cors";
 import mongoose from "mongoose";
-import thoughtRoutes from "./routes/thoughtRoutes"
+import thoughtRoutes from "./routes/thoughtRoutes";
 
-const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/happy-thoughts";
+const mongoUrl = process.env.MONGO_URL;
+
+// "mongodb://localhost/happy-thoughts"
+
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true });
 mongoose.Promise = Promise;
 
@@ -16,10 +19,10 @@ const app = express();
 // Add middlewares to enable cors and json body parsing
 app.use(cors());
 app.use(express.json());
-app.use(express.urlencoded({ extended: false})); //Cool method according to Diego
+app.use(express.urlencoded({ extended: false })); //Cool method according to Diego
 
 //Getting hold of the get/post-methods (routes) through the thoughtRoutes
-app.use(thoughtRoutes)
+app.use(thoughtRoutes);
 
 // Start the server
 app.listen(port, () => {
