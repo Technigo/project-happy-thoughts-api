ff1863a6c18696d416c24ca4f9dcd1e17e0eac03
FridafridaL <fridus.lindskog@gmail.com>
2023-12-01 10:32:09 +0100

    testing

--- server.js
+++ server.js
@@ -13,25 +13,6 @@ const mongoUrl =
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true });
 mongoose.Promise = Promise;
 
-// const Thought = mongoose.model("Thought", {
-//   message: {
-//     type: String,
-//     required: true,
-//     minlength: 5,
-//     maxlength: 140,
-//   },
-
-//   heart: {
-//     type: Number,
-//     default: 0,
-//   },
-
-//   createdAt: {
-//     type: Date,
-//     default: Date.now,
-//   },
-// });
-
 // wrap this part in an inviroment variabel
 
 // const seedData = [
@@ -67,65 +48,6 @@ app.use(express.json());
 //app.use(Routes);
 
 setUpRoutes(app); // Attach routes to the app
-
-// // Start defining your routes here
-// app.get("/", (req, res) => {
-//   res.send(listEndpoints(app));
-// });
-
-// //routes for all thoughts
-// app.get("/thoughts", async (req, res) => {
-//   const thoughts = await Thought.find()
-//     .sort({ createdAt: "desc" })
-//     .limit(20)
-//     .exec();
-//   res.json(thoughts);
-// });
-
-// //route for posting thought
-// app.post("/thoughts", async (req, res) => {
-//   //Retrieve the info sent by the user to uor API ebdpoint
-//   const { message, heart } = req.body;
-
-//   //Using mongoose model to create the data entry
-//   const thought = new Thought({ message, heart });
-
-//   try {
-//     // success
-//     const savedThought = await thought.save();
-//     res.status(201).json(savedThought);
-//   } catch (err) {
-//     res.status(400).json({
-//       message: "Could not save thought to the Database",
-//       error: err.errors,
-//     });
-//   }
-// });
-
-// //route for adding likes to a post
-// app.post("/thoughts/:thoughtId/like", async (req, res) => {
-//   const { thoughtId } = req.params;
-
-//   try {
-//     const thought = await Thought.findByIdAndUpdate(
-//       thoughtId,
-//       { $inc: { heart: 1 } }, // Increment the hearts property by 1
-//       { new: true } // Return the updated thought
-//     );
-
-//     if (!thought) {
-//       // If the thought with the given ID is not found
-//       return res.status(404).json({ message: "Thought not found" });
-//     }
-
-//     res.json(thought);
-//   } catch (error) {
-//     res.status(500).json({
-//       message: "Internal Server Error",
-//       error: error.message,
-//     });
-//   }
-// });
 
 // Start the server
 app.listen(port, () => {
