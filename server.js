3a2941c2fc7e052e1849e81db016d089be8bc574
Lisa Dahlkar <lisa.dahlkar@gmail.com>
2023-12-01 12:09:50 +0100

    first set of code

--- server.js
+++ server.js
@@ -6,22 +6,84 @@ const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/project-mongo";
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true });
 mongoose.Promise = Promise;
 
-// Defines the port the app will run on. Defaults to 8080, but can be overridden
-// when starting the server. Example command to overwrite PORT env variable value:
-// PORT=9000 npm start
 const port = process.env.PORT || 8080;
 const app = express();
 
-// Add middlewares to enable cors and json body parsing
 app.use(cors());
 app.use(express.json());
 
-// Start defining your routes here
-app.get("/", (req, res) => {
-  res.send("Hello Technigo!");
+const thoughtSchema = new mongoose.Schema({
+  message: {
+    type: String,
+    required: true,
+    minlength: 5,
+    maxlength: 140,
+  },
+  hearts: {
+    type: Number,
+    default: 0,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
 });
 
-// Start the server
+const Thought = mongoose.model('Thought', thoughtSchema);
+
+// New route to display available routes
+app.get("/", (req, res) => {
+  const routes = app._router.stack
+    .filter((r) => r.route)
+    .map((r) => ({
+      path: r.route.path,
+      methods: Object.keys(r.route.methods),
+      middleware: r.route.stack.map((m) => m.name),
+    }));
+  res.json(routes);
+});
+
+app.get("/thoughts", async (req, res) => {
+  try {
+    const thoughts = await Thought.find().sort({ createdAt: -1 }).limit(20);
+    res.json(thoughts);
+  } catch (error) {
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
+app.post("/thoughts", async (req, res) => {
+  const { message } = req.body;
+
+  try {
+    const newThought = new Thought({ message });
+    const savedThought = await newThought.save();
+
+    res.json(savedThought);
+  } catch (error) {
+    res.status(400).json({ error: 'Invalid input' });
+  }
+});
+
+app.post("/thoughts/:thoughtId/like", async (req, res) => {
+  const thoughtId = req.params.thoughtId;
+
+  try {
+    const thought = await Thought.findById(thoughtId);
+
+    if (!thought) {
+      return res.status(404).json({ error: 'Thought not found' });
+    }
+
+    thought.hearts += 1;
+    const updatedThought = await thought.save();
+
+    res.json(updatedThought);
+  } catch (error) {
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
 });
