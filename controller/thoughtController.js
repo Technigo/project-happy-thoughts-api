b1c42a8fe9941054d0339fb0a90acdb27ff4dd70
jforjanice <jjanice.cheng@gmail.com>
2023-12-01 16:46:36 +0100

    Added controller

--- controller/thoughtController.js
+++ controller/thoughtController.js
@@ -1,11 +1,13 @@
 import { ThoughtsModel } from "../model/ThoughtsModel";
-import expressAsyncHandler from "express-async-handler";
+import asyncHandler from "express-async-handler";
 
-// desciption: Get Thoughts
+// desciption: Get 20 latest thoughts
 // route: /thoughts
 // access: Private
-export const getThoughtsController = expressAsyncHandler(async (req, res) => {
+export const getThoughtsController = asyncHandler(async (req, res) => {
   await ThoughtsModel.find()
+    .limit(20)
+    .sort({ createdAt: -1 })
     .then((result) => res.json(result))
     .catch((err) => res.json(err));
 });
@@ -13,13 +15,13 @@ export const getThoughtsController = expressAsyncHandler(async (req, res) => {
 // desciption: ADD Thoughts
 // route: /thoughts
 // access: Private
-export const addThoughtController = expressAsyncHandler(async (req, res) => {
+export const addThoughtController = asyncHandler(async (req, res) => {
   const thought = req.body.thought;
   await ThoughtsModel.create({
     thought: thought,
   })
     .then((result) => res.json(result))
-    .catch((err) => res.json(err));
+    .catch((error) => res.status(400).json({ error: "Input was invalid" }));
 });
 
 // desciption: PUT/PATCH a specific task to give like
@@ -30,5 +32,5 @@ export const addHeartController = expressAsyncHandler(async (req, res) => {
   const { id } = req.params;
   await ThoughtsModel.findByIdAndUpdate({ _id: id })
     .then((result) => res.json(result))
-    .catch((err) => res.json(err));
+    .catch((error) => res.json({ error: "Item not found" }));
 });
--- model/ThoughtsModel.js
+++ model/ThoughtsModel.js
@@ -2,22 +2,21 @@ import mongoose from "mongoose";
 
 const { Schema } = mongoose;
 
-export const ThoughtSchema = new Schema(
-  {
-    thought: {
-      type: String,
-      required: true,
-      minlength: 5,
-      maxlength: 140,
-    },
-    heart: {
-      type: Number,
-      default: 0,
-    },
+export const ThoughtSchema = new Schema({
+  message: {
+    type: String,
+    required: true,
+    minlength: 5,
+    maxlength: 140,
   },
-  {
-    timestamps: true,
-  }
-);
+  hearts: {
+    type: Number,
+    default: 0,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
 
 export const ThoughtsModel = mongoose.model("Thought, ThoughtSchema");
