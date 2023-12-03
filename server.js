30178b5cd26ac29e597be24afdc0d7d8b7ad55c7
“mikaeloling” <mikael.oling@gmail.com>
2023-12-03 22:19:26 +0200

    env stuff

--- server.js
+++ server.js
@@ -16,6 +16,7 @@ const app = express();
 
 // Add middlewares to enable cors and json body parsing
 app.use(cors());
+//
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
