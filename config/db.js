3e9587fb681474d925a0304e5dce25442497c4bc
jforjanice <jjanice.cheng@gmail.com>
2023-12-06 09:50:20 +0100

    Set strictQuery

--- config/db.js
+++ config/db.js
@@ -4,12 +4,16 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
+// Try to follow the class example and breakdown this part
+
+// Keeping the following comment from the example for my reference in the future
 // Attempt to connect to the MongoDB database using the URL from the environment variables
 // Mongoose Method: mongoose.connect()
 // Description: This line of code serves the crucial purpose of connecting the Node.js application to the MongoDB database specified by the URL provided in the environment variable MONGO_URL. Once this connection is established, the application can perform various database operations, such as querying and modifying data in the MongoDB database. It's a critical step in setting up the database connection for the application to work with MongoDB.
 
 export const connectDB = asyncHandler(async () => {
   try {
+    mongoose.set("strictQuery", false);
     const connect = await mongoose.connect(process.env.MONGO_URL);
 
     console.log(`Mongo DB Connected: ${connect.connection.host}`);
