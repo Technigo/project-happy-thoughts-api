6d4d4586b2de898dc0eb08d9e7f6c139d80ab5db
Your Name alexandrameija@hotmail.com <alexandrameija@hotmail.com.com>
2023-12-06 14:32:29 +0100

    -m

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ import cors from "cors";
 import mongoose from "mongoose";
 import listEndpoints from "express-list-endpoints";
 
-const mongoUrl = process.env.MONGO_URL || "mongodb://127.0.0.1:27017/test";
+const mongoUrl = process.env.MONGO_URL || "mongodb://127.0.0.1:27017/Happyapp";
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true });
 mongoose.Promise = Promise;
 
