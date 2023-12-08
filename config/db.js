9fa0fbfa0dbbd4caca5a9f822cd3e015fbf0b901
jforjanice <jjanice.cheng@gmail.com>
2023-12-08 14:10:56 +0100

    updated data connect

--- config/db.js
+++ config/db.js
@@ -14,10 +14,9 @@ dotenv.config();
 export const connectDB = asyncHandler(async () => {
   try {
     mongoose.set("strictQuery", false);
-    const mongoUrl = process.env.MONGO_URI;
     mongoose.Promise = Promise;
 
-    const connect = await mongoose.connect(mongoUrl, {
+    const connect = await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
