fc504eaa3661c345d7dd3de7da5f821caf53d3f0
Your Name alexandrameija@hotmail.com <alexandrameija@hotmail.com.com>
2023-12-10 13:10:04 +0100

    -m

--- server.js
+++ server.js
@@ -7,18 +7,13 @@ const mongoUrl = process.env.MONGO_URL || "mongodb://127.0.0.1:27017/Happyapp";
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true });
 mongoose.Promise = Promise;
 
-// Defines the port the app will run on. Defaults to 8080, but can be overridden
-// when starting the server. Example command to overwrite PORT env variable value:
-// PORT=9000 npm start
 const port = process.env.PORT || 8080;
 const app = express();
 
-// Add middlewares to enable cors and json body parsing
 app.use(cors());
 app.use(express.json());
 
-
-const Thought = require('./models/thought'); // correct the path here
+import Thought from './models/thought.js';
 
 app.get('/', (req, res) => {
     try {
