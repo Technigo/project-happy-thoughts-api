65d6bc4dc38cf872b4ca620d5b503b3ff09fac4b
Linnea Johansson <Linneajoh@gmail.com>
2024-01-01 16:55:43 +0100

    requirements met

--- server.js
+++ server.js
@@ -2,26 +2,84 @@ import express from "express";
 import cors from "cors";
 import mongoose from "mongoose";
 
-const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/project-mongo";
+const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/HappyThoughts";
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true });
 mongoose.Promise = Promise;
 
-// Defines the port the app will run on. Defaults to 8080, but can be overridden
-// when starting the server. Example command to overwrite PORT env variable value:
-// PORT=9000 npm start
 const port = process.env.PORT || 8080;
 const app = express();
 
-// Add middlewares to enable cors and json body parsing
 app.use(cors());
 app.use(express.json());
 
-// Start defining your routes here
-app.get("/", (req, res) => {
-  res.send("Hello Technigo!");
+// Thought model
+const Thought = mongoose.model('Thought', {
+  message: {
+    type: String,
+    required: true,
+    minlength: 5,
+    maxlength: 140
+  },
+  hearts: {
+    type: Number,
+    default: 0
+  },
+  createdAt: {
+    type: Date,
+    default: () => new Date()
+  }
+});
+
+// Routes
+app.get('/', (req, res) => {
+  const endpoints = [
+    { path: "/", methods: ["GET"], middlewares: ["anonymous"] },
+    { path: "/thoughts", methods: ["GET", "POST"], middlewares: ["anonymous"] },
+    { path: "/thoughts/:thoughtId/like", methods: ["POST"], middlewares: ["anonymous"] }
+  ];
+  res.json({ endpoints });
+});
+
+// GET /thoughts
+app.get('/thoughts', async (req, res) => {
+  const thoughts = await Thought.find().sort({ createdAt: 'desc' }).limit(20);
+  res.json(thoughts);
+});
+
+// POST /thoughts
+app.post('/thoughts', async (req, res) => {
+  // Only allow the message field from the request to prevent setting hearts
+  const { message } = req.body;
+  try {
+    const newThought = await new Thought({ message }).save();
+    res.status(201).json(newThought);
+  } catch (error) {
+    res.status(400).json({ message: 'Could not save thought to the database', error: error.errors });
+  }
+});
+
+// POST thoughts/:thoughtId/like
+app.post('/thoughts/:thoughtId/like', async (req, res) => {
+  try {
+    // Find the thought by id and increment hearts by 1
+    const thought = await Thought.findByIdAndUpdate(
+      req.params.thoughtId,
+      { $inc: { hearts: 1 } },
+      { new: true }
+    );
+
+    if (thought) {
+      res.json(thought);
+    } else {
+      res.status(404).json({ message: 'Thought not found' });
+    }
+  } catch (error) {
+    res.status(400).json({ message: 'Invalid request', error });
+  }
 });
 
 // Start the server
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
 });
+
