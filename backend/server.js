e7f9ddc251dba956ce4a3bda01819cb4ac12927b
JuliaHolm <juliat.holm@gmail.com>
2024-01-23 21:07:46 +0100

    ...

--- backend/server.js
+++ backend/server.js
@@ -1,27 +1,44 @@
 import express from "express";
 import cors from "cors";
 import mongoose from "mongoose";
+import listEndpoints from "express-list-endpoints";
+import dotenv from "dotenv";
+dotenv.config();
 
-const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/project-mongo";
-mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true });
-mongoose.Promise = Promise;
+import { connectDB } from "./config/db.js";
+import thoughtRoutes from "./routes/thoughtRoutes.js";
 
-// Defines the port the app will run on. Defaults to 8080, but can be overridden
-// when starting the server. Example command to overwrite PORT env variable value:
-// PORT=9000 npm start
-const port = process.env.PORT || 8080;
+const port = process.env.PORT;
 const app = express();
 
 // Add middlewares to enable cors and json body parsing
 app.use(cors());
 app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
-// Start defining your routes here
-app.get("/", (req, res) => {
-  res.send("Hello Technigo!");
+// Connect to MongoDB
+connectDB();
+
+// Checks the state of your MongoDB connection
+app.use((req, res, next) => {
+  if (mongoose.connection.readyState === 1) {
+    // 1 = Connected
+    next();
+  } else {
+    // 0 = Disconnected
+    res.status(503).json({ error: "Service unavailable" });
+  }
 });
+
+// ROUTES
+app.get("/", (req, res) => {
+  res.json(listEndpoints(app)); // List all endpoints of the server
+});
+
+// Route definitions
+app.use(thoughtRoutes);
 
 // Start the server
 app.listen(port, () => {
-  console.log(`Server running on http://localhost:${port}`);
+  console.log(`Server running on http://localhost:${port} 🟢`);
 });
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,35 +1,103 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Header } from "./components/Header";
+import { Feed } from "./components/Feed";
+import { Form } from "./components/Form";
+import { useEffect, useState } from "react";
+import { LoadingComp } from "./components/LoadingComp";
 
-function App() {
-  const [count, setCount] = useState(0)
+export const App = () => {
+  // Define state variables and initialize them with default values.
+  const [thoughtsData, setThoughtsData] = useState([]); // Stores thoughts data.
+  const [loading, setLoading] = useState(true); // Indicates whether data is loading.
+  // Store the total number of likes, retrieved from localStorage.
+  const [totalLikes, setTotalLikes] = useState(
+    parseInt(localStorage.getItem("totalLikes")) || 0
+  );
 
+  // Define the API endpoint URL.
+  // This will return the latest 20 thoughts data from the API.
+  const apiUrl = "https://happy-thoughts-ux7hkzgmwa-uc.a.run.app/thoughts";
+
+  // Function to fetch data from the API.
+  const fetchData = async () => {
+    // Set a minimum loading time of 2 seconds (2000 milliseconds)
+    const minimumLoadingTime = 3000;
+
+    // Record the start time
+    const startTime = Date.now();
+
+    try {
+      const response = await fetch(apiUrl);
+      if (!response.ok) {
+        throw new Error("Failed to getch thoughts");
+      }
+      const data = await response.json();
+      setThoughtsData(data);
+    } catch (error) {
+      console.error("Error fetching data", error);
+    } finally {
+      // Calculate the elapsed time
+      const elapsedTime = Date.now() - startTime;
+
+      // Calculate the remaining time to meet the minimum loading time
+      const remainingTime = Math.max(0, minimumLoadingTime - elapsedTime);
+
+      // Delay the setLoading(false) call to meet the minimum loading time
+      setTimeout(() => {
+        setLoading(false);
+        console.log("Success fetching data");
+      }, remainingTime);
+    }
+  };
+
+  // Use the useEffect hook to run fetchData on component mount.
+  useEffect(() => {
+    fetchData();
+    // Set up a timer to fetch data periodically (every 5 seconds).
+    const intervalId = setInterval(fetchData, 5000);
+    // Clean up the interval when the component unmounts.
+    return () => {
+      clearInterval(intervalId);
+    };
+  }, []);
+
+  // Use the useEffect hook to save totalLikes to localStorage when it changes.
+  useEffect(() => {
+    localStorage.setItem("totalLikes", totalLikes.toString());
+  }, [totalLikes]);
+
+  // Callback function for when a new thought is submitted.
+  const addNewThought = (newThought) => {
+    const uniqueKey = Date.now(); // Use a timestamp as a unique key.
+    // Create a new thought object with the unique key.
+    const thoughtWithKey = {
+      ...newThought,
+      _id: uniqueKey, // Using `_id` for the key.
+    };
+    // Updating `thoughtsData` state by adding `thoughtWithKey` at the beginning of the array.
+    setThoughtsData([thoughtWithKey, ...thoughtsData]);
+  };
+
+  // Render the main content of the app.
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <Header totalLikes={totalLikes} />
+      <div className="main-wrapper">
+        <Form
+          newThought={addNewThought}
+          apiUrl={apiUrl}
+          fetchData={fetchData}
+        />
+        {loading ? (
+          <LoadingComp />
+        ) : (
+          <Feed
+            thoughtsData={thoughtsData}
+            onLikeChange={(likeChange) =>
+              setTotalLikes(totalLikes + likeChange)
+            }
+          />
+        )}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
-}
-
-export default App
+  );
+};
