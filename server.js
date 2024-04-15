abb1982ade131bd69354bb796f379f474337f8e5
hamdiolad <oladhamdi@gmail.com>
2024-04-15 19:28:51 +0200

    code added

--- server.js
+++ server.js
@@ -1,27 +1,35 @@
 import express from "express";
 import cors from "cors";
 import mongoose from "mongoose";
+import thoughtRoute from "./routes/thoughtRoute";
+import dotenv from "dotenv";
 
-const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/project-mongo";
+dotenv.config();
+
+// Define the MongoDB connection URL, default to a local database
+const mongoUrl = process.env.MONGO_URI || "mongodb://localhost/project-mongo";
+
+// Connect to the MongoDB database
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true });
 mongoose.Promise = Promise;
 
-// Defines the port the app will run on. Defaults to 8080, but can be overridden
-// when starting the server. Example command to overwrite PORT env variable value:
-// PORT=9000 npm start
 const port = process.env.PORT || 8080;
 const app = express();
 
-// Add middlewares to enable cors and json body parsing
 app.use(cors());
 app.use(express.json());
 
-// Start defining your routes here
 app.get("/", (req, res) => {
   res.send("Hello Technigo!");
 });
 
-// Start the server
+app.use("/", thoughtRoute);
+
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).json({ error: "Internal Server Error" });
+});
+
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
 });
