4e32cb051453df8306501e6e3223a45446bec5fd
Your El1an3 <eliane_w@yahoo.com>
2024-05-11 12:27:47 +0200

    update and import packages

--- server.js
+++ server.js
@@ -1,20 +1,24 @@
 import express from "express";
 import cors from "cors";
 import mongoose from "mongoose";
+import dotenv from "dotenv";
+import expressListEndpoints from "express-list-endpoints";
+
+dotenv.config();
 
 const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/project-mongo";
-mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect(mongoUrl);
 mongoose.Promise = Promise;
 
-// Defines the port the app will run on. Defaults to 8080, but can be overridden
-// when starting the server. Example command to overwrite PORT env variable value:
-// PORT=9000 npm start
+//defines the port the app will run on
 const port = process.env.PORT || 8080;
 const app = express();
 
-// Add middlewares to enable cors and json body parsing
+//add middlewares to enable cors and json body parsing
 app.use(cors());
 app.use(express.json());
+
+//create thoughts model
 
 // Start defining your routes here
 app.get("/", (req, res) => {
