1967e969a5d92cc4974902eab515521ff5d235c1
Viktoria Olofsson <viktoriaolofsson@gmail.com>
2024-05-12 22:53:53 +0200

    basics

--- server.js
+++ server.js
@@ -2,9 +2,30 @@ import express from "express";
 import cors from "cors";
 import mongoose from "mongoose";
 
-const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/project-mongo";
+const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/thoughts";
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true });
 mongoose.Promise = Promise;
+
+const { Schema } = mongoose;
+
+const thoughtSchema = new Schema({
+  message: {
+    type: String,
+    required: true,
+    minLength: 5,
+    maxLength: 140,
+  },
+  heart: {
+    type: Number,
+    default: 0,
+  },
+  createdAt: {
+    type: Date,
+    default: () => new Date(),
+  },
+});
+
+const Thought = mongoose.model("Thought", thoughtSchema);
 
 // Defines the port the app will run on. Defaults to 8080, but can be overridden
 // when starting the server. Example command to overwrite PORT env variable value:
@@ -19,6 +40,25 @@ app.use(express.json());
 // Start defining your routes here
 app.get("/", (req, res) => {
   res.send("Hello Technigo!");
+});
+
+app.get("/thoughts", async (req, res) => {
+  res.send("Here there be happy thoughtsx");
+});
+
+app.post("/thoughts", async (req, res) => {
+  try {
+    if (req.body.message.length < 5) {
+      throw new Error("Message must be at least 5 characters long");
+    } else if (req.body.message.length > 140) {
+      throw new Error("Message must be at less than 140 characters long");
+    }
+    const thought = new Thought({ message: req.body.message });
+    await thought.save();
+    res.status(201).json(thought);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
 });
 
 // Start the server
