50d0a26246d73c76a20e70fd684fdc516e6e1b4b
Alexandra Franzen <alexandra.s.franzen@gmail.com>
2024-05-15 17:57:28 +0200

    Added route for finding a post by Id to make sure I had it all in order

--- server.js
+++ server.js
@@ -61,7 +61,22 @@ app.post("/thoughts", async (req, res) => {
   }
 });
 
-//POST thoughts/:thoughtId/like`
+//POST thoughts/:thoughtId/like
+// This endpoint doesn't require a JSON body. 
+// Given a valid thought id in the URL, the API should 
+// find that thought, and update its `hearts` property to 
+// add one heart.
+app.get("/thoughts/:thoughtsId", async (req, res) => {
+  const { thoughtsId } = req.params;
+  const thoughtId = await Thought.findById(thoughtsId).exec();
+  
+  if (thoughtId) {
+    res.json(thoughtId);
+  } else {
+    res.status(404).send("No thought was found.")
+  }
+});
+
 
 //PATCH?
 
