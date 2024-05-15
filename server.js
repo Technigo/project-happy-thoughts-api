6f71d1de700edc376830111f244381c7247f798d
unknown <linda.frischknecht@bluewin.ch>
2024-05-15 10:21:23 +0200

    adding the get endpoint for the thoughts and troubleshooting some import error

--- server.js
+++ server.js
@@ -3,7 +3,11 @@ import express from "express";
 import expressListEndpoints from "express-list-endpoints";
 import mongoose from "mongoose";
 
-const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/project-mongo";
+//Importing the Model
+import { Thought } from "./models/ThoughtSchema";
+
+const mongoUrl =
+  process.env.MONGO_URL || "mongodb://localhost/project-thoughts-api";
 mongoose.connect(mongoUrl);
 mongoose.Promise = Promise;
 
@@ -17,10 +21,40 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-// Start defining your routes here
+// API documentation
+//TODO Add description!
 app.get("/", (req, res) => {
-  const endpoints = expressListEndpoints(app);
-  res.json(endpoints);
+  try {
+    const endpoints = expressListEndpoints(app);
+    res.status(200).json(endpoints);
+  } catch (error) {
+    console.error("The following error occured:", error);
+    res
+      .status(500)
+      .send(
+        "Sorry, this page is not available at the moment. Please try again later."
+      );
+  }
+});
+
+//GET the thoughts endpoint
+app.get("/thoughts", async (req, res) => {
+  try {
+    const allThoughts = await Thought.find();
+    console.log(allThoughts);
+    if (allThoughts.length > 0) {
+      res.status(200).json(allThoughts);
+    } else {
+      res.status(404).send("No Thoughts were found.");
+    }
+  } catch (error) {
+    console.error("The following error occured:", error);
+    res
+      .status(500)
+      .send(
+        "Sorry, this page is not available at the moment. Please try again later."
+      );
+  }
 });
 
 // Start the server
