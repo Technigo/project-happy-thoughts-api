e7ad4bd5ca203e357b268e44499a618847aec1ad
Alexandra Franzen <alexandra.s.franzen@gmail.com>
2024-05-15 17:22:46 +0200

    GET and POST endpoints are working

--- server.js
+++ server.js
@@ -2,24 +2,68 @@ import cors from "cors";
 import express from "express";
 import mongoose from "mongoose";
 
-const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/project-mongo";
+const mongoUrl =
+  process.env.MONGO_URL || "mongodb://localhost/project-happy-thoughts-mongo";
 mongoose.connect(mongoUrl);
 mongoose.Promise = Promise;
 
-// Defines the port the app will run on. Defaults to 8080, but can be overridden
-// when starting the server. Example command to overwrite PORT env variable value:
-// PORT=9000 npm start
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 8090;
 const app = express();
 
+const Thought = mongoose.model("Thought", {
+  message: {
+    type: String,
+    required: true,
+    minlength: 5,
+    maxlength: 140,
+  },
+  hearts: {
+    type: Number,
+    default: 0,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
 // Add middlewares to enable cors and json body parsing
 app.use(cors());
 app.use(express.json());
 
 // Start defining your routes here
-app.get("/", (req, res) => {
-  res.send("Hello Technigo!");
+//GET thoughts
+app.get("/thoughts", async (req, res) => {
+  const thoughts = await Thought.find()
+    .sort({ createdAt: "desc" })
+    .limit(20)
+    .exec();
+  res.json(thoughts);
 });
+
+//POST thoughts
+app.post("/thoughts", async (req, res) => {
+  const { message, hearts, createdAt } = req.body;
+  const thought = new Thought({ message, hearts, createdAt });
+
+  try {
+    //Success case
+    const savedThougth = await thought.save();
+    res.status(201).json(savedThougth);
+  } catch (err) {
+    //fail case
+    res
+      .status(400)
+      .json({
+        message:
+          "Couldn't save thought. Please try again, your thoughts are important.",
+        error:err.errors,
+      });
+  }
+});
+
+//POST thoughts/:thoughtId/like`
+
+//PATCH?
 
 // Start the server
 app.listen(port, () => {
