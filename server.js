d05af50294cfaa95f084a183e81e91b107ae9c9e
Mai Kanetaka <maikanetaka@gmail.com>
2024-05-16 22:24:56 +0200

    fix bugs

--- server.js
+++ server.js
@@ -6,14 +6,12 @@ import { Thought } from "./models/thought.js";
 
 // Set up MongoDB URL
 const mongoUrl =
-  process.env.MONGO_URL || "mongodb://localhost:27017/project-mongo";
+  process.env.MONGO_URL || "mongodb://localhost:27017/happy-thoughts";
 
 // Connect to MongoDB
 // useNewUrlParser: true means use a newer connecting system which makes the server to be more stable
 // useUnifiedTopology: true means use a newer search engine which makes the connection to be more efficient
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true });
-
-// ?? what is this
 mongoose.Promise = Promise;
 
 // Defines the port the app will run on. Defaults to 8080, but can be overridden
@@ -32,9 +30,7 @@ const routes = [
     description: "Get the last 20 thoughts in descending order",
     handler: async (req, res) => {
       try {
-        const thoughts = await Thought.find()
-          .sort({ createdAt: -1 })
-          .limit(20);
+        const thoughts = await Thought.find().sort({ createdAt: -1 }).limit(20);
         res.json(thoughts);
       } catch (error) {
         console.error("Error fetching thoughts", error);
@@ -49,58 +45,67 @@ const routes = [
     handler: async (req, res) => {
       const { message } = req.body;
       if (message.length < 5 || message.length > 140) {
-        return res.status(400).json({message: "Please type between between 5 and 140 characters"})
-      } try {
-        const newThought = new Thought({ message })
-        await newThought.save()
-        res.status(201).json(newThought) // ?? what is this
-      } catch (error) {
-        console.error("Error posting thought:", error)
-        res.status(500).json({message: "Server error" })
+        return res
+          .status(400)
+          .json({
+            message: "Please type between between 5 and 140 characters",
+          });
       }
-    }
+      try {
+        const newThought = new Thought({ message });
+        await newThought.save();
+        res.status(201).json(newThought);
+      } catch (error) {
+        console.error("Error posting thought:", error);
+        res.status(500).json({ message: "Server error" });
+      }
+    },
   },
   {
     path: "/thoughts/:thoughtId/like",
     method: "post",
     description: "Like a thought by providing the thought ID",
     handler: async (req, res) => {
-      const { thoughtId } = req.params
+      const { thoughtId } = req.params;
       try {
-        const likedThought = await Thought.findByIdAndUpdate(thoughtId, { $inc: { hearts: 1 } }, { new: true })
+        const likedThought = await Thought.findByIdAndUpdate(
+          thoughtId,
+          { $inc: { hearts: 1 } },
+          { new: true }
+        );
         if (likedThought) {
-          res.status(200).json(likedThought)
+          res.status(200).json(likedThought);
         } else {
-          res.status(404).json({ message: "Thought not found "})
+          res.status(404).json({ message: "Thought not found " });
         }
       } catch (error) {
-        console.error("Error liking thought:", error)
-        res.status(500).json({ message: "Server error" })
+        console.error("Error liking thought:", error);
+        res.status(500).json({ message: "Server error" });
       }
-    }
-  }
+    },
+  },
 ];
 
 routes.forEach((route) => {
-  app[route.method](route.path, route.handler)
-})
+  app[route.method](route.path, route.handler);
+});
 
 // API endpoints
 app.get("/", (req, res) => {
   try {
     const endpoints = expressListEndpoints(app).map((endpoint) => {
-      const matchedRoute = routes.find(route => route.path === endpoint.path)
+      const matchedRoute = routes.find((route) => route.path === endpoint.path);
       if (matchedRoute) {
-        endpoint.description = matchedRoute.description
+        endpoint.description = matchedRoute.description;
       }
-      return endpoint
-    })
-    res.status(200).json(endpoints)
+      return endpoint;
+    });
+    res.status(200).json(endpoints);
   } catch (error) {
-    console.error("Error generating API documentation:", error)
-    res.status(500).json({ message: "Server error"})
+    console.error("Error generating API documentation:", error);
+    res.status(500).json({ message: "Server error" });
   }
-})
+});
 
 // Start the server
 app.listen(port, () => {
