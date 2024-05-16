e6779953cdfaad802096fa6ee8be90beb5ba8860
Johanna Billingskog Nyberg <johanna@billingskognyberg.se>
2024-05-16 15:59:34 +0200

    Cleaned up code.

--- server.js
+++ server.js
@@ -1,31 +1,3 @@
-// import cors from "cors";
-// import express from "express";
-// import mongoose from "mongoose";
-
-// const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/project-mongo";
-// mongoose.connect(mongoUrl);
-// mongoose.Promise = Promise;
-
-// // Defines the port the app will run on. Defaults to 8080, but can be overridden
-// // when starting the server. Example command to overwrite PORT env variable value:
-// // PORT=9000 npm start
-// const port = process.env.PORT || 8080;
-// const app = express();
-
-// // Add middlewares to enable cors and json body parsing
-// app.use(cors());
-// app.use(express.json());
-
-// // Start defining your routes here
-// app.get("/", (req, res) => {
-//   res.send("Hello Technigo!");
-// });
-
-// // Start the server
-// app.listen(port, () => {
-//   console.log(`Server running on http://localhost:${port}`);
-// });
-
 import cors from "cors";
 import express from "express";
 import mongoose from "mongoose";
@@ -135,33 +107,6 @@ app.patch("/thoughts/:thoughtId/like", async (req, res) => {
     });
   }
 });
-
-//Update likes count
-// app.patch("/thoughts/:id", async (req, res) => {
-//   const { id } = req.params;
-
-//   const { hearts } = req.body;
-
-//   try {
-//     const thought = await Thought.findByIdAndUpdate(
-//       id,
-//       { hearts: hearts },
-//       { new: true, runValidators: true }
-//     );
-
-//     res.status(200).json({
-//       success: true,
-//       response: thought,
-//       message: "Number of likes updated",
-//     });
-//   } catch (error) {
-//     res.status(400).json({
-//       success: false,
-//       response: error,
-//       message: "Couldn't update number of likes",
-//     });
-//   }
-// });
 
 // Start the server
 app.listen(port, () => {
