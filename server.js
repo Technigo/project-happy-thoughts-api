1994d637b8cc95fa92243c6de00fa14e9f167466
Teddy <yoo.ji.yg@gmail.com>
2024-05-16 21:37:33 +0200

    changed port

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ mongoose.Promise = Promise;
 // Defines the port the app will run on. Defaults to 8080, but can be overridden
 // when starting the server. Example command to overwrite PORT env variable value:
 // PORT=9000 npm start
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 9000;
 const app = express();
 
 // Add middlewares to enable cors and json body parsing
