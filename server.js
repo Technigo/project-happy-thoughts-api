51172eb189989afc35f883d23b9b3589cc86817a
Pernilla Sterner <psterner@hotmail.com>
2024-05-18 14:58:58 +0200

    Update hearts value with +1 for each like

--- server.js
+++ server.js
@@ -30,26 +30,38 @@ app.get("/thoughts", async (req, res) => {
 
 // Routes to add new Thought
 app.post("/thoughts", async (req, res) => {
-  // Promises
-  // new Thought(req.body)
-  //   .save()
-  //   .then((thought) => {
-  //     res.status(200).json(thought);
-  //   })
-  //   .catch((err) => {
-  //     res
-  //       .status(400)
-  //       .json({ message: "No new thought was created", err: err.errors });
-  //   });
+  const { message } = req.body;
+  const thought = new Thought({ message });
 
   try {
-    const { message } = req.body;
-    const thought = await new Thought({ message }).save();
-    res.status(200).json(thought);
+    const savedThought = await thought.save();
+    res.status(200).json(savedThought);
   } catch (err) {
     res
       .status(400)
       .json({ message: "No new thought was created", err: err.errors });
+  }
+});
+
+app.get("/thoughts/:thoughtId/like", async (req, res) => {
+  // FInd the heart and update its value by one
+  const { thoughtId } = req.params;
+
+  try {
+    const thought = await Thought.findById(thoughtId);
+
+    if (!thought) {
+      return res.status(404).json({ message: "Thought not found" });
+    }
+
+    // Update hearts for each like
+    thought.hearts += 1;
+
+    const updateThought = await thought.save();
+
+    return res.status(200).json(updateThought);
+  } catch (err) {
+    res.status(400).json({ message: "No thought was found", err: err.errors });
   }
 });
 
