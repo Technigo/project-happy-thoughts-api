cfe75a3d5d7163b9086b4050ce06a41789ae9a8c
Your Name <you@example.com>
2024-05-18 15:13:25 +0200

    app.post '/thougts' create and test in postman, pause

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ import cors from "cors";
 import express from "express";
 import mongoose from "mongoose";
 import expressListEndpoints from "express-list-endpoints";
-import { thougthSchema } from "./schema";
+//import { thougthSchema } from "./schema";
 import { corsMiddleware, jsonMiddleware, mongoConnectionMiddleware } from "./middleware";
 
 const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/happy-thoughts";
@@ -19,12 +19,37 @@ app.use(corsMiddleware);
 app.use(jsonMiddleware);
 app.use(mongoConnectionMiddleware);
 
+//Thought Schema
+const { Schema } = mongoose;
+
+const thoughtSchema = new Schema({
+  message: {
+    type: String,
+    required: true,
+    minlength: 5,
+    maxlength: 140,
+  },
+  hearts: {
+    type: Number,
+    default: 0,
+  },
+  createdAt: {
+    type: Date,
+    default: () => new Date(),
+  },
+});
+
 //Thought model
-const Thought = mongoose.model("Thought", thougthSchema)
+const Thought = mongoose.model("Thought", thoughtSchema);
 
 // Start defining your routes here
 app.get("/", (req, res) => {
-  res.send("Hello Technigo!");
+  const endpoints = expressListEndpoints(app);
+  const documentation = {
+    Welcome: "This is the Happy Thoughts API!",
+    Endpoints: endpoints,
+  };
+  res.json(documentation);
 });
 
 app.post("/thoughts", async (req, res) => {
