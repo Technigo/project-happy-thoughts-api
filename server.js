35c75995bb909c7ecadb72e0855fa9d45450bdd1
Pernilla Sterner <psterner@hotmail.com>
2024-05-19 07:46:27 +0200

    Comment out parts of code to test db connection

--- server.js
+++ server.js
@@ -19,51 +19,52 @@ app.get("/", (req, res) => {
   res.send("Send me a good thought");
 });
 
-app.get("/thoughts", async (req, res) => {
-  const thoughts = await Thought.find()
-    .sort({ createdAt: "desc" })
-    .limit(20)
-    .exec();
+// app.get("/thoughts", async (req, res) => {
+//   const thoughts = await Thought.find()
+//     .sort({ createdAt: "desc" })
+//     .limit(20)
+//     .exec();
 
-  res.json(thoughts);
-});
+//   res.json(thoughts);
 
-// Routes to add new Thought
-app.post("/thoughts", async (req, res) => {
-  const { message } = req.body;
-  const thought = new Thought({ message });
+// });
 
-  try {
-    const savedThought = await thought.save();
-    res.status(200).json(savedThought);
-  } catch (err) {
-    res
-      .status(400)
-      .json({ message: "No new thought was created", err: err.errors });
-  }
-});
+// // Routes to add new Thought
+// app.post("/thoughts", async (req, res) => {
+//   const { message } = req.body;
+//   const thought = new Thought({ message });
 
-app.get("/thoughts/:thoughtId/like", async (req, res) => {
-  // FInd the heart and update its value by one
-  const { thoughtId } = req.params;
+//   try {
+//     const savedThought = await thought.save();
+//     res.status(200).json(savedThought);
+//   } catch (err) {
+//     res
+//       .status(400)
+//       .json({ message: "No new thought was created", err: err.errors });
+//   }
+// });
 
-  try {
-    const thought = await Thought.findById(thoughtId);
+// app.get("/thoughts/:thoughtId/like", async (req, res) => {
+//   // FInd the heart and update its value by one
+//   const { thoughtId } = req.params;
 
-    if (!thought) {
-      return res.status(404).json({ message: "Thought not found" });
-    }
+//   try {
+//     const thought = await Thought.findById(thoughtId);
 
-    // Update hearts for each like
-    thought.hearts += 1;
+//     if (!thought) {
+//       return res.status(404).json({ message: "Thought not found" });
+//     }
 
-    const updateThought = await thought.save();
+//     // Update hearts for each like
+//     thought.hearts += 1;
 
-    return res.status(200).json(updateThought);
-  } catch (err) {
-    res.status(400).json({ message: "No thought was found", err: err.errors });
-  }
-});
+//     const updateThought = await thought.save();
+
+//     return res.status(200).json(updateThought);
+//   } catch (err) {
+//     res.status(400).json({ message: "No thought was found", err: err.errors });
+//   }
+// });
 
 // Start the server
 app.listen(port, () => {
