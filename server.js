4693e4bfc97a7201f78df72a705a8b01183a6414
Pernilla Sterner <psterner@hotmail.com>
2024-05-21 07:27:41 +0200

    Testing connection

--- server.js
+++ server.js
@@ -1,15 +1,13 @@
 import cors from "cors";
 import express from "express";
 import mongoose from "mongoose";
+import { Thought } from "./models/thoughtModel.js";
 
-const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/project-mongo";
+const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/thoughts";
 mongoose.connect(mongoUrl);
 mongoose.Promise = Promise;
 
-// Defines the port the app will run on. Defaults to 8080, but can be overridden
-// when starting the server. Example command to overwrite PORT env variable value:
-// PORT=9000 npm start
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 8000;
 const app = express();
 
 // Add middlewares to enable cors and json body parsing
@@ -18,8 +16,54 @@ app.use(express.json());
 
 // Start defining your routes here
 app.get("/", (req, res) => {
-  res.send("Hello Technigo!");
+  res.send("Send me a good thought");
 });
+
+app.get("/thoughts", async (req, res) => {
+  const thoughts = await Thought.find()
+    .sort({ createdAt: "desc" })
+    .limit(20)
+    .exec();
+
+  res.json(thoughts);
+});
+
+// // Routes to add new Thought
+// app.post("/thoughts", async (req, res) => {
+//   const { message } = req.body;
+//   const thought = new Thought({ message });
+
+//   try {
+//     const savedThought = await thought.save();
+//     res.status(200).json(savedThought);
+//   } catch (err) {
+//     res
+//       .status(400)
+//       .json({ message: "No new thought was created", err: err.errors });
+//   }
+// });
+
+// app.get("/thoughts/:thoughtId/like", async (req, res) => {
+//   // FInd the heart and update its value by one
+//   const { thoughtId } = req.params;
+
+//   try {
+//     const thought = await Thought.findById(thoughtId);
+
+//     if (!thought) {
+//       return res.status(404).json({ message: "Thought not found" });
+//     }
+
+//     // Update hearts for each like
+//     thought.hearts += 1;
+
+//     const updateThought = await thought.save();
+
+//     return res.status(200).json(updateThought);
+//   } catch (err) {
+//     res.status(400).json({ message: "No thought was found", err: err.errors });
+//   }
+// });
 
 // Start the server
 app.listen(port, () => {
