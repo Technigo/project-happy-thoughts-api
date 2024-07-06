912aae3bb331baef04f2ca82864c87e4983b07bf
Looziolooz <lorenzo.dastoli@gmail.com>
2024-07-06 21:34:34 +0200

    update server.js

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ dotenv.config();
 mongoose.set('strictQuery', false); // Add this line
 
 // Connect to MongoDB database
-const mongoUrl = process.env.MONGO_URL || 'mongodb://localhost/project-happy-thoughts';
+const mongoUrl = process.env.MONGO_URL || 'mongodb://localhost/127.0.0.1/project-happy-thoughts';
 mongoose.connect(mongoUrl, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
