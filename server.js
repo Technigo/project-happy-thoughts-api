768c32133ce49fc91c0d8be1b4a9d8d1a5caab08
Gitte Beckmann <gitte.beckmann@hotmail.com>
2024-12-18 17:17:12 +0100

    - commented cors out

--- server.js
+++ server.js
@@ -51,7 +51,7 @@ const port = process.env.PORT || 8080;
 const app = express();
 
 // Add middlewares to enable cors and json body parsing
-app.use(cors());
+// app.use(cors());
 app.use(express.json());
 
 // Define allowed origins
